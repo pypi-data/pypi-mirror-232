0cc91a9a2e5d21d2cd37e5068c00c4f5afd12fc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-24 14:04:58 +0000

    Add package paraty-commit-jlvillada/0.1.34/paraty_commit_jlvillada-0.1.34.tar.gz

---
+++ packages/paraty-commit-jlvillada/paraty_commit_jlvillada-0.1.34.tar.gz/paraty_commit_jlvillada-0.1.34/setup.py
@@ -0,0 +1,32 @@
+
+from setuptools import setup
+from setuptools.command.install import install
+
+# Define una función que se ejecutará durante la instalación
+class custom_install_code(install):
+    def run(self):
+        print("Here is where I would be running my code...")
+        install.run(self)
+
+    def _post_install():
+        print("Here is where I would be running my code post install...")
+
+setup(
+    name='paraty_commit_jlvillada',
+    version='0.1.34',
+    description='Una biblioteca personalizada',
+    author='José Luis Villada',
+    author_email='jlvillada@paratytech.com',
+    cmdclass={
+        'install': custom_install_code
+      },
+    packages=['paraty_commit_jlvillada'],
+    install_requires=['colorama'],
+    # package_data={'paraty_commit_jlvillada': ['*', '.pre-commit-config.yaml']}
+    include_package_data=True,
+    entry_points={
+        'console_scripts': [
+            'my_executable = paraty_commit_jlvillada.precommit:main',
+        ],
+    },
+)
