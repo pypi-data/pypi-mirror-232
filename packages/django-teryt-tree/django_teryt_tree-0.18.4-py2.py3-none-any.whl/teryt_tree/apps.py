709ee641e1a21972ad3f0113d91b14bea3061ae2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-24 14:04:53 +0000

    Add package django-teryt-tree/0.18.4/django_teryt_tree-0.18.4-py2.py3-none-any.whl

---
+++ packages/django-teryt-tree/django_teryt_tree-0.18.4-py2.py3-none-any.whl/teryt_tree/apps.py
@@ -0,0 +1,8 @@
+from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
+
+
+class TerytTreeConfig(AppConfig):
+    name = "teryt_tree"
+    verbose_name = _("Administrative division")
+    default_auto_field = "django.db.models.AutoField"
