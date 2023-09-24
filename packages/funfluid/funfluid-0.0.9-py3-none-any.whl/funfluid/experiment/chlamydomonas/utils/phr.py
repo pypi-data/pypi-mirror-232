efa799c5a1c670b734a03cf36b2ca0dbb9049c3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-24 14:05:13 +0000

    Add package funfluid/0.0.9/funfluid-0.0.9-py3-none-any.whl

---
+++ packages/funfluid/funfluid-0.0.9-py3-none-any.whl/funfluid/experiment/chlamydomonas/utils/phr.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+
+def cul_phr(df, col_x="x", col_y="y", cx=0, cy=0, dr=0.1):
+    df['r'] = np.sqrt((df[col_x] - cx) ** 2 + (df[col_y] - cy) ** 2)
+    df['r'] = np.round(df['r'] / dr) * dr
+    r_df = df.groupby(['r'])[col_x].count().reset_index()
+    r_df.columns = ['r', 'hr']
+    r_df['phr'] = r_df['hr'] / (2 * np.pi * dr)
+    r_df['phr'] = r_df['phr'] / r_df['phr'].sum()
+    return r_df
