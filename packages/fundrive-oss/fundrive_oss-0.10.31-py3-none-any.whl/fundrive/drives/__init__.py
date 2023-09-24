741112acac6f52616f164fdac109efa26e1b8448
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-24 14:05:13 +0000

    Add package fundrive-oss/0.10.31/fundrive_oss-0.10.31-py3-none-any.whl

---
+++ packages/fundrive-oss/fundrive_oss-0.10.31-py3-none-any.whl/fundrive/drives/__init__.py
@@ -0,0 +1,5 @@
+from .tsinghua import TSingHuaDrive
+from .tsinghua import download as download_tsinghua
+from .tianchi import TianChiDrive
+from .oss import OSSDrive
+from .opendatalab import OpenDataLabDrive
