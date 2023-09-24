d8b52b5fdacbccf7f67ac05c44bb064b1508477f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-24 14:05:03 +0000

    Add package frrmvsm/0.0.8/frrmvsm-0.0.8.tar.gz

---
+++ packages/frrmvsm/frrmvsm-0.0.8.tar.gz/frrmvsm-0.0.8/frrmvsm/frrmvsm.py
@@ -0,0 +1,40 @@
+import sys 
+
+#from add import add
+#from subtract import subtract
+#from multiply import multiply
+#from divide import divide
+
+a = int(sys.argv[1])
+b = int(sys.argv[2])
+print(f"First number is: {a}")
+print(f"Second number is: {b}")
+
+def add(a,b):
+    return a + b 
+
+def subtract(a,b):
+    return a - b 
+
+def multiply(a,b):
+    return a * b 
+
+def divide(a,b):
+    return a / b 
+
+def operations(a,b):
+
+    my_sum = add(a,b)
+    my_subtraction = subtract(a,b)
+    my_multiplication = multiply(a,b)
+    my_division = divide(a,b)
+
+    print(f"My sum is {my_sum}")
+    print(f"My subtraction is {my_subtraction}")
+    print(f"My multiplication is {my_multiplication}")
+    print(f"My division is {my_division}")
+
+if __name__ == '__main__':
+    print(f"First number is: {a}")
+    print(f"Second number is: {b}")
+    operations(a,b)
