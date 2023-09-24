b8727ef627c7939763870367ef75981aa5c82b6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-24 14:05:15 +0000

    Add package k-diffusion/0.1.0/k-diffusion-0.1.0.tar.gz

---
+++ packages/k-diffusion/k-diffusion-0.1.0.tar.gz/k-diffusion-0.1.0/k_diffusion/models/flags.py
@@ -0,0 +1,28 @@
+from contextlib import contextmanager
+import os
+import threading
+
+
+def get_use_compile():
+    return os.environ.get("K_DIFFUSION_USE_COMPILE", "1") == "1"
+
+
+def get_use_flash_attention_2():
+    return os.environ.get("K_DIFFUSION_USE_FLASH_2", "1") == "1"
+
+
+state = threading.local()
+state.checkpointing = False
+
+
+@contextmanager
+def checkpointing(enable=True):
+    try:
+        old_checkpointing, state.checkpointing = state.checkpointing, enable
+        yield
+    finally:
+        state.checkpointing = old_checkpointing
+
+
+def get_checkpointing():
+    return state.checkpointing
