3471128cac627da00dbd13f8a3b5c43497bfc82f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-24 14:05:25 +0000

    Add package shub-query-adapter/0.1/shub_query_adapter-0.1.tar.gz

---
+++ packages/shub-query-adapter/shub_query_adapter-0.1.tar.gz/shub_query_adapter-0.1/shub_query_adapter/api_requester.py
@@ -0,0 +1,14 @@
+import requests
+
+API_ENDPOINT = 'http://storage.scrapinghub.com/items/'
+
+def send_request(path_param, params, api_key):
+    params['format'] = 'json'
+    
+    url = f"{API_ENDPOINT}{path_param}"
+    response = requests.get(url, params=params, auth=(api_key, ''))
+    print(response.request.url)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return f'Error {response.status_code}: {response.text}'
