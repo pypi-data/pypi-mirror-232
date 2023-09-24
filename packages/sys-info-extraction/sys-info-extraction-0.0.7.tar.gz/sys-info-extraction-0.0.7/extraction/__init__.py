6fee2625c37d80e4043a5377e1ef7fc223d6b8bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-24 14:05:23 +0000

    Add package sys-info-extraction/0.0.7/sys-info-extraction-0.0.7.tar.gz

---
+++ packages/sys-info-extraction/sys-info-extraction-0.0.7.tar.gz/sys-info-extraction-0.0.7/extraction/__init__.py
@@ -0,0 +1,9 @@
+"""
+    Welcome,
+    this project is used to extract the operating information and status of the current system.
+"""
+from .NetworkInfo import network
+
+from .LoadsInfo import loads
+
+from .DiskInfo import diskinfo
