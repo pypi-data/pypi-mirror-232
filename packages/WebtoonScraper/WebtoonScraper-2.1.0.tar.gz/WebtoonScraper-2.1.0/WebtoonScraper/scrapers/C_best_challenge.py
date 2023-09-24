ed827836354924bf2a64067ad7a297936bab81bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-24 14:04:54 +0000

    Add package WebtoonScraper/2.1.0/WebtoonScraper-2.1.0.tar.gz

---
+++ packages/WebtoonScraper/WebtoonScraper-2.1.0.tar.gz/WebtoonScraper-2.1.0/WebtoonScraper/scrapers/C_best_challenge.py
@@ -0,0 +1,15 @@
+'''Download Webtoons from Naver Webtoon Best Challenge.'''
+
+from __future__ import annotations
+
+if __name__ in ("__main__", "C_best_challenge"):
+    from B_naver_webtoon import NaverWebtoonScraper
+else:
+    from .B_naver_webtoon import NaverWebtoonScraper
+
+
+class BestChallengeScraper(NaverWebtoonScraper):
+    BASE_URL = 'https://comic.naver.com/bestChallenge'
+    TEST_WEBTOON_ID = 809971  # 까마귀
+    IS_BEST_CHALLENGE = True
+    EPISODE_IMAGES_URL_SELECTOR = '#comic_view_area > div > img'
