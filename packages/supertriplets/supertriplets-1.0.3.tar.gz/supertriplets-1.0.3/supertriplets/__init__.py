983c41a746eac35d048146e7c1848886ed8096be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-24 14:05:07 +0000

    Add package supertriplets/1.0.3/supertriplets-1.0.3.tar.gz

---
+++ packages/supertriplets/supertriplets-1.0.3.tar.gz/supertriplets-1.0.3/supertriplets/__init__.py
@@ -0,0 +1,3 @@
+__version__ = "1.0.3"
+
+__all__ = ["models", "dataset", "distance", "encoder", "evaluate", "index", "loss", "sample", "utils"]
---
+++ packages/supertriplets/supertriplets-1.0.3.tar.gz/supertriplets-1.0.3/supertriplets/sample.py
@@ -0,0 +1,57 @@
+import os
+from typing import Dict
+
+
+class TextSample:
+    def __init__(self, text: str, label: int) -> None:
+        self.text = text
+        self.label = label
+        self._validate_input()
+
+    def _validate_input(self) -> None:
+        assert type(self.text) == str, f"{self.__class__.__name__} 'text' should be a string"
+        assert type(self.label) == int, f"{self.__class__.__name__} 'label' should be an integer"
+
+    def data(self) -> Dict:
+        return dict(text=self.text, label=self.label)
+
+    def __repr__(self) -> str:
+        return f"(text={self.text}, label={self.label}))"
+
+
+class ImageSample:
+    def __init__(self, image_path: str, label: int) -> None:
+        self.image_path = image_path
+        self.label = label
+        self._validate_input()
+
+    def _validate_input(self) -> None:
+        assert type(self.image_path) == str, f"{self.__class__.__name__} 'image_path' should be a string"
+        assert os.path.exists(self.image_path), f"{self.__class__.__name__} 'image_path' should exist"
+        assert type(self.label) == int, f"{self.__class__.__name__} 'label' should be an integer"
+
+    def data(self) -> Dict:
+        return dict(image_path=self.image_path, label=self.label)
+
+    def __repr__(self) -> str:
+        return f"(image_path={self.image_path}, label={self.label})"
+
+
+class TextImageSample:
+    def __init__(self, text: str, image_path: str, label: int) -> None:
+        self.text = text
+        self.image_path = image_path
+        self.label = label
+        self._validate_input()
+
+    def _validate_input(self) -> None:
+        assert type(self.text) == str, f"{self.__class__.__name__} 'text' should be a string"
+        assert type(self.image_path) == str, f"{self.__class__.__name__} 'image_path' should be a string"
+        assert os.path.exists(self.image_path), f"{self.__class__.__name__} 'image_path' should exist"
+        assert type(self.label) == int, f"{self.__class__.__name__} 'label' should be an integer"
+
+    def data(self) -> Dict:
+        return dict(text=self.text, image_path=self.image_path, label=self.label)
+
+    def __repr__(self) -> str:
+        return f"(text={self.text}, image_path={self.image_path}, label={self.label})"
