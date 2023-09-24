f4dda075b742f54cf89122bafa6de7a3e74cec0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-24 14:05:11 +0000

    Add package deepeval/0.17.0/deepeval-0.17.0.tar.gz

---
+++ packages/deepeval/deepeval-0.17.0.tar.gz/deepeval-0.17.0/deepeval/__init__.py
@@ -0,0 +1,12 @@
+# from .test_utils import assert_exact_match, TestEvalCase
+# from . import (
+#     _version,
+#     dataset,
+#     evaluator,
+#     metrics,
+#     query_generator,
+#     test_case,
+#     test_utils,
+#     utils,
+# )
+from ._version import __version__
---
+++ packages/deepeval/deepeval-0.17.0.tar.gz/deepeval-0.17.0/deepeval/plugins/plugin.py
@@ -0,0 +1,36 @@
+import pytest
+import os
+from deepeval.api import Api, TestRun
+
+from deepeval.constants import PYTEST_RUN_ENV_VAR
+
+
+def pytest_sessionstart(session):
+    global test_filename
+    test_run = TestRun(
+        testFile="-",
+        testCases=[],
+        metricScores=[],
+        configurations={},
+    )
+    test_filename = test_run.save()
+
+
+@pytest.hookimpl(tryfirst=True, hookwrapper=True)
+def pytest_sessionfinish(session, exitstatus):
+    # Code before yield will run before the test teardown
+    api: Api = Api()
+
+    # yield control back to pytest for the actual teardown
+    yield
+
+    # Code after yield will run after the test teardown
+    if os.getenv(PYTEST_RUN_ENV_VAR):
+        test_run = TestRun.load(test_filename)
+        result = api.post_test_run(test_run)
+        run_id = result["id"]
+        print(
+            "✅ Tests finished! View results on "
+            + f"https://app.confident-ai.com/unit-tests/{run_id}"
+        )
+        os.remove(test_filename)
