70d661b0ae6ef8637aa31432b90977af8f766eae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-24 14:05:06 +0000

    Add package fakeopenai/0.5/fakeopenai-0.5.tar.gz

---
+++ packages/fakeopenai/fakeopenai-0.5.tar.gz/fakeopenai-0.5/fakeopenai/__init__.py
@@ -0,0 +1,1 @@
+from .api import ChatCompletion, api_key
---
+++ packages/fakeopenai/fakeopenai-0.5.tar.gz/fakeopenai-0.5/fakeopenai/api.py
@@ -0,0 +1,26 @@
+import requests
+
+# Global variable to hold the API key
+api_key = "pk-this-is-a-real-free-pool-token-for-everyone"
+
+class ChatCompletion:
+    @classmethod
+    def create(cls, model, messages, max_tokens=1000):
+        if api_key is None:
+            raise Exception("API key not set. Please set it using fakeopenai.api_key.")
+        
+        headers = {
+            'Authorization': f'Bearer {api_key}',
+            'Content-Type': 'application/json'
+        }
+        payload = {
+            'model': model,
+            'max_tokens': max_tokens,
+            'messages': messages
+        }
+
+        response = requests.post('xxx', headers=headers, json=payload)
+        if response.status_code != 200:
+            raise Exception(f"API call failed with status code {response.status_code}: {response.text}")
+
+        return {'choices': [{'message': response.json()}]}
