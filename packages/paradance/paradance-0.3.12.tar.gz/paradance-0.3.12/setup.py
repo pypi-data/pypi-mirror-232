98d56d1731e3bd137e0ee5d5ddd7231ae86ecedd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-25 03:08:34 +0000

    Add package paradance/0.3.12/paradance-0.3.12.tar.gz

---
+++ packages/paradance/paradance-0.3.12.tar.gz/paradance-0.3.12/setup.py
@@ -0,0 +1,41 @@
+from pathlib import Path
+
+from setuptools import find_packages, setup
+
+SETUP_DIRECTORY = Path(__file__).resolve().parent
+
+with (SETUP_DIRECTORY / "README.md").open() as ifs:
+    LONG_DESCRIPTION = ifs.read()
+
+install_requires = (
+    [
+        "imageio",
+        "matplotlib_venn",
+        "matplotlib",
+        "numpy",
+        "openpyxl",
+        "optuna",
+        "pandas",
+        "scikit-learn",
+        "tqdm",
+        "sqlalchemy>=2.0.15",
+    ],
+)
+
+__version__ = "0.3.12"
+
+setup(
+    name="paradance",
+    version=__version__,
+    author="Yin Cheng",
+    author_email="yin.sjtu@gmail.com",
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    url="https://github.com/yinsn/paradance",
+    python_requires=">=3.6",
+    description="Offers a toolset for comprehensive, multi-faceted analysis of data exported from Hive, accompanied by powerful data visualization capabilities.",
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    install_requires=install_requires,
+    include_package_data=True,
+)
