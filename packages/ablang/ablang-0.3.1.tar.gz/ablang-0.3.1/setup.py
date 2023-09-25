57eb08d4c94e718574ffccd8e8126e6d47c22456
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-25 03:09:05 +0000

    Add package ablang/0.3.1/ablang-0.3.1.tar.gz

---
+++ packages/ablang/ablang-0.3.1.tar.gz/ablang-0.3.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+    
+setup(
+    name='ablang',
+    version='0.3.1',
+    description='AbLang: A language model for antibodies.',
+    license='BSD 3-clause license',
+    maintainer='Tobias Hegelund Olsen',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    maintainer_email='tobias.olsen@stats.ox.ac.uk',
+    include_package_data=True,
+    packages=find_packages(include=('ablang', 'ablang.*')),
+    install_requires=[
+        'requests',
+        'torch>=1.6',
+        'numpy',
+        'pandas',
+        'numba',
+    ],
+)
