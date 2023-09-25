7fce2e2d3fc1437684cb3c5cc4e516b640ada081
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-25 04:04:31 +0000

    Add package cardiac-geometries/0.10.1/cardiac_geometries-0.10.1.tar.gz

---
+++ packages/cardiac-geometries/cardiac_geometries-0.10.1.tar.gz/cardiac_geometries-0.10.1/tests/test_cli.py
@@ -0,0 +1,77 @@
+from pathlib import Path
+
+from cardiac_geometries import cli
+from cardiac_geometries.geometry import Geometry
+from click.testing import CliRunner
+
+
+def test_create_slab(tmp_path: Path):
+    runner = CliRunner()
+    res1 = runner.invoke(cli.create_slab, ["--create-fibers", tmp_path.as_posix()])
+    assert res1.exit_code == 0
+    outfile = tmp_path / "geo.h5"
+    res2 = runner.invoke(cli.folder2h5, [tmp_path.as_posix(), "--outfile", outfile])
+    assert res2.exit_code == 0
+    assert outfile.is_file()
+    geo = Geometry.from_file(outfile)
+    assert geo.f0 is not None
+
+
+def test_create_slab_in_bath(tmp_path: Path):
+    runner = CliRunner()
+    res1 = runner.invoke(cli.create_slab_in_bath, [tmp_path.as_posix()])
+    assert res1.exit_code == 0
+    outfile = tmp_path / "geo.h5"
+    res2 = runner.invoke(cli.folder2h5, [tmp_path.as_posix(), "--outfile", outfile])
+    assert res2.exit_code == 0
+    assert outfile.is_file()
+    geo = Geometry.from_file(outfile)
+
+    assert "Bath" in geo.markers
+    bath_marker = geo.markers["Bath"][0]
+    assert bath_marker in geo.cfun.array()
+
+
+def test_create_lv_ellipsoid(tmp_path: Path):
+    runner = CliRunner()
+    res1 = runner.invoke(
+        cli.create_lv_ellipsoid,
+        ["--create-fibers", tmp_path.as_posix()],
+    )
+    assert res1.exit_code == 0
+    outfile = tmp_path / "geo.h5"
+    res2 = runner.invoke(cli.folder2h5, [tmp_path.as_posix(), "--outfile", outfile])
+    assert res2.exit_code == 0
+    assert outfile.is_file()
+    geo = Geometry.from_file(outfile)
+    assert geo.f0 is not None
+
+
+def test_create_biv_ellipsoid(tmp_path: Path):
+    runner = CliRunner()
+    res1 = runner.invoke(
+        cli.create_biv_ellipsoid,
+        ["--create-fibers", tmp_path.as_posix()],
+    )
+    assert res1.exit_code == 0
+    outfile = tmp_path / "geo.h5"
+    res2 = runner.invoke(cli.folder2h5, [tmp_path.as_posix(), "--outfile", outfile])
+    assert res2.exit_code == 0
+    assert outfile.is_file()
+    geo = Geometry.from_file(outfile)
+    assert geo.f0 is not None
+
+
+def test_create_biv_ellipsoid_torso(tmp_path: Path):
+    runner = CliRunner()
+    res1 = runner.invoke(
+        cli.create_biv_ellipsoid_torso,
+        ["--create-fibers", tmp_path.as_posix()],
+    )
+    assert res1.exit_code == 0
+    outfile = tmp_path / "geo.h5"
+    res2 = runner.invoke(cli.folder2h5, [tmp_path.as_posix(), "--outfile", outfile])
+    assert res2.exit_code == 0
+    assert outfile.is_file()
+    geo = Geometry.from_file(outfile)
+    assert geo.mesh is not None
