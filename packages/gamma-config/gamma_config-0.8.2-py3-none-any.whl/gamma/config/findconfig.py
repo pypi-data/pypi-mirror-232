f51333a5e92063f670474bb9e11d92675a7ce874
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-25 04:04:30 +0000

    Add package gamma-config/0.8.2/gamma_config-0.8.2-py3-none-any.whl

---
+++ packages/gamma-config/gamma_config-0.8.2-py3-none-any.whl/gamma/config/findconfig.py
@@ -0,0 +1,165 @@
+"""Module for discovering configuration files (entries)"""
+
+import logging
+import os
+from pathlib import Path
+from typing import Literal
+
+from beartype.typing import Any, List, Optional, Tuple
+
+from gamma.config import dispatch
+
+from .confignode import ConfigNode
+from .load import load_node
+
+logger = logging.getLogger(__name__)
+
+CONFIG_FIND_ORDER = ["ENV", "LOCAL", "JUPYTER"]
+CONFIG_ROOT_ENV = "GAMMA_CONFIG_ROOT"
+
+
+class MissingMetaConfigFile(Exception):
+    pass
+
+
+@dispatch
+def get_entries() -> List[Tuple[str, Any]]:
+    """Discover the config root folder and get all entries"""
+    config_root = get_config_root()
+    load_dotenv(config_root)
+    return get_entries(config_root)
+
+
+@dispatch
+def get_entries(folder: Path, *, meta_include_folders=True) -> List[Tuple[str, Any]]:
+    """Get all entries in a given folder.
+
+    Args:
+        meta_include_folder: If `True`, try to load the `00-meta.yaml` file in the
+            folder and follow `include_folder` entries.
+    """
+    paths = list(folder.glob("*.yaml")) + list(folder.glob("*.yml"))
+    entries = [(p.name, p) for p in paths]
+    if meta_include_folders:
+        meta = load_meta(folder)
+        for subfolder in meta["include_folders"]:
+            if subfolder is None:
+                continue
+            subpath = folder / subfolder
+            for k, e in get_entries(subpath, meta_include_folders=False):
+                entries.append((k, e))
+
+    return entries
+
+
+@dispatch
+def load_meta(config_root: Path) -> ConfigNode:
+    """Load the `00-meta.yaml` file in a given folder"""
+    meta_path = config_root / "00-meta.yaml"
+    with meta_path.open("r") as fo:
+        return ConfigNode(load_node(fo))
+
+
+@dispatch
+def get_config_root() -> Path:
+    """Return the location for config root path.
+
+    The mechanisms used to find the config root are set in the ``CONFIG_LOAD_ORDER``
+    module variable.
+    """
+
+    for mechanism in CONFIG_FIND_ORDER:
+        root = get_config_root(mechanism)
+        if root is not None:
+            return root
+
+    config_folder = Path("config").absolute()
+    if config_folder.exists():
+        raise MissingMetaConfigFile(
+            f"We found a config folder at '{config_folder}' "
+            f"but no '00-meta.yaml' file. You must have a '00-meta.yaml' file at the "
+            f"config folder location even if it's empty."
+        )
+
+    raise Exception("Cannot locate a gamma config root in any expected location.")
+
+
+@dispatch
+def get_config_root(_: Literal["ENV"]) -> Optional[Path]:
+    f"""Try the path set by {CONFIG_ROOT_ENV} as a root config folder"""
+    if CONFIG_ROOT_ENV not in os.environ:
+        return None
+
+    root = Path(os.getenv(CONFIG_ROOT_ENV)).absolute()
+    if not _has_meta(root):
+        raise ValueError(
+            f"Could not find {root}/00-meta.yaml file as pointed by "
+            f"{CONFIG_ROOT_ENV} environment variable"
+        )
+
+    return root
+
+
+@dispatch
+def get_config_root(_: Literal["LOCAL"]) -> Optional[Path]:
+    """Try the path `$PWD/config` as root config folder"""
+    root = Path("config").absolute()
+    if _has_meta(root):
+        return root
+    return None
+
+
+@dispatch
+def get_config_root(_: Literal["JUPYTER"]) -> Optional[Path]:
+    """Try `<parent>/config` folders iteratively if we're in a Jupyter (IPython)
+    environment"""
+
+    if not _isnotebook():
+        return None
+
+    path = Path(".").absolute()
+    while path != path.parent:
+        candidate = path / "config"
+        if _has_meta(candidate):
+            return candidate
+        path = path.parent
+
+    return None
+
+
+def _has_meta(root: Path):
+    return (root / "00-meta.yaml").exists()
+
+
+def _isnotebook():  # pragma: no cover
+    try:
+        shell = get_ipython().__class__.__name__  # type: ignore
+        if shell == "ZMQInteractiveShell":
+            return True  # Jupyter notebook or qtconsole
+        elif shell == "TerminalInteractiveShell":
+            return True  # Terminal running IPython
+        else:
+            return True  # Other type of IPython kernel (?)
+    except NameError:
+        return False  # Probably standard Python interpreter
+
+
+def load_dotenv(root: Path = None):
+    """Load dotenv files located in:
+
+    - `$PWD/config.local.env`
+    - `{config_root}/../config.local.env`
+    - `$PWD/config.env`
+    - `{config_root}/../config.env`
+
+    """
+
+    import dotenv
+
+    home = root.parent
+    dotenv.load_dotenv("./.env")
+    dotenv.load_dotenv(f"{home}/.env")
+    dotenv.load_dotenv("./config.local.env")
+    dotenv.load_dotenv(f"{home}/config.local.env")
+    dotenv.load_dotenv("./config.env")
+    dotenv.load_dotenv(f"{home}/config.env")
---
+++ packages/gamma-config/gamma_config-0.8.2-py3-none-any.whl/gamma/config/render_context.py
@@ -0,0 +1,135 @@
+"""Module handling rendering context variables (eg. for !expr and !j2)"""
+from functools import partial
+
+from beartype.typing import Any, Callable, Dict, List, NamedTuple, Optional, Union
+
+from gamma.config.confignode import ConfigNode
+
+from .cache import cache
+
+
+class ContextVar(NamedTuple):
+    """A specification for a context variable used for rendering dynamic values.
+
+    Either if ``function`` is specified, we'll call the function to get the actual
+    value.
+    """
+
+    name: str
+    """The name of the variable"""
+
+    value: Any = None
+    """The value of the variable"""
+
+    function: Optional[Callable[..., Any]] = None
+    """Function to call to resolve the variable"""
+
+    cacheable: bool = False
+    """If True, will cache the function result, otherwise will call on each render."""
+
+
+def get_render_context(**kwargs) -> Dict[str, Any]:
+    """Return the render context by calling each function in ``context_provider``.
+
+    A context provider must be a function with the signature:
+        `(**kwargs) -> List[ContextVar]`
+    or simply a list of `ContextVar` objects
+
+    The provided `**kwargs` are the same available in the `render_node` function
+    """
+    out = {}
+    for provider in context_providers:
+        var: ContextVar
+        vars: List[ContextVar] = provider(**kwargs) if callable(provider) else provider
+        for var in vars:
+            if var.cacheable:
+                cache_key = f"render_context/{var.name}"
+                try:
+                    out[var.name] = cache[cache_key]
+                    continue
+                except KeyError:
+                    pass
+
+            if var.function is not None:
+                val = var.function()
+            else:
+                val = var.value
+
+            if var.cacheable:
+                cache[cache_key] = val
+
+            out[var.name] = val
+
+    return out
+
+
+###
+# Built-in providers
+###
+
+
+def base_provider(**kwargs):
+    """Defaults to context
+
+    - `env` -> os.environ
+    - `c` -> the global RootConfig
+    """
+    import os
+
+    from .globalconfig import get_config
+
+    return [
+        ContextVar("env", os.environ),
+        ContextVar("c", function=partial(get_config, False)),
+    ]
+
+
+def underscore_context_provider(*, config: ConfigNode = None, **kwargs):
+    """Look in parent config nodes and add all entries under the `_context` key"""
+
+    from .dump_dict import to_dict
+    from .merge import merge_nodes
+    from .rawnodes import as_node
+
+    if not config:
+        return []
+
+    stack = []
+
+    cur = config
+
+    while cur:
+        _context = cur.get("_context", {})
+
+        if _context:
+            stack.append(as_node(_context))
+
+        cur = cur._parent
+        if cur is None:
+            break
+
+    if not stack:
+        return []
+
+    # merge context stack and render
+    stack = list(reversed(stack))
+    _, merged_node = merge_nodes(stack)
+    new_args = kwargs.copy()
+    new_args.pop("node", None)
+    new_args.pop("tag", None)
+    merged = to_dict(merged_node, config=config, **new_args)
+
+    # wrap in context vars
+    out = []
+    for key, value in merged.items():
+        out.append(ContextVar(key, value))
+
+    return out
+
+
+context_providers: List[Union[Callable, List[ContextVar]]] = [
+    base_provider,
+    underscore_context_provider,
+]
+"""Provides a list of context providers. See :func:get_render_context() for details
+"""
