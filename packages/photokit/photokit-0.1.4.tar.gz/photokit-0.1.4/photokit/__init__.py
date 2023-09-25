49afaecbe02549b6c89c7b4409d1bcf5f378991e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-25 14:05:16 +0000

    Add package photokit/0.1.4/photokit-0.1.4.tar.gz

---
+++ packages/photokit/photokit-0.1.4.tar.gz/photokit-0.1.4/photokit/__init__.py
@@ -0,0 +1,44 @@
+"""Python package for accessing the macOS Photos.app library via Apple's native PhotoKit framework."""
+
+import logging
+
+from .album import Album
+from .asset import LivePhotoAsset, PhotoAsset, VideoAsset
+from .exceptions import (
+    PhotoKitAlbumAddAssetError,
+    PhotoKitAlbumCreateError,
+    PhotoKitAlbumDeleteError,
+    PhotoKitAuthError,
+    PhotoKitCreateLibraryError,
+    PhotoKitError,
+    PhotoKitExportError,
+    PhotoKitFetchFailed,
+    PhotoKitImportError,
+    PhotoKitMediaTypeError,
+)
+from .photolibrary import PhotoLibrary, PhotoLibrarySmartAlbumType
+
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(filename)s - %(lineno)d - %(message)s",
+)
+
+__version__ = "0.1.4"
+
+__all__ = [
+    "Album",
+    "LivePhotoAsset",
+    "PhotoAsset",
+    "PhotoKitAlbumAddAssetError",
+    "PhotoKitAlbumCreateError",
+    "PhotoKitAlbumDeleteError",
+    "PhotoKitAuthError",
+    "PhotoKitCreateLibraryError",
+    "PhotoKitError",
+    "PhotoKitExportError",
+    "PhotoKitFetchFailed",
+    "PhotoKitImportError",
+    "PhotoKitMediaTypeError",
+    "PhotoLibrary",
+    "PhotoLibrarySmartAlbumType",
+    "VideoAsset",
+]
