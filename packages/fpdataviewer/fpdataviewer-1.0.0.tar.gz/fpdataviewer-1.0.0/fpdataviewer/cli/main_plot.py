5d55ec3ebdcece148c0e7e4e625063eb82e6d12d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-25 03:08:27 +0000

    Add package fpdataviewer/1.0.0/fpdataviewer-1.0.0.tar.gz

---
+++ packages/fpdataviewer/fpdataviewer-1.0.0.tar.gz/fpdataviewer-1.0.0/fpdataviewer/cli/main_plot.py
@@ -0,0 +1,31 @@
+import json
+
+from fpdataviewer.cli.config import set_config, default_config
+from fpdataviewer.mlab import parsing, validation
+
+
+def plot(args) -> None:
+    # Load config
+    config = default_config
+
+    if args.config_file is not None:
+        with args.config_file.open(mode="rt") as file:
+            config.update(json.load(file))
+
+    set_config(config)
+
+    # Load MLAB file
+    with args.input_file.open(mode="rt") as file:
+        mlab = parsing.load(file)
+    if args.strict:
+        validation.validate(mlab)
+
+    # Plot
+    if args.interactive:
+        import fpdataviewer.cli.plotting.plot_mpl as output
+    else:
+        import fpdataviewer.cli.plotting.plot_pdf as output
+
+    output.run(args, mlab)
+
+    print("\r", flush=True)
---
+++ packages/fpdataviewer/fpdataviewer-1.0.0.tar.gz/fpdataviewer-1.0.0/fpdataviewer/cli/main_validate.py
@@ -0,0 +1,16 @@
+from fpdataviewer.mlab import parsing, validation
+
+
+def validate(args) -> None:
+    with args.input_file.open(mode="rt") as file:
+        mlab = parsing.load(file)
+
+    print("format ok")
+
+    try:
+        validation.validate(mlab)
+    except validation.ValidationException as e:
+        print(e)
+        print("note this may not be the only problem!")
+    else:
+        print("no problems found")
