8da5c267e7fe9d5af075be6304db2a50e288533a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-25 04:04:21 +0000

    Add package animplotlib/0.2.1/animplotlib-0.2.1.tar.gz

---
+++ packages/animplotlib/animplotlib-0.2.1.tar.gz/animplotlib-0.2.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+with open('README.md') as f:
+    long_description = f.read()
+
+setup(
+    name='animplotlib',
+    version='0.2.1',
+    author='Aymen Hafeez',
+    author_email='aymennh@gmail.com',
+    packages=['animplotlib',],
+    # packages = find_packages(),
+    url='https://github.com/aymenhafeez/animplotlib/',
+    license='MIT',
+    description='A thin wrapper around the matplotlib FuncAnimation class',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    python_requires='>=3.5',
+    install_requires=["matplotlib >= 1.1"],
+)
