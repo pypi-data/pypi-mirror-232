b764d675548028ce682b12abf20bee251d25973f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-25 03:07:53 +0000

    Add package manage-django-project/0.6.0/manage_django_project-0.6.0.tar.gz

---
+++ packages/manage-django-project/manage_django_project-0.6.0.tar.gz/manage_django_project-0.6.0/manage_django_project/tests/test_command_coverage.py
@@ -0,0 +1,23 @@
+from django.test import SimpleTestCase
+
+from manage_django_project.management.commands import coverage
+from manage_django_project.tests.command_test_utils import call_command_capture_subprocess
+
+
+class CoverageTestCase(SimpleTestCase):
+    maxDiff = None
+
+    def test_basic_update_req(self):
+        popenargs = call_command_capture_subprocess(cmd_module=coverage)
+        self.assertEqual(
+            popenargs,
+            [
+                ['.../bin/coverage', 'erase'],
+                ['.../bin/coverage', 'run'],
+                ['.../bin/coverage', 'combine', '--append'],
+                ['.../bin/coverage', 'report'],
+                ['.../bin/coverage', 'xml'],
+                ['.../bin/coverage', 'json'],
+                ['.../bin/coverage', 'erase'],
+            ],
+        )
