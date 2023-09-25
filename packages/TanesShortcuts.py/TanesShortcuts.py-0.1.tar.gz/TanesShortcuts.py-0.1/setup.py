a9b0b2d2160aa79141dd255136a2cfebe03dbd6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-25 14:05:42 +0000

    Add package TanesShortcuts.py/0.1/TanesShortcuts.py-0.1.tar.gz

---
+++ packages/TanesShortcuts.py/TanesShortcuts.py-0.1.tar.gz/TanesShortcuts.py-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="TanesShortcuts.py",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+        # List your library dependencies here
+    ],
+)
