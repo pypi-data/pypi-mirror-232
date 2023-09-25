d24dd13cfe89380eb5f6b8a209d4c45c60f00d29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-25 15:04:45 +0000

    Add package panax/0.0.36/panax-0.0.36.tar.gz

---
+++ packages/panax/panax-0.0.36.tar.gz/panax-0.0.36/panax/database.py
@@ -0,0 +1,5 @@
+import sys, os
+
+sys.path.append(os.getcwd())
+
+from models import *
---
+++ packages/panax/panax-0.0.36.tar.gz/panax-0.0.36/panax/tool.py
@@ -0,0 +1,65 @@
+import argparse
+
+import sys
+import os
+sys.path.append(os.getcwd())
+
+from panax import db, BaseModel
+import panax.database
+
+from config import APP_SETTING
+
+
+from peewee_migrate import Router
+
+
+def init():
+    print("==init==")
+    print("==finished==")
+
+
+def migrate():
+    print("==migrateing==")
+
+    db.connect()
+
+    router = Router(db)
+    router.create(auto=panax.database)
+    router.run()
+
+    db.close()
+
+    print("==migrate finished==")
+
+
+# def upgrade():
+#     print("==upgrade==")
+#     repo = os.path.join(os.getcwd(), 'db_migrate')
+#     if not os.path.exists(repo):
+#         print("Repo Not Found!")
+#
+#     api.upgrade(APP_SETTING["connection"], repo)
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    # parser.add_argument("-f", help="")
+    parser.add_argument("exec", help="参数: [init, migrate, upgrade]")
+
+    # 解析
+    args = parser.parse_args()
+    exec = args.exec
+    # f = args.f
+
+    if exec == "init":
+        init()
+    elif exec == "migrate":
+        migrate()
+    # elif exec == "upgrade":
+    #     upgrade()
+    else:
+        print("参数错误")
+
+
+if __name__ == '__main__':
+    main()
