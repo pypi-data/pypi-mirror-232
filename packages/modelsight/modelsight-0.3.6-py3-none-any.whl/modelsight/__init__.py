59ec37caf017ab0915e2ad76d060f87fb9de05c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-25 03:07:42 +0000

    Add package modelsight/0.3.6/modelsight-0.3.6-py3-none-any.whl

---
+++ packages/modelsight/modelsight-0.3.6-py3-none-any.whl/modelsight/__init__.py
@@ -0,0 +1,11 @@
+# read version from installed package
+from importlib.metadata import version
+
+from src.modelsight import calibration
+
+__all__ = [
+    "calibration"
+]
+
+# __version__ = version("modelsight")
+
