702956c3babc2416d9b87b7138ab64b9ba5d852c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-25 14:05:26 +0000

    Add package django-outbox-menu/1.0.31/django-outbox-menu-1.0.31.tar.gz

---
+++ packages/django-outbox-menu/django-outbox-menu-1.0.31.tar.gz/django-outbox-menu-1.0.31/menu/_version.py
@@ -0,0 +1,11 @@
+"""
+Provides menu version information.
+"""
+
+# This file is auto-generated! Do not edit!
+# Use `python -m incremental.update menu` to change this file.
+
+from incremental import Version
+
+__version__ = Version("menu", 1, 0, 31)
+__all__ = ["__version__"]
