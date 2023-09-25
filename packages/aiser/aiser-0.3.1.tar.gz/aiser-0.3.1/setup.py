23863fcd7b3c3194bfd45c26adf1526303705caf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-25 03:07:56 +0000

    Add package aiser/0.3.1/aiser-0.3.1.tar.gz

---
+++ packages/aiser/aiser-0.3.1.tar.gz/aiser-0.3.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='aiser',
+    version='0.3.1',
+    url='https://github.com/penlight-ai/aiapi',
+    author='Penlight AI Inc.',
+    author_email='support@penlight.ai',
+    description='Python package to serve AI agents and knowledge bases for Penlight AI.',
+    long_description='Python package to serve AI agents and knowledge bases for Penlight AI.',
+    packages=find_packages(),
+    install_requires=[
+        'uvicorn',
+        'fastapi',
+        'pyjwt[crypto]',
+        'httpx',
+    ],
+    license='Apache License 2.0',
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3.11',
+    ],
+)
