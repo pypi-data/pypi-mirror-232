6031cd16572635675a709f922e139c6e3b7c1144
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-25 03:07:20 +0000

    Add package pyVisualCrossing/0.1.9/pyVisualCrossing-0.1.9.tar.gz

---
+++ packages/pyVisualCrossing/pyVisualCrossing-0.1.9.tar.gz/pyVisualCrossing-0.1.9/pyVisualCrossing/__init__.py
@@ -0,0 +1,8 @@
+"""Python Wrapper for Visual Crossing Weather API."""
+from __future__ import annotations
+
+
+__title__ = "pyVisualCrossing"
+__version__ = "0.1.9"
+__author__ = "briis"
+__license__ = "MIT"
