92e6183138d5d27708561d8462c5c0a957a7cedf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-25 03:07:11 +0000

    Add package stupidwalletapi/1.3.0/stupidwalletapi-1.3.0-py3-none-any.whl

---
+++ packages/stupidwalletapi/stupidwalletapi-1.3.0-py3-none-any.whl/stupidwalletapi/base.py
@@ -0,0 +1,33 @@
+import asyncio
+
+import httpx
+
+#ыы пися
+class SWError(Exception):
+  def __init__(self, message: str):
+    self.message = message
+    super().__init__(message)
+    
+
+
+class BaseAPI:
+    def __init__(self, token: str, base_url: str):
+        self.TOKEN = token
+        self.BASE_URL = base_url
+        self.lock = asyncio.Lock()
+        self._headers = {
+            'accept': 'application/json',
+            'api-key': self.TOKEN,
+        }
+
+    async def _make_request(self, method, url, params=None) -> dict:
+        async with self.lock:
+            async with httpx.AsyncClient() as client:
+                response = await client.request(method, self.BASE_URL + url, params=params, headers=self._headers)
+                print(url, response.status_code, response.json())
+
+            await asyncio.sleep(0.42)
+            if (response.json()).get('detail') is None:
+                return response.json()
+            else:
+                raise SWError((response.json()).get('detail'))
