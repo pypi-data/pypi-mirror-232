09686a760eb7de8f5e39d9116a39bcfd9527411b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-25 03:07:54 +0000

    Add package mub/0.1.2/mub-0.1.2.tar.gz

---
+++ packages/mub/mub-0.1.2.tar.gz/mub-0.1.2/src/mub/brainz.py
@@ -0,0 +1,38 @@
+"""
+Musicbrainz related functions.
+"""
+import time
+
+import musicbrainzngs as mus
+
+from .__init__ import __version__
+
+
+def init():
+    """Initialize musicbrainz."""
+    mus.set_useragent("python-mub: A cover art daemon.",
+                      __version__,
+                      "https://github.com/dylanaraps/bum")
+
+
+def get_cover(song, size=250, retry_delay=5, retries=5):
+    """Download the cover art."""
+    try:
+        data = mus.search_releases(artist=song["artist"],
+                                   release=song["album"],
+                                   limit=1)
+        release_id = data["release-list"][0]["release-group"]["id"]
+        #print(f"album: Using release-id: {data['release-list'][0]['id']}")
+
+        return mus.get_release_group_image_front(release_id, size=size)
+
+    except mus.NetworkError:
+        if retries == 0:
+            raise mus.NetworkError("Failure connecting to MusicBrainz.org")
+        print(f"warning: Retrying download. {retries} retries left!")
+        time.sleep(retry_delay)
+        get_cover(song, size, retries=retries - 1)
+
+    except mus.ResponseError:
+        print("error: Couldn't find album art for",
+              f"{song['artist']} - {song['album']}")
