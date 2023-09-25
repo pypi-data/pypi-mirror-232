3240c109b114949f577e1fb2d7496a95cd9d6f44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-25 15:05:02 +0000

    Add package anya-table/0.0.4/anya_table-0.0.4-py3-none-any.whl

---
+++ packages/anya-table/anya_table-0.0.4-py3-none-any.whl/anya_table/_imports_.py
@@ -0,0 +1,11 @@
+from .AnyaTable import AnyaTable
+from .AnyaTheme import AnyaTheme
+from .AnyaTitle import AnyaTitle
+from .Table import Table
+
+__all__ = [
+    "AnyaTable",
+    "AnyaTheme",
+    "AnyaTitle",
+    "Table"
+]
