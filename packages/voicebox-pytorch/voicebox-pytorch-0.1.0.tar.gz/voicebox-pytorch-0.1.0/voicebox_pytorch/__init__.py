3b3be44def42445550f2ecbc81faa3baf0d4da03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-25 03:08:32 +0000

    Add package voicebox-pytorch/0.1.0/voicebox-pytorch-0.1.0.tar.gz

---
+++ packages/voicebox-pytorch/voicebox-pytorch-0.1.0.tar.gz/voicebox-pytorch-0.1.0/voicebox_pytorch/__init__.py
@@ -0,0 +1,11 @@
+from voicebox_pytorch.voicebox_pytorch import (
+    Transformer,
+    EncodecVoco,
+    VoiceBox,
+    DurationPredictor,
+    ConditionalFlowMatcherWrapper,
+)
+
+from spear_tts_pytorch import TextToSemantic
+
+from audiolm_pytorch import HubertWithKmeans
