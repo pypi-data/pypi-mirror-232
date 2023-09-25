4bfe8ab9b0b2862a4a9e769a335f89131e7cd5f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-25 04:04:16 +0000

    Add package pycommon-kit/0.0.8/pycommon_kit-0.0.8-py3-none-any.whl

---
+++ packages/pycommon-kit/pycommon_kit-0.0.8-py3-none-any.whl/pycommon/db/tests/test_session_manager.py
@@ -0,0 +1,12 @@
+from unittest import mock
+
+import pytest
+
+from pycommon.db.utils import SessionManager
+
+
+# @pytest.fixture(name="session_manager")
+# def get_session_manager():
+#     # TODO: create setUp and tearDown with db.
+#     # test_base.py
+#     return SessionManager(mock.MagicMock())
