0c85e2b0817300334732f6e7e396479b9e1d3667
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-25 14:06:21 +0000

    Add package ezmse/0.0.7/ezmse-0.0.7.tar.gz

---
+++ packages/ezmse/ezmse-0.0.7.tar.gz/ezmse-0.0.7/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+from setuptools import find_packages
+
+setup(
+    name='ezmse',
+    version='0.0.7',
+    description='some package description',
+    author='Tom Tkacz',
+    author_email='thomasatkacz@gmail.com',
+    url='https://github.com/TomTkacz/EasyMSE',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'ezmse-cli = ezmse.main:main',
+        ]
+    },
+    long_description="package description, but long"
+)
