bb9497e57ed696c9c2a55727be316f89e5d3de92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-25 03:07:45 +0000

    Add package as3lib/0.0.5/as3lib-0.0.5-py3-none-any.whl

---
+++ packages/as3lib/as3lib-0.0.5-py3-none-any.whl/as3lib/configmodule.py
@@ -0,0 +1,23 @@
+platform = ""
+windowmanagertype = ""
+
+#toplevel
+as3DebugEnable = False
+ErrorReportingEnable = 0
+MaxWarnings = 100
+TraceOutputFileEnable = 0
+TraceOutputFileName = ""
+CurrentWarnings = 0
+MaxWarningsReached = False
+ClearLogsOnStartup = 1
+defaultTraceFilePath = ""
+defaultTraceFilePath_Flash = ""
+
+#display
+width = ""
+height = ""
+refreshrate = ""
+colordepth = ""
+
+#initcheck
+initdone = False
