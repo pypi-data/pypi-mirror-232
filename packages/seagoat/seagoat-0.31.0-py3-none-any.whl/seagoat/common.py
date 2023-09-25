edcad8e1e55b477e79c4438b419fc12bbe1d8f69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-25 04:04:17 +0000

    Add package seagoat/0.31.0/seagoat-0.31.0-py3-none-any.whl

---
+++ packages/seagoat/seagoat-0.31.0-py3-none-any.whl/seagoat/common.py
@@ -0,0 +1,22 @@
+IGNORED_BRANCHES = {"gh-pages"}
+
+SUPPORTED_FILE_TYPES = {
+    ".txt",
+    ".md",
+    ".py",
+    ".c",
+    ".cc",
+    ".cpp",
+    ".cxx",
+    ".h",
+    ".hpp",
+    ".ts",
+    ".js",
+    ".tsx",
+    ".jsx",
+    ".html",
+    ".go",
+    ".java",
+    ".php",
+    ".rb",
+}
