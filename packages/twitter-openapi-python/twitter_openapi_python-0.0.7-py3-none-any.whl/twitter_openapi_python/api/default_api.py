68d97d7cd2d6d0623787e47bd3dfa613dfb8b816
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-25 03:07:48 +0000

    Add package twitter-openapi-python/0.0.7/twitter_openapi_python-0.0.7-py3-none-any.whl

---
+++ packages/twitter-openapi-python/twitter_openapi_python-0.0.7-py3-none-any.whl/twitter_openapi_python/api/default_api.py
@@ -0,0 +1,59 @@
+from typing import Any, Callable, Optional, Type, TypeVar
+
+import twitter_openapi_python_generated as twitter
+import twitter_openapi_python_generated.models as models
+
+from twitter_openapi_python.models import TwitterApiUtilsResponse
+from twitter_openapi_python.utils import build_response, check_error, get_kwargs
+
+T1 = TypeVar("T1")
+T2 = TypeVar("T2")
+ApiFnType = Callable[..., twitter.ApiResponse]
+ParamType = dict[str, Any]
+
+
+class DefaultApiUtils:
+    api: twitter.DefaultApi
+    flag: ParamType
+
+    def __init__(self, api: twitter.DefaultApi, flag: ParamType):
+        self.api = api
+        self.flag = flag
+
+    def request(
+        self,
+        apiFn: ApiFnType,
+        convertFn: Callable[[T1], T2],
+        type1: Type[T1],
+        type2: Type[T2],
+        key: str,
+        param: ParamType,
+    ) -> TwitterApiUtilsResponse[T2]:
+        args = get_kwargs(flag=self.flag[key], additional=param)
+        res = apiFn(**args)
+        checked = check_error(data=res, type=type1)
+
+        data = convertFn(checked)
+
+        return build_response(response=res, data=data)
+
+    def get_profile_spotlights_query(
+        self,
+        screen_name: Optional[str] = None,
+        extra_param: Optional[ParamType] = None,
+    ) -> TwitterApiUtilsResponse[models.UserResultByScreenName]:
+        param: ParamType = {}
+        if screen_name is not None:
+            param["screen_name"] = screen_name
+        if extra_param is not None:
+            param.update(extra_param)
+
+        response = self.request(
+            apiFn=self.api.get_profile_spotlights_query_with_http_info,
+            convertFn=lambda x: x.data.user_result_by_screen_name,
+            type1=models.ProfileResponse,
+            type2=models.UserResultByScreenName,
+            key="ProfileSpotlightsQuery",
+            param=param,
+        )
+        return response
---
+++ packages/twitter-openapi-python/twitter_openapi_python-0.0.7-py3-none-any.whl/twitter_openapi_python/client.py
@@ -0,0 +1,99 @@
+from typing import Any
+
+import twitter_openapi_python_generated as twitter
+import twitter_openapi_python_generated.configuration as conf
+import urllib3
+
+from twitter_openapi_python.api import (
+    DefaultApiUtils,
+    InitialStateApiUtils,
+    PostApiUtils,
+    TweetApiUtils,
+    UserApiUtils,
+    UserListApiUtils,
+    UsersApiUtils,
+    V11GetApiUtils,
+    V11PostApiUtils,
+)
+
+ParamType = dict[str, Any]
+
+
+class TwitterOpenapiPythonClient:
+    api: twitter.ApiClient
+    placeholder: ParamType
+
+    def __init__(self, api: twitter.ApiClient, placeholder: ParamType):
+        self.api = api
+        self.placeholder = placeholder
+
+    def get_default_api(self) -> DefaultApiUtils:
+        return DefaultApiUtils(twitter.DefaultApi(self.api), self.placeholder)
+
+    def get_initial_state_api(self) -> InitialStateApiUtils:
+        return InitialStateApiUtils(self.api)
+
+    def get_post_api(self) -> PostApiUtils:
+        return PostApiUtils(twitter.PostApi(self.api), self.placeholder)
+
+    def get_tweet_api(self) -> TweetApiUtils:
+        return TweetApiUtils(twitter.TweetApi(self.api), self.placeholder)
+
+    def get_user_api(self) -> UserApiUtils:
+        return UserApiUtils(twitter.UserApi(self.api), self.placeholder)
+
+    def get_users_api(self) -> UsersApiUtils:
+        return UsersApiUtils(twitter.UsersApi(self.api), self.placeholder)
+
+    def get_user_list_api(self) -> UserListApiUtils:
+        return UserListApiUtils(twitter.UserListApi(self.api), self.placeholder)
+
+    def get_v11_get_api(self) -> V11GetApiUtils:
+        return V11GetApiUtils(twitter.V11GetApi(self.api), self.placeholder)
+
+    def get_v11_post_api(self) -> V11PostApiUtils:
+        return V11PostApiUtils(twitter.V11PostApi(self.api), self.placeholder)
+
+
+class TwitterOpenapiPython:
+    hash: str = "2d477a0fb84d249a30b5af535b467efc25b34923"
+    placeholder_url = "https://raw.githubusercontent.com/fa0311/twitter-openapi/{hash}/src/config/placeholder.json"
+    placeholder: ParamType
+    user_agent: str = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36"
+
+    def get_twitter_openapi_python_client(self, api: twitter.ApiClient):
+        http = urllib3.PoolManager()
+        flag = http.request("GET", self.placeholder_url.format(hash=self.hash)).json()
+        return TwitterOpenapiPythonClient(api, flag)
+
+    def get_client(self, api_client: twitter.ApiClient) -> TwitterOpenapiPythonClient:
+        return self.get_twitter_openapi_python_client(api_client)
+
+    def get_client_from_cookies(
+        self,
+        cookies: dict[str, str],
+    ) -> TwitterOpenapiPythonClient:
+        api_conf = conf.Configuration(
+            api_key={
+                "Accept": "*/*",
+                "AcceptEncoding": "gzip, deflate, br",
+                "AcceptLanguage": "en-US,en;q=0.9",
+                "CacheControl": "no-cache",
+                "Pragma": "no-cache",
+                "SecChUa": '"Chromium";v="116", "Not)A;Brand";v="24", "Google Chrome";v="116"',
+                "SecChUaMobile": "?0",
+                "SecChUaPlatform": '"Windows"',
+                "SecFetchDest": "empty",
+                "SecFetchMode": "cors",
+                "SecFetchSite": "same-origin",
+                "ClientLanguage": "en",
+                "ActiveUser": "yes",
+                "AuthType": "OAuth2Session",
+                "CsrfToken": cookies.get("ct0", ""),
+            },
+        )
+        api_conf.access_token = "AAAAAAAAAAAAAAAAAAAAANRILgAAAAAAnNwIzUejRCOuH5E6I8xnZz4puTs%3D1Zv7ttfk8LF81IUq16cHjhLTvJu4FA33AGWWjCpTnA"
+        str_cookie = "; ".join([f"{key}={value}" for key, value in cookies.items()])
+        api_client = twitter.ApiClient(configuration=api_conf, cookie=str_cookie)
+        api_client.user_agent = self.user_agent
+        return self.get_twitter_openapi_python_client(api_client)
