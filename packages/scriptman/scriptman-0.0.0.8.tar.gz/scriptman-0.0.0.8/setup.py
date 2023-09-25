b4fc29d39f5b3980742307bd32601845ce95bbbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-25 04:04:21 +0000

    Add package scriptman/0.0.0.8/scriptman-0.0.0.8.tar.gz

---
+++ packages/scriptman/scriptman-0.0.0.8.tar.gz/scriptman-0.0.0.8/setup.py
@@ -0,0 +1,35 @@
+import os
+
+from setuptools import find_packages, setup
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+VERSION = "0.0.0.8"
+DESCRIPTION = "Script Manager assists with managing python scripts."
+
+setup(
+    name="scriptman",
+    version=VERSION,
+    author="Nelson Ombuya",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    author_email="nelson.ombuya@zohomail.com",
+    long_description_content_type="text/markdown",
+    keywords=["python", "scripts", "etl", "selenium"],
+    install_requires=[
+        "tqdm",
+        "pyodbc",
+        "pandas",
+        "selenium",
+        "requests",
+        "webdriver_manager",
+    ],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ],
+)
