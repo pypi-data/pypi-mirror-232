f85550634b8a046c111ae6f71799a896f93cf011
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-25 14:05:15 +0000

    Add package fastrepl/0.0.11/fastrepl-0.0.11-py3-none-any.whl

---
+++ packages/fastrepl/fastrepl-0.0.11-py3-none-any.whl/fastrepl/__init__.py
@@ -0,0 +1,24 @@
+from typing import Optional
+
+api_base: Optional[str] = "https://yujonglee--fastrepl-api.modal.run"  # TODO
+api_key: Optional[str] = None
+
+from fastrepl.eval import (
+    load_metric,
+    HumanClassifierRich,
+    LLMGradingHead,
+    LLMClassificationHead,
+    LLMGradingHeadCOT,
+    LLMClassificationHeadCOT,
+    Evaluator,
+    SimpleEvaluator,
+    RAGEvaluator,
+)
+from fastrepl.analyze import Analyzer
+from fastrepl.generate import Generator, QuestionGenerator
+
+from fastrepl.utils import DEBUG
+
+from fastrepl.runner import local_runner, remote_runner
+
+from fastrepl.pytest_plugin import TestReport
