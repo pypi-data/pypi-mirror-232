8372d0f1d907a07365acbe968d517f30a09ff4ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-25 03:08:34 +0000

    Add package strategy-bridge/0.1.2/strategy_bridge-0.1.2-py3-none-any.whl

---
+++ packages/strategy-bridge/strategy_bridge-0.1.2-py3-none-any.whl/strategy_bridge/processors/robot_commands_sender.py
@@ -0,0 +1,22 @@
+import attr
+import zmq
+
+from strategy_bridge.bus import DataReader
+from strategy_bridge.common import config
+from strategy_bridge.processors import BaseProcessor
+
+
+@attr.s(auto_attribs=True)
+class RobotCommandsSender(BaseProcessor):
+
+    commands_reader: DataReader = attr.ib(init=False, default=DataReader(config.ROBOT_COMMANDS_TOPIC))
+
+    def __attrs_post_init__(self) -> None:
+        context = zmq.Context()
+        self.socket = context.socket(zmq.PUB)
+        self.socket.bind(f"tcp://*:{config.COMMANDS_PUBLISH_PORT}")
+
+    async def process(self):
+        commands = self.commands_reader.read_new()
+        for command in commands:
+            self.socket.send(command.content)
