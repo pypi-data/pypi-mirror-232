d970ff921589813824aaff39623ff6f21d01d3c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-25 14:05:25 +0000

    Add package create-fusion-app/0.1.13/create-fusion-app-0.1.13.tar.gz

---
+++ packages/create-fusion-app/create-fusion-app-0.1.13.tar.gz/create-fusion-app-0.1.13/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='create-fusion-app',
+    version='0.1.13',
+    packages=find_packages(),
+    install_requires=[
+    ],
+    entry_points={
+        'console_scripts': [
+            'fusion = create_fusion_app.main:main',
+        ],
+    },
+    author='Matthew Ford',
+    author_email='matthew@symbiotic.love',
+    description='Create Fusion App creates a specifically formatted React/Redux app called a Fusion app.',
+    license='MIT',
+)
