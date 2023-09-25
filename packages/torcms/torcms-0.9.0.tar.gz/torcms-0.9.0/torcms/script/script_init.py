6bf8927d365070c0c8305a6c8f62e1c08a3c74fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-25 03:08:57 +0000

    Add package torcms/0.9.0/torcms-0.9.0.tar.gz

---
+++ packages/torcms/torcms-0.9.0.tar.gz/torcms-0.9.0/torcms/script/script_init.py
@@ -0,0 +1,46 @@
+'''
+script for initialization.
+'''
+
+import sys
+
+from config import DB_CON
+
+from .autocrud.base_crud import build_dir
+from .autocrud.gen_html_file import generate_html_files as run_auto
+from .script_funcs import run_create_admin, run_whoosh
+from .script_gen_category import run_gen_category
+from .script_init_tabels import run_init_tables
+from .script_gen_process import run_gen_process
+from .script_zero import run_zero
+from .script_gen_role_permission import run_gen_role_permission
+from pathlib import Path
+
+XLSX_FILE = './database/role_perm.xlsx'
+
+
+def run_init(*args):
+    '''
+    running init.
+    '''
+    build_dir()
+    run_auto()
+
+    try:
+        DB_CON.cursor()
+    except Exception as err:
+        print(repr(err))
+        print('Could not connect to database ...')
+        run_zero()
+        sys.exit()
+
+    run_init_tables()
+
+    if Path(XLSX_FILE).exists():
+        run_gen_role_permission()
+
+    run_gen_category()
+    run_create_admin()
+    run_whoosh()
+
+    # run_gen_process()
