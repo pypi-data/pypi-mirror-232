6d623b3a6cbd56db39cecdec6a70b06be4127624
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-25 03:08:54 +0000

    Add package import-linter/1.12.0/import_linter-1.12.0-py3-none-any.whl

---
+++ packages/import-linter/import_linter-1.12.0-py3-none-any.whl/importlinter/adapters/building.py
@@ -0,0 +1,24 @@
+from typing import List, Optional
+
+import grimp
+from grimp import ImportGraph
+
+from importlinter.application.ports import building as ports
+
+
+class GraphBuilder(ports.GraphBuilder):
+    """
+    GraphBuilder that just uses Grimp's standard build_graph function.
+    """
+
+    def build(
+        self,
+        root_package_names: List[str],
+        cache_dir: Optional[str],
+        include_external_packages: bool = False,
+    ) -> ImportGraph:
+        return grimp.build_graph(
+            *root_package_names,
+            include_external_packages=include_external_packages,
+            cache_dir=cache_dir
+        )
---
+++ packages/import-linter/import_linter-1.12.0-py3-none-any.whl/importlinter/application/ports/building.py
@@ -0,0 +1,15 @@
+import abc
+from typing import List, Optional
+
+from grimp import ImportGraph
+
+
+class GraphBuilder(abc.ABC):
+    @abc.abstractmethod
+    def build(
+        self,
+        root_package_names: List[str],
+        cache_dir: Optional[str],
+        include_external_packages: bool = False,
+    ) -> ImportGraph:
+        raise NotImplementedError
