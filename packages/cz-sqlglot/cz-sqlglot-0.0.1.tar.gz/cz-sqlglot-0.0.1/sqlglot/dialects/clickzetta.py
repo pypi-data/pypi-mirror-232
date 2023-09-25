2a3c7a15829612c93d5604699722ddc7c8604f0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-25 14:05:33 +0000

    Add package cz-sqlglot/0.0.1/cz-sqlglot-0.0.1.tar.gz

---
+++ packages/cz-sqlglot/cz-sqlglot-0.0.1.tar.gz/cz-sqlglot-0.0.1/sqlglot/dialects/clickzetta.py
@@ -0,0 +1,93 @@
+from __future__ import annotations
+
+from sqlglot import exp, transforms
+from sqlglot.dialects.spark import Spark
+from sqlglot.tokens import Tokenizer, TokenType
+
+def _transform_create(expression: exp.Expression) -> exp.Expression:
+    """Remove index column constraints.
+    Remove unique column constraint (due to not buggy input)."""
+    schema = expression.this
+    if isinstance(expression, exp.Create) and isinstance(schema, exp.Schema):
+        to_remove = []
+        for e in schema.expressions:
+            if isinstance(e, exp.IndexColumnConstraint) or \
+                isinstance(e, exp.UniqueColumnConstraint):
+                to_remove.append(e)
+        for e in to_remove:
+            schema.expressions.remove(e)
+    return expression
+
+def _groupconcat_to_wmconcat(self: ClickZetta.Generator, expression: exp.GroupConcat) -> str:
+    this = self.sql(expression, "this")
+    sep = expression.args.get('separator')
+    if not sep:
+        sep = exp.Literal.string(',')
+    return f"WM_CONCAT({sep}, {self.sql(this)})"
+
+class ClickZetta(Spark):
+
+    NULL_ORDERING = "nulls_are_small"
+
+    class Tokenizer(Spark.Tokenizer):
+        KEYWORDS = {
+            **Tokenizer.KEYWORDS,
+            "CREATE USER": TokenType.COMMAND,
+            "DROP USER": TokenType.COMMAND,
+            "SHOW USER": TokenType.COMMAND,
+            "REVOKE": TokenType.COMMAND,
+        }
+
+    class Parser(Spark.Parser):
+        pass
+
+    class Generator(Spark.Generator):
+
+        TYPE_MAPPING = {
+            **Spark.Generator.TYPE_MAPPING,
+            exp.DataType.Type.MEDIUMTEXT: "STRING",
+            exp.DataType.Type.LONGTEXT: "STRING",
+            exp.DataType.Type.VARIANT: "STRING",
+            exp.DataType.Type.ENUM: "STRING",
+            exp.DataType.Type.ENUM16: "STRING",
+            exp.DataType.Type.ENUM8: "STRING",
+            exp.DataType.Type.UINT: "INT",
+            exp.DataType.Type.UTINYINT: "TINYINT",
+            exp.DataType.Type.USMALLINT: "SMALLINT",
+            exp.DataType.Type.UMEDIUMINT: "INT",
+            exp.DataType.Type.UBIGINT: "BIGINT",
+        }
+
+        TRANSFORMS = {
+            **Spark.Generator.TRANSFORMS,
+            exp.DefaultColumnConstraint: lambda self, e: '',
+            exp.OnUpdateColumnConstraint: lambda self, e: '',
+            exp.AutoIncrementColumnConstraint: lambda self, e: '',
+            exp.CollateColumnConstraint: lambda self, e: '',
+            exp.CharacterSetColumnConstraint: lambda self, e: '',
+            exp.Create: transforms.preprocess([_transform_create]),
+            exp.GroupConcat: _groupconcat_to_wmconcat,
+        }
+
+        def datatype_sql(self, expression: exp.DataType) -> str:
+            """Remove unsupported type params from int types: eg. int(10) -> int
+            Remove type param from enum series since it will be mapped as STRING."""
+            type_value = expression.this
+            type_sql = (
+                self.TYPE_MAPPING.get(type_value, type_value.value)
+                if isinstance(type_value, exp.DataType.Type)
+                else type_value
+            )
+            if type_value in exp.DataType.INTEGER_TYPES or \
+                type_value in {
+                    exp.DataType.Type.UTINYINT,
+                    exp.DataType.Type.USMALLINT,
+                    exp.DataType.Type.UMEDIUMINT,
+                    exp.DataType.Type.UINT,
+                    exp.DataType.Type.UINT128,
+                    exp.DataType.Type.UINT256,
+
+                    exp.DataType.Type.ENUM,
+               }:
+                return type_sql
+            return super().datatype_sql(expression)
