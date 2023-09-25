ef7cea254c6ad9680b31c6a282bc8f1d7dc0bfee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-25 14:05:42 +0000

    Add package arithmetic.py/0.1/arithmetic.py-0.1.tar.gz

---
+++ packages/arithmetic.py/arithmetic.py-0.1.tar.gz/arithmetic.py-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="arithmetic.py",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+        # List your library dependencies here
+    ],
+)
