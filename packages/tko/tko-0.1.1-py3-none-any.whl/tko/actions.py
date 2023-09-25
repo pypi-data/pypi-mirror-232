37bdd7b5b40bb9526c1309c5468f0c772a8d7595
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-25 03:06:37 +0000

    Add package tko/0.1.1/tko-0.1.1-py3-none-any.whl

---
+++ packages/tko/tko-0.1.1-py3-none-any.whl/tko/actions.py
@@ -0,0 +1,104 @@
+from typing import List
+import subprocess
+
+from .wdir import Wdir
+from .basic import DiffMode, ExecutionResult, CompilerError, Param, Unit
+from .diff import Diff
+from .format import Symbols, Colored, Color, Report
+from .writer import Writer
+from .solver import Solver
+from .runner import Runner
+
+
+class Execution:
+
+    def __init__(self):
+        pass
+
+    # run a unit using a solver and return if the result is correct
+    @staticmethod
+    def run_unit(solver: Solver, unit: Unit) -> ExecutionResult:
+        cmd = solver.executable.split(" ")
+        return_code, stdout, stderr = Runner.subprocess_run(cmd, unit.input)
+        unit.user = stdout + stderr
+        if return_code != 0:
+            unit.user += Symbols.execution
+            return ExecutionResult.EXECUTION_ERROR
+        if unit.user == unit.output:
+            return ExecutionResult.SUCCESS
+        return ExecutionResult.WRONG_OUTPUT
+
+
+class Actions:
+
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def exec(target_list: List[str]):
+        try:
+            wdir = Wdir().set_target_list(target_list).build()
+        except CompilerError as e:
+            print(e)
+            return 0
+        
+        if wdir.solver is None:
+            print("\n" + Colored.paint("fail:", Color.RED) + " no solver found\n")
+            return        
+        
+        print(Report.centralize(" Exec " + wdir.solver.executable + " ", Symbols.hbar))
+        subprocess.run(wdir.solver.executable, shell=True)
+
+    @staticmethod
+    def list(target_list: List[str], param: Param.Basic):
+        wdir = Wdir().set_target_list(target_list).build().filter(param)
+        print(wdir.resume())
+        print(wdir.unit_list_resume())
+
+    @staticmethod
+    def run(target_list: List[str], param: Param.Basic) -> int:
+        try:
+            wdir = Wdir().set_target_list(target_list).build().filter(param)
+        except CompilerError as e:
+            print(e)
+            return 0
+        
+        print(wdir.resume(), end="")
+
+        if wdir.solver is None:
+            print("\n" + Colored.paint("fail:", Color.RED) + " no solver found\n")
+            return 0
+        
+
+        ## print top line
+        print("[ ", end="")
+        for unit in wdir.unit_list:
+            unit.result = Execution.run_unit(wdir.solver, unit)
+            print(ExecutionResult.get_symbol(unit.result) + " ", end="")
+        print("]")
+
+        if param.diff_mode != DiffMode.QUIET:        
+            results = [unit.result for unit in wdir.unit_list]
+            if (ExecutionResult.EXECUTION_ERROR in results) or (ExecutionResult.WRONG_OUTPUT in results):
+                
+                # print cases lines
+                print(wdir.unit_list_resume())
+
+                # printing only the first wrong case
+                wrong = [unit for unit in wdir.unit_list if unit.result != ExecutionResult.SUCCESS][0]
+                if param.is_up_down:
+                    print(Diff.mount_up_down_diff(wrong))
+                else:
+                    print(Diff.mount_side_by_side_diff(wrong))
+        return wdir.calc_grade()
+
+    @staticmethod
+    def build(target_out: str, source_list: List[str], param: Param.Manip, to_force: bool) -> bool:
+        try:
+            wdir = Wdir().set_sources(source_list).build()
+            wdir.manipulate(param)
+            Writer.save_target(target_out, wdir.unit_list, to_force)
+        except FileNotFoundError as e:
+            print(str(e))
+            return False
+        return True
---
+++ packages/tko/tko-0.1.1-py3-none-any.whl/tko/guide.py
@@ -0,0 +1,38 @@
+tko_guide = """
+╭────────────────── TKO SIMPLE GUIDE ─────────────────╮
+│Mostrar esse guia : tko -g                           │
+│Atualizar scripts : update.sh                        │
+├────────────────────── BAIXAR ───────────────────────┤
+│Baixar o problema : tko down [fup|poo|ed] _id _ext   │
+│ Exemplo para poo : tko down poo 012 cpp             │
+├────────────────────── TESTAR ───────────────────────┤
+│Rodar teste: tko run _codigo[s] _testes [-v] [-i ind]│
+│    exemplo: tko run solver.cpp cases.tio            │
+│  diff vert: tko run solver.cpp cases.tio -v         │
+│ só teste 6: tko run solver.cpp cases.tio -i 6       │
+├────────────────────── EXECUTAR ─────────────────────┤
+│Executar sem testar: tko exec _codigo                │
+│           exemplo : tko exec solver.ts              │
+╰─────────────────────────────────────────────────────╯
+"""
+
+bash_guide = """
+╭────────────────── BASH SIMPLE GUIDE ────────────────╮
+│Mostrar esse guia : tko -b                           │
+├────────────────────── NAVEGAR ──────────────────────┤
+│Mostrar arquivos  : ls                               │
+│Mudar de pasta    : cd _nome_da_pasta                │
+│Subir um nível    : cd ..                            │
+├─────────────────────── CRIAR ───────────────────────┤
+│Criar um arquivo  : touch _nome_do_arquivo           │
+│Criar uma pasta   : mkdir _nome_da_pasta             │
+├────────────────────── REMOVER ──────────────────────┤
+│Apagar um arquivo : rm _nome_do_arquivo              │
+│Apagar uma pasta  : rm -r _nome_da_pasta             │
+│Renomear ou mover : mv _antigo _novo                 │
+├───────────────────── CONTROLAR ─────────────────────┤
+│Limpar console    : Control L                        │
+│Cancelar execução : Control C                        │
+│Finalizar entrada : Control D                        │
+╰─────────────────────────────────────────────────────╯
+"""
