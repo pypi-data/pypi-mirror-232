f8e0a01ea296d0b53e5709616e1c22d16b2813a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-25 15:05:05 +0000

    Add package balanna/2.0.0/balanna-2.0.0.tar.gz

---
+++ packages/balanna/balanna-2.0.0.tar.gz/balanna-2.0.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+
+REQUIRED = [
+    'numpy>=1.21',
+    'trimesh[easy]>=3.16.0',
+    'vedo',
+    'PyQt5',
+    'image_grid'
+    # 'opencv-python-headless'
+]
+
+setup(
+    name='balanna',
+    version='2.0.0',
+    description='Visualization Tools for 2D & 3D stuff working out-of-the-box',
+    author='Simon Schaefer & Dorian Henning',
+    author_email='simon.k.schaefer@gmail.com',
+    packages=find_packages(),
+    install_requires=REQUIRED,
+    python_requires='>=3.8'
+)
