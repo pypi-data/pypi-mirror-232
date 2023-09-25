9e385106a0c9cb4c774dddc87997374ff7cfd1d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-25 03:07:57 +0000

    Add package finterstellar/0.2.291/finterstellar-0.2.291-py3-none-any.whl

---
+++ packages/finterstellar/finterstellar-0.2.291-py3-none-any.whl/finterstellar/__init__.py
@@ -0,0 +1,11 @@
+from .data_prep import *
+from .trading import *
+from .trend import *
+from .visualization import *
+from .financials import *
+from .krx import *
+import local
+
+__doc__ = '''
+python library for quantitative analysis
+'''
