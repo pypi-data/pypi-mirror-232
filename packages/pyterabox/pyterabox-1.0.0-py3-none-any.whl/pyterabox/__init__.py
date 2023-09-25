6e17f8ff55261faab056c586410433c17d59cb5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-25 04:04:15 +0000

    Add package pyterabox/1.0.0/pyterabox-1.0.0-py3-none-any.whl

---
+++ packages/pyterabox/pyterabox-1.0.0-py3-none-any.whl/pyterabox/__init__.py
@@ -0,0 +1,24 @@
+import requests
+from json import loads
+from bs4 import BeautifulSoup
+
+session = requests.session()
+
+def download(url):
+	ids = url.split("/")[-1]
+	resp = session.get(f"https://terabox-dl.qtcloud.workers.dev/api/get-info?shorturl={ids}&pwd=")
+	data = loads(resp.text)
+	body = {
+		'shareid': data["shareid"],
+		'uk': data["uk"],
+		'sign': data["sign"],
+		'timestamp': data["timestamp"],
+	}
+	try:
+		body['fs_id'] = data["list"][0]["children"][0]["fs_id"]
+	except:
+		body['fs_id'] = data["list"][0]["fs_id"]
+	resp = session.post("https://terabox-dl.qtcloud.workers.dev/api/get-download",json=body,headers={"Content-Type":"application/json"})
+	data = loads(resp.text)
+	url = data["downloadLink"]
+	return url
