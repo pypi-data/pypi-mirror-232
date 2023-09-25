748d727c2423d9ddabac4678d4056e6103078d54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-25 14:06:18 +0000

    Add package viper-lib/1.1/viper_lib-1.1-py3-none-any.whl

---
+++ packages/viper-lib/viper_lib-1.1-py3-none-any.whl/Viper/__init__.py
@@ -0,0 +1,5 @@
+"""
+The Viper library contains many useful tools to improve the capabilities of Python.
+"""
+
+__all__ = ["abc", "debugging", "meta", "exceptions", "format", "frozendict", "interactive", "pickle_utils", "warnings", "io"]
