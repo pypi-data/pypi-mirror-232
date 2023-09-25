738cd872d1680d9f2ba8ec38dfd92785242ba966
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-25 03:08:44 +0000

    Add package private-values/0.4.5/private_values-0.4.5.tar.gz

---
+++ packages/private-values/private_values-0.4.5.tar.gz/private_values-0.4.5/private_values/auto.py
@@ -0,0 +1,33 @@
+from .main import Type_PvDict, PrivateAuth, PrivateTgBotAddress
+from .env import PrivateEnv
+from .ini import PrivateIni
+from .json import PrivateJson
+
+from typing import *
+
+
+# =====================================================================================================================
+class PrivateAuto(PrivateJson, PrivateIni, PrivateEnv):
+    def as_dict(self) -> Union[Type_PvDict, NoReturn]:
+        for cls in [PrivateAuto, PrivateJson, PrivateIni]:
+            try:
+                self.FILENAME = super(cls, self).FILENAME
+                self.filepath_apply_new()
+
+                data = super(cls, self).as_dict()
+                if self.check_by_annotations(data):
+                    return data
+            except:
+                pass
+
+
+# =====================================================================================================================
+class PrivateAuthAuto(PrivateAuth, PrivateAuto):
+    pass
+
+
+class PrivateTgBotAddressAuto(PrivateTgBotAddress, PrivateAuto):
+    pass
+
+
+# =====================================================================================================================
