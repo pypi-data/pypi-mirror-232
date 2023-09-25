02870c457129d3e0d3b4e8e11e5e16f7f7ecf2ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-25 03:07:53 +0000

    Add package pyproject-maker/0.6.2/pyproject_maker-0.6.2.tar.gz

---
+++ packages/pyproject-maker/pyproject_maker-0.6.2.tar.gz/pyproject_maker-0.6.2/py_maker/commands/new.py
@@ -0,0 +1,58 @@
+"""Create a new project."""
+from typing import Annotated, Optional
+
+import typer
+from rich import print  # pylint: disable=W0622
+
+from py_maker.config.settings import Settings
+from py_maker.constants import ExitErrors
+from py_maker.pymaker import PyMaker
+
+app = typer.Typer(no_args_is_help=True)
+settings = Settings()
+
+
+@app.callback(invoke_without_command=True)
+def new(
+    location: Annotated[
+        str,
+        typer.Argument(
+            ..., help="Where to create the project.", show_default=False
+        ),
+    ],
+    accept_defaults: Annotated[
+        bool, typer.Option("--yes", "-y", help="Accept all defaults.")
+    ] = False,
+    git: Annotated[
+        Optional[bool],
+        typer.Option(help="Initialize a git repository.", show_default=False),
+    ] = None,
+    test: Annotated[
+        Optional[bool],
+        typer.Option(help="Add testing libraries.", show_default=False),
+    ] = None,
+    lint: Annotated[
+        Optional[bool],
+        typer.Option(help="Add linting libraries.", show_default=False),
+    ] = None,
+    docs: Annotated[
+        Optional[bool],
+        typer.Option(help="Add the MkDocs boilerplate.", show_default=False),
+    ] = None,
+) -> None:
+    """Create a new Python project."""
+    options = {
+        "git": settings.use_git if git is None else git,
+        "test": settings.include_testing if test is None else test,
+        "lint": settings.include_linters if lint is None else lint,
+        "docs": settings.include_mkdocs if docs is None else docs,
+        "accept_defaults": accept_defaults,
+    }
+
+    if " " in location:
+        print("\n[red]  --> The location cannot contain spaces, [b]Aborting.")
+        raise typer.Exit(ExitErrors.LOCATION_ERROR)
+
+    pymaker = PyMaker(location, options)
+
+    pymaker.run()
