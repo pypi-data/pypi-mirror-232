d81bdfb5084b4b2425b97b46760129ee31dfec01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-25 03:08:36 +0000

    Add package badapple/0.0.2/badapple-0.0.2-py3-none-any.whl

---
+++ packages/badapple/badapple-0.0.2-py3-none-any.whl/badapple/audio.py
@@ -0,0 +1,49 @@
+import os
+from multiprocessing import Process
+from typing import Tuple
+
+from .util import get_info
+from .players import realias, playa
+from .players import is_player, is_available, get_players, get_availables
+
+
+def help_audio() -> None:
+    l = get_players()
+    r = get_availables()
+    s = 'usage: badapple --audio_player AUDIO_PLAYER [options] ... \n\n'
+    s += get_info() + '\n\navailable AUDIO_PLAYER:\n'
+    for i in l:
+        s += '  ' + i + ' '*(14-len(i)) + ('  ' if i in r else 'un') + 'available\n'
+    print(s)
+
+
+def preplaya(
+    audio: str,
+    player: str,
+    video: str = None,
+    check_player: bool = True
+) -> Tuple[str, str]:
+    if not audio:
+        if not player:
+            return '', ''
+        if video is None:
+            raise FileNotFoundError(audio)
+        audio = video
+
+    audio = os.path.abspath(audio)
+    open(audio, 'rb').close()
+
+    if not is_player(player):
+        raise ValueError('%s is not a player' % player)
+    player = realias(player)
+    if check_player:
+        if not is_available(player):
+            raise ValueError('Player %s is not available' % player)
+
+    return audio, player
+
+
+def p_playa(audio: str, player: str) -> Process:
+    p = Process(target=playa, args=(audio, player))
+    p.start()
+    return p
---
+++ packages/badapple/badapple-0.0.2-py3-none-any.whl/badapple/replay.py
@@ -0,0 +1,50 @@
+import os
+from multiprocessing import Process
+
+from .util import get_func, Timer
+from .audio import p_playa, preplaya
+
+
+def replay(
+    p_list: list[Process],
+    video: str, audio: str, player: str,
+    need_clear: bool = True, check_player: bool = True,
+    debug: bool = False
+) -> None:
+    video = os.path.abspath(video)
+    open(video, 'r').close()
+    audio, player = preplaya(audio, player, check_player=check_player)
+    if debug:
+        print(audio, player)
+
+    s = open(video, 'r').read().split('\n\n')
+    x, y, clk = s[0].split()
+    x = int(x)
+    y = int(y)
+    clk = float(clk) / 1000.0
+
+    print('[%d:%d %.2lfHz replay]' % (x, y, 1.0/clk))
+
+    rewind, clear, console_size = get_func(need_clear)
+
+    timer = Timer(clk)
+    print('BEGINNING...', flush=True)
+    timer.slp()
+    if audio:
+        p = p_playa(audio, player)
+        p_list.append(p)
+        timer.slp(0.01)
+    if debug:
+        timer.slp(5)
+    rewind()
+    clear()
+    if not debug:
+        console_size(x, y//2+1)
+        rewind()
+        clear()
+    timer.bg()
+
+    for i in s[1:]:
+        rewind()
+        print(i, flush=True)
+        timer.wait()
