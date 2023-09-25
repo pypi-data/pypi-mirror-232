738cfe876c09cfbb91043ac871bfa7dc01131ca1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-25 14:05:12 +0000

    Add package types-psycopg2/2.9.21.14/types-psycopg2-2.9.21.14.tar.gz

---
+++ packages/types-psycopg2/types-psycopg2-2.9.21.14.tar.gz/types-psycopg2-2.9.21.14/setup.py
@@ -0,0 +1,45 @@
+from setuptools import setup
+
+name = "types-psycopg2"
+description = "Typing stubs for psycopg2"
+long_description = '''
+## Typing stubs for psycopg2
+
+This is a PEP 561 type stub package for the `psycopg2` package. It
+can be used by type-checking tools like
+[mypy](https://github.com/python/mypy/),
+[pyright](https://github.com/microsoft/pyright),
+[pytype](https://github.com/google/pytype/),
+PyCharm, etc. to check code that uses
+`psycopg2`. The source for this package can be found at
+https://github.com/python/typeshed/tree/main/stubs/psycopg2. All fixes for
+types and metadata should be contributed there.
+
+See https://github.com/python/typeshed/blob/main/README.md for more details.
+This package was generated from typeshed commit `dd00f9d89983db060612bec3ea74ae253e2a7420` and was tested
+with mypy 1.5.1, pyright 1.1.328, and
+pytype 2023.8.31.
+'''.lstrip()
+
+setup(name=name,
+      version="2.9.21.14",
+      description=description,
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url="https://github.com/python/typeshed",
+      project_urls={
+          "GitHub": "https://github.com/python/typeshed",
+          "Changes": "https://github.com/typeshed-internal/stub_uploader/blob/main/data/changelogs/psycopg2.md",
+          "Issue tracker": "https://github.com/python/typeshed/issues",
+          "Chat": "https://gitter.im/python/typing",
+      },
+      install_requires=[],
+      packages=['psycopg2-stubs'],
+      package_data={'psycopg2-stubs': ['__init__.pyi', '_ipaddress.pyi', '_json.pyi', '_psycopg.pyi', '_range.pyi', 'errorcodes.pyi', 'errors.pyi', 'extensions.pyi', 'extras.pyi', 'pool.pyi', 'sql.pyi', 'tz.pyi', 'METADATA.toml']},
+      license="Apache-2.0 license",
+      classifiers=[
+          "License :: OSI Approved :: Apache Software License",
+          "Programming Language :: Python :: 3",
+          "Typing :: Stubs Only",
+      ]
+)
