d29cf69840d0a693c89763b115512d012f31a31d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-25 04:04:32 +0000

    Add package didjvu/0.10.1/didjvu-0.10.1-py3-none-any.whl

---
+++ packages/didjvu/didjvu-0.10.1-py3-none-any.whl/didjvu/__init__.py
@@ -0,0 +1,20 @@
+# encoding=UTF-8
+
+# Copyright © 2009-2021 Jakub Wilk <jwilk@jwilk.net>
+#
+# This file is part of didjvu.
+#
+# didjvu is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License version 2 as
+# published by the Free Software Foundation.
+#
+# didjvu is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+# for more details.
+
+"""
+didjvu's private modules
+"""
+
+# vim:ts=4 sts=4 sw=4 et
---
+++ packages/didjvu/didjvu-0.10.1-py3-none-any.whl/didjvu/__main__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+def main(prog=None):
+    from didjvu.didjvu import Main
+    Main(prog=prog)
+
+
+if __name__ == '__main__':
+    main(prog='didjvu.__main__')
