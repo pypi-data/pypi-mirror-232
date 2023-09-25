0367e885143f801a1e7c1748b66d5f42df03c328
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-25 03:06:37 +0000

    Add package tko/0.1.1/tko-0.1.1.tar.gz

---
+++ packages/tko/tko-0.1.1.tar.gz/tko-0.1.1/changelog.md
@@ -0,0 +1,24 @@
+# Changelog
+
+- 0.1.1
+  - added -m monocolor option
+  - improved layout for both diff
+- 0.1.0
+  - added bash guide with -b
+  - added tko guide with -g
+- 0.0.9
+  - removed tab from wdir.resume
+  - fixed type in whitespace
+  - added mismatch arrow
+  - added -g option to show simple guide
+- 0.0.8
+  - add colors in diff inside the line
+- 0.0.7
+  - fix esbuild incompatibility in windows
+- 0.0.6
+  - fix wls incompatibility
+- 0.0.5
+  - adding draft models to down
+  - removing conversion in js and ts entry code
+  - adding --config to tko
+  - changing @ for _ in downloaded problems folder name
---
+++ packages/tko/tko-0.1.1.tar.gz/tko-0.1.1/src/tko/__main__.py
@@ -0,0 +1,196 @@
+from __future__ import annotations
+
+import argparse
+import sys
+
+from .actions import Actions
+from .basic import Param
+from .pattern import PatternLoader
+from .basic import DiffMode
+from .format import Report, Colored
+from .down import Down
+from .settings import SettingsParser
+from .guide import tko_guide
+from .guide import bash_guide
+from .format import symbols
+from .__init__ import __version__
+
+
+class Main:
+    @staticmethod
+    def execute(args):
+        Actions.exec(args.target_list)
+
+    @staticmethod
+    def run(args):
+        if args.width is not None:
+            Report.set_terminal_size(args.width)
+        PatternLoader.pattern = args.pattern
+        param = Param.Basic().set_index(args.index)
+        if args.quiet:
+            param.set_diff_mode(DiffMode.QUIET)
+        if args.vertical:
+            param.set_up_down(True)
+        Actions.run(args.target_list, param)
+
+    @staticmethod
+    def list(args):
+        if args.width is not None:
+            Report.set_terminal_size(args.width)
+        PatternLoader.pattern = args.pattern
+        param = Param.Basic().set_index(args.index)
+        Actions.list(args.target_list, param)
+
+    @staticmethod
+    def build(args):
+        if args.width is not None:
+            Report.set_terminal_size(args.width)
+        PatternLoader.pattern = args.pattern
+        manip = Param.Manip().set_unlabel(args.unlabel).set_to_sort(args.sort).set_to_number(args.number)
+        Actions.build(args.target, args.target_list, manip, args.force)
+    
+    @staticmethod
+    def settings(args):
+        sp = SettingsParser()
+        print("Settings file at " + sp.get_settings_file())
+
+        if (args.color):
+            SettingsParser().toggle_color()
+        if (args.encoding):
+           SettingsParser().toggle_ascii()
+        if (args.diff):
+            SettingsParser().toggle_hdiff()
+
+        print("COLORMODE: " + ("COLORED" if sp.get_color() else "MONO"))
+        print("DIFF MODE: " + ("SIDE_BY_SIDE" if sp.get_hdiff() else "UP_DOWN"))
+        print("ENCODING : " + ("ASCII" if sp.get_ascii() else "UNICODE"))
+
+        
+
+
+
+    # @staticmethod
+    # def rebuild(args):
+    #     if args.width is not None:
+    #         Report.set_terminal_size(args.width)
+    #     PatternLoader.pattern = args.pattern
+    #     manip = Param.Manip().set_unlabel(args.unlabel).set_to_sort(args.sort).set_to_number(args.number)
+    #     Actions.update(args.target_list, manip, args.cmd)
+    #     return 0
+
+    @staticmethod
+    def update(_args):
+        Down.update()
+
+    @staticmethod
+    def down(args):
+        destiny = Down.create_problem_folder(args.disc, args.index, args.extension)
+        Down.entry_unpack(destiny, args.disc, args.index, args.extension)
+
+    @staticmethod
+    def main():
+        parent_basic = argparse.ArgumentParser(add_help=False)
+        parent_basic.add_argument('--width', '-w', type=int, help="term width")
+        parent_basic.add_argument('--index', '-i', metavar="I", type=int, help='run a specific index.')
+        parent_basic.add_argument('--pattern', '-p', metavar="P", type=str, default='@.in @.sol',
+                                  help='pattern load/save a folder, default: "@.in @.sol"')
+
+        parent_manip = argparse.ArgumentParser(add_help=False)
+        parent_manip.add_argument('--width', '-w', type=int, help="term width.")
+        parent_manip.add_argument('--unlabel', '-u', action='store_true', help='remove all labels.')
+        parent_manip.add_argument('--number', '-n', action='store_true', help='number labels.')
+        parent_manip.add_argument('--sort', '-s', action='store_true', help="sort test cases by input size.")
+        parent_manip.add_argument('--pattern', '-p', metavar="@.in @.out", type=str, default='@.in @.sol',
+                                  help='pattern load/save a folder, default: "@.in @.sol"')
+
+        parser = argparse.ArgumentParser(prog='tko', description='A tool for competitive programming.')
+        parser.add_argument('-v', '--version', action='store_true', help='show version.')
+        parser.add_argument('-g', '--guide', action='store_true', help='show simple tko guide.')
+        parser.add_argument('-b', '--bash', action='store_true', help='show simple bash guide.')
+        parser.add_argument('-m', '--mono', action='store_true', help='monochromatic.')
+        parser.add_argument('-c', '--config', type=str, help='config file.')
+        
+        subparsers = parser.add_subparsers(title='subcommands', help='help for subcommand.')
+
+        # list
+        parser_l = subparsers.add_parser('list', parents=[parent_basic], help='show test entries.')
+        parser_l.add_argument('target_list', metavar='T', type=str, nargs='*', help='targets.')
+        parser_l.set_defaults(func=Main.list)
+
+        # exec
+        parser_e = subparsers.add_parser('exec', parents=[parent_basic], help='run without test.')
+        parser_e.add_argument('target_list', metavar='T', type=str, nargs='*', help='target.')
+        parser_e.set_defaults(func=Main.execute)
+
+        # run
+        parser_r = subparsers.add_parser('run', parents=[parent_basic], help='run with test cases.')
+        parser_r.add_argument('target_list', metavar='T', type=str, nargs='*', help='solvers, test cases or folders.')
+        parser_r.add_argument('--vertical', '-v', action='store_true', help="use vertical mode.")
+        parser_r.add_argument('--quiet', '-q', action='store_true', help='quiet mode, do not show diffs')
+        parser_r.set_defaults(func=Main.run)
+
+        # build
+        parser_b = subparsers.add_parser('build', parents=[parent_manip], help='build a test target.')
+        parser_b.add_argument('target', metavar='T_OUT', type=str, help='target to be build.')
+        parser_b.add_argument('target_list', metavar='T', type=str, nargs='+', help='input test targets.')
+        parser_b.add_argument('--force', '-f', action='store_true', help='enable overwrite.')
+        parser_b.set_defaults(func=Main.build)
+
+        # down
+        parser_d = subparsers.add_parser('down', help='download problem from repository.')
+        parser_d.add_argument('disc', type=str, help=" [ fup | ed | poo ]")
+        parser_d.add_argument('index', type=str, help="3 digits label like 021")
+        parser_d.add_argument('extension', type=str, nargs='?', default="-", help="[ c | cpp | js | ts | py | java ]")
+        parser_d.set_defaults(func=Main.down)
+
+        # update
+        parser_u = subparsers.add_parser('update', help='update problem from repository.')
+        parser_u.set_defaults(func=Main.update)
+
+        # settings
+        parser_s = subparsers.add_parser('config', help='settings tool.')
+        parser_s.add_argument('--encoding', '-e', action='store_true', help='toggle [ascii | unicode] mode.')
+        parser_s.add_argument('--color', '-c', action='store_true', help='toggle [colored | mono] mode.')
+        parser_s.add_argument('--diff', '-d', action='store_true', help='toggle [side_by_side | up_down] mode.')
+        parser_s.set_defaults(func=Main.settings)
+
+        args = parser.parse_args()
+
+        if args.config:
+            SettingsParser().set_settings_file(args.config)
+
+        if len(sys.argv) == 1:
+            parser.print_help()
+            return
+        
+        
+        if SettingsParser().get_ascii():
+            symbols.set_ascii()
+        else:
+            symbols.set_unicode()
+
+        if not args.mono and SettingsParser().get_color():
+            Colored.enabled = True
+            symbols.set_colors()
+
+        if args.version or args.guide or args.bash:
+            if args.version:
+                print("tko version " + __version__)
+            if args.bash:
+                print(bash_guide[1:], end="")
+            if args.guide:
+                print(tko_guide[1:], end="")
+        else:
+            try:
+                args.func(args)
+            except ValueError as e:
+                print(str(e))
+
+
+if __name__ == '__main__':
+    try:
+        Main.main()
+        sys.exit(0)
+    except KeyboardInterrupt:
+        print("\n\nKeyboard Interrupt")
+        sys.exit(1)
