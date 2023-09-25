8e687a9f039937bda8f5ea3d756eab4a37c7108e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-25 15:04:53 +0000

    Add package sumbeanDay01/0.0.2/sumbeanDay01-0.0.2.tar.gz

---
+++ packages/sumbeanDay01/sumbeanDay01-0.0.2.tar.gz/sumbeanDay01-0.0.2/setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+setup(name='sumbeanDay01',# 包名
+      version='0.0.2',
+      description='测试',
+      author='sumbean',
+      author_email='1041267952@qq.com',
+      packages=['sumbeanDay01'],
+      ) 
