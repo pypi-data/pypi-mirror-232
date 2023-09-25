d25dd3500d839c802d6dbc217d9cada013d2c7e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-25 03:08:43 +0000

    Add package PyLibWeb3/0.0.1/PyLibWeb3-0.0.1.tar.gz

---
+++ packages/PyLibWeb3/PyLibWeb3-0.0.1.tar.gz/PyLibWeb3-0.0.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+
+VERSION = '0.0.1'
+DESCRIPTION = 'A package that allows you to import your modules and libraries and updates them automatically.'
+
+# Setting up
+setup(
+    name="PyLibWeb3",
+    version=VERSION,
+    author="Makeley",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['requests', 'colorama'],
+    keywords=['python', 'video', 'stream', 'video stream', 'camera stream', 'sockets'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
