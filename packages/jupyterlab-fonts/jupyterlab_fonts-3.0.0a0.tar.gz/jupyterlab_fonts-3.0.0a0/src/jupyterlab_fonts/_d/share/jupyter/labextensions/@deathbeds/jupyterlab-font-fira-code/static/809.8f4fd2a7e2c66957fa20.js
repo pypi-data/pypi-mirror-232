bd91025b134c0e15653658f810a32debbeb9a584
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-25 04:04:16 +0000

    Add package jupyterlab-fonts/3.0.0a0/jupyterlab_fonts-3.0.0a0.tar.gz

---
+++ packages/jupyterlab-fonts/jupyterlab_fonts-3.0.0a0.tar.gz/jupyterlab_fonts-3.0.0a0/src/jupyterlab_fonts/_d/share/jupyter/labextensions/@deathbeds/jupyterlab-font-fira-code/static/809.8f4fd2a7e2c66957fa20.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunk_deathbeds_jupyterlab_font_fira_code=self.webpackChunk_deathbeds_jupyterlab_font_fira_code||[]).push([[809],{792:(e,f,a)=>{a.r(f),a.d(f,{default:()=>t});const t=a.p+"bf5ea52fa0094c7f7621a920f6059540.woff2"}}]);
+//# sourceMappingURL=809.8f4fd2a7e2c66957fa20.js.map?v=8f4fd2a7e2c66957fa20
