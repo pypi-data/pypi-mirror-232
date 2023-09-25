37d1035e3f144fc16df042489de95205960ca035
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-25 03:07:43 +0000

    Add package cortisol/0.3.0/cortisol-0.3.0-py3-none-any.whl

---
+++ packages/cortisol/cortisol-0.3.0-py3-none-any.whl/cortisol/commands/logs.py
@@ -0,0 +1,134 @@
+import json
+from pathlib import Path
+
+import typer
+import yaml
+
+from cortisol.cortisollib.log_cost_estimator import get_cost_estimate
+
+app = typer.Typer()
+
+
+def _config_reader(file_path: Path):
+    try:
+        file_content = file_path.read_text()
+
+        try:
+            data = yaml.safe_load(file_content)
+            return data
+        except yaml.YAMLError:
+            # If parsing as YAML fails, try parsing as JSON
+            import pdb
+
+            pdb.set_trace()
+            data = json.loads(file_content)
+            return data
+        except json.JSONDecodeError:
+            raise ValueError("Invalid YAML or JSON format in the input file.")
+    except FileNotFoundError:
+        raise FileNotFoundError(f"File not found at path: {file_path}")
+
+
+def _check_keys_in_file(file_path: Path):
+    data = _config_reader(file_path)
+
+    keys_to_check = [
+        "cortisol-file",
+        "host",
+        "log-file",
+        "run-time",
+        "spawn-rate",
+        "users",
+    ]
+    missing_keys = [key for key in keys_to_check if key not in data]
+
+    if missing_keys:
+        raise KeyError(f"Required keys are missing in the input file: {missing_keys}")
+
+
+@app.command()
+def cost_estimate(
+    cortisol_file: Path = typer.Option(
+        None, "-f", "--cortisol-file", help="Path to the CORTISOL_FILE"
+    ),
+    host: str = typer.Option(
+        None,
+        "-h",
+        "--host",
+        help="Host in the following format: http://10.20.31.32 or http://10.20.31.32:8000",
+    ),
+    log_file: Path = typer.Option(None, "-l", "--log-file", help="Path to log file"),
+    num_users: int = typer.Option(
+        None, "-u", "--users", help="Peak number of concurrent users"
+    ),
+    spawn_rate: int = typer.Option(
+        None, "-r", "--spawn-rate", help="Rate to spawn users at (users per second)"
+    ),
+    run_time: str = typer.Option(
+        None,
+        "-t",
+        "--run-time",
+        help="Stop after the specified amount of time, e.g. (50, 30s, 200m, 5h, 2h30m, etc.). Default unit in seconds.",
+    ),
+    container_id: str = typer.Option(
+        None,
+        "-c",
+        "--container-id",
+        help="Optional docker container id where your application runs",
+    ),
+    config: Path = typer.Option(None, "--config", help="Path to config file"),
+    stats_file: Path = typer.Option(
+        None,
+        "-s",
+        "--stats-file",
+        help="Optional stats file path where the stats will be stored as csv",
+    ),
+):
+    """
+    Forecast log costs pre-production with Cortisol for Datadog, New Relic, and Grafana
+    """
+    if not config and any(
+        var is None
+        for var in (cortisol_file, host, log_file, num_users, spawn_rate, run_time)
+    ):
+        typer.echo(
+            "Option '--config' is required or the following options '-f' / '--cortisol-file', "
+            "'-l' / '--log-file', '-h' / '--host', '-u' / '--users', '-r' / '--spawn-rate', '-t' / '--run-time"
+            "'-c' / '--container-id' is required only if your application runs in a Docker container"
+        )
+        raise typer.Abort()
+
+    if config:
+        try:
+            _check_keys_in_file(config)
+            data = _config_reader(config)
+            cortisol_file = data["cortisol-file"]
+            host = data["host"]
+            log_file = data["log-file"]
+            num_users = data["users"]
+            spawn_rate = data["spawn-rate"]
+            run_time = data["run-time"]
+            container_id = data.get("container-id", "")
+            stats_file = data.get("stats-file", None)
+        except (FileNotFoundError, ValueError, KeyError) as e:
+            typer.echo(str(e))
+            raise typer.Abort()
+
+    typer.echo("Cost estimate command in the making")
+    if not container_id:
+        container_id = ""
+
+    get_cost_estimate(
+        cortisol_file=cortisol_file,
+        host=host,
+        log_file=log_file,
+        num_users=num_users,
+        spawn_rate=spawn_rate,
+        run_time=run_time,
+        container_id=container_id,
+        stats_file=stats_file,
+    )
+
+
+if __name__ == "__main__":
+    app()
---
+++ packages/cortisol/cortisol-0.3.0-py3-none-any.whl/cortisol/cortisollib/log_cost_estimator.py
@@ -0,0 +1,254 @@
+import ast
+import threading
+import time
+import subprocess
+from pathlib import Path
+import os
+from jinja2 import Template
+
+_FILE_DIR_PATH = os.path.dirname(os.path.realpath(__file__))
+
+
+def animation_process(timeout, done):
+    animation = ["⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏"]
+    start_time = time.time()
+    while True:
+        elapsed_time = time.time() - start_time
+        if done.is_set():
+            break
+        remaining_seconds = max(0.0, timeout - elapsed_time)
+        minutes = int(remaining_seconds / 60)
+        seconds = int(remaining_seconds % 60)
+        timer_text = f"{minutes:02d}:{seconds:02d}"
+        animation_index = int(elapsed_time * 10) % len(animation)
+        animation_char = animation[animation_index]
+        print(
+            f"{animation_char} Hold tight cortisol is estimating your monthly log cost {animation_char} Remaining time: {timer_text}",
+            end="\r",
+        )
+        time.sleep(0.1)
+
+
+def _get_classes_extending_httpuser(code):
+    tree = ast.parse(code)
+
+    class_names = []
+    for node in ast.walk(tree):
+        if isinstance(node, ast.ClassDef):
+            for base in node.bases:
+                if isinstance(base, ast.Name) and base.id == "CortisolHttpUser":
+                    class_names.append(node.name)
+
+    formatted_string = "[" + ", ".join(class_names) + "]"
+    return formatted_string
+
+
+def render_locustfile(cortisol_file: Path):
+    """
+    Render a Locustfile template with user input and save the result.
+
+    This function reads a Jinja2 template for a Locust load test scenario from the template file,
+    merges it with the user-provided input from the specified 'cortisol_file', and then writes
+    the rendered content to a new Locustfile. The rendered content is returned.
+
+    Args:
+        cortisol_file (Path): Path to the user input file containing parameters for the load test.
+
+    Returns:
+        str: The rendered content of the Locustfile after merging the template with user input.
+
+    Example:
+        cortisol_file = Path("path/to/user_input.yaml")
+        rendered_content = render_locustfile(cortisol_file)
+    """
+    with open(
+        os.path.join(_FILE_DIR_PATH, "./templates/cli_loadtest.py.j2"), "r"
+    ) as template_file:
+        template_content = template_file.read()
+
+    # Create a Jinja template object
+    template = Template(template_content)
+
+    with open(cortisol_file, "r") as user_input_file:
+        user_input = user_input_file.read()
+        user_classes = _get_classes_extending_httpuser(user_input)
+
+    # Render the template with the user input
+    rendered_content = template.render(
+        cortisolfile=user_input, user_classes=user_classes
+    )
+
+    with open(
+        os.path.join(_FILE_DIR_PATH, "./templates/locustfile.py"), "w"
+    ) as merged_file:
+        merged_file.write(rendered_content)
+
+    return rendered_content
+
+
+def render_locust_command(
+    host: str,
+    log_file: Path,
+    num_users: int,
+    spawn_rate: int,
+    run_time: str,
+    container_id: str,
+    stats_file: Path = None,
+):
+    """
+    Generate a command for running a Locust load test in headless mode.
+
+    This function generates a command list that can be used to run a Locust load test
+    in headless mode. The command includes options such as the Locustfile path,
+    number of users, spawn rate, run time, container ID, and log file path.
+
+    Args:
+        log_file (Path): Path to the log file where Locust logs will be stored.
+        num_users (int): Number of concurrent users/clients to simulate.
+        spawn_rate (int): The rate at which new users are spawned per second.
+        run_time (str): The duration of the load test run (e.g., '10m' for 10 minutes).
+        container_id (str): Identifier for the Docker container, if applicable.
+        stats_file (Path): Optional. File path where the cortisol stats will be stored as csv
+
+    Returns:
+        List[str]: A list representing the command for running the Locust load test.
+
+    Example:
+        log_file = Path("path/to/locust_logs.log")
+        num_users = 100
+        spawn_rate = 10
+        run_time = '15m'
+        container_id = 'my-locust-container'
+
+        command = render_locust_command(log_file, num_users, spawn_rate, run_time, container_id)
+        # Execute the command using subprocess or other method
+    """
+    command = [
+        "locust",
+        "-f",
+        os.path.join(_FILE_DIR_PATH, "./templates/locustfile.py"),
+        "--headless",
+        "--host",
+        host,
+        "--users",
+        str(num_users),
+        "--spawn-rate",
+        str(spawn_rate),
+        "--run-time",
+        str(run_time),
+        "--container-id",
+        container_id,
+        "--log-file",
+        log_file,
+    ]
+
+    if stats_file:
+        command += ["--stats-file", stats_file]
+
+    return command
+
+
+def get_cost_estimate(
+    cortisol_file: Path,
+    host: str,
+    log_file: Path,
+    num_users: int,
+    spawn_rate: int,
+    run_time: str,
+    container_id: str = "",
+    stats_file: Path = None,
+):
+    """
+    Calculate the estimated cost of logs.
+
+    This function calculates the estimated cost of logs using the Locust
+    load testing tool. It renders the Locustfile specified in 'cortisol_file', generates
+    the Locust command based on the provided parameters, executes the command, captures
+    the output, and returns the return code of the process.
+
+    Args:
+        cortisol_file (Path): Path to the Locustfile containing the load test scenario.
+        host (str): Host to load test in the following format: http://10.21.32.33
+        log_file (Path): Path to the log file where Locust logs will be stored.
+        num_users (int): Number of concurrent users/clients to simulate.
+        spawn_rate (int): The rate at which new users are spawned per second.
+        run_time (str): The duration of the load test run (e.g., '10m' for 10 minutes).
+        container_id (str): Identifier for the Docker container, if applicable.
+        stats_file (Path): Optional. File path where the cortisol stats will be stored as csv
+
+    Returns:
+        int: The return code of the subprocess that executed the Locust load test.
+
+    Example:
+        cortisol_file = Path("path/to/locustfile.py")
+        log_file = Path("path/to/locust_logs.log")
+        num_users = 100
+        spawn_rate = 10
+        run_time = '15m'
+        container_id = 'my-locust-container'
+
+        return_code = get_cost_estimate(cortisol_file, log_file, num_users, spawn_rate, run_time, container_id)
+    """
+    render_locustfile(cortisol_file)
+
+    command = render_locust_command(
+        host, log_file, num_users, spawn_rate, run_time, container_id, stats_file
+    )
+
+    done = threading.Event()
+    process = None
+    animation_thread = None
+    try:
+        process = subprocess.Popen(
+            command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True
+        )
+
+        if run_time.endswith("m"):
+            total_seconds = float(run_time[:-1]) * 60
+        elif run_time.endswith("s"):
+            total_seconds = float(run_time[:-1])
+        else:
+            raise ValueError(
+                "Invalid runtime format. Use 'Xm' for minutes or 'Xs' for seconds."
+            )
+
+        animation_thread = threading.Thread(
+            target=animation_process,
+            args=(
+                total_seconds,
+                done,
+            ),
+        )
+        animation_thread.start()
+
+        try:
+            output, error = process.communicate(timeout=total_seconds + 2)
+        except subprocess.TimeoutExpired as e:
+            process.kill()
+            output, error = process.communicate()
+            stderr_output = error.strip()
+            print(stderr_output)
+            raise TimeoutError(stderr_output) from e
+
+        if process.returncode == 0:
+            print(output)
+        else:
+            print(error.strip())
+
+    except KeyboardInterrupt as e:
+        if process is not None:
+            process.terminate()
+            _, error = process.communicate()
+            stderr_output = error.strip()
+            print(stderr_output)
+            raise KeyboardInterrupt(stderr_output) from e
+        raise KeyboardInterrupt from e
+
+    finally:
+        if process and process.poll() is None:
+            process.terminate()
+        if animation_thread is not None:
+            done.set()
+            animation_thread.join()
+
+    return process.returncode
