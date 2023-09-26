d57a90c5825bffa6fe718035782e7302eeb24aa0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-26 05:06:58 +0000

    Add package llama-cpp-python/0.2.7/llama_cpp_python-0.2.7.tar.gz

---
+++ packages/llama-cpp-python/llama_cpp_python-0.2.7.tar.gz/llama_cpp_python-0.2.7/vendor/llama.cpp/build-info.h
@@ -0,0 +1,9 @@
+#ifndef BUILD_INFO_H
+#define BUILD_INFO_H
+
+#define BUILD_NUMBER 1
+#define BUILD_COMMIT "a98b163"
+#define BUILD_COMPILER ""
+#define BUILD_TARGET "unknown"
+
+#endif // BUILD_INFO_H
