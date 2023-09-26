5223936e7057deb2e57fd916a5969dc1b8cb2e81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-26 05:06:26 +0000

    Add package forward-decl/0.1/forward_decl-0.1.tar.gz

---
+++ packages/forward-decl/forward_decl-0.1.tar.gz/forward_decl-0.1/setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+    name="forward_decl",
+    version="0.1",
+    author="LMauricius",
+    description="A simple module for supporting forward references",
+    packages=["forward_decl"]
+)
