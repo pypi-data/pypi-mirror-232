82c075a72a1207283ff9df039534e8fcfced00ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-26 03:07:28 +0000

    Add package anya-table/0.0.6/anya_table-0.0.6.tar.gz

---
+++ packages/anya-table/anya_table-0.0.6.tar.gz/anya_table-0.0.6/anya_table/_imports_.py
@@ -0,0 +1,13 @@
+from .AnyaTable import AnyaTable
+from .AnyaTheme import AnyaTheme
+from .AnyaTitle import AnyaTitle
+from .Table import Table
+from .comm import getCats
+
+__all__ = [
+    "AnyaTable",
+    "AnyaTheme",
+    "AnyaTitle",
+    "Table",
+    "getCats"
+]
