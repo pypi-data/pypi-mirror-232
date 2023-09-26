e3dfd087cc234119cac3dd03e4a1981343c56dd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-26 15:08:09 +0000

    Add package skny-dsnd-prob/0.1/skny_dsnd_prob-0.1.tar.gz

---
+++ packages/skny-dsnd-prob/skny_dsnd_prob-0.1.tar.gz/skny_dsnd_prob-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='skny_dsnd_prob',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['skny_dsnd_prob'],
+      zip_safe=False)
