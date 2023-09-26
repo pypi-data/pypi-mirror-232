90137f5947633345c4aca3c075c304dd6fff818e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-26 04:06:27 +0000

    Add package ternyxmimosa/0.0.7/ternyxmimosa-0.0.7-py3-none-any.whl

---
+++ packages/ternyxmimosa/ternyxmimosa-0.0.7-py3-none-any.whl/mimosa/cli.py
@@ -0,0 +1,28 @@
+"""Entry point for mimosa package command line interface.
+
+A minimal modern data stack with working data pipelines in a single Docker container.
+"""
+from mimosa.pipelines import GEI
+
+
+def main_orig():
+    """_summary_."""
+    print("Hello world from mimosa.")  # noqa T201
+
+    pipeline = GEI(destination="motherduck")
+    pipeline.run_landing_pipeline()
+
+    return -1
+
+
+def main():
+    """_summary_."""
+    print("Hello world from mimosa helper.")  # noqa T201
+
+    pipeline = GEI(destination="motherduck")
+    print("Starting the simple reportig pipeline.")  # noqa T201
+    # pipeline.run_reporting_pipeline()
+
+
+if __name__ == "__main__":
+    main()
