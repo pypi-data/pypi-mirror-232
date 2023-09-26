0d8fa49c33f0cd515de984a4873f6fefe911ae40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-26 03:04:43 +0000

    Add package octopus-sdk/0.4.13/octopus_sdk-0.4.13.tar.gz

---
+++ packages/octopus-sdk/octopus_sdk-0.4.13.tar.gz/octopus_sdk-0.4.13/setup.py
@@ -0,0 +1,41 @@
+# vim:fenc=utf-8
+#
+# Copyright (C) 2023 dbpunk.com Author imotai <codego.me@gmail.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+""" """
+from setuptools import setup, find_packages
+
+setup(
+    name="octopus_sdk",
+    version="0.4.13",
+    description="Open source code interpreter agent for LLM",
+    author="imotai",
+    author_email="wangtaize@dbpunk.com",
+    url="https://github.com/dbpunk-labs/octopus",
+    long_description= open('README.md').read(),
+    long_description_content_type='text/markdown',
+
+    packages=[
+        "octopus_sdk",
+    ],
+
+    package_dir={
+        "octopus_sdk": "src/octopus_sdk",
+    },
+
+    install_requires=[
+        "octopus_proto",
+    ]
+)
