c67c3d8cfd0a757c208153b8a80e17b5dd397b66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-26 03:05:18 +0000

    Add package pdm/2.9.3/pdm-2.9.3-py3-none-any.whl

---
+++ packages/pdm/pdm-2.9.3-py3-none-any.whl/pdm/cli/commands/show.py
@@ -0,0 +1,72 @@
+import argparse
+
+from packaging.version import Version
+
+from pdm.cli.commands.base import BaseCommand
+from pdm.cli.options import venv_option
+from pdm.exceptions import PdmUsageError
+from pdm.models.candidates import Candidate
+from pdm.models.project_info import ProjectInfo
+from pdm.models.requirements import parse_requirement
+from pdm.project import Project
+from pdm.utils import normalize_name
+
+
+def filter_stable(candidate: Candidate) -> bool:
+    assert candidate.version
+    return not Version(candidate.version).is_prerelease
+
+
+class Command(BaseCommand):
+    """Show the package information"""
+
+    metadata_keys = ("name", "version", "summary", "license", "platform", "keywords")
+
+    def add_arguments(self, parser: argparse.ArgumentParser) -> None:
+        venv_option.add_to_parser(parser)
+        parser.add_argument(
+            "package",
+            type=normalize_name,
+            nargs=argparse.OPTIONAL,
+            help="Specify the package name, or show this package if not given",
+        )
+        for option in self.metadata_keys:
+            parser.add_argument(f"--{option}", action="store_true", help=f"Show {option}")
+
+    def handle(self, project: Project, options: argparse.Namespace) -> None:
+        package = options.package
+        if package:
+            req = parse_requirement(package)
+            repository = project.get_repository()
+            # reverse the result so that latest is at first.
+            matches = repository.find_candidates(req, True, True)
+            latest = next(iter(matches), None)
+            if not latest:
+                project.core.ui.echo(
+                    f"No match found for the package {package!r}",
+                    err=True,
+                    style="warning",
+                )
+                return
+            latest_stable = next(filter(filter_stable, matches), None)
+            metadata = latest.prepare(project.environment).metadata
+        else:
+            if not project.name:
+                raise PdmUsageError("This project is not a package")
+            package = normalize_name(project.name)
+            metadata = project.make_self_candidate(False).prepare(project.environment).prepare_metadata(True)
+            latest_stable = None
+        project_info = ProjectInfo.from_distribution(metadata)
+
+        if any(getattr(options, key, None) for key in self.metadata_keys):
+            for key in self.metadata_keys:
+                if getattr(options, key, None):
+                    project.core.ui.echo(getattr(project_info, key))
+            return
+
+        installed = project.environment.get_working_set().get(package)
+        if latest_stable:
+            project_info.latest_stable_version = str(latest_stable.version)
+        if installed:
+            project_info.installed_version = str(installed.version)
+        project.core.ui.display_columns(list(project_info.generate_rows()))
---
+++ packages/pdm/pdm-2.9.3-py3-none-any.whl/pdm/models/auth.py
@@ -0,0 +1,89 @@
+from __future__ import annotations
+
+import urllib.parse
+
+from unearth.auth import MaybeAuth, MultiDomainBasicAuth, get_keyring_provider
+from unearth.utils import split_auth_from_netloc
+
+from pdm._types import RepositoryConfig
+from pdm.exceptions import PdmException
+from pdm.termui import UI, Verbosity
+
+
+class PdmBasicAuth(MultiDomainBasicAuth):
+    """A custom auth class that differs from Pip's implementation in the
+    following ways:
+
+        - It shows an error message when credentials are not provided or correct.
+    """
+
+    def __init__(self, ui: UI, sources: list[RepositoryConfig]) -> None:
+        super().__init__(prompting=True)
+        self.sources = sources
+        self.ui = ui
+
+    def _get_auth_from_index_url(self, netloc: str) -> tuple[MaybeAuth, str | None]:
+        if not self.sources:
+            return None, None
+        for source in self.sources:
+            assert source.url
+            parsed = urllib.parse.urlparse(source.url)
+            auth, index_netloc = split_auth_from_netloc(parsed.netloc)
+            if index_netloc == netloc:
+                if source.username:
+                    auth = (source.username, source.password)
+                return auth, source.url
+        return None, None
+
+    def _prompt_for_password(self, netloc: str) -> tuple[str | None, str | None, bool]:
+        if self.ui.verbosity < Verbosity.DETAIL:
+            raise PdmException(
+                f"The credentials for {netloc} are not provided. To give them via interactive shell, "
+                "please rerun the command with `-v` option."
+            )
+        return super()._prompt_for_password(netloc)
+
+    def _should_save_password_to_keyring(self) -> bool:
+        if get_keyring_provider() is None:
+            self.ui.echo(
+                "The provided credentials will not be saved into the keyring.\n"
+                "You can enable this by installing keyring:\n"
+                "    [success]pdm self add keyring[/]",
+                err=True,
+                style="info",
+            )
+        return super()._should_save_password_to_keyring()
+
+
+class Keyring:
+    def __init__(self) -> None:
+        self.provider = get_keyring_provider()
+        self.enabled = self.provider is not None
+
+    def get_auth_info(self, url: str, username: str | None) -> tuple[str, str] | None:
+        """Return the password for the given url and username.
+        The username can be None.
+        """
+        if self.provider is None or not self.enabled:
+            return None
+        try:
+            return self.provider.get_auth_info(url, username)
+        except Exception:
+            self.enabled = False
+            return None
+
+    def save_auth_info(self, url: str, username: str, password: str) -> bool:
+        """Set the password for the given url and username.
+        Returns whether the operation is successful.
+        """
+        if self.provider is None or not self.enabled:
+            return False
+        try:
+            self.provider.save_auth_info(url, username, password)
+            return True
+        except Exception:
+            self.enabled = False
+            return False
+
+
+keyring = Keyring()
