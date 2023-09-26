09fb2dc113d887dfedcd753db050c65322146b2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-26 04:07:03 +0000

    Add package auto-anno-2/0.1.6/auto_anno_2-0.1.6.tar.gz

---
+++ packages/auto-anno-2/auto_anno_2-0.1.6.tar.gz/auto_anno_2-0.1.6/auto_anno_2/auto_anno_2.py
@@ -0,0 +1,3 @@
+from .utils.anno.cls.text_classification import text_classification as cls
+from .utils.anno.ner.entity_extract import extract_named_entities as ner
+from .local_config import config
