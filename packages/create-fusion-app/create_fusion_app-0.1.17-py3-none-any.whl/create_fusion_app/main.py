ee92dece19994cd8c8351245d24a5e421f4b4966
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-26 14:06:38 +0000

    Add package create-fusion-app/0.1.17/create_fusion_app-0.1.17-py3-none-any.whl

---
+++ packages/create-fusion-app/create_fusion_app-0.1.17-py3-none-any.whl/create_fusion_app/main.py
@@ -0,0 +1,9 @@
+from create_project import create_project
+
+def main():
+    create_project()
+    print("Project created. Navigate to your project directory and run 'npm start' to launch the dev server")
+
+if __name__ == "__main__":
+    create_project()
+    print("Project created. Navigate to your project directory and run 'npm start' to launch the dev server")
