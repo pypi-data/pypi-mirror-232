c10dd1c77134282670426c355a0c26c094c935fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-26 05:06:05 +0000

    Add package descarteslabs-dynamic-compute/0.9.1/descarteslabs_dynamic_compute-0.9.1-py3-none-any.whl

---
+++ packages/descarteslabs-dynamic-compute/descarteslabs_dynamic_compute-0.9.1-py3-none-any.whl/descarteslabs/dynamic_compute/graft/client/__init__.py
@@ -0,0 +1,31 @@
+from .client import (
+    apply_graft,
+    consistent_guid,
+    function_graft,
+    guid,
+    is_delayed,
+    is_function_graft,
+    isolate_keys,
+    keyref_graft,
+    merge_value_grafts,
+    normalize_graft,
+    parametrize,
+    splice,
+    value_graft,
+)
+
+__all__ = [
+    "is_delayed",
+    "is_function_graft",
+    "value_graft",
+    "keyref_graft",
+    "apply_graft",
+    "function_graft",
+    "merge_value_grafts",
+    "normalize_graft",
+    "guid",
+    "isolate_keys",
+    "parametrize",
+    "splice",
+    "consistent_guid",
+]
