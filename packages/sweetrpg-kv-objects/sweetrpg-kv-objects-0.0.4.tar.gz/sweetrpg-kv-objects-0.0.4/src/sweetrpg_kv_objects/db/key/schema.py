810406165e31a239334834e026be29689ca1f6cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-26 04:07:01 +0000

    Add package sweetrpg-kv-objects/0.0.4/sweetrpg-kv-objects-0.0.4.tar.gz

---
+++ packages/sweetrpg-kv-objects/sweetrpg-kv-objects-0.0.4.tar.gz/sweetrpg-kv-objects-0.0.4/src/sweetrpg_kv_objects/db/key/schema.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+__author__ = "Paul Schifferer <dm@sweetrpg.com>"
+"""
+Schema for Key data.
+"""
+
+from marshmallow import fields
+from sweetrpg_kv_objects.model.key import Key
+from sweetrpg_model_core.schema.base import BaseSchema
+
+
+class KeySchema(BaseSchema):
+    model_class = Key
+
+    name = fields.String(required=True)  # , load_only=True)
+    type = fields.String(required=True)  # , load_only=True)
+    description = fields.String(required=True)  # , load_only=True)
+    tags = fields.List(fields.Dict(keys=fields.String(required=True), values=fields.String()))
+    expression = fields.String(required=True)  # , load_only=True)
+    value_ids = fields.List(fields.String(required=True))  # , load_only=True)
