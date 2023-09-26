003a4ffcf259b762943efc57858ae72fb2c53cdc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-26 15:06:34 +0000

    Add package accelbyte-py-sdk/0.50.0/accelbyte_py_sdk-0.50.0.tar.gz

---
+++ packages/accelbyte-py-sdk/accelbyte_py_sdk-0.50.0.tar.gz/accelbyte_py_sdk-0.50.0/accelbyte_py_sdk/api/gdpr/models/__init__.py
@@ -0,0 +1,28 @@
+# Copyright (c) 2021 AccelByte Inc. All Rights Reserved.
+# This is licensed software from AccelByte Inc, for limitations
+# and restrictions contact your company contract manager.
+#
+# Code generated. DO NOT EDIT!
+
+# template file: ags_py_codegen
+
+"""Auto-generated package that contains models used by the AccelByte Gaming Services Gdpr Service."""
+
+__version__ = "2.2.3"
+__author__ = "AccelByte"
+__email__ = "dev@accelbyte.net"
+
+# pylint: disable=line-too-long
+
+from .models_data_retrieval_response import ModelsDataRetrievalResponse
+from .models_deletion_data import ModelsDeletionData
+from .models_deletion_status import ModelsDeletionStatus
+from .models_list_deletion_data_response import ModelsListDeletionDataResponse
+from .models_list_personal_data_response import ModelsListPersonalDataResponse
+from .models_pagination import ModelsPagination
+from .models_personal_data import ModelsPersonalData
+from .models_request_delete_response import ModelsRequestDeleteResponse
+from .models_user_data_url import ModelsUserDataURL
+from .models_user_personal_data import ModelsUserPersonalData
+from .models_user_personal_data_response import ModelsUserPersonalDataResponse
+from .response_error import ResponseError
