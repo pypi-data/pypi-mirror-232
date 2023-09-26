b4ceb5e5c50cfa94a0fcc310ec13c9e3983678a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-26 03:05:37 +0000

    Add package flatpack/0.2.1/flatpack-0.2.1-py3-none-any.whl

---
+++ packages/flatpack/flatpack-0.2.1-py3-none-any.whl/flatpack/train.py
@@ -0,0 +1,41 @@
+import os
+import time
+from tqdm.notebook import tqdm
+
+
+def train(train_function, save_dir, model_type='rnn', framework='pytorch', *args, **kwargs):
+    """
+    A universal training function provided by flatpack.
+    """
+    os.makedirs(save_dir, exist_ok=True)
+
+    # Accessing command line arguments
+    epochs = kwargs.get('epochs', 10)
+    batch_size = kwargs.get('batch_size', 32)
+
+    print(f"Training {model_type} model with epochs: {epochs} and batch size: {batch_size}")
+
+    # Initialize a progress bar using tqdm.notebook
+    with tqdm(total=epochs, desc='Training', unit='epoch') as pbar:
+        # Initialize a timer
+        start_time = time.time()
+
+        # Call the user-provided training function and update the progress bar after each epoch
+        for epoch in range(epochs):
+            output = train_function(*args, **kwargs)
+            # Update the progress bar and display the loss
+            pbar.set_postfix(loss=output.get('loss', 'N/A'))
+            pbar.update(1)
+
+            # Calculate and display the elapsed time at each iteration
+            current_time = time.time()
+            elapsed_time = current_time - start_time
+            pbar.set_postfix(loss=output.get('loss', 'N/A'), elapsed_time=f"{elapsed_time:.2f} seconds")
+
+    # Save the trained model based on the specified framework
+    if framework == 'pytorch':
+        import torch
+        # Save the model with a filename based on the model_type
+        torch.save(output['model'].state_dict(), os.path.join(save_dir, f'{model_type}_model.pth'))
+
+    return output
