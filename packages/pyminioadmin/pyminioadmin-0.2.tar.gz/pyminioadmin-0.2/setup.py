7e2e6e88c65b404ab69e41ffdcb58a9b9da8434f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-26 03:05:15 +0000

    Add package pyminioadmin/0.2/pyminioadmin-0.2.tar.gz

---
+++ packages/pyminioadmin/pyminioadmin-0.2.tar.gz/pyminioadmin-0.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='pyminioadmin',
+    version='0.2',
+    description='A Python library for the MinIO admin client',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Iuri Peniche',
+    author_email='iuripeniche@hotmail.com',
+    packages=['pyminioadmin'],
+)
