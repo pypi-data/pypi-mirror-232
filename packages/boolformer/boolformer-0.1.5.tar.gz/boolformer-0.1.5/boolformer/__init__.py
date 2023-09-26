090f114a977dd71dbe117224d67a0f0cea91f494
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-26 03:04:24 +0000

    Add package boolformer/0.1.5/boolformer-0.1.5.tar.gz

---
+++ packages/boolformer/boolformer-0.1.5.tar.gz/boolformer-0.1.5/boolformer/__init__.py
@@ -0,0 +1,7 @@
+from .model import build_modules, load_boolformer
+from .envs import  build_env
+from .trainer import Trainer
+from .evaluator import Evaluator, idx_to_infix
+from .envs.generators import RandomBooleanFunctions
+import .utils
+import .notebook_utils
