f232cdb8d4c90a80cac9fceebab19c67e24c8eb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-26 03:06:39 +0000

    Add package flake8-fill-one-line/0.3.4/flake8-fill-one-line-0.3.4.tar.gz

---
+++ packages/flake8-fill-one-line/flake8-fill-one-line-0.3.4.tar.gz/flake8-fill-one-line-0.3.4/flake8_fill_one_line/utils.py
@@ -0,0 +1,46 @@
+import ast
+import re
+from typing import Optional, Union
+
+import astunparse
+
+
+def is_one_line(node: Union[ast.AST, ast.expr, ast.keyword]) -> Optional[bool]:
+    if hasattr(node, "lineno") and hasattr(node, "end_lineno"):
+        return node.lineno == node.end_lineno
+
+    return None
+
+
+def get_node_length(node: Union[ast.AST, ast.expr, ast.keyword]) -> int:
+    if is_one_line(node):
+        return node.end_col_offset - node.col_offset
+
+    code = astunparse.unparse(node).strip()
+    code = fix_tuple(code)
+    return len(code)
+
+
+def fix_tuple(code: str) -> str:
+    matches = reversed([match for match in re.finditer(r"Tuple\[\(", code)])
+
+    for match in matches:
+        start, end = match.span()
+        brace = 0
+        bracket = 0
+
+        for i, c in enumerate(code[end + 1:]):
+            if c == "[":
+                brace += 1
+            elif c == "]":
+                brace -= 1
+            elif c == "(":
+                bracket += 1
+            elif c == ")":
+                bracket -= 1
+
+                if bracket < 0:
+                    code = f"{code[:end - 1]}{code[end:end + i + 1]}{code[end + i + 2:]}"
+                    break
+
+    return code
