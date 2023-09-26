9c47f02faf3c5727893d2e72f436718262ee8a23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-26 04:06:43 +0000

    Add package colourise-output/1.1.2/colourise_output-1.1.2.tar.gz

---
+++ packages/colourise-output/colourise_output-1.1.2.tar.gz/colourise_output-1.1.2/colourise_output/__init__.py
@@ -0,0 +1,50 @@
+"""
+File in charge of adding colour to the terminal while respecting the windows colour norm
+"""
+
+from .colourise_output import ColouriseOutput
+
+
+class colourise_output(ColouriseOutput):
+    """ The class in charge of adding colour to the terminal while respecting the windows colour norm """
+    pass
+
+
+class colouriseoutput(ColouriseOutput):
+    """ The class in charge of adding colour to the terminal while respecting the windows colour norm """
+    pass
+
+
+class Colourise_Output(ColouriseOutput):
+    """ The class in charge of adding colour to the terminal while respecting the windows colour norm """
+    pass
+
+
+class COLOURISE_OUTPUT(ColouriseOutput):
+    """ The class in charge of adding colour to the terminal while respecting the windows colour norm """
+    pass
+
+
+class COLOURISEOUTPUT(ColouriseOutput):
+    """ The class in charge of adding colour to the terminal while respecting the windows colour norm """
+    pass
+
+
+class co(ColouriseOutput):
+    """ The class in charge of adding colour to the terminal while respecting the windows colour norm """
+    pass
+
+
+class c_o(ColouriseOutput):
+    """ The class in charge of adding colour to the terminal while respecting the windows colour norm """
+    pass
+
+
+class CO(ColouriseOutput):
+    """ The class in charge of adding colour to the terminal while respecting the windows colour norm """
+    pass
+
+
+class C_O(ColouriseOutput):
+    """ The class in charge of adding colour to the terminal while respecting the windows colour norm """
+    pass
