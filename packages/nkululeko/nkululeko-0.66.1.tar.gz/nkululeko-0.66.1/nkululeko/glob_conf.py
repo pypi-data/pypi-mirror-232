f81a92481e9af021d9938482d0ca98784160b256
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-26 04:06:50 +0000

    Add package nkululeko/0.66.1/nkululeko-0.66.1.tar.gz

---
+++ packages/nkululeko/nkululeko-0.66.1.tar.gz/nkululeko-0.66.1/nkululeko/glob_conf.py
@@ -0,0 +1,20 @@
+# global_config.py
+
+
+def init_config(config_obj):
+    global config
+    config = config_obj
+
+
+def set_label_encoder(encoder):
+    global label_encoder
+    label_encoder = encoder
+
+
+def set_util(util_obj):
+    global util
+    util = util_obj
+
+def set_report(report_obj):
+    global report
+    report = report_obj
