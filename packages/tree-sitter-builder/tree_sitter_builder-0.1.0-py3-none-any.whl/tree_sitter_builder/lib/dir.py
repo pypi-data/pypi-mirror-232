6b8c1b23ade4a86a89a6e91604c28a9700a95890
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-26 14:07:27 +0000

    Add package tree-sitter-builder/0.1.0/tree_sitter_builder-0.1.0-py3-none-any.whl

---
+++ packages/tree-sitter-builder/tree_sitter_builder-0.1.0-py3-none-any.whl/tree_sitter_builder/lib/dir.py
@@ -0,0 +1,24 @@
+import pathlib
+
+import platformdirs
+
+
+def get_data_dir() -> pathlib.Path:
+    data_dir = pathlib.Path(platformdirs.user_data_dir('tree-sitter-builder'))
+    data_dir.mkdir(parents=True, exist_ok=True)
+
+    return data_dir
+
+
+def get_repo_dir(data_dir: pathlib.Path) -> pathlib.Path:
+    repo_dir = data_dir / 'repos'
+    repo_dir.mkdir(parents=True, exist_ok=True)
+
+    return repo_dir
+
+
+def get_build_dir(data_dir: pathlib.Path) -> pathlib.Path:
+    build_dir = data_dir / 'build'
+    build_dir.mkdir(parents=True, exist_ok=True)
+
+    return build_dir
