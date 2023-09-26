b56af3a28799a2c6cd5a4afd2f83bfc12e81f593
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-26 15:06:35 +0000

    Add package teapy/0.4.11/teapy-0.4.11-cp38-abi3-win_amd64.whl

---
+++ packages/teapy/teapy-0.4.11-cp38-abi3-win_amd64.whl/teapy/mod_func.py
@@ -0,0 +1,29 @@
+from .teapy import PyDataDict, stack
+
+
+def hcorr(exprs, method="pearson", stable=False):
+    return PyDataDict(exprs).corr(method=method, stable=stable)
+
+
+def hmax(exprs, par=False):
+    """horizontal max"""
+    return stack(exprs, axis=-1).max(axis=-1, par=par)
+
+
+def hmin(exprs, par=False):
+    """horizontal min"""
+    return stack(exprs, axis=-1).min(axis=-1, par=par)
+
+
+def hmean(exprs, par=False):
+    """horizontal mean"""
+    return stack(exprs, axis=-1).mean(axis=-1, par=par)
+
+
+def hstd(exprs, par=False):
+    """horizontal std"""
+    return stack(exprs, axis=-1).std(axis=-1, par=par)
+
+
+def hsum(exprs, par=False):
+    return stack(exprs, axis=-1).sum(axis=-1, par=par)
