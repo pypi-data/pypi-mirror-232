71a22fc8d1c7af79f67f90e311d5e05c1f7507bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-26 03:07:02 +0000

    Add package jupyterlab-git/0.43.0/jupyterlab_git-0.43.0.tar.gz

---
+++ packages/jupyterlab-git/jupyterlab_git-0.43.0.tar.gz/jupyterlab_git-0.43.0/jupyterlab_git/tests/test_tag.py
@@ -0,0 +1,92 @@
+from unittest.mock import patch
+
+import pytest
+
+from jupyterlab_git.git import Git
+
+from .testutils import maybe_future
+
+
+@pytest.mark.asyncio
+async def test_git_tag_success():
+    with patch("jupyterlab_git.git.execute") as mock_execute:
+        tag = "1.0.0"
+        # Given
+        mock_execute.return_value = maybe_future((0, tag, ""))
+
+        # When
+        actual_response = await Git().tags("test_curr_path")
+
+        # Then
+        mock_execute.assert_called_once_with(
+            ["git", "tag", "--list"],
+            cwd="test_curr_path",
+            timeout=20,
+            env=None,
+            username=None,
+            password=None,
+            is_binary=False,
+        )
+
+        assert {"code": 0, "tags": [tag]} == actual_response
+
+
+@pytest.mark.asyncio
+async def test_git_tag_checkout_success():
+    with patch("os.environ", {"TEST": "test"}):
+        with patch("jupyterlab_git.git.execute") as mock_execute:
+            tag = "mock_tag"
+            # Given
+            mock_execute.return_value = maybe_future((0, "", ""))
+
+            # When
+            actual_response = await Git().tag_checkout("test_curr_path", "mock_tag")
+
+            # Then
+            mock_execute.assert_called_once_with(
+                ["git", "checkout", "tags/{}".format(tag)],
+                cwd="test_curr_path",
+                timeout=20,
+                env=None,
+                username=None,
+                password=None,
+                is_binary=False,
+            )
+
+            assert {
+                "code": 0,
+                "message": "Tag {} checked out".format(tag),
+            } == actual_response
+
+
+@pytest.mark.asyncio
+async def test_set_tag_succes():
+    with patch("os.environ", {"TEST": "test"}):
+        with patch("jupyterlab_git.git.execute") as mock_execute:
+            tag = "mock_tag"
+            commitId = "mock_commit_id"
+            # Given
+            mock_execute.return_value = maybe_future((0, "", ""))
+
+            # When
+            actual_response = await Git().set_tag(
+                "test_curr_path", "mock_tag", "mock_commit_id"
+            )
+
+            # Then
+            mock_execute.assert_called_once_with(
+                ["git", "tag", tag, commitId],
+                cwd="test_curr_path",
+                timeout=20,
+                env=None,
+                username=None,
+                password=None,
+                is_binary=False,
+            )
+
+            assert {
+                "code": 0,
+                "message": "Tag {} created, pointing to commit {}".format(
+                    tag, commitId
+                ),
+            } == actual_response
