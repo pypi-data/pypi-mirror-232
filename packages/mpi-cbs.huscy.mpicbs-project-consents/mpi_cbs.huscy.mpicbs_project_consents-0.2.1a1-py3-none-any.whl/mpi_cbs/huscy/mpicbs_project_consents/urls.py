5053b0bd0867c79d2c9366a536575b122009602f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-26 03:06:37 +0000

    Add package mpi-cbs.huscy.mpicbs-project-consents/0.2.1a1/mpi_cbs.huscy.mpicbs_project_consents-0.2.1a1-py3-none-any.whl

---
+++ packages/mpi-cbs.huscy.mpicbs-project-consents/mpi_cbs.huscy.mpicbs_project_consents-0.2.1a1-py3-none-any.whl/mpi_cbs/huscy/mpicbs_project_consents/urls.py
@@ -0,0 +1,14 @@
+from django.urls import include, path
+from rest_framework_nested.routers import NestedDefaultRouter
+
+from huscy.project_consents.urls import project_router
+from mpi_cbs.huscy.mpicbs_project_consents.views import MPICBSProjectConsentTokenViewSet
+
+
+consent_router = NestedDefaultRouter(project_router, 'consents', lookup='consent')
+consent_router.register('wrapped_token', MPICBSProjectConsentTokenViewSet)
+
+
+urlpatterns = [
+    path('api/', include(consent_router.urls)),
+]
