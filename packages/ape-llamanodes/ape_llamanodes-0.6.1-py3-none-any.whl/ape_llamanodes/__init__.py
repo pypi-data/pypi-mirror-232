52531ce41bff980740ce58112cadb44a88800f5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-26 05:06:26 +0000

    Add package ape-llamanodes/0.6.1/ape_llamanodes-0.6.1-py3-none-any.whl

---
+++ packages/ape-llamanodes/ape_llamanodes-0.6.1-py3-none-any.whl/ape_llamanodes/__init__.py
@@ -0,0 +1,32 @@
+from ape import plugins
+
+from .provider import LlamaNodes
+
+NETWORKS = {
+    "arbitrum": [
+        "mainnet",
+    ],
+    "base": [
+        "mainnet",
+    ],
+    "bsc": [
+        "mainnet",
+    ],
+    "ethereum": [
+        "mainnet",
+        "goerli",
+    ],
+    "optimism": [
+        "mainnet",
+    ],
+    "polygon": [
+        "mainnet",
+    ],
+}
+
+
+@plugins.register(plugins.ProviderPlugin)
+def providers():
+    for ecosystem_name in NETWORKS:
+        for network_name in NETWORKS[ecosystem_name]:
+            yield ecosystem_name, network_name, LlamaNodes
