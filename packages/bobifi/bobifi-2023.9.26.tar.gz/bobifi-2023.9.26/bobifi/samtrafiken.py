d2ef1075d986736656f39fe7f927b49d8580c3b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-26 15:07:35 +0000

    Add package bobifi/2023.9.26/bobifi-2023.9.26.tar.gz

---
+++ packages/bobifi/bobifi-2023.9.26.tar.gz/bobifi-2023.9.26/bobifi/samtrafiken.py
@@ -0,0 +1,48 @@
+import json
+
+from . import DATADIR
+
+DEFAULT_VERSION = 2
+
+JWKS_TEST_FILENAME = str(DATADIR / "samtrafiken-test.json")
+JWKS_PROD_FILENAME = str(DATADIR / "samtrafiken-prod.json")
+
+
+_ENVIRONMENTS = {
+    1: {
+        "test": {
+            "keys_filename": JWKS_TEST_FILENAME,
+            "jwks": json.load(open(JWKS_TEST_FILENAME)),
+            "metadata_url": "https://bobmetadata-pp.samtrafiken.se/api/v1/participantMetadata",
+        },
+        "prod": {
+            "keys_filename": JWKS_PROD_FILENAME,
+            "jwks": json.load(open(JWKS_PROD_FILENAME)),
+            "metadata_url": "https://bobmetadata.samtrafiken.se/api/v1/participantMetadata",
+        },
+    },
+    2: {
+        "test": {
+            "keys_filename": JWKS_TEST_FILENAME,
+            "jwks": json.load(open(JWKS_TEST_FILENAME)),
+            "metadata_url": "https://bobmetadata-pp.samtrafiken.se/api/v2/participantMetadata",
+        },
+        "prod": {
+            "keys_filename": JWKS_PROD_FILENAME,
+            "jwks": json.load(open(JWKS_PROD_FILENAME)),
+            "metadata_url": "https://bobmetadata.samtrafiken.se/api/v2/participantMetadata",
+        },
+    },
+}
+
+
+def where(env: str = "prod", version: int = DEFAULT_VERSION) -> str:
+    return _ENVIRONMENTS[version][env]["keys_filename"]
+
+
+def trusted_jwks(env: str = "prod", version: int = DEFAULT_VERSION) -> dict:
+    return _ENVIRONMENTS[version][env]["jwks"]
+
+
+def metadata_url(env: str = "prod", version: int = DEFAULT_VERSION) -> str:
+    return _ENVIRONMENTS[version][env]["metadata_url"]
