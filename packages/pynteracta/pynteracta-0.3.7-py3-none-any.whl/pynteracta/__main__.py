31bb32e429718bd85eb9224e257428c7d7bd9de1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-26 04:07:06 +0000

    Add package pynteracta/0.3.7/pynteracta-0.3.7-py3-none-any.whl

---
+++ packages/pynteracta/pynteracta-0.3.7-py3-none-any.whl/pynteracta/__main__.py
@@ -0,0 +1,27 @@
+"""Pynteracta
+
+Usage:
+------
+
+    $ pynteracta
+
+Contact:
+--------
+
+- https://www.unionerenolavinosamoggia.bo.it/index.php/contatti
+
+More information is available at:
+
+- https://github.com/simodalla/pynteracta
+
+
+Version:
+--------
+
+- pynteracta v0.3.7
+"""
+
+from .cli.commands import app
+
+if __name__ == "__main__":
+    app()
