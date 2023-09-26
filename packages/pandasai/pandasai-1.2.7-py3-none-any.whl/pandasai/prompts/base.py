b12dec99cdba332a0163f5b09c776b4f74b136dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-26 03:05:47 +0000

    Add package pandasai/1.2.7/pandasai-1.2.7-py3-none-any.whl

---
+++ packages/pandasai/pandasai-1.2.7-py3-none-any.whl/pandasai/prompts/base.py
@@ -0,0 +1,61 @@
+""" Base class to implement a new Prompt
+In order to better handle the instructions, this prompt module is written.
+"""
+
+from pandasai.exceptions import MethodNotImplementedError
+
+
+class Prompt:
+    """Base class to implement a new Prompt"""
+
+    text = None
+    _args = {}
+
+    def __init__(self, **kwargs):
+        """
+        __init__ method of Base class of Prompt Module
+        Args:
+            **kwargs: Inferred Keyword Arguments
+        """
+        if kwargs:
+            self._args = kwargs
+
+    def _generate_dataframes(self, dfs):
+        """
+        Generate the dataframes metadata
+        Args:
+            dfs: List of Dataframes
+        """
+        dataframes = []
+        for index, df in enumerate(dfs, start=1):
+            description = """<dataframe>
+Dataframe """
+            if df.table_name is not None:
+                description += f"{df.table_name} (dfs[{index-1}])"
+            else:
+                description += f"dfs[{index-1}]"
+            description += (
+                f", with {df.rows_count} rows and {df.columns_count} columns."
+            )
+            if df.table_description is not None:
+                description += f"\nDescription: {df.table_description}"
+            description += f"""
+This is the metadata of the dataframe dfs[{index-1}]:
+{df.head_csv}</dataframe>"""  # noqa: E501
+            dataframes.append(description)
+
+        return "\n\n".join(dataframes)
+
+    def set_var(self, var, value):
+        if var == "dfs":
+            self._args["dataframes"] = self._generate_dataframes(value)
+        self._args[var] = value
+
+    def to_string(self):
+        if self.text is None:
+            raise MethodNotImplementedError
+
+        return self.text.format(**self._args)
+
+    def __str__(self):
+        return self.to_string()
