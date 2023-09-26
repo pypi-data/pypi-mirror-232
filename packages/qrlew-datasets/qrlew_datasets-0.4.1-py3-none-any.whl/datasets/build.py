fa0d1c4c8aecb799057e55765162c9481ebe46fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-26 14:06:42 +0000

    Add package qrlew-datasets/0.4.1/qrlew_datasets-0.4.1-py3-none-any.whl

---
+++ packages/qrlew-datasets/qrlew_datasets-0.4.1-py3-none-any.whl/datasets/build.py
@@ -0,0 +1,16 @@
+import logging
+from datasets.sources import Loader, Financial, IMDB, Hepatitis
+from datasets.databases import PostgreSQL
+
+logging.basicConfig(level=logging.DEBUG)
+
+database = PostgreSQL()
+loader = Loader(database)
+
+# Build all dumps
+for source in [Financial(), IMDB(), Hepatitis()]:
+    print(source)
+    loader.load(source)
+    database.set_schema(source.schema())
+    database.dump(f'/tmp/{source.schema()}.sql')
+    print(database.declaration())
