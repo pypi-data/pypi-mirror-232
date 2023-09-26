69e9d9874dc1e7011a2d29951000062971150737
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-26 14:05:07 +0000

    Add package forward-decl/0.1.2/forward_decl-0.1.2.tar.gz

---
+++ packages/forward-decl/forward_decl-0.1.2.tar.gz/forward_decl-0.1.2/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+from pathlib import Path
+
+# read the contents of your README file
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setuptools.setup(
+    name="forward_decl",
+    version="0.1.2",
+    author="LMauricius",
+    packages=["forward_decl"],
+    description="A simple module for supporting forward references",
+    long_description=long_description,
+    long_description_content_type='text/markdown'
+)
