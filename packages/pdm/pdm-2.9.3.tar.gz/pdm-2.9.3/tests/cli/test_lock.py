ad425cfd5f3d2a3fc64426a7974c7e0c30535e90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-26 03:05:18 +0000

    Add package pdm/2.9.3/pdm-2.9.3.tar.gz

---
+++ packages/pdm/pdm-2.9.3.tar.gz/pdm-2.9.3/tests/cli/test_lock.py
@@ -0,0 +1,162 @@
+import sys
+from unittest.mock import ANY
+
+import pytest
+from unearth import Link
+
+from pdm.cli import actions
+from pdm.models.requirements import parse_requirement
+from pdm.models.specifiers import PySpecSet
+
+
+def test_lock_command(project, pdm, mocker):
+    m = mocker.patch.object(actions, "do_lock")
+    pdm(["lock"], obj=project)
+    m.assert_called_with(project, refresh=False, groups=["default"], hooks=ANY, cross_platform=True, static_urls=None)
+
+
+@pytest.mark.usefixtures("repository")
+def test_lock_dependencies(project):
+    project.add_dependencies({"requests": parse_requirement("requests")})
+    actions.do_lock(project)
+    assert project.lockfile.exists
+    locked = project.locked_repository.all_candidates
+    for package in ("requests", "idna", "chardet", "certifi"):
+        assert package in locked
+
+
+def test_lock_refresh(pdm, project, repository):
+    project.add_dependencies({"requests": parse_requirement("requests")})
+    result = pdm(["lock"], obj=project)
+    assert result.exit_code == 0
+    assert project.is_lockfile_hash_match()
+    package = next(p for p in project.lockfile["package"] if p["name"] == "requests")
+    assert not package.get("files")
+    project.add_dependencies({"requests": parse_requirement("requests>=2.0")})
+    url_hashes = {
+        "http://example.com/requests-2.19.1-py3-none-any.whl": "sha256:abcdef123456",
+        "http://example2.com/requests-2.19.1-py3-none-AMD64.whl": "sha256:abcdef123456",
+        "http://example1.com/requests-2.19.1-py3-none-any.whl": "sha256:abcdef123456",
+    }
+    repository.get_hashes = (
+        lambda c: [{"url": url, "file": Link(url).filename, "hash": hash} for url, hash in url_hashes.items()]
+        if c.identify() == "requests"
+        else []
+    )
+    assert not project.is_lockfile_hash_match()
+    result = pdm(["lock", "--refresh", "-v"], obj=project)
+    assert result.exit_code == 0
+    package = next(p for p in project.lockfile["package"] if p["name"] == "requests")
+    assert package["files"] == [
+        {"file": "requests-2.19.1-py3-none-AMD64.whl", "hash": "sha256:abcdef123456"},
+        {"file": "requests-2.19.1-py3-none-any.whl", "hash": "sha256:abcdef123456"},
+    ]
+    assert project.is_lockfile_hash_match()
+    result = pdm(["lock", "--refresh", "--static-urls", "-v"], obj=project)
+    assert result.exit_code == 0
+    package = next(p for p in project.lockfile["package"] if p["name"] == "requests")
+    assert package["files"] == [{"url": url, "hash": hash} for url, hash in sorted(url_hashes.items())]
+
+
+def test_lock_refresh_keep_consistent(pdm, project, repository):
+    project.add_dependencies({"requests": parse_requirement("requests")})
+    result = pdm(["lock"], obj=project)
+    assert result.exit_code == 0
+    assert project.is_lockfile_hash_match()
+    previous = project.lockfile._path.read_text()
+    result = pdm(["lock", "--refresh"], obj=project)
+    assert result.exit_code == 0
+    assert project.lockfile._path.read_text() == previous
+
+
+def test_lock_check_no_change_success(pdm, project, repository):
+    project.add_dependencies({"requests": parse_requirement("requests")})
+    result = pdm(["lock"], obj=project)
+    assert result.exit_code == 0
+    assert project.is_lockfile_hash_match()
+
+    result = pdm(["lock", "--check"], obj=project)
+    assert result.exit_code == 0
+
+
+def test_lock_check_change_fails(pdm, project, repository):
+    project.add_dependencies({"requests": parse_requirement("requests")})
+    result = pdm(["lock"], obj=project)
+    assert result.exit_code == 0
+    assert project.is_lockfile_hash_match()
+
+    project.add_dependencies({"pyyaml": parse_requirement("pyyaml")})
+    result = pdm(["lock", "--check"], obj=project)
+    assert result.exit_code == 1
+
+
+@pytest.mark.usefixtures("repository")
+def test_innovations_with_specified_lockfile(pdm, project, working_set):
+    project.add_dependencies({"requests": parse_requirement("requests")})
+    lockfile = str(project.root / "mylock.lock")
+    pdm(["lock", "--lockfile", lockfile], strict=True, obj=project)
+    assert project.lockfile._path == project.root / "mylock.lock"
+    assert project.is_lockfile_hash_match()
+    locked = project.locked_repository.all_candidates
+    assert "requests" in locked
+    pdm(["sync", "--lockfile", lockfile], strict=True, obj=project)
+    assert "requests" in working_set
+
+
+@pytest.mark.usefixtures("repository", "vcs")
+def test_skip_editable_dependencies_in_metadata(project, capsys):
+    project.pyproject.metadata["dependencies"] = [
+        "-e git+https://github.com/test-root/demo.git@1234567890abcdef#egg=demo"
+    ]
+    actions.do_lock(project)
+    _, err = capsys.readouterr()
+    assert "WARNING: Skipping editable dependency" in err
+    assert not project.locked_repository.all_candidates
+
+
+@pytest.mark.usefixtures("repository")
+def test_lock_selected_groups(project, pdm):
+    project.add_dependencies({"requests": parse_requirement("requests")}, to_group="http")
+    project.add_dependencies({"pytz": parse_requirement("pytz")})
+    pdm(["lock", "-G", "http", "--no-default"], obj=project, strict=True)
+    assert project.lockfile.groups == ["http"]
+    assert "requests" in project.locked_repository.all_candidates
+    assert "pytz" not in project.locked_repository.all_candidates
+
+
+@pytest.mark.usefixtures("repository")
+@pytest.mark.parametrize("to_dev", [False, True])
+def test_lock_self_referencing_groups(project, pdm, to_dev):
+    name = project.name
+    project.add_dependencies({"requests": parse_requirement("requests")}, to_group="http", dev=to_dev)
+    project.add_dependencies(
+        {"pytz": parse_requirement("pytz"), f"{name}[http]": parse_requirement(f"{name}[http]")},
+        to_group="dev",
+        dev=True,
+    )
+    pdm(["lock", "-G", "dev"], obj=project, strict=True)
+    assert project.lockfile.groups == ["default", "dev"]
+    assert "requests" in project.locked_repository.all_candidates
+
+
+@pytest.mark.usefixtures("local_finder")
+def test_lock_multiple_platform_wheels(project, pdm):
+    project.environment.python_requires = PySpecSet(">=3.7")
+    project.add_dependencies({"pdm-hello": parse_requirement("pdm-hello")})
+    pdm(["lock"], obj=project, strict=True)
+    assert project.lockfile.cross_platform
+    package = next(p for p in project.lockfile["package"] if p["name"] == "pdm-hello")
+    file_hashes = package["files"]
+    assert len(file_hashes) == 2
+
+
+@pytest.mark.usefixtures("local_finder")
+def test_lock_current_platform_wheels(project, pdm):
+    project.environment.python_requires = PySpecSet(">=3.7")
+    project.add_dependencies({"pdm-hello": parse_requirement("pdm-hello")})
+    pdm(["lock", "--no-cross-platform"], obj=project, strict=True)
+    assert project.lockfile.cross_platform is False
+    package = next(p for p in project.lockfile["package"] if p["name"] == "pdm-hello")
+    file_hashes = package["files"]
+    wheels_num = 2 if sys.platform == "win32" and not project.python.is_32bit else 1
+    assert len(file_hashes) == wheels_num
---
+++ packages/pdm/pdm-2.9.3.tar.gz/pdm-2.9.3/tests/test_utils.py
@@ -0,0 +1,153 @@
+import pathlib
+import sys
+
+import pytest
+import tomlkit
+from packaging.version import Version
+
+from pdm import utils
+from pdm.cli import utils as cli_utils
+from pdm.cli.filters import GroupSelection
+from pdm.exceptions import PdmUsageError
+
+
+@pytest.mark.parametrize(
+    "given,expected",
+    [
+        ("test", "test"),
+        ("", ""),
+        ("${FOO}", "hello"),
+        ("$FOO", "$FOO"),
+        ("${BAR}", "${BAR}"),
+        ("%FOO%", "%FOO%"),
+        ("${FOO}_${FOO}", "hello_hello"),
+    ],
+)
+def test_expand_env_vars(given, expected, monkeypatch):
+    monkeypatch.setenv("FOO", "hello")
+    assert utils.expand_env_vars(given) == expected
+
+
+@pytest.mark.parametrize(
+    "given,expected",
+    [
+        ("https://example.org/path?arg=1", "https://example.org/path?arg=1"),
+        (
+            "https://${FOO}@example.org/path?arg=1",
+            "https://hello@example.org/path?arg=1",
+        ),
+        (
+            "https://${FOO}:${BAR}@example.org/path?arg=1",
+            "https://hello:wo%3Arld@example.org/path?arg=1",
+        ),
+        (
+            "https://${FOOBAR}@example.org/path?arg=1",
+            "https://%24%7BFOOBAR%7D@example.org/path?arg=1",
+        ),
+    ],
+)
+def test_expend_env_vars_in_auth(given, expected, monkeypatch):
+    monkeypatch.setenv("FOO", "hello")
+    monkeypatch.setenv("BAR", "wo:rld")
+    assert utils.expand_env_vars_in_auth(given) == expected
+
+
+def compare_python_paths(path1, path2):
+    return path1.parent == path2.parent
+
+
+@pytest.mark.path
+def test_find_python_in_path(tmp_path):
+    assert utils.find_python_in_path(sys.executable) == pathlib.Path(sys.executable).absolute()
+
+    posix_path_to_executable = pathlib.Path(sys.executable)
+    assert compare_python_paths(
+        utils.find_python_in_path(sys.prefix),
+        posix_path_to_executable,
+    )
+
+    assert not utils.find_python_in_path(tmp_path)
+
+
+def test_merge_dictionary():
+    target = tomlkit.item(
+        {
+            "existing_dict": {"foo": "bar", "hello": "world"},
+            "existing_list": ["hello"],
+        }
+    )
+    input_dict = {
+        "existing_dict": {"foo": "baz"},
+        "existing_list": ["world"],
+        "new_dict": {"name": "Sam"},
+    }
+    cli_utils.merge_dictionary(target, input_dict)
+    assert target == {
+        "existing_dict": {"foo": "baz", "hello": "world"},
+        "existing_list": ["hello", "world"],
+        "new_dict": {"name": "Sam"},
+    }
+
+
+def setup_dependencies(project):
+    project.pyproject.metadata.update(
+        {
+            "dependencies": ["requests"],
+            "optional-dependencies": {"web": ["flask"], "auth": ["passlib"]},
+        }
+    )
+    project.pyproject.settings.update({"dev-dependencies": {"test": ["pytest"], "doc": ["mkdocs"]}})
+    project.pyproject.write()
+
+
+@pytest.mark.parametrize(
+    "args,golden",
+    [
+        ({"default": True, "dev": None, "groups": ()}, ["default", "test", "doc"]),
+        (
+            {"default": True, "dev": None, "groups": [":all"]},
+            ["default", "web", "auth", "test", "doc"],
+        ),
+        (
+            {"default": True, "dev": True, "groups": ["web"]},
+            ["default", "web", "test", "doc"],
+        ),
+        (
+            {"default": True, "dev": None, "groups": ["web"]},
+            ["default", "web", "test", "doc"],
+        ),
+        ({"default": True, "dev": None, "groups": ["test"]}, ["default", "test"]),
+        (
+            {"default": True, "dev": None, "groups": ["test", "web"]},
+            ["default", "test", "web"],
+        ),
+        ({"default": True, "dev": False, "groups": ["web"]}, ["default", "web"]),
+        ({"default": False, "dev": None, "groups": ()}, ["test", "doc"]),
+    ],
+)
+def test_dependency_group_selection(project, args, golden):
+    setup_dependencies(project)
+    selection = GroupSelection(project, **args)
+    assert sorted(golden) == sorted(selection)
+
+
+def test_prod_should_not_be_with_dev(project):
+    setup_dependencies(project)
+    selection = GroupSelection(project, default=True, dev=False, groups=["test"])
+    with pytest.raises(PdmUsageError):
+        list(selection)
+
+
+def test_deprecation_warning():
+    with pytest.warns(FutureWarning) as record:
+        utils.deprecation_warning("Test warning", raise_since="99.99")
+    assert len(record) == 1
+    assert str(record[0].message) == "Test warning"
+
+    with pytest.raises(FutureWarning):
+        utils.deprecation_warning("Test warning", raise_since="0.0")
+
+
+def test_comparable_version():
+    assert utils.comparable_version("1.2.3") == Version("1.2.3")
+    assert utils.comparable_version("1.2.3a1+local1") == Version("1.2.3a1")
