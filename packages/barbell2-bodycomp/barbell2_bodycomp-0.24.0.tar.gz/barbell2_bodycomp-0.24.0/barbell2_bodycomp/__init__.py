53a5adff23c45b3ba0b481345b2f271f23e94654
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-26 03:05:07 +0000

    Add package barbell2-bodycomp/0.24.0/barbell2_bodycomp-0.24.0.tar.gz

---
+++ packages/barbell2-bodycomp/barbell2_bodycomp-0.24.0.tar.gz/barbell2_bodycomp-0.24.0/barbell2_bodycomp/__init__.py
@@ -0,0 +1,9 @@
+__author__ = 'Ralph Brecheisen'
+__email__ = 'r.brecheisen@maastrichtuniversity.nl'
+__version__ = '0.24.0'
+
+from barbell2_bodycomp.calculator import BodyCompositionCalculator
+from barbell2_bodycomp.seg import MuscleFatSegmentator
+from barbell2_bodycomp.selectroi import RoiSelector
+from barbell2_bodycomp.selectslice import SliceSelector
+from barbell2_bodycomp.totalseg import TotalSegmentator
