b569ba8d48c83f370b1758a6ee9a553302731634
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-26 03:06:49 +0000

    Add package comics/0.2.0/comics-0.2.0-py3-none-any.whl

---
+++ packages/comics/comics-0.2.0-py3-none-any.whl/comics/__init__.py
@@ -0,0 +1,11 @@
+"""
+comics
+~~~~~~
+"""
+
+from comics import exceptions
+from comics.api import *
+from comics.constants import directory
+from comics.gocomics import InvalidDateError
+
+__version__ = "0.2.0"
