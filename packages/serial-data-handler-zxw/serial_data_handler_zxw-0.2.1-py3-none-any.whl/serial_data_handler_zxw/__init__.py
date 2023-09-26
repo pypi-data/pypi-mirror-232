1d43ed649becdd399f424e8827a76bb7763eb950
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-26 14:07:27 +0000

    Add package serial-data-handler-zxw/0.2.1/serial_data_handler_zxw-0.2.1-py3-none-any.whl

---
+++ packages/serial-data-handler-zxw/serial_data_handler_zxw-0.2.1-py3-none-any.whl/serial_data_handler_zxw/__init__.py
@@ -0,0 +1,9 @@
+"""
+# File       : __init__.py.py
+# Time       ：2023/9/25 16:53
+# Author     ：xuewei zhang
+# Email      ：jingmu_predict@qq.com
+# version    ：python 3.8
+# Description：
+"""
+from .get_train_data import SplitTimeSerialData
