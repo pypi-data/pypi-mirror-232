c7ae8577e227e98de0f846dc5a0ac498f3e61f05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-26 14:07:06 +0000

    Add package rblx-open-cloud/1.5.1/rblx-open-cloud-1.5.1.tar.gz

---
+++ packages/rblx-open-cloud/rblx-open-cloud-1.5.1.tar.gz/rblx-open-cloud-1.5.1/rblxopencloud/user.py
@@ -0,0 +1,29 @@
+from .creator import Creator
+import datetime
+from typing import Optional
+
+__all__ = (
+    "User",
+)
+
+class User(Creator):
+    """
+    Represents a user on Roblox. It is used to provide information about a user in OAuth2, and to upload assets to a user.
+    ### Paramaters
+    id: int - The user's ID.
+    api_key: str - Your API key created from [Creator Dashboard](https://create.roblox.com/credentials) with access to this user.
+    """
+    def __init__(self, id: int, api_key: str) -> None:
+        self.username: Optional[str] = None
+        self.id: int = id
+        self.display_name: Optional[str] = None
+        self.profile_uri: str = f"https://roblox.com/users/{self.id}/profile"
+        self.headshot_uri: Optional[str] = None
+        self.created_at: Optional[datetime.datetime] = None
+
+        self.__api_key = api_key
+
+        super().__init__(id, api_key, "User")
+    
+    def __repr__(self) -> str:
+        return f"rblxopencloud.User({self.id})"
