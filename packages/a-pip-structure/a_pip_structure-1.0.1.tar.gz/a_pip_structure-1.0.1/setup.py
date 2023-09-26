1f6ef85a3f212c9e98832392e3f892861e42bbc8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-26 14:07:20 +0000

    Add package a-pip-structure/1.0.1/a_pip_structure-1.0.1.tar.gz

---
+++ packages/a-pip-structure/a_pip_structure-1.0.1.tar.gz/a_pip_structure-1.0.1/setup.py
@@ -0,0 +1,44 @@
+
+
+from 	setuptools import setup, find_packages
+import 	pathlib
+
+here = pathlib.Path(__file__).parent.resolve()
+
+# Get the long description from the README file
+long_description = (here / "README.md").read_text(encoding="utf-8")
+
+# Arguments marked as "Required" below must be included for upload to PyPI.
+# Fields marked as "Optional" may be commented out.
+
+setup (
+    name 				= 	"a_pip_structure",
+    version 			= 	"1.0.1",
+	packages 			= 	find_packages (where = "STRUCTURE"),
+	python_requires		=	">=3.7, <4",
+	install_requires	=	[ "flask" ],
+	
+    description = "",
+    long_description = "",
+    long_description_content_type = "text/markdown",
+    # url="",
+	
+	
+    #author = "", 
+    #author_email = "",
+    #classifiers = [],
+    
+	keywords = "structure",
+    
+	package_dir = { "": "src" }, 
+
+    #extras_require={
+    #    "dev": [ "check-manifest"],
+    #    "test": [ "coverage" ],
+    #},
+	
+	# package_data
+	# entry_points
+
+    project_urls = {}
+)
