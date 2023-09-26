5d8b45d3e3020139d77e7cff0fcb2ea23b8f70ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-26 04:07:03 +0000

    Add package streamlit-monaco/0.1.0/streamlit_monaco-0.1.0-py3-none-any.whl

---
+++ packages/streamlit-monaco/streamlit_monaco-0.1.0-py3-none-any.whl/streamlit_monaco/render.py
@@ -0,0 +1,13 @@
+from pathlib import Path
+from streamlit.components.v1 import declare_component
+from streamlit_monaco import version
+import os
+
+if os.environ.get("RELEASE_BUILD") == "true":
+    ELEMENTS_FRONTEND = {
+        "path": (Path(version.__file__).parent / "frontend/build").resolve()
+    }
+else:
+    ELEMENTS_FRONTEND = {"url": "http://localhost:3001"}
+
+render_component = declare_component("streamlit_monaco", **ELEMENTS_FRONTEND)
