09ade7ceef8598dcdee94058ebeb0ba625c797b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-26 03:06:20 +0000

    Add package taichu-storage/1.0.6/taichu-storage-1.0.6.tar.gz

---
+++ packages/taichu-storage/taichu-storage-1.0.6.tar.gz/taichu-storage-1.0.6/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+if __name__ == '__main__':
+    name = 'taichu-storage'
+
+    with open('requirements.txt', 'r') as f:
+        requirements = f.read().splitlines()
+
+    long_description = 'storage sdks aggregation'
+    # with open('README.md', 'r') as f:
+    #     long_description = f.read()
+
+    setup(
+        name=name,
+        version='1.0.6',
+        description='storage sdks aggregation',
+        long_description=long_description,
+        author='taichu platform team',
+        python_requires=">=3.6.0",
+        url='',
+        keywords='taichu',
+        packages=find_packages(),
+        install_requires=requirements,
+        include_package_data=True,
+        package_data={
+            '': ['*.sh'],
+        }
+    )
