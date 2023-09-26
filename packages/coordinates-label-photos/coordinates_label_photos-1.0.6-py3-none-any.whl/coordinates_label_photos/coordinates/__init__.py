c2b3027703c8973957cf0d98d8e0ef9b06beb27b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-26 15:07:14 +0000

    Add package coordinates-label-photos/1.0.6/coordinates_label_photos-1.0.6-py3-none-any.whl

---
+++ packages/coordinates-label-photos/coordinates_label_photos-1.0.6-py3-none-any.whl/coordinates_label_photos/coordinates/__init__.py
@@ -0,0 +1,135 @@
+from datetime import datetime
+from fractions import Fraction
+
+from piexif import GPSIFD, ImageIFD
+import geopy.distance
+
+
+class Coordinates:
+    lat: float
+    lon: float
+    elevation: float
+    timestamp: datetime
+    label: str
+    horiz_accuracy: float
+    vert_accuracy: float
+    positioning_quality: str
+
+    def __init__(self,
+                 lat: float,
+                 lon: float,
+                 elevation: float = None,
+                 timestamp: datetime = None,
+                 label: str = None,
+                 horiz_accuracy: float = None,
+                 vert_accuracy: float = None,
+                 positioning_quality: str = None
+                 ):
+        self.lat = lat
+        self.lon = lon
+        self.elevation = elevation
+        self.timestamp = timestamp
+        self.label = label
+        self.horiz_accuracy = horiz_accuracy
+        self.vert_accuracy = vert_accuracy
+        self.positioning_quality = positioning_quality
+
+    def distance(self, other):
+        return geopy.distance.distance(
+            (self.lat, self.lon),
+            (other.lat, other.lon),
+            ellipsoid='WGS-84'
+        ).m
+
+    def __repr__(self):
+        return '(%.10f, %.10f, %s) %s %s' % (self.lat, self.lon, self.elevation, self.label, self.timestamp)
+
+    def exif_gps(self):
+        return {
+            GPSIFD.GPSVersionID: (2, 0, 0, 0),
+            GPSIFD.GPSAltitudeRef: 1,
+            GPSIFD.GPSAltitude: _to_rational(self.elevation),
+            GPSIFD.GPSDateStamp: u"2021:08:10 10:55:55",
+            GPSIFD.GPSLatitudeRef: self.lat_dir(),
+            GPSIFD.GPSLatitude: self.exiv_lat(),
+            GPSIFD.GPSLongitudeRef: self.lon_dir(),
+            GPSIFD.GPSLongitude: self.exiv_lon(),
+            #GPSIFD.GPSHPositioningError: 0.4
+        }
+
+    def exiv_lat(self):
+        return [_to_rational(x) for x in to_abs_deg_min_sec(self.lat)]
+
+    def exiv_lon(self):
+        return [_to_rational(x) for x in to_abs_deg_min_sec(self.lon)]
+
+    def lat_dir(self) -> str:
+        if self.lat > 0:
+            return 'N'
+        else:
+            if self.lat < 0:
+                return 'S'
+            else:
+                return ''
+
+    def lon_dir(self) -> str:
+        if self.lat > 0:
+            return 'E'
+        else:
+            if self.lat < 0:
+                return 'W'
+            else:
+                return ''
+
+
+def _to_rational(number: float):
+    """
+    returns a numerator,denominator pair. Truncated the number to 7 decimals, to avoid  > long klimit in exif
+    """
+    f = Fraction(str(round(number, 7)))
+    return f.numerator, f.denominator
+
+
+def to_abs_deg_min_sec(value):
+    """
+    return the value to the absolute value in degree, minute, seconds
+    """
+    abs_value = abs(value)
+    degrees = int(abs_value)
+    rem = (abs_value - degrees) * 60
+    minutes = int(rem)
+    seconds = round((rem - minutes) * 60, 8)
+    return degrees, minutes, seconds
+
+
+def get_decimal_from_dms(dms, ref):
+    """From https://developer.here.com/blog/getting-started-with-geocoding-exif-image-metadata-in-python3"""
+    degrees = dms[0][0] / dms[0][1]
+    minutes = dms[1][0] / dms[1][1] / 60.0
+    seconds = dms[2][0] / dms[2][1] / 3600.0
+
+    if ref in ['S', 'W']:
+        degrees = -degrees
+        minutes = -minutes
+        seconds = -seconds
+
+    return round(degrees + minutes + seconds, 8)
+
+
+def exif_to_coordinates(exif) -> Coordinates:
+    if 'GPS' not in exif:
+        raise Exception('not "GPS" tag in exif image')
+
+    exif_gps = exif['GPS']
+    lat = get_decimal_from_dms(exif_gps[GPSIFD.GPSLatitude], exif_gps[GPSIFD.GPSLatitudeRef].decode('utf-8'))
+    lon = get_decimal_from_dms(exif_gps[GPSIFD.GPSLongitude], exif_gps[GPSIFD.GPSLongitudeRef].decode('utf-8'))
+    elev = round(exif_gps[GPSIFD.GPSAltitude][0] / exif_gps[GPSIFD.GPSAltitude][1], 2)
+
+    timestamp = datetime.strptime(exif['0th'][ImageIFD.DateTime].decode('utf-8'), '%Y:%m:%d %H:%M:%S')
+
+    return Coordinates(
+        lat=lat,
+        lon=lon,
+        elevation=elev,
+        timestamp=timestamp,
+    )
---
+++ packages/coordinates-label-photos/coordinates_label_photos-1.0.6-py3-none-any.whl/coordinates_label_photos/parsers/__init__.py
@@ -0,0 +1,138 @@
+import csv
+import logging
+import os
+from datetime import datetime, timezone, timedelta
+
+import gpxpy as gpxpy
+
+from coordinates_label_photos.coordinates import Coordinates
+from coordinates_label_photos.coordinates.coordinates_collection import CoordinatesCollection
+
+
+class NoParserForCoordinateFormatError:
+    def __init__(self, extension: str):
+        self.extension = extension
+
+    def __str__(self):
+        return 'No parser is available for %s among %s' % (self.extension, ['.llh', '.gpx'])
+
+
+def coords_parser(filename) -> CoordinatesCollection:
+    """
+    Read coordinates from various file format
+    :param filename:
+    :type filename:
+    :return:
+    :rtype:
+    """
+    logging.info('Loading coordinates track from %s' % filename)
+    _, file_extension = os.path.splitext(filename)
+    file_extension = file_extension.lower()
+    if file_extension == '.gpx':
+        return gpx_parser(filename)
+    else:
+        if file_extension in {'.llh', '.pos'}:
+            return emlid_fwf_parser(filename)
+        else:
+            raise NoParserForCoordinateFormatError(file_extension)
+
+
+def gpx_parser(filename) -> CoordinatesCollection:
+    logging.info('Loading GPX track from %s' % filename)
+    with open(filename) as fd:
+        gpx = gpxpy.parse(fd)
+
+        coords = []
+        for track in gpx.tracks:
+            for segment in track.segments:
+                for point in segment.points:
+                    coords.append(Coordinates(
+                        lat=point.latitude,
+                        lon=point.longitude,
+                        elevation=point.elevation,
+                        timestamp=point.time
+                    ))
+        collect = CoordinatesCollection(coords)
+        logging.info('Loaded GPX with %d track points between %s and %s' % (
+            len(collect), collect.start_time(), collect.end_time()))
+        return collect
+
+
+def csv_parser(filename) -> CoordinatesCollection:
+    logging.info('Loading CSV track from %s' % filename)
+    with open(filename, newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        coords = []
+
+        for row in reader:
+            coords.append(
+                Coordinates(
+                    lat=float(row['latitude_decimal_degree']),
+                    lon=float(row['longitude_decimal_degree']),
+                    elevation=float(row['ellipsoidal_height_m']),
+                    timestamp=None,
+                )
+            )
+        collect = CoordinatesCollection(coords)
+        logging.info('Loaded CSV with %d track points between %s and %s' % (
+            len(collect), collect.start_time(), collect.end_time()))
+        return collect
+
+
+def emlid_fwf_parser(filename) -> CoordinatesCollection:
+    """
+    Parse Emlid fixed width format.
+    IF a .pos file, check the last comment list for GPST and eventually correct the infamous 18 seconds
+    :param filename: the source file path
+    :type filename: string
+    :return: the list of coordinates
+    :rtype: CoordinatesCollection
+    """
+    logging.info('Loading Emlid .LLH or .POS from %s' % filename)
+
+    is_gpst = False
+    with open(filename) as fd:
+        coords = []
+        for line in fd.readlines():
+            if line.startswith('%'):
+                if 'GPST' in line:
+                    is_gpst = True
+                continue
+            c = emlid_fwf_line_parser(line)
+            if is_gpst:
+                c.timestamp = c.timestamp + timedelta(seconds=-18)
+            coords.append(c)
+        return CoordinatesCollection(coords)
+
+
+def _parse_emlid_timestamp(text: str):
+    return datetime.strptime(text + '000', '%Y/%m/%d %H:%M:%S.%f').replace(tzinfo=timezone.utc)
+
+
+def emlid_fwf_line_parser(line: str) -> Coordinates:
+    timestamp = _parse_emlid_timestamp(line[0:23])
+    lat = float(line[25:38])
+    lon = float(line[39:53])
+    elev = float(line[55:64])
+    sd_n = float(line[73:82])
+    sd_e = float(line[82:91])
+    sd_u = float(line[91:100])
+    return Coordinates(
+        lat=lat,
+        lon=lon,
+        elevation=elev,
+        timestamp=timestamp,
+        horiz_accuracy=max([sd_n, sd_e]) * 1.96,
+        vert_accuracy=sd_u * 1.96,
+        positioning_quality=__positioning_qualitiy[int(line[67:70])]
+    )
+
+
+__positioning_qualitiy = {
+    1: 'FIX',
+    2: 'FLOAT',
+    3: 'SBAS',
+    4: 'DGPS',
+    5: 'SINGLE',
+    6: 'PPP'
+}
