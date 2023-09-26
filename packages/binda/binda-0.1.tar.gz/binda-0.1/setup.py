258d167faa468689f4d3283a8bcd3db0998b02f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-26 05:06:42 +0000

    Add package binda/0.1/binda-0.1.tar.gz

---
+++ packages/binda/binda-0.1.tar.gz/binda-0.1/setup.py
@@ -0,0 +1,30 @@
+from distutils.core import setup
+setup(
+  name = 'binda',
+  #packages = ['binda'],
+  version = '0.1',
+  license='MIT', 
+  description = 'Read and write binary data using Pandas',
+  author = 'Jamie Cash',
+  author_email = 'jlcash@gmail.com',
+  url = 'https://github.com/jamiecash',
+  download_url = 'https://github.com/jamiecash/binda/archive/v0.1.0.tar.gz',
+  keywords = ['Binary', 'Data', 'Analytics', 'Analyse', 'Pandas', 'DataFrame'], 
+  install_requires=[
+          'pandas',
+          'numpy',
+      ],
+  classifiers=[
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+    'Programming Language :: Python :: 3.10',
+    'Programming Language :: Python :: 3.11',
+  ],
+)
