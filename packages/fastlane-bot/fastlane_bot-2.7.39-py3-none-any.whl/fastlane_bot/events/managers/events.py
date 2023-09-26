e6add79c6396e119f2216df508c5ffb26c45adfb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-26 03:04:59 +0000

    Add package fastlane-bot/2.7.39/fastlane_bot-2.7.39-py3-none-any.whl

---
+++ packages/fastlane-bot/fastlane_bot-2.7.39-py3-none-any.whl/fastlane_bot/events/managers/events.py
@@ -0,0 +1,33 @@
+# coding=utf-8
+"""
+Contains the events manager module for handling event related functionality of data fetching.
+
+(c) Copyright Bprotocol foundation 2023.
+Licensed under MIT
+"""
+from typing import Dict, Any, List, Type, Tuple
+
+from web3.contract import Contract
+from brownie.network.contract import Contract as BrownieContract
+
+from fastlane_bot.events.managers.base import BaseManager
+
+
+class EventManager(BaseManager):
+    @property
+    def events(self) -> List[Contract or BrownieContract]:
+        """
+        Get the events from the exchanges.
+
+        Returns
+        -------
+        List[Type[Contract]]
+            The events.
+        """
+        return [
+            event
+            for exchange in self.exchanges.values()
+            for event in exchange.get_events(
+                self.event_contracts[exchange.exchange_name]
+            )
+        ]
