1039c3c89187f726126bbaba06d535809ee9595f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-26 05:06:03 +0000

    Add package scpup/0.12.14/scpup-0.12.14-py3-none-any.whl

---
+++ packages/scpup/scpup-0.12.14-py3-none-any.whl/scpup/__init__.py
@@ -0,0 +1,27 @@
+"""scpup package contains the classes, constants, types, etc., needed in the
+game SCPUP (Super Crystal Pokebros Ultimate Party) and any other AramEau game.
+"""
+
+import pygame
+from .services import EauService  # noqa
+from .text import *  # noqa
+from .loader import *  # noqa
+from .sprite import *  # noqa
+from .group import *  # noqa
+from .view import *  # noqa
+from .player import *  # noqa
+from .ctrl import *  # noqa
+from .position import *  # noqa
+
+__name__ = "scpup"
+__package__ = "scpup"
+
+
+def init():
+  from .services import EauDisplayService, EauEventService
+  from .text import EauText
+  pygame.init()
+  EauDisplayService()
+  EauEventService()
+  if not EauText._fontpath_:
+    raise RuntimeError('Font path has not been set!!!')
