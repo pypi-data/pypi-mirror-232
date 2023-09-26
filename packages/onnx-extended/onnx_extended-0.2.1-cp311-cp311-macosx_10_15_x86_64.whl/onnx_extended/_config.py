9ad72836dec271a9c7433552520927114610a742
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-26 03:06:27 +0000

    Add package onnx-extended/0.2.1/onnx_extended-0.2.1-cp311-cp311-macosx_10_15_x86_64.whl

---
+++ packages/onnx-extended/onnx_extended-0.2.1-cp311-cp311-macosx_10_15_x86_64.whl/onnx_extended/_config.py
@@ -0,0 +1,5 @@
+HAS_CUDA = 0
+CXX_FLAGS = ' -mavx -fPIC -Xpreprocessor -fopenmp -I/usr/local/opt/libomp/include'
+CMAKE_CXX_STANDARD_REQUIRED = ''
+CMAKE_CXX_EXTENSIONS = ''
+CMAKE_CXX_STANDARD = 17
