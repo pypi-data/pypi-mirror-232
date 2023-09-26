4698d8370f09127e348c232c23f9b4fa6dec7414
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-26 14:06:34 +0000

    Add package pytest-mock-resources/2.9.2/pytest_mock_resources-2.9.2.tar.gz

---
+++ packages/pytest-mock-resources/pytest_mock_resources-2.9.2.tar.gz/pytest_mock_resources-2.9.2/src/pytest_mock_resources/cli.py
@@ -0,0 +1,92 @@
+from __future__ import annotations
+
+import argparse
+import importlib
+
+from pytest_mock_resources.config import DockerContainerConfig
+from pytest_mock_resources.container.base import container_name, get_container
+
+
+class StubPytestConfig:
+    pmr_multiprocess_safe = False
+    pmr_cleanup_container = False
+
+    class option:
+        pmr_multiprocess_safe = False
+        pmr_cleanup_container = False
+
+    def getini(self, attr):
+        return getattr(self, attr)
+
+
+def main():
+    parser = create_parser()
+    args = parser.parse_args()
+
+    if args.load:
+        for module in args.load:
+            importlib.import_module(module)
+
+    pytestconfig = StubPytestConfig()
+
+    stop = args.stop
+    start = not stop
+
+    for fixture in args.fixtures:
+        if fixture not in DockerContainerConfig.subclasses:
+            valid_options = ", ".join(DockerContainerConfig.subclasses)
+            raise argparse.ArgumentError(
+                args.fixtures,
+                f"'{fixture}' invalid. Valid options include: {valid_options}",
+            )
+
+        execute(fixture, pytestconfig, start=start, stop=stop)
+
+
+def create_parser():
+    parser = argparse.ArgumentParser(
+        description="Preemptively run docker containers to speed up initial startup of PMR Fixtures."
+    )
+    parser.add_argument(
+        "fixtures",
+        metavar="Fixture",
+        type=str,
+        nargs="+",
+        help="Available Fixtures: {}".format(", ".join(DockerContainerConfig.subclasses)),
+    )
+    parser.add_argument(
+        "--stop", action="store_true", help="Stop previously started PMR containers"
+    )
+
+    parser.add_argument(
+        "--load",
+        action="append",
+        help="Import a module in order to load 3rd party resources.",
+    )
+    return parser
+
+
+def execute(fixture: str, pytestconfig: StubPytestConfig, start=True, stop=False):
+    config_cls = DockerContainerConfig.subclasses[fixture]
+    config = config_cls()
+
+    if start:
+        generator = get_container(pytestconfig, config)
+        for _ in generator:
+            pass
+
+    if stop:
+        from python_on_whales import docker
+
+        assert config.port
+        name = container_name(fixture, int(config.port))
+        try:
+            container = docker.container.inspect(name)
+        except Exception:
+            print(f"Failed to stop {fixture} container")
+        else:
+            container.kill()
+
+
+if __name__ == "__main__":
+    main()
