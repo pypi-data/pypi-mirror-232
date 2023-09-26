b1cc329c34162b1bd87848b6b1ba7469f0105208
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-26 03:05:22 +0000

    Add package scinumtools/2.5.2/scinumtools-2.5.2-py3-none-any.whl

---
+++ packages/scinumtools/scinumtools-2.5.2-py3-none-any.whl/scinumtools/units/__init__.py
@@ -0,0 +1,13 @@
+from .QuantityClass import Quantity
+from .QuantityClass  import Quantity as quant
+from .UnitClass import Unit
+from .UnitClass import Unit as unit
+from .ConstantClass import Constant
+from .ConstantClass import Constant as const
+from .NaNClass import NaN
+from .NaNClass import NaN as nan
+from .MagnitudeClass import Magnitude
+from .DimensionsClass import Dimensions
+from .FractionClass import Fraction
+from .BaseUnitsClass import BaseUnits
+from .UnitSolver import UnitSolver
