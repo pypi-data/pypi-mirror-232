d1e194ee0530ef14633904bc03ee0338a74ed04d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-26 05:06:57 +0000

    Add package fal/0.10.9/fal-0.10.9.tar.gz

---
+++ packages/fal/fal-0.10.9.tar.gz/fal-0.10.9/openapi-fal-rest/openapi_fal_rest/models/__init__.py
@@ -0,0 +1,49 @@
+""" Contains all the data models used in inputs/outputs """
+
+from .body_upload_file import BodyUploadFile
+from .body_upload_local_file import BodyUploadLocalFile
+from .customer_details import CustomerDetails
+from .file_spec import FileSpec
+from .grouped_usage_detail import GroupedUsageDetail
+from .hash_check import HashCheck
+from .http_validation_error import HTTPValidationError
+from .invoice import Invoice
+from .invoice_item import InvoiceItem
+from .key_scope import KeyScope
+from .log_entry import LogEntry
+from .new_user_key import NewUserKey
+from .payment_method import PaymentMethod
+from .persisted_usage_record import PersistedUsageRecord
+from .persisted_usage_record_meta import PersistedUsageRecordMeta
+from .run_type import RunType
+from .uploaded_file_result import UploadedFileResult
+from .url_file_upload import UrlFileUpload
+from .usage_per_machine_type import UsagePerMachineType
+from .usage_run_detail import UsageRunDetail
+from .user_key_info import UserKeyInfo
+from .validation_error import ValidationError
+
+__all__ = (
+    "BodyUploadFile",
+    "BodyUploadLocalFile",
+    "CustomerDetails",
+    "FileSpec",
+    "GroupedUsageDetail",
+    "HashCheck",
+    "HTTPValidationError",
+    "Invoice",
+    "InvoiceItem",
+    "KeyScope",
+    "LogEntry",
+    "NewUserKey",
+    "PaymentMethod",
+    "PersistedUsageRecord",
+    "PersistedUsageRecordMeta",
+    "RunType",
+    "UploadedFileResult",
+    "UrlFileUpload",
+    "UsagePerMachineType",
+    "UsageRunDetail",
+    "UserKeyInfo",
+    "ValidationError",
+)
