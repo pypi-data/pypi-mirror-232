ad7b24179b6352bd97b9c53be5a4f6cd99f8480e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-26 03:06:58 +0000

    Add package YooMoney-async/0.0.6/YooMoney_async-0.0.6.tar.gz

---
+++ packages/YooMoney-async/YooMoney_async-0.0.6.tar.gz/YooMoney_async-0.0.6/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages, Extension
+
+setup(
+    name='YooMoney_async',
+    version='0.0.6',
+    packages=find_packages(),
+    include_package_data=True,
+    url='https://github.com/Navigatoroleg/yoomoney_asunc.git',
+    author='OlesMokovoz',
+    author_email='oleg-oleg-2023@mail.ru',
+    description='Unofficial YooMoney_async API python library'
+)
