14009440be83b5a432833a1e9150069233d35ba3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-26 05:06:29 +0000

    Add package udlr/0.0.1/udlr-0.0.1.tar.gz

---
+++ packages/udlr/udlr-0.0.1.tar.gz/udlr-0.0.1/setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup, Extension
+from Cython.Build import cythonize
+import numpy as np
+from pathlib import Path
+import sys
+import os
+
+if sys.platform == "darwin":
+    os.environ["CC"] = "gcc-12"
+    os.environ["CXX"] = "g++-12"
+
+this_directory = Path(__file__).parent
+
+cpp_modules = ["gf2sparse"]
+
+c_extensions = []
+for module in cpp_modules:
+    c_extensions.append(
+        Extension(
+            name=f"udlr.{module}._{module}",
+            sources=[f"src_python/udlr/{module}/_{module}.pyx"],
+            libraries=[],
+            library_dirs=[],
+            include_dirs=[np.get_include(), "src_cpp"],
+            extra_compile_args=["-std=c++2a", "-O3", "-fopenmp"],
+            extra_link_args=["-lgomp", "-fopenmp"],
+            language="c++",
+        )
+    )
+
+
+setup(
+    ext_modules=cythonize(
+        c_extensions,
+        compiler_directives={
+            "boundscheck": False,
+            "wraparound": False,
+            "initializedcheck": False,
+            "cdivision": True,
+            "embedsignature": True,
+        },
+    ),
+)
