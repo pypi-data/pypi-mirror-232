1a70d88da94a1e1ad4260fc6dc3c2140c2a4074c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-26 05:06:24 +0000

    Add package flatpack/0.4.4/flatpack-0.4.4-py3-none-any.whl

---
+++ packages/flatpack/flatpack-0.4.4-py3-none-any.whl/flatpack/models.py
@@ -0,0 +1,77 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+from torch.utils.data import DataLoader
+
+
+class RNNLM(nn.Module):
+    def __init__(self, vocab_size, embed_size, hidden_size, num_layers):
+        super(RNNLM, self).__init__()
+        self.embedding = nn.Embedding(vocab_size, embed_size)
+        self.rnn = nn.RNN(embed_size, hidden_size, num_layers, batch_first=True)
+        self.fc = nn.Linear(hidden_size, vocab_size)
+
+    def forward(self, x):
+        x = self.embedding(x)
+        out, _ = self.rnn(x)
+        out = self.fc(out)
+        return out
+
+    @staticmethod
+    def train_model(dataset, vocab_size, embed_size, hidden_size, num_layers, epochs, batch_size):
+        dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+        model = RNNLM(vocab_size, embed_size, hidden_size, num_layers)
+        criterion = nn.CrossEntropyLoss()
+        optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+        for epoch in range(epochs):
+            total_loss = 0.0
+            total_accuracy = 0.0
+            total_batches = 0
+
+            for inputs, targets in dataloader:
+                inputs = inputs.long()
+                outputs = model(inputs)
+                loss = criterion(outputs.view(-1, vocab_size), targets.view(-1))
+
+                _, predicted = torch.max(outputs.data, 2)
+                correct = (predicted == targets)
+                accuracy = correct.sum().item() / (targets.size(0) * targets.size(1))
+
+                optimizer.zero_grad()
+                loss.backward()
+                torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1)
+                optimizer.step()
+
+                total_loss += loss.item()
+                total_accuracy += accuracy
+                total_batches += 1
+
+            # Print epoch-wise progress
+            average_loss = total_loss / total_batches
+            average_accuracy = total_accuracy / total_batches
+            print(f"Epoch {epoch + 1}/{epochs}, Loss: {average_loss:.4f}, Accuracy: {average_accuracy:.4f}")
+
+        return {'model': model}
+
+    def generate_text(self, char_to_index, index_to_char, start_sequence="In the beginning", generate_length=1024,
+                      temperature=1.0):
+        input_sequence = [char_to_index[char] for char in start_sequence]
+        input_tensor = torch.tensor(input_sequence).long().unsqueeze(0)
+        generated_text = start_sequence
+
+        self.eval()
+
+        with torch.no_grad():
+            for _ in range(generate_length):
+                output = self(input_tensor)
+                probabilities = F.softmax(output[0, -1] / temperature, dim=0)
+                next_index = torch.multinomial(probabilities, 1).item()
+                next_token = index_to_char[next_index]
+
+                generated_text += next_token
+                input_sequence = input_sequence[1:] + [next_index]
+                input_tensor = torch.tensor(input_sequence).long().unsqueeze(0)
+
+        return generated_text
