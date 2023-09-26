72468e395d9759f99fcecbf88b936739aa7fe12f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-26 05:06:03 +0000

    Add package RSFlib/0.1.7/RSFlib-0.1.7-py3-none-any.whl

---
+++ packages/RSFlib/RSFlib-0.1.7-py3-none-any.whl/RSFlib/Constants.py
@@ -0,0 +1,15 @@
+''' Physical constants '''
+class PhysConst:
+    def __init__(self):
+        self.planck = 6.62607015e-34
+        self.light_speed = 299792458
+        self.elem_charge = 1.602176e-19
+        self.electron_mass = 9.109382e-31
+        self.proton_mass = 1.67262192e-27
+        self.neutron_mass = 1.67492749804e-27
+        self.vac_el_perm = 8.8541878128e-12
+        self.vac_mag_perm = 1.25663706212e-6
+        self.boltzman = 1.380649e-23
+        self.newt_grav = 6.67430e-11
+
+
