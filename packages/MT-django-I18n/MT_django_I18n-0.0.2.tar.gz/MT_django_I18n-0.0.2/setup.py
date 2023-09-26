bc3f900e62e071b4c0eb2ccd08aac818f667c965
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-26 03:07:04 +0000

    Add package MT-django-I18n/0.0.2/MT_django_I18n-0.0.2.tar.gz

---
+++ packages/MT-django-I18n/MT_django_I18n-0.0.2.tar.gz/MT_django_I18n-0.0.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='MT_django_I18n',
+    version='0.1',
+    description='This package helps for creating translation files(locale) from JSON file in django ',
+    author='Viraj Poojari',
+    author_email='virajp.in@mouritech.com',
+    packages=['mt_django_I18n']
+)
