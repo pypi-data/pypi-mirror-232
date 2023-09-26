cbcc3607e9d1cfcbc5030a8c127bfe9e4e0d0b8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-26 03:07:07 +0000

    Add package tired/0.0.5/tired-0.0.5.tar.gz

---
+++ packages/tired/tired-0.0.5.tar.gz/tired-0.0.5/tired/git.py
@@ -0,0 +1,13 @@
+def get_current_branch_name():
+    # TODO
+    pass
+
+
+def get_current_branch_hash():
+    # TODO
+    pass
+
+
+def get_remote_repository_name(remote_tag="origin"):
+    # TODO
+    pass
