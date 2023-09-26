b6c4ff9bbe6d670e6d463b33db013f8c21769db7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-26 14:07:32 +0000

    Add package not-my-ip/0.0.3/not-my-ip-0.0.3.tar.gz

---
+++ packages/not-my-ip/not-my-ip-0.0.3.tar.gz/not-my-ip-0.0.3/README.md
@@ -0,0 +1,12 @@
+# Not My IP!
+
+Get an IP address that both isn't yours and isn't in a reserved range.
+
+```
+Usage: not-my-ip [OPTIONS]
+
+Options:
+  --actually-yes-my-ip  Actually return my IP
+  --allow-reserved      Allow reserved IP addresses
+  --help                Show this message and exit.
+```
---
+++ packages/not-my-ip/not-my-ip-0.0.3.tar.gz/not-my-ip-0.0.3/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+with open('requirements.txt', 'r') as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name='not-my-ip',
+    version='0.0.3',
+    install_requires=requirements,
+    author='Samuel Gunter',
+    author_email='sgunter@utexas.edu',
+    description='A CLI tool to generate a random IP address that is not your own',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    entry_points={
+        'console_scripts': [
+            'not-my-ip = main:cli'
+        ]
+    }
+)
