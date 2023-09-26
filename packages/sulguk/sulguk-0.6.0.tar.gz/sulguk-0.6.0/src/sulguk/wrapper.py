8f830c60d955b3e728548e7cd0105f68ad65b266
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-26 05:06:00 +0000

    Add package sulguk/0.6.0/sulguk-0.6.0.tar.gz

---
+++ packages/sulguk/sulguk-0.6.0.tar.gz/sulguk-0.6.0/src/sulguk/wrapper.py
@@ -0,0 +1,22 @@
+from dataclasses import dataclass
+from typing import List
+
+from sulguk.render.state import MessageEntity, State
+from .transformer import Transformer
+
+
+@dataclass
+class RenderResult:
+    text: str
+    entities: List[MessageEntity]
+
+
+def transform_html(raw_html: str) -> RenderResult:
+    transformer = Transformer()
+    transformer.feed(raw_html)
+    state = State()
+    transformer.root.render(state)
+    return RenderResult(
+        text=state.canvas.text,
+        entities=state.entities,
+    )
