fd861635235a3ec512f251d2fb0cbf8d8cce3892
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-27 16:05:22 +0000

    Add package pyscanasdk/1.0.3/pyscanasdk-1.0.3.tar.gz

---
+++ packages/pyscanasdk/pyscanasdk-1.0.3.tar.gz/pyscanasdk-1.0.3/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='pyscanasdk',
+    version='1.0.3',
+    author='scana',
+    author_email='scanaservice@knownsec.com',
+    description='',
+    packages=['pyscanasdk'],
+    url='https://github.com/scanasdk/scana-sdk-python',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
