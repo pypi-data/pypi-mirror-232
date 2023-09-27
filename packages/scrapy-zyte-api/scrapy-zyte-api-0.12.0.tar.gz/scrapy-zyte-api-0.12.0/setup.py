c324a9207891eedac2eae84e5037ae199deeb221
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-27 04:05:44 +0000

    Add package scrapy-zyte-api/0.12.0/scrapy-zyte-api-0.12.0.tar.gz

---
+++ packages/scrapy-zyte-api/scrapy-zyte-api-0.12.0.tar.gz/scrapy-zyte-api-0.12.0/setup.py
@@ -0,0 +1,40 @@
+import setuptools
+
+setuptools.setup(
+    name="scrapy-zyte-api",
+    version="0.12.0",
+    description="Client library to process URLs through Zyte API",
+    long_description=open("README.rst").read(),
+    long_description_content_type="text/x-rst",
+    author="Zyte Group Ltd",
+    author_email="info@zyte.com",
+    url="https://github.com/scrapy-plugins/scrapy-zyte-api",
+    packages=["scrapy_zyte_api"],
+    # Sync with [pinned] @ tox.ini
+    install_requires=[
+        "packaging>=20.0",
+        "scrapy>=2.0.1",
+        "zyte-api>=0.4.0",
+    ],
+    extras_require={
+        # Sync with [testenv:provider-pinned] @ tox.ini
+        "provider": [
+            "scrapy-poet>=0.10.0",
+            "web-poet>=0.13.0",
+            "zyte-common-items>=0.7.0",
+        ]
+    },
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
+)
