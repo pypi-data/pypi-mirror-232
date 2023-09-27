858394fed03a022ba660ba8bff57b4fbf06d7faf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-27 03:06:19 +0000

    Add package spektro/0.0.1/spektro-0.0.1.tar.gz

---
+++ packages/spektro/spektro-0.0.1.tar.gz/spektro-0.0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="spektro",
+    version="0.0.1",
+    description="A Python package for hyperspectral/multispectral imaging research.",
+    author="Yannis Kalfas",
+    packages=find_packages(),
+    install_requires=[
+        "h5py>=3.9.0",
+        "matplotlib>=3.8.0",
+        "numpy>=1.26.0",
+    ],
+)
