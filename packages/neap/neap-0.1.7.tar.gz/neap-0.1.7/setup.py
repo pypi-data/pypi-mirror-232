b9e21a094037608167ab39613234feeb910a0219
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-27 03:06:47 +0000

    Add package neap/0.1.7/neap-0.1.7.tar.gz

---
+++ packages/neap/neap-0.1.7.tar.gz/neap-0.1.7/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='neap',
+    version='0.1.7',
+    description='A Python wrapper for the NEAP API',
+    author='Bard',
+    author_email='bard@google.com',
+    packages=['neap'],
+    install_requires=['requests'],
+)
