a399e760083d8c186d77b33687d05d79a9593385
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-27 04:05:52 +0000

    Add package wuji/0.1.3/wuji-0.1.3.tar.gz

---
+++ packages/wuji/wuji-0.1.3.tar.gz/wuji-0.1.3/wuji/Evaluator/NightlySleepMetrics.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+
+"""
+@File        :   NightlySleepMetrics 
+@Time        :   2023/9/26 14:06
+@Author      :   Xuesong Chen
+@Description :   
+"""
+
+class NightlySleepMetricsEvaluator:
+    def __init__(self):
+        pass
