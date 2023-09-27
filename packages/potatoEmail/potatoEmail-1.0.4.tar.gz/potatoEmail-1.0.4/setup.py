8ac163ff0b78636db6fbbdf2ddf570ad71c65a31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-27 03:05:19 +0000

    Add package potatoEmail/1.0.4/potatoEmail-1.0.4.tar.gz

---
+++ packages/potatoEmail/potatoEmail-1.0.4.tar.gz/potatoEmail-1.0.4/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='potatoEmail',
+    version='1.0.4',
+    description='A simple library for sending email',
+    author='ks lim',
+    author_email='kianseng.lim@sony.com',
+    packages=['potatoscript'],
+)
