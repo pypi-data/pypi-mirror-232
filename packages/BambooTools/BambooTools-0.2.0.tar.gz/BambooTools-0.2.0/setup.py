cccee1f9eaf14d775c831ca6a328873e0ab47f05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-27 05:05:39 +0000

    Add package BambooTools/0.2.0/BambooTools-0.2.0.tar.gz

---
+++ packages/BambooTools/BambooTools-0.2.0.tar.gz/BambooTools-0.2.0/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name='BambooTools',
+    version='0.2.0',
+    author='Konstantinos Maravegias',
+    author_email='kwstas.maras@gmail.com',
+    packages=find_packages(),
+    url='https://github.com/KwstasMCPU/BambooTools',
+    license='MIT',
+    description='Useful Bamboo stuff.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    install_requires=[
+        "pandas >= 1.5.3"
+        ],
+    extras_require={
+        "dev": ["pytest", "twine", "seaborn"]
+        },
+    classifiers=["Development Status :: 3 - Alpha",
+                 "Programming Language :: Python :: 3",
+                 "Intended Audience :: Developers",
+                 "Intended Audience :: Science/Research",
+                 "License :: OSI Approved :: MIT License"
+                 ],
+    keywords='pandas, pandas extensions, data analysis, data science',
+    python_requires='>=3.8'
+)
