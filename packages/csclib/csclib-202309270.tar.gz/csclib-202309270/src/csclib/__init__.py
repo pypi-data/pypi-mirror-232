ca38c76d639f83ab23c3f057f1c9e98d45d8cbb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-27 16:05:51 +0000

    Add package csclib/202309270/csclib-202309270.tar.gz

---
+++ packages/csclib/csclib-202309270.tar.gz/csclib-202309270/src/csclib/__init__.py
@@ -0,0 +1,13 @@
+#__version__ = '0.0.1'
+
+#from ccsclib import Csclib_core, Csclib_bits, Csclib_start
+#from src.csclib.pycsclib import Share as Share
+#from src.csclib.pycsclib import Bits as Bits
+#from ccsclib import *
+
+
+from .pycsclib import *
+from .SA import *
+from .LOUDS import *
+#Csclib_start(-1)
+#print("csclib.init2")
