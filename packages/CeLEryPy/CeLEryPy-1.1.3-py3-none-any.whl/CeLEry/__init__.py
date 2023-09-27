a616ad0dfc1db23c3e4ceb19982402177ace6fb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-27 04:05:59 +0000

    Add package CeLEryPy/1.1.3/CeLEryPy-1.1.3-py3-none-any.whl

---
+++ packages/CeLEryPy/CeLEryPy-1.1.3-py3-none-any.whl/CeLEry/__init__.py
@@ -0,0 +1,7 @@
+__version__ = '1.1.2'
+from . util import *
+from . datasetgenemap import *
+from . DNN import *
+from . util_Mouse import *
+from . fit_functions import *
+from . data_augmentation import *
