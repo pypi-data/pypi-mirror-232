0c5de221e1c3b04aa42517bc56f71bae37b8d8dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-27 14:07:53 +0000

    Add package risk-distributions/2.0.13/risk_distributions-2.0.13.tar.gz

---
+++ packages/risk-distributions/risk_distributions-2.0.13.tar.gz/risk_distributions-2.0.13/src/risk_distributions/__about__.py
@@ -0,0 +1,22 @@
+__all__ = [
+    "__title__",
+    "__summary__",
+    "__uri__",
+    "__version__",
+    "__author__",
+    "__email__",
+    "__license__",
+    "__copyright__",
+]
+
+__title__ = "risk_distributions"
+__summary__ = "Components for building distributions. Compatible for use with ``vivarium``"
+__uri__ = "https://github.com/ihmeuw/risk_distributions"
+
+__version__ = "2.0.13"
+
+__author__ = "The risk_distributions developers"
+__email__ = "vivarium.dev@gmail.com"
+
+__license__ = "BSD-3-Clause"
+__copyright__ = f"Copyright 2022 {__author__}"
