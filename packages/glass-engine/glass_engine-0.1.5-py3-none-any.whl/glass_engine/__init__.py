6b06733828547a369de55ca706bb54e42f9f6b4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-27 16:05:31 +0000

    Add package glass-engine/0.1.5/glass_engine-0.1.5-py3-none-any.whl

---
+++ packages/glass-engine/glass_engine-0.1.5-py3-none-any.whl/glass_engine/__init__.py
@@ -0,0 +1,17 @@
+from .Scene import Scene
+from .SceneNode import SceneNode
+from .SinglePathNode import SinglePathNode
+from .BasicScene import SceneRoam, ModelView
+
+from .Camera import Camera
+from .Screen import Screen
+from .Material import Material
+from .Mesh import Mesh
+from .Model import Model
+
+from .ColorMap import ColorMap
+from .Frame import Frame
+from .Fog import Fog
+
+import glm
+from OpenGL import GL
