ea79705581dcbd32cafd4b6c099962cd79d1c2c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-27 15:07:20 +0000

    Add package torio/0.1.0/torio-0.1.0.tar.gz

---
+++ packages/torio/torio-0.1.0.tar.gz/torio-0.1.0/setup.py
@@ -0,0 +1,14 @@
+"""Python setup.py for torio package"""
+from setuptools import find_packages, setup
+
+setup(
+    name="torio",
+    version="0.1.0",
+    description="",
+    url="https://pypi.org/project/torio/",
+    long_description="Placeholder",
+    long_description_content_type="text/markdown",
+    author="",
+    packages=find_packages(),
+    install_requires=[],
+)
