5bfd23b82742d6fed69e38628582e1be0482a6c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-27 04:06:49 +0000

    Add package coraline/0.2.1/coraline-0.2.1.tar.gz

---
+++ packages/coraline/coraline-0.2.1.tar.gz/coraline-0.2.1/coraline/__init__.py
@@ -0,0 +1,24 @@
+from asbool import asbool
+from loguru import logger
+from stela import env
+
+from coraline.config import CoralConfig
+from coraline.field import KeyField, TTLField
+from coraline.model import CoralModel
+from coraline.types import BillingMode, HashType
+
+__version__ = "0.2.1"
+
+__all__ = [
+    "CoralModel",
+    "KeyField",
+    "BillingMode",
+    "HashType",
+    "CoralConfig",
+    "TTLField",
+]
+
+if asbool(env.get_or_default("CORALINE_SHOW_LOGS", True)):
+    logger.enable("coraline")
+else:
+    logger.disable("coraline")
