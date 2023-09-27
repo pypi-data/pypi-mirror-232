a409656c3d7ad13a6a00e5ac259a9ddadd3ed4a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-27 05:06:12 +0000

    Add package equality-check/0.0.1/equality-check-0.0.1.tar.gz

---
+++ packages/equality-check/equality-check-0.0.1.tar.gz/equality-check-0.0.1/setup.py
@@ -0,0 +1,21 @@
+
+#
+#	https://setuptools.pypa.io/en/latest/userguide/quickstart.html
+#
+
+from setuptools import setup, find_packages
+
+#print ("PACKAGES:", find_packages ())
+
+NAME = 'equality-check'
+
+setup (
+    name = NAME,
+    version = '0.0.1',
+    install_requires = [],
+	
+	package_dir = { NAME: 'src'},
+	#package_data = {
+	#	NAME: [ 'DATA/**/*' ]
+	#}
+)
