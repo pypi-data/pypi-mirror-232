275ef85178940fdcfda7ca19b3b75e3d53ee1468
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-27 15:06:02 +0000

    Add package utils-base-nuuuwan/1.0.1/utils_base-nuuuwan-1.0.1.tar.gz

---
+++ packages/utils-base-nuuuwan/utils_base-nuuuwan-1.0.1.tar.gz/utils_base-nuuuwan-1.0.1/README.md
@@ -0,0 +1,23 @@
+# Utils (Base)
+
+General utilities complimenting and expanding on standard python.
+
+## Setup & Install
+
+https://pypi.org/project/utils_base-nuuuwan/#history
+
+```
+pip install utils_base-nuuuwan
+```
+
+## Version History
+
+### 1.0.1
+* [#4](https://github.com/nuuuwan/utils_base/issues/4)
+* [#3](https://github.com/nuuuwan/utils_base/issues/3)
+* [#2](https://github.com/nuuuwan/utils_base/issues/2)
+* [#1](https://github.com/nuuuwan/utils_base/issues/1) - partially
+
+### 1.0.0 (CURRENT)
+
+* Base Version
---
+++ packages/utils-base-nuuuwan/utils_base-nuuuwan-1.0.1.tar.gz/utils_base-nuuuwan-1.0.1/src/utils_base/Log.py
@@ -0,0 +1,35 @@
+"""Utils."""
+
+import logging
+
+from py_console import console, textColor
+
+LEVEL_TO_COLOR = {
+    logging.CRITICAL: textColor.MAGENTA,
+    logging.ERROR: textColor.RED,
+    logging.WARNING: textColor.YELLOW,
+    logging.INFO: textColor.BLUE,
+    logging.DEBUG: textColor.WHITE,
+    logging.NOTSET: textColor.WHITE,
+}
+
+
+class CustomLoggingFormatter(logging.Formatter):
+    def format(self, record):
+        color = LEVEL_TO_COLOR[record.levelno]
+        return console.highlight(f'{record.name}> {record.msg}', color)
+
+
+class Log(logging.Logger):
+    def __init__(self, name: str, level: int = logging.DEBUG):
+        super(Log, self).__init__(name, level)
+        self.propagate = False
+
+        formatter = CustomLoggingFormatter()
+        sh = logging.StreamHandler()
+        sh.setLevel(logging.DEBUG)
+        sh.setFormatter(formatter)
+        self.handlers = [sh]
+
+
+_log = Log('default')
