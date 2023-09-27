5c6be1eac3651dfbfb6c8a63a310d99090695713
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-27 03:05:37 +0000

    Add package poltergust-luigi-utils/0.0.9/poltergust-luigi-utils-0.0.9.tar.gz

---
+++ packages/poltergust-luigi-utils/poltergust-luigi-utils-0.0.9.tar.gz/poltergust-luigi-utils-0.0.9/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+import setuptools
+
+setuptools.setup(
+    name='poltergust-luigi-utils',
+    version='0.0.9',
+    description='Luigi utils',
+    long_description='''Luigi utils for use together with Poltergust
+''',
+    long_description_content_type="text/markdown",
+    author='Egil Moeller',
+    author_email='em@emrld.no',
+    url='https://github.com/emerald-geomodelling/poltergust-luigi-utils',
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "luigi",
+        "requests",
+        "google-auth",
+        "google-api-python-client",
+    ],
+)
