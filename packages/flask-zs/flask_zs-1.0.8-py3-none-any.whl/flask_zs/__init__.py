b4a9d9cc0b6b75a48acbea136f2526fb5c48d701
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-27 04:07:46 +0000

    Add package flask-zs/1.0.8/flask_zs-1.0.8-py3-none-any.whl

---
+++ packages/flask-zs/flask_zs-1.0.8-py3-none-any.whl/flask_zs/__init__.py
@@ -0,0 +1,19 @@
+from .errorhandler import register_error_handlers
+from .helpers import (
+    BaseModel,
+    CustomFlask,
+    PaginationMixin,
+    register_api,
+    register_blueprints,
+)
+from .response import make_errmsg
+
+__all__ = (
+    "BaseModel",
+    "CustomFlask",
+    "PaginationMixin",
+    "register_api",
+    "register_blueprints",
+    "make_errmsg",
+    "register_error_handlers",
+)
