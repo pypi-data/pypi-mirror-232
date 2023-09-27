bf515ae09582ed7d60e3ea884aa5f92a0ae26b86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-27 03:06:02 +0000

    Add package chatllm/2023.9.26.17.19.40/chatllm-2023.9.26.17.19.40-py3-none-any.whl

---
+++ packages/chatllm/chatllm-2023.9.26.17.19.40-py3-none-any.whl/chatllm/llmchain/completions/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Project      : AI.  @by PyCharm
+# @File         : completion
+# @Time         : 2023/7/31 16:12
+# @Author       : betterme
+# @WeChat       : meutils
+# @Software     : PyCharm
+# @Description  :
+
+
+from chatllm.llmchain.completions.ernie import ErnieBotCompletion
+from chatllm.llmchain.completions.spark import SparkBotCompletion
+from chatllm.llmchain.completions.chatglm import ChatGLMCompletion
