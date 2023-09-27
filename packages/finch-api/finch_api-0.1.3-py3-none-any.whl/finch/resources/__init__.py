26048544ad19ba9b4be99b69bc8f000c5371539b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-27 04:05:39 +0000

    Add package finch-api/0.1.3/finch_api-0.1.3-py3-none-any.whl

---
+++ packages/finch-api/finch_api-0.1.3-py3-none-any.whl/finch/resources/__init__.py
@@ -0,0 +1,8 @@
+# File generated from our OpenAPI spec by Stainless.
+
+from .hris import HRIS, AsyncHRIS
+from .account import Account, AsyncAccount
+from .webhooks import Webhooks, AsyncWebhooks
+from .providers import Providers, AsyncProviders
+
+__all__ = ["HRIS", "AsyncHRIS", "Providers", "AsyncProviders", "Account", "AsyncAccount", "Webhooks", "AsyncWebhooks"]
