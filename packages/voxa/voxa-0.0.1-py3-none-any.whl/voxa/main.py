a9c64ed7954504ffb9ef383c3b5c74cbb8f490ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-27 16:05:04 +0000

    Add package voxa/0.0.1/voxa-0.0.1-py3-none-any.whl

---
+++ packages/voxa/voxa-0.0.1-py3-none-any.whl/voxa/main.py
@@ -0,0 +1,21 @@
+import requests
+
+API_URL = "https://voxa.dark9015.repl.co/api"
+
+class Translator:
+    def __init__(self, source='en', target='fr'):
+        self.source = source
+        self.target = target
+
+    def translate(self, text, target=None):
+        target_language = target if target else self.target
+
+        response = requests.get(API_URL, params={
+            'translate': text,
+            'from': self.source,
+            'to': target_language
+        })
+
+        data = response.json()
+        return data.get("translated", "")
+
