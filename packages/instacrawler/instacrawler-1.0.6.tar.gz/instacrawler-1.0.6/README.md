a8b7758abcbc8288249b2a8c308be4c8a8323889
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-27 03:06:47 +0000

    Add package instacrawler/1.0.6/instacrawler-1.0.6.tar.gz

---
+++ packages/instacrawler/instacrawler-1.0.6.tar.gz/instacrawler-1.0.6/README.md
@@ -0,0 +1,39 @@
+![PyPI - Version](https://img.shields.io/pypi/v/instacrawler?color=red)
+![PyPI - Downloads](https://img.shields.io/pypi/dm/instacrawler?color=orange)
+![PyPI - License](https://img.shields.io/pypi/l/instacrawler?color=green)
+![PyPI - Python Version](https://img.shields.io/pypi/pyversions/instacrawler?color=blue)
+
+# instacrawler
+**instacrawler** is the fastest and simplest way to check someone's instagram feed/story.
+- No instagram account required.
+- Available on VM
+
+You can find more detailed information in [Caution](#caution).
+
+**<ins>If it helped you, please give me a star⭐️. Thank you!</ins>**
+
+
+## Installation
+- Python >= 3.7
+```bash
+pip install --upgrade pip
+pip install instacrawler
+```
+
+## Quick start
+```py
+import instacrawler
+
+instacrawler.get_latest_feed(username='starbucks')
+# OUTPUT : (text, PIL.Image)
+
+instacrawler.get_latest_story(username='starbucks')
+# OUTPUT : PIL.Image
+```
+
+## Caution
+- This library use the mirror site named [imgsed](https://imgsed.com/). So if the site changes, this may not be worked.
+- Due to the nature of the mirror site, it is hard to identify fixed feeds.
+- When a new feed/story is posted, it needs time to be reflected.
+  - New feed : *maybe* up to 4 hours
+  - New Story : *maybe* up to 3 hours
---
+++ packages/instacrawler/instacrawler-1.0.6.tar.gz/instacrawler-1.0.6/instacrawler/crawling.py
@@ -0,0 +1,64 @@
+import requests
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from PIL import Image
+
+opts = webdriver.ChromeOptions() 
+opts.add_argument('--headless=new')
+opts.add_experimental_option('excludeSwitches', ['enable-logging'])
+
+driver = webdriver.Chrome(options=opts)
+driver.implicitly_wait(30)
+
+def get_latest_feed(username, k=1) :
+    assert username != None, "Enter username as a parameter."
+    assert k >= 1, "k must be greater than 1."
+ 
+    url = f"https://imgsed.com/{username}"
+    resp = requests.get(url)
+    html = resp.text
+    soup = BeautifulSoup(html, 'html.parser')
+    
+    feeds = soup.find_all('img', loading="lazy")
+    if len(feeds) == 0 : return None
+    
+    outputs = []
+    for feed in feeds[:k] :
+        text = feed.get('alt')
+        src = feed.get('src').split('?', maxsplit=1)[-1]
+        img = Image.open(requests.get(src, stream=True).raw).convert('RGB')
+        outputs.append((text, img))
+    
+    if len(outputs) == 1 : return outputs[0]
+    return outputs
+
+def get_latest_story(username, k=1) :
+    assert username != None, "Enter username as a parameter."
+    assert k >= 1, "k must be greater than 1."
+    
+    url = f"https://imgsed.com/stories/{username}"
+    driver.get(url)
+    
+    highlight_selected = driver.find_element(By.CSS_SELECTOR, 'li.swiper-slide-active')
+    highlight_selected_name = highlight_selected.find_element(By.TAG_NAME, 'div').text
+    if highlight_selected_name != username : return None
+
+    wraps = driver.find_elements(By.CLASS_NAME, 'media')
+    if len(wraps) == 0 : return None
+    
+    outputs = []
+    for wrap in reversed(wraps[-k:]) :
+        src = wrap.find_element(By.TAG_NAME, 'img').get_attribute('src').split('?', maxsplit=1)[-1]
+        img = Image.open(requests.get(src, stream=True).raw).convert('RGB')
+        outputs.append(img)
+    
+    if len(outputs) == 1 : return outputs[0]
+    return outputs
+
+if __name__ == "__main__" :
+    get_latest_feed(username='starbucks')    
+    get_latest_feed(username='starbucks', k=3)
+    
+    get_latest_story(username='hipkr_')
+    get_latest_story(username='hipkr_', k=3)
