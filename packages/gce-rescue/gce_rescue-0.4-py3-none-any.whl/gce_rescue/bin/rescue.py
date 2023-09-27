09f3b500019db3d7b970ac16f2214769bdaf29f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-27 03:06:53 +0000

    Add package gce-rescue/0.4/gce_rescue-0.4-py3-none-any.whl

---
+++ packages/gce-rescue/gce_rescue-0.4-py3-none-any.whl/gce_rescue/bin/rescue.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python3
+
+# Copyright 2021 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+""" Main script to be used to set/reset rescue mode. """
+
+from datetime import datetime
+import logging
+
+from gce_rescue.config import process_args, set_configs
+from gce_rescue import messages
+from gce_rescue.gce import Instance
+from gce_rescue.tasks.actions import call_tasks
+from gce_rescue.utils import read_input, set_logging
+
+def main():
+  """ Main script function. """
+  parser = process_args()
+  args = parser.parse_args()
+  set_configs(args)
+
+  set_logging(vm_name=args.name)
+
+  parse_kwargs = {
+      'zone': args.zone,
+      'name': args.name,
+  }
+
+  if args.project:
+    parse_kwargs['project'] = args.project
+
+  vm = Instance(test_mode=False, **parse_kwargs)
+  rescue_on = vm.rescue_mode_status['rescue-mode']
+  if not rescue_on:
+    if not args.force:
+      info = (f'This option will boot the instance {vm.name} in '
+              'RESCUE MODE. \nIf your instance is running it will be rebooted. '
+              '\nDo you want to continue [y/N]: ')
+      read_input(msg=info)
+
+    print('Starting...')
+    # save in the log file current configuration of the VM as backup.
+    logging.info('RESTORE#%s\n', vm.data)
+    action = 'set_rescue_mode'
+    msg = messages.tip_connect_ssh(vm)
+
+  else:
+    rescue_ts = vm.rescue_mode_status['ts']
+    rescue_date = datetime.fromtimestamp(int(rescue_ts))
+
+    if not args.force:
+      info = (f'The instance \"{vm.name}\" is currently configured '
+              f'to boot as rescue mode since {rescue_date}.\nWould you like to'
+              ' restore the original configuration ? [y/N]: ')
+      read_input(msg=info)
+
+    print('Restoring VM...')
+    action = 'reset_rescue_mode'
+    has_snapshot = vm.snapshot
+    msg = messages.tip_restore_disk(vm, snapshot=has_snapshot)
+
+  call_tasks(vm=vm, action=action)
+  print(msg)
+
+
+if __name__ == '__main__':
+  main()
---
+++ packages/gce-rescue/gce_rescue-0.4-py3-none-any.whl/gce_rescue/utils.py
@@ -0,0 +1,143 @@
+# Copyright 2021 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# pylint: disable=broad-exception-raised
+
+""" List of classes and functions to be used across the code. """
+
+from time import sleep
+import logging
+import multiprocessing
+from threading import Thread
+import sys
+from gce_rescue.config import get_config
+
+
+_logger = logging.getLogger(__name__)
+
+class Tracker():
+  """ Track tasks using multiprocessing and print progress bar. """
+
+  def __init__(self, target):
+    self.target = target
+    self._pivot = multiprocessing.Value('i', 1)
+    self._proc = None
+
+  def start(self):
+    self._proc = multiprocessing.Process(target=self._run)
+    self._proc.start()
+    print('┌── Configuring...')
+
+  def advance(self, step=None):
+    if not step:
+      step = 1
+    self._pivot.value += step
+
+  def finish(self):
+    self._pivot.value = self.target
+    self._proc.join()
+    sleep(0.5)
+    print('├── Configurations finished.')
+
+  def _loading(self):
+    chars = ['-', '|', '/', '|', '\\']
+    i = 0
+    while True:
+      yield chars[i]
+      i += 1
+      if i == len(chars):
+        i = 0
+
+  def _run(self):
+    self._gen = self._loading()
+    while self._pivot.value < self.target:
+      try:
+        sleep(0.001)
+        self._print()
+      except Exception as exc:
+        raise f'{exc}: {self._pivot.value} = {self.target}'
+
+    self._print()
+    print('\r')
+
+  def _print(self):
+    size = 60
+    loading = next(self._gen)
+    if self._pivot.value == self.target:
+      loading = '█'
+    count = self._pivot.value
+    total = self.target
+    x = int(size * self._pivot.value / self.target)
+    progress = '█' * x
+    bar = '.' * (size-x)
+    print(f'│   └── Progress {count}/{total} [{progress}{loading}{bar}]',
+      end='\r',
+      file=sys.stderr,
+      flush=True)
+
+
+class ThreadHandler(Thread):
+  """Handler for multithread tasks."""
+
+  def __init__(
+      self,
+      group=None,
+      target=None,
+      name=None,
+      args=None,
+      kwargs=None
+    ):
+
+    if not args:
+      args = ()
+
+    if not kwargs:
+      kwargs = {}
+
+    Thread.__init__(self, group, target, name, args, kwargs)
+    self._result = None
+
+  def run(self):
+    if self._target is not None:
+      self._result = self._target(*self._args, **self._kwargs)
+
+  def result(self, *args):
+    Thread.join(self, *args)
+    return self._result
+
+
+def set_logging(vm_name: str) -> None:
+  """ Set logfile and verbosity. """
+
+  level = 'DEBUG' if get_config('debug') else 'INFO'
+  log_level = getattr(logging, level.upper())
+  file_name = f'{vm_name}.log'
+  logging.basicConfig(
+    filename=file_name,
+    filemode='a',
+    format='%(asctime)s,%(msecs)03d %(levelname)-8s [%(filename)s:%(lineno)d]\
+      %(message)s',
+    datefmt='%Y-%m-%d:%H:%M:%S',
+    level=log_level)
+
+
+def read_input(msg: str) -> None:
+  """Read user input if --force is not provided."""
+  print(msg, end='')
+  input_answer = input()
+  input_answer = input_answer.strip()
+  if input_answer.upper() != 'Y':
+    print(f'got input: "{input_answer}". Aborting')
+    sys.exit(1)
+    
