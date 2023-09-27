e990133c30e4327fca86eb2bf7b6ef3dc2028cda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-27 14:12:16 +0000

    Add package atomica/1.26.6/atomica-1.26.6.tar.gz

---
+++ packages/atomica/atomica-1.26.6.tar.gz/atomica-1.26.6/atomica/version.py
@@ -0,0 +1,11 @@
+"""
+Atomica version file.
+
+Standard location for module version number and date.
+"""
+
+import sciris as sc
+
+version = "1.26.6"
+versiondate = "2023-09-26"
+gitinfo = sc.gitinfo(__file__)
