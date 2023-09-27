d7fe30f00c22ec8830b37c95e6806e44fe0150af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-27 05:06:10 +0000

    Add package Antiland/0.85/antiland-0.85.tar.gz

---
+++ packages/Antiland/antiland-0.85.tar.gz/antiland-0.85/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Antiland",
+    version="0.85",
+    packages=find_packages(),
+    install_requires=[
+        "requests",
+        "base64",
+        "requests",
+        "json",
+        "requests",
+        "base64",
+        "requests",
+        "threading",
+        "num2words",
+        "datetime",
+        "inflect"
+        ],
+)
