724b3cc2f08c1a6471fb8ff617a16b54d204ceae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-27 03:05:18 +0000

    Add package vectorview/0.1/vectorview-0.1.tar.gz

---
+++ packages/vectorview/vectorview-0.1.tar.gz/vectorview-0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="vectorview",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[],
+    author="Vectorview",
+    author_email="lukas.petersson.1999@gmail.com",
+    description="A simple Hello World library",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/Vectorview/vectorview_py",
+)
