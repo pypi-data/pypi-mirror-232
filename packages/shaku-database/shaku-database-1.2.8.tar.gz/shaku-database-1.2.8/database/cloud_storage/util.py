181ef0377d1cecf2f328e4c79bea44f949cdd9aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-27 16:05:22 +0000

    Add package shaku-database/1.2.8/shaku-database-1.2.8.tar.gz

---
+++ packages/shaku-database/shaku-database-1.2.8.tar.gz/shaku-database-1.2.8/database/cloud_storage/util.py
@@ -0,0 +1,56 @@
+from typing import List
+
+import pandas as pd
+from google.cloud import storage
+from pathlib import Path
+
+
+def get_csv_from_gcs(bucket_name, path):
+    full_path = 'gs://{bucket}/{path}'.format(bucket=bucket_name, path=path)
+    file_type = path.split(".")[-1]
+    result_df = pd.DataFrame()
+    if file_type == 'csv':
+        result_df = pd.read_csv(full_path)
+    elif file_type == 'xlsx' or file_type == 'xls':
+        result_df = pd.read_excel(full_path, engine='openpyxl')
+    return result_df
+
+
+def get_all_file_from_gcs_folder(bucket_name, path, file_type='csv'):
+    file_dict = {}
+    try:
+        storage_client = storage.Client()
+        bucket = storage_client.get_bucket(bucket_name)
+        for blob in bucket.list_blobs(prefix=str(path)):
+            current_path = Path(blob.name)
+            if str(current_path) != str(path):
+                full_path = 'gs://{bucket}/{path}'.format(bucket=bucket_name, path=str(current_path))
+                file = pd.DataFrame()
+                if file_type == 'csv':
+                    file = pd.read_csv(full_path)
+                elif file_type == 'excel':
+                    file = pd.read_excel(full_path)
+                file_dict[current_path] = file
+    except Exception as e:
+        raise e
+    return file_dict
+
+
+def get_file_as_string_from_gcs(bucket_name, path) -> bytes:
+    storage_client = storage.Client()
+    bucket = storage_client.get_bucket(bucket_name)
+    blob = bucket.blob(path)
+
+    # Download the contents of the blob as a string and then parse it using json.loads() method
+    return blob.download_as_string(client=None)
+
+
+def get_all_json_from_gcs(bucket_name, folder_path) -> List[bytes]:
+    client = storage.Client()
+    bucket = client.bucket(bucket_name)
+    blobs = bucket.list_blobs(prefix=folder_path, delimiter='/')
+    result_list = []
+    for blob in blobs:
+        if blob.name.endswith('.json'):
+            result_list.append(blob.download_as_string(client=None))
+    return result_list
