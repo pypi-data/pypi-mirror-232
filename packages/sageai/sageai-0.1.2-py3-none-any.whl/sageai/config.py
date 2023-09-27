dc00fbef58af00dc23f5d4a597522bd0bc9d0954
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-27 03:05:19 +0000

    Add package sageai/0.1.2/sageai-0.1.2-py3-none-any.whl

---
+++ packages/sageai/sageai-0.1.2-py3-none-any.whl/sageai/config.py
@@ -0,0 +1,30 @@
+from typing import Optional
+from pydantic import BaseModel, Field, ValidationError
+
+
+class Config(BaseModel):
+    functions_path: Optional[str] = Field(
+        None, description="File path to the functions directory"
+    )
+    openai_key: Optional[str] = Field(None, description="OpenAI key")
+    model: Optional[str] = Field(
+        "gpt-3.5-turbo-0613",
+        description="OpenAI model to use (has to support function-calling)",
+    )
+
+
+_config = Config()
+
+
+def set_config(**kwargs: "Config"):
+    """Set the configuration parameters."""
+    global _config
+    try:
+        _config = Config(**kwargs)
+    except ValidationError as e:
+        raise TypeError(f"Invalid configuration: {e}")
+
+
+def get_config():
+    """Retrieve the configuration."""
+    return _config
