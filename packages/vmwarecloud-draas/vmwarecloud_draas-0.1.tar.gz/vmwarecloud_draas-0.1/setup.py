8f7dfa509b756750987a71550c4d73ea14072d27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-27 16:05:05 +0000

    Add package vmwarecloud-draas/0.1/vmwarecloud_draas-0.1.tar.gz

---
+++ packages/vmwarecloud-draas/vmwarecloud_draas-0.1.tar.gz/vmwarecloud_draas-0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="vmwarecloud_draas",
+    version="0.1",
+    author="VMware, Inc.",
+    description="Client bindings for VMware Cloud on AWS - Disaster Recovery As A Service",
+    packages=find_packages(),
+    install_requires=[
+        # List your package dependencies here
+    ],
+)
+
