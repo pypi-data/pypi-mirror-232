b0889b59b5366862e754ed6ef50aaf14ff11a559
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-27 05:06:37 +0000

    Add package x-data-processing/0.1/x-data-processing-0.1.tar.gz

---
+++ packages/x-data-processing/x-data-processing-0.1.tar.gz/x-data-processing-0.1/README.md
@@ -0,0 +1,2 @@
+# x-data-processing
+It is a base app to start the x-data processing
---
+++ packages/x-data-processing/x-data-processing-0.1.tar.gz/x-data-processing-0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="x-data-processing",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[],
+    author="CoteDevent",
+    author_email="srwind18@gmail.com",
+    description="X-data-processing is a package for data processing.",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url="https://github.com/cdevent/x-data-processing",
+)
