789ca5b45acd4995489d688a5e5a4648b38c3898
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-27 05:05:41 +0000

    Add package pollination-dash-io/1.4.1/pollination_dash_io-1.4.1.tar.gz

---
+++ packages/pollination-dash-io/pollination_dash_io-1.4.1.tar.gz/pollination_dash_io-1.4.1/src/jl/''_createstudy.jl
@@ -0,0 +1,21 @@
+# AUTO GENERATED FILE - DO NOT EDIT
+
+export ''_createstudy
+
+"""
+    ''_createstudy(;kwargs...)
+
+A CreateStudy component.
+Use this component to run studies
+Keyword arguments:
+- `id` (String; optional): Unique ID to identify this component in Dash callbacks.
+- `accessToken` (String; optional): JWT token
+- `apiKey` (String; optional): API key from Pollination Cloud
+- `basePath` (String; optional): Base path of the API.
+"""
+function ''_createstudy(; kwargs...)
+        available_props = Symbol[:id, :accessToken, :apiKey, :basePath]
+        wild_props = Symbol[]
+        return Component("''_createstudy", "CreateStudy", "pollination_dash_io", available_props, wild_props; kwargs...)
+end
+
