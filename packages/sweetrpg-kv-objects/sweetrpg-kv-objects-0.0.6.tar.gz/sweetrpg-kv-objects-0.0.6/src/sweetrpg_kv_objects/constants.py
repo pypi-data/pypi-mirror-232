0438a8345216b86ac4c2a6e47a956de859989ae2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-27 04:05:45 +0000

    Add package sweetrpg-kv-objects/0.0.6/sweetrpg-kv-objects-0.0.6.tar.gz

---
+++ packages/sweetrpg-kv-objects/sweetrpg-kv-objects-0.0.6.tar.gz/sweetrpg-kv-objects-0.0.6/src/sweetrpg_kv_objects/constants.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+__author__ = "Paul Schifferer <dm@sweetrpg.com>"
+"""constants.py
+
+Constants.
+"""
+
+EVENT_EXPRESSION_CHANGED = "expression_changed"
+EVENT_RECALCULATE = "recalculate"
+EVENT_VALUE_CHANGED = "value_changed"
+EVENT_PING = "ping"
