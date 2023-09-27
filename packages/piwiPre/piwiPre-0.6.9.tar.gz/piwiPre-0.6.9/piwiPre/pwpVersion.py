e2459840643263d0e8360b47aa650dfc28a8f718
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-27 14:07:37 +0000

    Add package piwiPre/0.6.9/piwiPre-0.6.9.tar.gz

---
+++ packages/piwiPre/piwiPre-0.6.9.tar.gz/piwiPre-0.6.9/piwiPre/pwpVersion.py
@@ -0,0 +1,17 @@
+# ---------------------------------------------------------------------------------------------------------------
+# piwiPre project
+# This program and library is licenced under the European Union Public Licence v1.2 (see LICENCE)
+# developed by fabien.battini(at)gmail.com
+# ---------------------------------------------------------------------------------------------------------------
+
+
+class PwpVersion:
+    help = '0.6.9 at 09/26/2023 21:47:26'
+    spec = '0.6.9'
+    release = 0
+    version = 6
+    patch = 9
+    date = '09/26/2023 21:47:26'
+
+    def __init__(self):
+        pass
