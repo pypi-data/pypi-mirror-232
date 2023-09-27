cbfe05993df2ddb961b77e39d1c657c93ac60809
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-27 03:05:14 +0000

    Add package aws-pcluster-bootstrap-helpers/3.23.0/aws_pcluster_bootstrap_helpers-3.23.0-py2.py3-none-any.whl

---
+++ packages/aws-pcluster-bootstrap-helpers/aws_pcluster_bootstrap_helpers-3.23.0-py2.py3-none-any.whl/aws_pcluster_bootstrap_helpers/__init__.py
@@ -0,0 +1,9 @@
+"""Top-level package for aws_pcluster_bootstrap_helpers."""
+
+__author__ = """Jillian Rowe"""
+__email__ = "jillian@dabbleofdevops.com"
+__version__ = "0.1.0"
+
+from . import _version
+
+__version__ = _version.get_versions()["version"]
