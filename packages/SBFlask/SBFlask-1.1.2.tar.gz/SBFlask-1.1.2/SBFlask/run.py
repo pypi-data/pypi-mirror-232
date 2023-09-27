f8c824460d0430ec96ba9c131e084065a385ea09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-27 04:07:12 +0000

    Add package SBFlask/1.1.2/SBFlask-1.1.2.tar.gz

---
+++ packages/SBFlask/SBFlask-1.1.2.tar.gz/SBFlask-1.1.2/SBFlask/run.py
@@ -0,0 +1,13 @@
+import sys
+import logging
+import traceback
+from FlaskMain import create_app
+
+
+def main(args):
+    if 'webserver' in str(args).lower():
+        create_app()
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
