185d7c02d047b8fe177fa603b5a7849030b81401
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-27 15:07:22 +0000

    Add package cavsiopy/1.2.2/cavsiopy-1.2.2-py3-none-any.whl

---
+++ packages/cavsiopy/cavsiopy-1.2.2-py3-none-any.whl/cavsiopy/__init__.py
@@ -0,0 +1,10 @@
+def citing_warning():
+    """
+    prints a citation warning for cavsiopy users to remind them to cite
+    cavsiopy in publications.
+    """
+    print() # create a newline
+    print("IMPORTANT: Please make sure to cite cavsiopy in publications that use cavsiopy functions/modules/plots."
+          "Citation instructions are available at: https://cavsiopy.readthedocs.io/en/latest/")
+          
+citing_warning()
