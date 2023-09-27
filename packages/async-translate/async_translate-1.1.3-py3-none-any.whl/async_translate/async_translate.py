0be1a369d2080597c68dcb2eaef2dec53469aca5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-27 14:10:32 +0000

    Add package async-translate/1.1.3/async_translate-1.1.3-py3-none-any.whl

---
+++ packages/async-translate/async_translate-1.1.3-py3-none-any.whl/async_translate/async_translate.py
@@ -0,0 +1,85 @@
+from functools import cached_property
+from types import MappingProxyType
+from typing import Optional, Dict, Set, Mapping
+from .caseinsensitivedict import CaseInsensitiveDict
+from .abc import BaseProvider, Translation
+from .errors import *
+
+
+class AsyncTranslate:
+    def __init__(self):
+        self._providers: Dict[str, BaseProvider] = {}  # {'provider_name': ProviderInstance() }
+        self._languages: Dict[str, Set[str]] = {}  # {'language_name': {'provider_name'} }
+        self._language_names: Dict[str, str] = {}  # {'en': 'English'}
+
+    @property
+    def providers(self) -> Mapping[str, BaseProvider]:
+        """Returns read-only copy of the providers"""
+        return MappingProxyType(self._providers)
+
+    @property
+    def languages(self) -> Mapping[str, Set[str]]:
+        """Returns read-only copy of the languages"""
+        return MappingProxyType(self._languages)
+
+    @property
+    def language_names(self) -> Mapping[str, str]:
+        return CaseInsensitiveDict(self._language_names)
+
+    @cached_property
+    def language_by_names(self) -> Mapping[str, str]:
+        """Returns read-only copy of language codes with their English names"""
+        return CaseInsensitiveDict({name: code for code, name in self._language_names.items()})
+
+    @cached_property
+    def default_provider(self) -> BaseProvider:
+        """Returns the default (first) provider"""
+        return next(iter(self._providers.values()))
+
+    async def close(self):
+        """Close all Provider aiohttp loops"""
+        for provider in self._providers.values():
+            await provider.close()
+
+    async def add_provider(self, provider: BaseProvider):
+        """Add a translator provider"""
+        if not isinstance(provider, BaseProvider):
+            raise TypeError('Providers must derive from BaseProvider')
+
+        # Add to internal store
+        provider_name = provider.name.casefold()
+        if provider_name in self._providers:
+            raise ProviderAlreadyAdded(provider_name)
+        self._providers[provider_name] = provider
+
+        languages = await provider.get_languages()
+        for code, language_name in languages.items():
+            self._language_names[code] = language_name
+            if code in self._languages:
+                self._languages[code].add(provider_name)
+            else:
+                self._languages[code] = {provider_name}
+
+    async def add_providers(self, *backends: [BaseProvider]):
+        """Add multiple providers"""
+        for b in backends:
+            await self.add_provider(b)
+
+    def provider_for(self, language: str, preferred: Optional[str] = "") -> BaseProvider:
+        try:
+            available_providers = list(self._languages[language])
+        except KeyError:
+            raise LanguageNotSupported(language)
+        return self._providers[preferred if preferred in available_providers else available_providers[0]]
+
+    async def translate(self, to: str, content: str, provider: BaseProvider,
+                        source_language: Optional[str] = None, **options) -> Translation:
+        # Assumes to & source_language are valid language codes
+        if source_language and source_language == to:
+            raise DetectedAsSameError(to_language=to, detected_language=source_language)
+
+        # Detect translating to/from same language
+        detected_language = await provider.detect(content.strip())
+        if to == detected_language:
+            raise DetectedAsSameError(to_language=to, detected_language=detected_language)
+        return await provider.translate(content, to=to, source=source_language, **options)
---
+++ packages/async-translate/async_translate-1.1.3-py3-none-any.whl/async_translate/providers/azure/azure.py
@@ -0,0 +1,138 @@
+import uuid
+from json import JSONDecodeError
+from typing import Dict, Optional, Union, Sequence, List, Any
+
+from aiohttp_retry import RetryClient, ExponentialRetry
+
+from async_translate.abc import BaseProvider, Translation
+from async_translate.errors import TranslatorException, LanguageNotSupported
+from async_translate.providers.azure.errors import NoAPIKeys, AllKeysExhausted, RequestException
+
+MS_API_VER = "?api-version=3.0"
+retry_options = ExponentialRetry(attempts=10, start_timeout=8.0, factor=1.3, statuses={429})
+
+
+class Azure(BaseProvider):
+    """
+    Microsoft Azure Cognitive Services translator Backend
+    """
+    backend = "azure"
+    ms_endpoint = "https://api.cognitive.microsofttranslator.com/"
+    icon = "https://connectoricons-prod.azureedge.net/microsofttranslator/icon_1.0.1303.1871.png"
+
+    def __init__(self, api_keys: Union[str, Sequence[str]]):
+        if isinstance(api_keys, str):
+            self._api_keys: List[str] = [api_keys]
+        else:
+            if len(api_keys) < 1:
+                raise NoAPIKeys("No API keys provided")
+            self._api_keys: Sequence[str] = api_keys
+        self.api_iter = iter(self._api_keys)
+        self.current_key = next(self.api_iter)
+        self.session: Optional[RetryClient] = None
+
+    @property
+    def headers(self):
+        return {
+            'Ocp-Apim-Subscription-Key': self.current_key,
+            'Content-type': 'application/json',
+            'X-ClientTraceId': str(uuid.uuid4())
+        }
+
+    async def _request(self, endpoint, method='post', params: Optional[Dict] = None, json: Any = None,
+                       api_key_override: Optional[str] = None):
+        if params is None:
+            params = {}
+
+        headers = self.headers
+
+        if api_key_override:
+            headers['Ocp-Apim-Subscription-Key'] = api_key_override
+
+        if accept_language := params.pop('accept_language', None):
+            headers['Accept-Language'] = accept_language
+        url = self.ms_endpoint + endpoint + MS_API_VER + "&"
+        async with self.session.request(method=method, url=url, params=params, headers=headers, json=json) as resp:
+            try:
+                data = await resp.json(content_type=None)
+                try:
+                    if error := data.get('error'):
+                        # Handle Free Quota Errors
+                        if error['code'] in (401000, 403000, 403001, 429000, 429001, 429002):
+                            try:
+                                self.current_key = next(self.api_iter)
+                            except StopIteration:
+                                self.api_iter = iter(self._api_keys)
+                                self.current_key = next(self.api_iter)
+                                full_stop = True
+                            else:
+                                return await self._request(endpoint, method, params, json)
+                            if full_stop:
+                                raise AllKeysExhausted("All API Keys Exhausted", error)
+                        else:  # Handle Generic Errors
+                            raise RequestException(error, **error)
+                except AttributeError:
+                    pass
+                return data
+            except JSONDecodeError as e:
+                raise TranslatorException(await resp.text()) from e
+
+    async def get_languages(self, locale=None, *args, **kwargs) -> Dict[str, str]:
+        # Setup retry client
+        if not self.session:
+            self.session = RetryClient(retry_options=retry_options)
+
+        raw_languages = await self._request('languages', 'get',
+                                            {'scope': 'translation', 'accept_language': locale})
+        return {key: value['name'] for key, value in raw_languages['translation'].items()}
+
+    async def close(self):
+        # noinspection PyProtectedMember
+        if self.session._closed:
+            return
+        await self.session.close()
+
+    async def detect(self, content, key_override: Optional[str] = None) -> str:
+        """Detect the language of the given content"""
+        json_content = [{"text": content}]
+        result = await self._request('detect', json=json_content, api_key_override=key_override)
+        return result[0]['language']
+
+    async def translate(self, content: str, to: str, source="", **options) -> Translation:
+        """
+        Translates text using the Azure API
+        :param content: the text to translate
+        :param to: the language code to translate to
+        :param source: Optional language to translate from
+        :return: the translation
+        """
+        params = {
+            'to': to,
+            'profanityAction': ('NoAction', 'Marked', 'Marked')[options.get('profanity_filter') or 0]
+        }
+        if source:
+            params['from'] = source
+
+        if params['profanityAction'] == 1:
+            params['profanityMarker'] = 'Tag'
+
+        json_content = [{"text": content}]
+        try:
+            translation_response = (await self._request('translate', params=params, json=json_content))
+        except RequestException as te:
+            if te.code in (400004, 400036, 400018, 400035):  # Invalid/missing language
+                if te.code in (400018, 400035):  # Invalid 'from'
+                    language = source
+                    direction = "from"
+                else:  # Invalid 'to'
+                    language = to
+                    direction = "to"
+                raise LanguageNotSupported(language=language, direction=direction) from te
+            raise
+
+        entry = translation_response[0]
+        return Translation(
+            text=entry['translations'][0]['text'],
+            to=entry['translations'][0]['to'],
+            source=entry.get('detectedLanguage', {}).get('language')
+        )
