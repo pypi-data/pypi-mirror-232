f6598c71644de0c473a3e073247c150727299825
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-27 15:07:33 +0000

    Add package sensorfabric/2.0.1/sensorfabric-2.0.1.tar.gz

---
+++ packages/sensorfabric/sensorfabric-2.0.1.tar.gz/sensorfabric-2.0.1/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+long_description = 'Python library for UA Sensor Fabric'
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setup(
+    name='sensorfabric',
+    version='2.0.1',
+    description='Python library for UA Sensor Fabric',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license='MIT',
+    url='https://github.com/UArizonaCB2/sensorfabric-py.git',
+    author="Shravan Aras",
+    author_email='shravanaras@arizona.edu',
+    packages=['sensorfabric'],
+    keywords='sensors sensorfabric',
+    python_requires='>=3',
+    install_requires=[
+          'boto3',
+          'pandas',
+          'configparser',
+          'pathlib',
+          'numpy',
+          'requests',
+          'pyjwt==2.8.0'
+      ],
+)
