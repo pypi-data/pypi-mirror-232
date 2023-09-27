7e10ce03870dd4908176e9564a8cb4fca2a2382a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-27 14:07:46 +0000

    Add package kami-style/0.1/kami-style-0.1.tar.gz

---
+++ packages/kami-style/kami-style-0.1.tar.gz/kami-style-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='kami-style',
+    version='0.1',
+    description='Want make some style? This for you!',
+    author='_Kamikotto_',
+    author_email='kamikotto3@email.com',
+    packages=['kstyle'],
+)
