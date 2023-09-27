fa62369c373906468379e7b75dd58512a0c44e0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-27 05:05:44 +0000

    Add package kani/0.3.4/kani-0.3.4-py3-none-any.whl

---
+++ packages/kani/kani-0.3.4-py3-none-any.whl/kani/__init__.py
@@ -0,0 +1,5 @@
+from . import engines, exceptions, utils
+from .ai_function import AIFunction, AIParam, ai_function
+from .kani import Kani
+from .models import ChatMessage, ChatRole, FunctionCall
+from .utils.cli import chat_in_terminal, chat_in_terminal_async
