aa2ca6e661837316ffb5c979830e40e1ec0727a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-27 03:05:24 +0000

    Add package edspdf/0.8.1/edspdf-0.8.1-py3-none-any.whl

---
+++ packages/edspdf/edspdf-0.8.1-py3-none-any.whl/edspdf/__init__.py
@@ -0,0 +1,9 @@
+# ruff: noqa: F401
+from .trainable_pipe import TrainablePipe
+from .pipeline import Pipeline, load
+from .registry import registry
+from .structures import Box, Page, PDFDoc, Text, TextBox, TextProperties
+
+from . import utils  # isort:skip
+
+__version__ = "0.8.1"
