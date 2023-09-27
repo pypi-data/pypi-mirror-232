531b0c0bb0106bf4e4cf9142774e66afd33127a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-27 15:06:11 +0000

    Add package f5-openstack-lbaasv2-inventory/1.2.0/f5-openstack-lbaasv2-inventory-1.2.0.tar.gz

---
+++ packages/f5-openstack-lbaasv2-inventory/f5-openstack-lbaasv2-inventory-1.2.0.tar.gz/f5-openstack-lbaasv2-inventory-1.2.0/neutron_lbaas_inventory/common/constants.py
@@ -0,0 +1,6 @@
+LBAAS_EXTS = "LBAAS_EXTENSIONS"
+LBAAS_EXTS_PREFIX = "/lbaas-exts"
+
+# Device provisioning or operating status
+IDLE = "IDLE"
+ONLINE = "ONLINE"
---
+++ packages/f5-openstack-lbaasv2-inventory/f5-openstack-lbaasv2-inventory-1.2.0.tar.gz/f5-openstack-lbaasv2-inventory-1.2.0/neutron_lbaas_inventory/common/exceptions.py
@@ -0,0 +1,20 @@
+from neutron_lib import exceptions as n_exc
+from neutron._i18n import _
+
+
+class DeviceNotFound(n_exc.NotFound):
+    message = _("Device %(device_id)s could not be found")
+
+
+class DeviceInUse(n_exc.InUse):
+    message = _("Device %(device_id)s is currently used by "
+                "loadbalancers: %(loadbalancer_ids)s")
+
+
+class DeviceMemberNotFound(n_exc.NotFound):
+    message = _("Device member %(device_member_id)s could not be found")
+
+
+class DeviceMemberInUse(n_exc.InUse):
+    message = _("Device %(device_id)s is currently used by "
+                "device members: %(member_ids)s")
