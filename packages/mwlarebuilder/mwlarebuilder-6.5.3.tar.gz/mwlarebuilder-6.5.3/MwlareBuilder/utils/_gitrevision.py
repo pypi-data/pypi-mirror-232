7f5b5b5429eddad1d5e87373cba710fd9bce0f54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-27 04:06:37 +0000

    Add package mwlarebuilder/6.5.3/mwlarebuilder-6.5.3.tar.gz

---
+++ packages/mwlarebuilder/mwlarebuilder-6.5.3.tar.gz/mwlarebuilder-6.5.3/MwlareBuilder/utils/_gitrevision.py
@@ -0,0 +1,12 @@
+#
+# The content of this file will be filled in with meaningful data when creating an archive using `git archive` or by
+# downloading an archive from github, e.g., from github.com/.../archive/develop.zip
+#
+rev = "1e18e1483f"  # abbreviated commit hash
+commit = "1e18e1483f58107c2c7634f2eb6f7df65c2da5c0"  # commit hash
+date = "2023-09-24 16:05:08 +0000"  # commit date
+author = "pyup.io bot <github-bot@pyup.io>"
+ref_names = "HEAD -> develop"  # incl. current branch
+commit_message = """Tests: Requirements: Scheduled weekly dependency update for week 39 (#7953)
+
+"""
