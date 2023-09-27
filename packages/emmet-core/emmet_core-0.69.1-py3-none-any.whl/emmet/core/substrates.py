92f30047ab640c9beaa2886808137235e24ea4f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-27 14:10:49 +0000

    Add package emmet-core/0.69.1/emmet_core-0.69.1-py3-none-any.whl

---
+++ packages/emmet-core/emmet_core-0.69.1-py3-none-any.whl/emmet/core/substrates.py
@@ -0,0 +1,48 @@
+from pydantic import BaseModel, Field
+from typing import Optional
+
+
+class SubstratesDoc(BaseModel):
+    """
+    Possible growth substrates for a given material.
+    """
+
+    sub_form: Optional[str] = Field(
+        None,
+        description="Reduced formula of the substrate.",
+    )
+
+    sub_id: Optional[str] = Field(
+        None,
+        description="Materials Project ID of the substrate material. This comes in the form: mp-******.",
+    )
+
+    film_orient: Optional[str] = Field(
+        None,
+        description="Surface orientation of the film material.",
+    )
+
+    area: Optional[float] = Field(
+        None,
+        description="Minimum coincident interface area in Å².",
+    )
+
+    energy: Optional[float] = Field(
+        None,
+        description="Elastic energy in meV.",
+    )
+
+    film_id: Optional[str] = Field(
+        None,
+        description="The Materials Project ID of the film material. This comes in the form: mp-******.",
+    )
+
+    _norients: Optional[int] = Field(
+        None,
+        description="Number of possible surface orientations for the substrate.",
+    )
+
+    orient: Optional[str] = Field(
+        None,
+        description="Surface orientation of the substrate material.",
+    )
