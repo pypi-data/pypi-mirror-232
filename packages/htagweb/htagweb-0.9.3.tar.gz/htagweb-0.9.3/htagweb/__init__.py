bf26537ec0b19a3b90362ced64355387b7db7988
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-27 04:07:40 +0000

    Add package htagweb/0.9.3/htagweb-0.9.3.tar.gz

---
+++ packages/htagweb/htagweb-0.9.3.tar.gz/htagweb-0.9.3/htagweb/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# #############################################################################
+# Copyright (C) 2023 manatlan manatlan[at]gmail(dot)com
+#
+# MIT licence
+#
+# https://github.com/manatlan/htagweb
+# #############################################################################
+
+from .appserver import AppServer  # a completly different beast, but compatible with ^^
+from .htagserver import HtagServer  # a completly different beast.
+from .usot import Usot
+
+__all__= ["AppServer"]
+
+__version__ = "0.9.3" # auto updated
