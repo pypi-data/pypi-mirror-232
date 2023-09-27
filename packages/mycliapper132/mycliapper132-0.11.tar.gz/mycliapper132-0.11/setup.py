54990703bf643b7df5eb02498b703e7e899d7389
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-27 03:07:06 +0000

    Add package mycliapper132/0.11/mycliapper132-0.11.tar.gz

---
+++ packages/mycliapper132/mycliapper132-0.11.tar.gz/mycliapper132-0.11/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mycliapper132',
+    version='0.11',
+    packages=find_packages(),
+    install_requires=[
+        'click',
+        'databricks-sql-connector',
+        'requests',
+        'pytz',
+        'typing',
+        'datetime'
+    ],
+    entry_points={
+        'console_scripts': [
+            'mycliapp = mycliapp.main:hello',
+        ],
+    },
+)
