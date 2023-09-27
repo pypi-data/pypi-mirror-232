a53403124c2eaa997f63584b5210645063db795c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-27 05:06:11 +0000

    Add package vlcSim/0.0.10/vlcSim-0.0.10.tar.gz

---
+++ packages/vlcSim/vlcSim-0.0.10.tar.gz/vlcSim-0.0.10/README.md
@@ -0,0 +1,9 @@
+VLCSim is an Event-Oriented simulator package for Visible Light Communication.
+
+# Features
+
+- Dynamic Environment with in/out connections 
+- Flexible resource allocation algorithm 
+- Flexible VLC/room parameters
+
+
---
+++ packages/vlcSim/vlcSim-0.0.10.tar.gz/vlcSim-0.0.10/vlcsim/__init__.py
@@ -0,0 +1,8 @@
+from .vled import VLed
+from .receiver import Receiver
+from .scenario import Scenario
+from .connection import Connection
+from .event import Event
+from .rf import RF
+
+__all__ = ["VLed", "Receiver", "Scenario", "Connection", "Event", "Simulator"]
