1be1aeaaf7ac41db069a63e6cff76f37a324e72e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-27 03:06:21 +0000

    Add package ignf_gpf_sdk/0.1.15/ignf_gpf_sdk-0.1.15-py3-none-any.whl

---
+++ packages/ignf_gpf_sdk/ignf_gpf_sdk-0.1.15-py3-none-any.whl/ignf_gpf_sdk/store/StoredData.py
@@ -0,0 +1,20 @@
+from ignf_gpf_sdk.store.StoreEntity import StoreEntity
+from ignf_gpf_sdk.store.interface.TagInterface import TagInterface
+from ignf_gpf_sdk.store.interface.EventInterface import EventInterface
+from ignf_gpf_sdk.store.interface.CommentInterface import CommentInterface
+from ignf_gpf_sdk.store.interface.SharingInterface import SharingInterface
+from ignf_gpf_sdk.store.interface.PartialEditInterface import PartialEditInterface
+
+
+class StoredData(TagInterface, CommentInterface, SharingInterface, EventInterface, PartialEditInterface, StoreEntity):
+    """Classe Python représentant l'entité StoredData (donnée stockée)."""
+
+    _entity_name = "stored_data"
+    _entity_title = "donnée stockée"
+
+    STATUS_CREATED = "CREATED"
+    STATUS_GENERATING = "GENERATING"
+    STATUS_MODIFYING = "MODIFYING"
+    STATUS_GENERATED = "GENERATED"
+    STATUS_DELETED = "DELETED"
+    STATUS_UNSTABLE = "UNSTABLE"
