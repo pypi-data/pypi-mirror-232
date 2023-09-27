4cd2b3c52aee9ae469f0eef161e93264b4d3ab96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-27 14:07:53 +0000

    Add package tesp-support/1.3.0/tesp_support-1.3.0.tar.gz

---
+++ packages/tesp-support/tesp_support-1.3.0.tar.gz/tesp_support-1.3.0/tesp_support/matpower/__init__.py
@@ -0,0 +1,5 @@
+# Copyright (C) 2017-2022 Battelle Memorial Institute
+# file: __init__.py
+"""Support files for using MATPOWER - also see instructions on
+readthedocs for building the FNCS wrapper around the MATLAB run-time
+"""
