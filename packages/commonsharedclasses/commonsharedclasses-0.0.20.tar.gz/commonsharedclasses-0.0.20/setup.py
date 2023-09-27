8e57c770e829c3eb7dd86e665cb3957a4867813f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-27 03:06:29 +0000

    Add package commonsharedclasses/0.0.20/commonsharedclasses-0.0.20.tar.gz

---
+++ packages/commonsharedclasses/commonsharedclasses-0.0.20.tar.gz/commonsharedclasses-0.0.20/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='commonsharedclasses',
+    version='0.0.20',
+    description='This package will contain all common classes, version 0.0.20 updates:- Added funcURL and response logging in dynamiccosmosretrieve',
+    author='Ankit Kumar',
+    packages=['commonsharedclasses'],
+    zip_safe=False
+)
