f964c31194d73ffa9e765c5a0d52ecadd028fe1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-27 16:05:58 +0000

    Add package mygnuhealth/2.0.0/mygnuhealth-2.0.0-py3-none-any.whl

---
+++ packages/mygnuhealth/mygnuhealth-2.0.0-py3-none-any.whl/mygnuhealth/__init__.py
@@ -0,0 +1,7 @@
+####################################################################
+#   Copyright (C) 2020-2023 Luis Falcon <falcon@gnuhealth.org>
+#   Copyright (C) 2020-2023 GNU Solidario <health@gnusolidario.org>
+#   License: GPL v3+
+#   Please read the COPYRIGHT and LICENSE files of the package
+####################################################################
+__version__ = '2.0.0'
