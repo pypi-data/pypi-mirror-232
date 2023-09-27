f5db86d7579f2b29e374b09062169d0808bd29a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-27 05:05:39 +0000

    Add package pulp-file/1.14.5/pulp_file-1.14.5-py3-none-any.whl

---
+++ packages/pulp-file/pulp_file-1.14.5-py3-none-any.whl/pulp_file/tests/functional/api/from_pulpcore/test_content_path.py
@@ -0,0 +1,57 @@
+"""Tests related to content path."""
+import pytest
+import uuid
+
+from pulp_smash import utils
+from pulp_smash.pulp3.utils import gen_distribution
+from urllib.parse import urljoin
+
+from .constants import PULP_CONTENT_BASE_URL
+
+
+@pytest.mark.parallel
+def test_content_directory_listing(
+    file_distribution_api_client,
+    gen_object_with_cleanup,
+    tls_certificate_authority_cert,
+    x509_content_guards_api_client,
+    pulp_settings,
+):
+    """Checks that distributions are grouped by base-path when listing content directories."""
+
+    HIDE_GUARDED_DISTRIBUTIONS = getattr(pulp_settings, "HIDE_GUARDED_DISTRIBUTIONS", False)
+
+    content_guard1 = gen_object_with_cleanup(
+        x509_content_guards_api_client,
+        {"name": str(uuid.uuid4()), "ca_certificate": tls_certificate_authority_cert},
+    )
+
+    base_path = str(uuid.uuid4())
+    for path, content_guard in [
+        ("/foo1", None),
+        ("/foo2", content_guard1.pulp_href),
+        ("/boo1/foo1", None),
+        ("/boo2/foo1", content_guard1.pulp_href),
+    ]:
+        gen_object_with_cleanup(
+            file_distribution_api_client,
+            gen_distribution(base_path=base_path + path, content_guard=content_guard),
+        )
+
+    base_url = PULP_CONTENT_BASE_URL
+    if pulp_settings.DOMAIN_ENABLED:
+        base_url = urljoin(base_url, "default/")
+    response = utils.http_get(base_url).decode("utf-8")
+    assert response.count(f'a href="{base_path}/"') == 1
+    assert response.count('a href="../"') == 0
+
+    url = urljoin(base_url, base_path + "/")
+    response = utils.http_get(url).decode("utf-8")
+    assert response.count('a href="foo1/"') == 1
+    assert response.count('a href="foo2/"') == (0 if HIDE_GUARDED_DISTRIBUTIONS else 1)
+    assert response.count('a href="boo1/"') == 1
+    assert response.count('a href="boo2/"') == (0 if HIDE_GUARDED_DISTRIBUTIONS else 1)
+    assert response.count('a href="../"') == 1
+
+    response = utils.http_get(urljoin(url, "boo1/")).decode("utf-8")
+    assert response.count('a href="foo1/"') == 1
---
+++ packages/pulp-file/pulp_file-1.14.5-py3-none-any.whl/pulp_file/tests/functional/api/from_pulpcore/test_unlinking_repo.py
@@ -0,0 +1,34 @@
+"""Tests that perform action over remotes"""
+import pytest
+from pulp_smash.pulp3.utils import gen_repo
+
+
+@pytest.mark.parallel
+@pytest.mark.skip("Makes S3 runner get stuck, skipping for now")
+def test_shared_remote_usage(
+    file_repository_api_client,
+    file_content_api_client,
+    file_remote_ssl_factory,
+    basic_manifest_path,
+    gen_object_with_cleanup,
+    monitor_task,
+):
+    """Verify remotes can be used with different repos."""
+    remote = file_remote_ssl_factory(manifest_path=basic_manifest_path, policy="on_demand")
+
+    # Create and sync repos.
+    repos = []
+    for _ in range(2):
+        repo = gen_object_with_cleanup(file_repository_api_client, gen_repo())
+        monitor_task(
+            file_repository_api_client.sync(repo.pulp_href, {"remote": remote.pulp_href}).task
+        )
+        repos.append(file_repository_api_client.read(repo.pulp_href))
+
+    # Compare contents of repositories.
+    contents = set()
+    for repo in repos:
+        content = file_content_api_client.list(repository_version=repo.latest_version_href)
+        assert content.count == 3
+        contents.update({c.pulp_href for c in content.results})
+    assert len(contents) == 3
