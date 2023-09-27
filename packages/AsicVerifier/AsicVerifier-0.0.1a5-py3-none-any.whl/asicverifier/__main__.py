b700e032d5ffb51b629040089552d7b68f8a6f52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-27 15:06:24 +0000

    Add package AsicVerifier/0.0.1a5/AsicVerifier-0.0.1a5-py3-none-any.whl

---
+++ packages/AsicVerifier/AsicVerifier-0.0.1a5-py3-none-any.whl/asicverifier/__main__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+# This module is part of AsicVerifier and is released under
+# the AGPL-3.0-only License: https://opensource.org/license/agpl-v3/
+
+def run():
+    pass
+
+
+if __name__ == '__main__':
+    run()
