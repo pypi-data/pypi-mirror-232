95975960ac83d352f1869e667857742491dca359
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-27 04:05:58 +0000

    Add package suva-test/0.0.0/suva-test-0.0.0.tar.gz

---
+++ packages/suva-test/suva-test-0.0.0.tar.gz/suva-test-0.0.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    author="none",
+    author_email="none@none.com",
+    python_requires="==3.9.*",
+    classifiers=[
+        "Programming Language :: Python :: 3.9",
+        "Development Status :: 1 - Planning"
+    ],
+    packages=find_packages(include=[]),
+    description="placeholder repo",
+    include_package_data=True,
+    name="suva-test",
+    version="0.0.0",
+)
