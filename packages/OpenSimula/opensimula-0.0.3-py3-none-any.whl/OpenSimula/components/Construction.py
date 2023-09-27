93b64603e6ac7124a0219c30174bb0c8a5f631c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-27 03:06:17 +0000

    Add package OpenSimula/0.0.3/opensimula-0.0.3-py3-none-any.whl

---
+++ packages/OpenSimula/opensimula-0.0.3-py3-none-any.whl/OpenSimula/components/Construction.py
@@ -0,0 +1,14 @@
+from OpenSimula.Parameters import Parameter_component_list, Parameter_float_list
+from OpenSimula.Component import Component
+
+
+class Construction(Component):
+    def __init__(self, project):
+        Component.__init__(self, project)
+        self.parameter("type").value = "Construction"
+        self.parameter("name").value = "Construction_x"
+        self.parameter("description").value = "Construction using layers of material"
+        self.add_parameter(
+            Parameter_float_list("solar_absortivity", [0.8, 0.8], "frac", 0, 1)
+        )
+        self.add_parameter(Parameter_component_list("materials", ["not_defined"]))
