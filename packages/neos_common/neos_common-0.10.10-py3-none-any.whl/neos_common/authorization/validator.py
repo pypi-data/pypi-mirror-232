1489a28a18bd0d3e75c962ebe173c4132c7bd7a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-27 16:05:02 +0000

    Add package neos_common/0.10.10/neos_common-0.10.10-py3-none-any.whl

---
+++ packages/neos_common/neos_common-0.10.10-py3-none-any.whl/neos_common/authorization/validator.py
@@ -0,0 +1,127 @@
+import logging
+import typing
+import uuid
+
+import fastapi
+
+from neos_common import error, schema
+from neos_common.authorization import access, base, signer
+from neos_common.base import Action, ResourceLike
+from neos_common.client import IAMClient
+
+logger = logging.getLogger(__name__)
+
+
+async def validate(
+    action: typing.Union[Action, str],
+    resource: ResourceLike,
+    principals: typing.Union[schema.Principals, typing.List[str]],
+    statements: schema.Statements,
+    owner: typing.Union[str, None] = None,
+) -> bool:
+    """Validate access using PARC.
+
+    Validate access (authorize) by checking PARC (principals, actions, resources, conditions) and
+    effect (allow/deny) of endpoint requirements against statements stored in IAM.
+
+    Statements adhear to priorities.
+
+    User vs Group user type.
+    User is stronger.
+    +----------------+-----------------+-------------------+
+    | user statement | group statement | validation result |
+    +----------------+-----------------+-------------------+
+    | deny           | deny            | deny              |
+    | deny           | allow           | deny              |
+    | allow          | deny            | allow             |
+    | allow          | allow           | allow             |
+    +----------------+-----------------+-------------------+
+
+    Allow vs Deny effect.
+    Deny is stronger.
+
+    User type vs Effect.
+    User type is stronger.
+    """
+    action_str: str = action.value if isinstance(action, Action) else action
+
+    error_message = "The principal <{}> must have <{}> action for the resource <{}>.".format(
+        owner or principals,
+        action_str,
+        resource.urn,
+    )
+
+    def raise_on_empty(statements: schema.Statements) -> None:
+        if not statements.statements:
+            raise error.InsufficientPermissionsError(error_message)
+
+    filtered_statements = access.filter_by_principals(statements, principals)
+    raise_on_empty(filtered_statements)
+
+    filtered_statements = access.filter_by_action(filtered_statements, action)
+    raise_on_empty(filtered_statements)
+
+    filtered_statements = access.filter_by_resource(filtered_statements, resource)
+    raise_on_empty(filtered_statements)
+
+    ordered_priority_statements = access.order_by_priority_ascending(filtered_statements, principals)
+
+    if not ordered_priority_statements.statements[-1].is_allowed():
+        raise error.InsufficientPermissionsError(error_message)
+
+    return True
+
+
+class AccessValidator(base.AccessValidator):
+    def __init__(self, iam_client: IAMClient) -> None:
+        self._iam_client = iam_client
+
+    async def validate(
+        self,
+        user_id: uuid.UUID,
+        actions: typing.List[Action],
+        resources: typing.List[ResourceLike],
+        *,
+        return_allowed_resources: bool = False,
+    ) -> typing.Tuple[uuid.UUID, typing.List[str]]:
+        return await self._iam_client.validate_token(
+            principal=user_id,
+            actions=actions,
+            resources=[resource.urn for resource in resources],
+            return_allowed_resources=return_allowed_resources,
+        )
+
+
+class SignatureValidator(base.SignatureValidator):
+    def __init__(self, iam_client: IAMClient) -> None:
+        self._iam_client = iam_client
+
+    async def validate(
+        self,
+        request: fastapi.Request,
+        actions: typing.List[Action],
+        resources: typing.List[ResourceLike],
+        *,
+        return_allowed_resources: bool = False,
+    ) -> typing.Tuple[uuid.UUID, typing.List[str]]:
+        validator = signer.Validator()
+
+        payload = await request.body()
+
+        auth_type, access_key_id, scope, challenge, signature = validator.challenge_v4(
+            request.method,
+            request.url,
+            request.headers,
+            payload,
+        )
+
+        return await self._iam_client.validate_signature(
+            access_key_id,
+            auth_type.split("-")[0],
+            scope,
+            challenge,
+            signature,
+            actions,
+            [resource.urn for resource in resources],
+            return_allowed_resources=return_allowed_resources,
+        )
---
+++ packages/neos_common/neos_common-0.10.10-py3-none-any.whl/neos_common/client/iam_client.py
@@ -0,0 +1,169 @@
+import logging
+import typing
+from uuid import UUID
+
+import httpx
+
+from neos_common import error
+from neos_common.authorization.signer import KeyPair
+from neos_common.base import Action
+from neos_common.client.base import NeosClient
+
+logger = logging.getLogger(__name__)
+
+
+class IAMClient(NeosClient):
+    """Identity Access Manager client.
+
+    Access to the resources based on the access statements that stores in access manager.
+    """
+
+    @property
+    def service_name(self) -> str:
+        return "Identity Access Manager"
+
+    @property
+    def known_errors(self) -> typing.Set[str]:
+        return {"I002"}
+
+    def process_response_with_mapping(self, response: httpx.Response) -> dict:
+        try:
+            return self.process_response(response)
+        except (error.ServiceApiError, error.UnhandledServiceApiError) as exc:
+            if exc.code == "A001":
+                raise error.AuthorizationRequiredError(exc.debug_message) from exc
+            if exc.code == "A002":
+                raise error.InvalidAuthorizationError(exc.debug_message) from exc
+            if exc.code == "A100":
+                raise error.InsufficientPermissionsError(exc.debug_message) from exc
+            if exc.code == "A102":
+                raise error.InvalidResourceFormatError(exc.debug_message) from exc
+            if exc.code == "A103":
+                raise error.IdentityAccessManagerError(exc.debug_message) from exc
+            if exc.code == "S001":
+                raise error.SignatureError(exc.debug_message) from exc
+            raise
+
+    def __init__(
+        self,
+        host: str,
+        token: typing.Union[str, None],
+        key_pair: typing.Union[KeyPair, None],
+        account: str,
+        partition: str,
+    ) -> None:
+        assert token is not None or key_pair is not None
+
+        self._token = token
+        self._key_pair = key_pair
+
+        self._account = account
+        self._partition = partition
+
+        self._host = host
+        self._principals = None
+
+    @property
+    def token(self) -> typing.Union[str, None]:
+        return self._token
+
+    @property
+    def key_pair(self) -> typing.Union[KeyPair, None]:
+        return self._key_pair
+
+    @property
+    def validate_token_url(self) -> str:
+        return f"{self._host}/validate/token"
+
+    @property
+    def validate_signature_url(self) -> str:
+        return f"{self._host}/validate/signature"
+
+    @property
+    def publish_data_product_url(self) -> str:
+        return f"{self._host}/data_product/publish"
+
+    async def validate_token(
+        self,
+        principal: UUID,
+        actions: typing.List[Action],
+        resources: typing.List[str],
+        *,
+        return_allowed_resources: bool = False,
+    ) -> typing.Tuple[UUID, typing.List[str]]:
+        r = await self._get(
+            url=self.validate_token_url,
+            params={
+                "principal_id": principal,
+                "action": [action.value for action in actions],
+                "resource": resources,
+                "return_allowed_resources": return_allowed_resources,
+            },
+            headers={"X-Account": self._account, "X-Partition": self._partition},
+        )
+
+        data = self.process_response_with_mapping(r)
+        logger.info(data)
+        return data["principal_id"], data["resources"]
+
+    async def validate_signature(
+        self,
+        access_key: str,
+        auth_schema: str,
+        scope: str,
+        challenge: str,
+        signed_challenge: str,
+        actions: typing.List[Action],
+        resources: typing.List[str],
+        *,
+        return_allowed_resources: bool = False,
+    ) -> typing.Tuple[UUID, typing.List[str]]:
+        r = await self._get(
+            url=self.validate_signature_url,
+            params={
+                "access_key_id": access_key,
+                "auth_schema": auth_schema,
+                "scope": scope,
+                "challenge": challenge,
+                "signed_challenge": signed_challenge,
+                "action": [action.value for action in actions],
+                "resource": resources,
+                "return_allowed_resources": return_allowed_resources,
+            },
+            headers={"X-Account": self._account, "X-Partition": self._partition},
+        )
+
+        data = self.process_response_with_mapping(r)
+
+        return data["principal_id"], data["resources"]
+
+    async def publish_data_product(
+        self,
+        identifier: str,
+        urn: str,
+    ) -> str:
+        r = await self._post(
+            url=self.publish_data_product_url,
+            json={
+                "identifier": identifier,
+                "urn": urn,
+            },
+            headers={"X-Account": self._account, "X-Partition": self._partition},
+        )
+
+        data = self.process_response(r)
+        return data["identifier"]
+
+    async def unpublish_data_product(
+        self,
+        group_identifier: str,
+    ) -> None:
+        r = await self._delete(
+            url=self.publish_data_product_url,
+            json={
+                "group_identifier": group_identifier,
+            },
+            headers={"X-Account": self._account, "X-Partition": self._partition},
+        )
+
+        self.process_response(r)
