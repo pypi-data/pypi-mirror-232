8814ddde4c58e44a81522a5663fd5bebb54549bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-27 03:05:16 +0000

    Add package neap/0.1.9/neap-0.1.9.tar.gz

---
+++ packages/neap/neap-0.1.9.tar.gz/neap-0.1.9/neap/report_card.py
@@ -0,0 +1,90 @@
+import requests
+
+class ReportCard:
+    def __init__(self, api_key=None):
+        self.api_key = api_key
+
+    def get_adhoc_data(self, assessment_year, subject, grade, subscale):
+        url = "https://api.neap.gov/api/GetAdhocData"
+        params = {
+            "assessmentYear": assessment_year,
+            "subject": subject,
+            "grade": grade,
+            "subscale": subscale,
+        }
+
+        if self.api_key:
+            headers = {
+                "Authorization": f"Bearer {self.api_key}",
+            }
+        else:
+            headers=None
+
+        response = requests.get(url, params=params, headers=headers)
+        response.raise_for_status()
+
+        return response.json()
+
+    def get_overall(self, assessment_year, subject, grade):
+        url = "https://api.neap.gov/api/GetOverall"
+        params = {
+            "assessmentYear": assessment_year,
+            "subject": subject,
+            "grade": grade,
+        }
+        if self.api_key: 
+            headers = {
+                "Authorization": f"Bearer {self.api_key}",
+            }
+        else:
+            headers=None
+
+        response = requests.get(url, params=params, headers=headers)
+        response.raise_for_status()
+
+        return response.json()
+
+    def get_trends(self, subject, grade, subscales=None):
+        url = "https://api.neap.gov/api/GetTrends"
+        params = {
+            "subject": subject,
+            "grade": grade,
+        }
+        if subscales:
+            params["subscales"] = ",".join(subscales)
+        if self.api_key:
+            headers = {
+                "Authorization": f"Bearer {self.api_key}",
+            }
+        else:
+            headers=None
+
+        response = requests.get(url, params=params, headers=headers)
+        response.raise_for_status()
+
+        return response.json()
+
+    def get_gaps(self, subject, grade, gap_type, groups=None, jurisdictions=None, assessment_years=None):
+        url = "https://api.neap.gov/api/GetGaps"
+        params = {
+            "subject": subject,
+            "grade": grade,
+            "gapType": gap_type,
+        }
+        if groups:
+            params["groups"] = ",".join(groups)
+        if jurisdictions:
+            params["jurisdictions"] = ",".join(jurisdictions)
+        if assessment_years:
+            params["assessmentYears"] = ",".join(assessment_years)
+        if self.api_key:
+            headers = {
+                "Authorization": f"Bearer {self.api_key}",
+            }
+        else:
+            headers=None
+
+        response = requests.get(url, params=params, headers=headers)
+        response.raise_for_status()
+
+        return response.json()
