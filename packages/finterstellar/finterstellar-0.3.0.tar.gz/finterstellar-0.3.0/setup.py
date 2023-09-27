d5f181cdb185303c07989c01a893c36318b066aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-27 04:05:40 +0000

    Add package finterstellar/0.3.0/finterstellar-0.3.0.tar.gz

---
+++ packages/finterstellar/finterstellar-0.3.0.tar.gz/finterstellar-0.3.0/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="finterstellar",
+    version="0.3.0",
+    author="finterstellar",
+    author_email="finterstellar@naver.com",
+    description="Quantitative analysis tools for investment",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/finterstellar/library",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'lxml>=4.2.6',
+        'numpy>=1.19.3',
+        'pandas>=1.1.4',
+        'requests>=2.23.0',
+    ],
+    python_requires='>=3.5',
+)
