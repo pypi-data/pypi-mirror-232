72750afb561ea8f621b36f850670d30cffed8a3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-27 14:10:17 +0000

    Add package pyppe/0.0.1/pyppe-0.0.1.tar.gz

---
+++ packages/pyppe/pyppe-0.0.1.tar.gz/pyppe-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='pyppe',
+    version='0.0.1',
+    packages=['pyppe'],
+    url='https://github.com/RickySal/Pyppe',
+    license='GNU General Public License v3.0',
+    author='RickySal',
+    author_email='41027013+RickySal@users.noreply.github.com',
+    description='A modular data pipeline for Python'
+)
