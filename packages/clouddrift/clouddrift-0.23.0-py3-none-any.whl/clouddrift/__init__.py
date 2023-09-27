01fa31b16e0deba2d12c0429c429818e8a3bccd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-27 14:07:55 +0000

    Add package clouddrift/0.23.0/clouddrift-0.23.0-py3-none-any.whl

---
+++ packages/clouddrift/clouddrift-0.23.0-py3-none-any.whl/clouddrift/__init__.py
@@ -0,0 +1,11 @@
+from ._version import version
+
+__version__ = version
+
+from clouddrift.raggedarray import RaggedArray
+import clouddrift.adapters
+import clouddrift.analysis
+import clouddrift.datasets
+import clouddrift.sphere
+import clouddrift.signal
+import clouddrift.wavelet
