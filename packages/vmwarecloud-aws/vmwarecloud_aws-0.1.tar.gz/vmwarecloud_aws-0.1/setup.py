3fd0db59ccd962a6b3d2351612c7586e54de64b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-27 16:05:05 +0000

    Add package vmwarecloud-aws/0.1/vmwarecloud_aws-0.1.tar.gz

---
+++ packages/vmwarecloud-aws/vmwarecloud_aws-0.1.tar.gz/vmwarecloud_aws-0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="vmwarecloud_aws",
+    version="0.1",
+    author="VMware, Inc.",
+    description="Client bindings for VMware Cloud on AWS",
+    packages=find_packages(),
+    install_requires=[
+        # List your package dependencies here
+    ],
+)
+
