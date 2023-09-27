b0295b8f13bd8cc996faa0c9a73ad962ccc8f3d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-27 15:06:10 +0000

    Add package kuto/0.0.66/kuto-0.0.66-py3-none-any.whl

---
+++ packages/kuto/kuto-0.0.66-py3-none-any.whl/demo/page/win_page.py
@@ -0,0 +1,13 @@
+"""
+@Author: kang.yang
+@Date: 2023/9/20 10:53
+"""
+import kuto
+from kuto.win import Elem
+
+
+class WinPage(kuto.Page):
+    num_3 = Elem(image='../static/calculator_3_win.png', desc="数字3")
+    x = Elem(image='../static/calculator_x_win.png', desc="乘以号")
+    num_5 = Elem(image='../static/calculator_5_win.png', desc="数字5")
+    equal = Elem(image='../static/calculator_=_win.png', desc="等于")
---
+++ packages/kuto/kuto-0.0.66-py3-none-any.whl/kuto/win/__init__.py
@@ -0,0 +1,7 @@
+"""
+@Author: kang.yang
+@Date: 2023/9/20 10:37
+"""
+from kuto.win.case import Case
+from kuto.win.driver import Driver
+from kuto.win.element import Elem
