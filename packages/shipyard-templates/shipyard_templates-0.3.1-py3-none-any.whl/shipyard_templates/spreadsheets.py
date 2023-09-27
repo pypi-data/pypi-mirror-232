fd27dc299e36f656466f465566b47f722aebefc6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-27 05:06:00 +0000

    Add package shipyard-templates/0.3.1/shipyard_templates-0.3.1-py3-none-any.whl

---
+++ packages/shipyard-templates/shipyard_templates-0.3.1-py3-none-any.whl/shipyard_templates/spreadsheets.py
@@ -0,0 +1,12 @@
+from abc import ABC, abstractmethod
+from .shipyard_logger import ShipyardLogger
+
+
+class Spreadsheets(ABC):
+    def __init__(self, **kwargs) -> None:
+        self.logger = ShipyardLogger().logger
+        super().__init__()
+
+    @abstractmethod
+    def connect(self):
+        pass
