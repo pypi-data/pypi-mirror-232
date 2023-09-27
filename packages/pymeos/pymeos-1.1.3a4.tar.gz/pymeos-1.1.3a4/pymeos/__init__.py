5129b2942d7ba2377ea3f8f0426f920a15265ce7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-27 03:05:58 +0000

    Add package pymeos/1.1.3a4/pymeos-1.1.3a4.tar.gz

---
+++ packages/pymeos/pymeos-1.1.3a4.tar.gz/pymeos-1.1.3a4/pymeos/__init__.py
@@ -0,0 +1,39 @@
+from .aggregators import *
+from .boxes import *
+from .main import *
+from .meos_init import *
+from .temporal import *
+from .collections import Period, PeriodSet, TimestampSet, Time, TextSet
+
+__version__ = '1.1.3a1'
+__all__ = [
+    # initialization
+    'pymeos_initialize', 'pymeos_finalize',
+    # boxes
+    'Box', 'TBox', 'STBox',
+    # main
+    'TBool', 'TBoolInst', 'TBoolSeq', 'TBoolSeqSet',
+    'TInt', 'TIntInst', 'TIntSeq', 'TIntSeqSet',
+    'TFloat', 'TFloatInst', 'TFloatSeq', 'TFloatSeqSet',
+    'TText', 'TTextInst', 'TTextSeq', 'TTextSeqSet',
+    'TPointInst', 'TPointSeq', 'TPointSeqSet',
+    'TGeomPoint', 'TGeomPointInst', 'TGeomPointSeq', 'TGeomPointSeqSet',
+    'TGeogPoint', 'TGeogPointInst', 'TGeogPointSeq', 'TGeogPointSeqSet',
+    # temporal
+    'Temporal', 'TInstant', 'TSequence', 'TSequenceSet',
+    # Collections
+    'Time', 'Period', 'TimestampSet', 'PeriodSet',
+    'TextSet',
+    # extras
+    'TInterpolation',
+    # aggregators
+    'TemporalInstantCountAggregator', 'TemporalPeriodCountAggregator', 'TemporalExtentAggregator',
+    'TemporalAndAggregator', 'TemporalOrAggregator',
+    'TemporalAverageAggregator',
+    'TemporalNumberExtentAggregator',
+    'TemporalIntMaxAggregator', 'TemporalIntMinAggregator', 'TemporalIntSumAggregator',
+    'TemporalFloatMaxAggregator', 'TemporalFloatMinAggregator', 'TemporalFloatSumAggregator',
+    'TemporalTextMaxAggregator', 'TemporalTextMinAggregator',
+    'TemporalPointExtentAggregator',
+    'TimeInstantaneousUnionAggregator', 'TimeContinuousUnionAggregator',
+]
---
+++ packages/pymeos/pymeos-1.1.3a4.tar.gz/pymeos-1.1.3a4/pymeos/collections/__init__.py
@@ -0,0 +1,10 @@
+from .base import *
+from .time import *
+from .text import *
+
+__all__ = [
+    'Set', 'Span', 'SpanSet',
+    'Time', 'TimestampSet', 'Period', 'PeriodSet', 'datetime', 'timedelta',
+    'TextSet',
+
+]
