1a74a0cd1eec6b40423ddde867b23879280a513d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-27 16:05:03 +0000

    Add package qprogress/1.0.1/qprogress-1.0.1.tar.gz

---
+++ packages/qprogress/qprogress-1.0.1.tar.gz/qprogress-1.0.1/qprogress/formatters.py
@@ -0,0 +1,5 @@
+def default_format_func(current_index: int, total_count: int) -> None:
+    percent = (current_index / total_count) * 100
+    fill_count = int(percent / 2)
+    progress_bar = f"{'=' * fill_count}{' ' * (50 - fill_count)}"
+    print(f"\rProcessing [{current_index + 1} / {total_count}] [{progress_bar}] {percent:.1f}%", end="")
