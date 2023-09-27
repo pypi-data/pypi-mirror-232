e632cc1e22fcd7dbb647f2243c62264933800833
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-27 03:05:17 +0000

    Add package csnlp/1.5.8rc3/csnlp-1.5.8rc3.tar.gz

---
+++ packages/csnlp/csnlp-1.5.8rc3.tar.gz/csnlp-1.5.8rc3/src/csnlp/__init__.py
@@ -0,0 +1,6 @@
+__all__ = ["Nlp", "Solution", "multistart", "scaling"]
+
+import csnlp.core.scaling as scaling
+import csnlp.multistart as multistart
+from csnlp.core.solutions import Solution
+from csnlp.nlps.nlp import Nlp
