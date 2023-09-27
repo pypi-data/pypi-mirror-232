57d68d67d7c39b2f29e68787b8ad07a9a8fd7a80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-27 03:07:05 +0000

    Add package randomizer-ml/0.11.3/randomizer_ml-0.11.3.tar.gz

---
+++ packages/randomizer-ml/randomizer_ml-0.11.3.tar.gz/randomizer_ml-0.11.3/README.md
@@ -0,0 +1,9 @@
+# Randomizer ML
+
+This library allows you to do randomizer-ML
+
+You can see a talk about this library 
+
+* [here](https://www.youtube.com/watch?v=AHQaQrii1vM&t=439s&ab_channel=DataScienceConnect)
+
+[![Documentation Status](https://readthedocs.org/projects/honest-ml/badge/?version=latest)](https://honest-ml.readthedocs.io/en/latest/?badge=latest)
---
+++ packages/randomizer-ml/randomizer_ml-0.11.3.tar.gz/randomizer_ml-0.11.3/setup.py
@@ -0,0 +1,40 @@
+import pathlib
+from setuptools import setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="randomizer_ml",
+    version="0.11.3",
+    description="Training for models conforming to the scikit-learn api",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/EricSchles/randomizer_ml",
+    author="Eric Schles",
+    author_email="ericschles@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10"
+    ],
+    packages=[
+        "randomizer_ml",
+        'randomizer_ml.trainer',
+        'randomizer_ml.visualizer',
+        'randomizer_ml.experiment',
+        'randomizer_ml.utils'
+    ],
+    include_package_data=True,
+    install_requires=["scikit-learn", "scipy", "numpy",
+                      "statsmodels", "mlxtend", "pytest", "seaborn"],
+)
