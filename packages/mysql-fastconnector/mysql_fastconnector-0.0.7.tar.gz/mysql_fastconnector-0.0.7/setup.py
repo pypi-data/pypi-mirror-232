c91bc4c54cdb31fe1a4e532f1adbc7b372731e16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-27 03:05:32 +0000

    Add package mysql-fastconnector/0.0.7/mysql_fastconnector-0.0.7.tar.gz

---
+++ packages/mysql-fastconnector/mysql_fastconnector-0.0.7.tar.gz/mysql_fastconnector-0.0.7/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+with open("README.md", "r", encoding = "utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name = "mysql_fastconnector",
+    version = "0.0.7",
+    author = "ilayaraja",
+    author_email = "ilayaraja.python@gmail.com",
+    description = "it helps to connect mysql and do CURD action easily ",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    url = "https://github.com/ilayaraja1984/mysqleasyquery",
+    project_urls = {
+        "Bug Tracker": "https://github.com/ilayaraja1984/mysqleasyquery/issues",
+    },
+    install_requires=[
+        'mysql-connector-python==8.1.0',
+        'mysqlclient==2.0.3',
+    ],
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir = {"": "src"},
+    packages = setuptools.find_packages(where="src"),
+    python_requires = ">=3.6")
+
+
+    
