76c80c6b7603ba9552a6936e58ed618e19ba3a74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-27 03:06:18 +0000

    Add package devchat/0.2.7/devchat-0.2.7.tar.gz

---
+++ packages/devchat/devchat-0.2.7.tar.gz/devchat-0.2.7/devchat/_cli/__init__.py
@@ -0,0 +1,12 @@
+from .optimize import *
+from .log import log
+from .prompt import prompt
+from .run import run
+from .topic import topic
+
+__all__ = [
+    'log',
+    'prompt',
+    'run',
+    'topic'
+]
