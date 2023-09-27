cf8cf12892f93241eec8cfaae810fe8ebc525100
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-27 05:06:18 +0000

    Add package blandai/0.1.1/blandai-0.1.1.tar.gz

---
+++ packages/blandai/blandai-0.1.1.tar.gz/blandai-0.1.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='blandai',
+    version='0.1.1',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+    ],
+    author='LanKabelGezogen',
+    author_email='cybertom1234@gmail.com',
+    description='A Python wrapper for the Bland.ai API',
+    license='MIT', 
+    keywords='blandai API wrapper',
+    url='https://github.com/lankabelgezogen/blandailibrary'
+)
