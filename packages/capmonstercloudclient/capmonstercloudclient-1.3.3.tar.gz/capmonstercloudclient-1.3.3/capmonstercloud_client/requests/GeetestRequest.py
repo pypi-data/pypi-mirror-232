6779562e31b0e7a840cb360732d68b811724dcab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-28 04:06:58 +0000

    Add package capmonstercloudclient/1.3.3/capmonstercloudclient-1.3.3.tar.gz

---
+++ packages/capmonstercloudclient/capmonstercloudclient-1.3.3.tar.gz/capmonstercloudclient-1.3.3/capmonstercloud_client/requests/GeetestRequest.py
@@ -0,0 +1,39 @@
+from pydantic import Field
+from typing import Dict, Union
+
+from .GeetestRequestBase import GeetestRequestBase
+from .proxy_info import ProxyInfo
+
+class GeetestRequest(GeetestRequestBase, ProxyInfo):
+    type: str = Field(default='GeeTestTask')
+
+    def getTaskDict(self) -> Dict[str, Union[str, int, bool]]:
+        task = {}
+        task['type'] = self.type
+        task['websiteURL'] = self.websiteUrl
+        task['gt'] = self.gt
+        task['version'] = self.version
+        
+        if self.version == 3:
+            if self.challenge is None:
+                raise ValueError(f'Challenge value is required for 3 version Geetest.')
+            task['challenge'] = self.challenge
+        
+        if self.version == 4:
+            if self.initParameters is not None:
+                task['initParameters'] = self.initParameters
+                
+        if self.geetestApiServerSubdomain is not None:
+            task['geetestApiServerSubdomain'] = self.geetestApiServerSubdomain
+        if self.geetestGetLib is not None:
+            task['geetestGetLib'] = self.geetestGetLib
+        if self.user_agent is not None:
+            task['userAgent'] = self.user_agent
+        
+        
+        task['proxyType'] = self.proxy_type
+        task['proxyAddress'] = self.proxy_address
+        task['proxyPort'] = self.proxy_port
+        task['proxyLogin'] = self.proxy_login
+        task['proxyPassword'] = self.proxy_password
+        return task
