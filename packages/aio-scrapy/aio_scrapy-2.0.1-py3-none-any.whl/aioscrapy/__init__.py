8444cd319f46197ca617454ad9fb9b5938362bbe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-28 03:07:27 +0000

    Add package aio-scrapy/2.0.1/aio_scrapy-2.0.1-py3-none-any.whl

---
+++ packages/aio-scrapy/aio_scrapy-2.0.1-py3-none-any.whl/aioscrapy/__init__.py
@@ -0,0 +1,32 @@
+"""
+aioscrapy - a web crawling and web scraping framework written for Python
+"""
+
+import pkgutil
+import sys
+
+# Declare top-level shortcuts
+from aioscrapy.spiders import Spider
+from aioscrapy.http import Request, FormRequest
+from aioscrapy.settings import Settings
+from aioscrapy.crawler import Crawler
+
+
+__all__ = [
+    '__version__', 'version_info', 'Spider', 'Request', 'FormRequest', 'Crawler', 'Settings'
+]
+
+
+# aioscrapy versions
+__version__ = (pkgutil.get_data(__package__, "VERSION") or b"").decode("ascii").strip()
+version_info = tuple(int(v) if v.isdigit() else v for v in __version__.split('.'))
+
+
+# Check minimum required Python version
+if sys.version_info < (3, 9):
+    print("aioscrapy %s requires Python 3.9+" % __version__)
+    sys.exit(1)
+
+
+del pkgutil
+del sys
