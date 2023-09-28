fbcf6a8440b93c86f6f3e0d646007ceb2e1cfb3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-28 03:07:29 +0000

    Add package mstrio-py/11.3.11.101/mstrio_py-11.3.11.101.tar.gz

---
+++ packages/mstrio-py/mstrio_py-11.3.11.101.tar.gz/mstrio_py-11.3.11.101/mstrio/api/exceptions.py
@@ -0,0 +1,17 @@
+from mstrio.helpers import (  # NOQA
+    IServerException,
+    MstrException,
+    MstrTimeoutError,
+    PartialSuccess,
+    PromptedContentError,
+    Success,
+    VersionException,
+)
+from mstrio.utils.helper import deprecation_warning
+
+deprecation_warning(
+    deprecated=("mstrio.api.exceptions"),
+    new=("mstrio.helpers"),
+    version='11.3.11.103',  # NOSONAR
+    module=True,
+)
