cbb6d4734768c9225e2fdf47463e5a89871e32d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-28 04:08:38 +0000

    Add package nepattern/0.6.4/nepattern-0.6.4-py3-none-any.whl

---
+++ packages/nepattern/nepattern-0.6.4-py3-none-any.whl/nepattern/util.py
@@ -0,0 +1,34 @@
+from __future__ import annotations
+
+import dataclasses
+from pathlib import Path
+import sre_compile
+import sys
+from typing import TYPE_CHECKING, List, Pattern, Union
+
+from tarina.lang import lang
+
+if sys.version_info >= (3, 9):  # pragma: no cover
+    from types import GenericAlias as CGenericAlias  # noqa
+else:
+    CGenericAlias: type = type(List[int])  # noqa
+
+if sys.version_info >= (3, 10):   # pragma: no cover
+    from types import UnionType as CUnionType  # noqa
+else:
+    CUnionType: type = type(Union[int, str])  # noqa
+
+if TYPE_CHECKING:
+    TPattern = Pattern[str]
+else:
+    TPattern: type[Pattern[str]] = type(sre_compile.compile("", 0))
+GenericAlias: type = type(List[int])
+UnionType: type = type(Union[int, str])
+
+
+@dataclasses.dataclass
+class RawStr:
+    value: str
+
+
+lang.load(Path(__file__).parent / "i18n")
