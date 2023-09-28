3cd129720084060f9fe7c7cabd5bbe8ed23e1d5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-28 14:07:32 +0000

    Add package vecs/0.4.1/vecs-0.4.1.tar.gz

---
+++ packages/vecs/vecs-0.4.1.tar.gz/vecs-0.4.1/src/vecs/__init__.py
@@ -0,0 +1,14 @@
+from vecs import exc
+from vecs.client import Client
+from vecs.collection import Collection, IndexMeasure, IndexMethod
+
+__project__ = "vecs"
+__version__ = "0.4.1"
+
+
+__all__ = ["IndexMethod", "IndexMeasure", "Collection", "Client", "exc"]
+
+
+def create_client(connection_string: str) -> Client:
+    """Creates a client from a Postgres connection string"""
+    return Client(connection_string)
