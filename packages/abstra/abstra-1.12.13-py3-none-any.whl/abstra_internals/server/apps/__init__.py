ab3c896df6864281aa3ef8b93d4d3027a061ec47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-28 14:08:33 +0000

    Add package abstra/1.12.13/abstra-1.12.13-py3-none-any.whl

---
+++ packages/abstra/abstra-1.12.13-py3-none-any.whl/abstra_internals/server/apps/__init__.py
@@ -0,0 +1,47 @@
+import os, flask, flask_cors, threading, webbrowser
+
+from ..fs_watcher import watch_py_root_files
+from ..overloads import overloads
+from .editor import get_editor_bp
+from .player import get_player_bp
+from ..api import API
+
+
+HOST = os.getenv("ABSTRA_HOST", "localhost")
+
+
+def create_app(api: API) -> flask.Flask:
+    app = flask.Flask(__name__)
+    flask_cors.CORS(app)
+
+    editor = get_editor_bp(api)
+    app.register_blueprint(editor, url_prefix="/_editor")
+
+    player = get_player_bp(api)
+    app.register_blueprint(player)
+
+    return app
+
+
+def serve_local(port: int, debug, use_reloader, load_dotenv):
+    os.environ["ABSTRA_SERVER"] = "true"
+    overloads()
+
+    api = API()
+    credential = api.get_credentials()
+    if credential:
+        os.environ["ABSTRA_API_TOKEN"] = credential
+
+    # TODO: use flask reloader
+    if use_reloader:
+        watch_py_root_files()
+
+    app = create_app(api)
+    threading.Timer(1, lambda: webbrowser.open(f"http://{HOST}:{port}/_editor")).start()
+    app.run(
+        host=HOST,
+        port=port,
+        debug=debug,
+        use_reloader=False,
+        load_dotenv=load_dotenv,
+    )
