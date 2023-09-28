bb63e68df4e1003fda7df813a23cc4e681ce4040
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-28 05:06:24 +0000

    Add package cosmicfrog/0.3.15/cosmicfrog-0.3.15.tar.gz

---
+++ packages/cosmicfrog/cosmicfrog-0.3.15.tar.gz/cosmicfrog-0.3.15/cosmicfrog/suppress_stdio.py
@@ -0,0 +1,10 @@
+import sys
+import os
+
+class Suppress_stdio:
+    def __enter__(self):
+        self._original_stdout = sys.stdout
+        sys.stdout = open(os.devnull, 'w')
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        sys.stdout = self._original_stdout
