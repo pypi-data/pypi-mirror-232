726683a1353f019da4d215e87892ec5f2c1d9047
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-28 04:07:43 +0000

    Add package ssmdevices/0.19/ssmdevices-0.19.tar.gz

---
+++ packages/ssmdevices/ssmdevices-0.19.tar.gz/ssmdevices-0.19/ssmdevices/instruments/switches.pyi
@@ -0,0 +1,8 @@
+class MiniCircuitsUSBSwitch(SwitchAttenuatorBase):
+    def __init__(
+        self,
+        resource: str = "NoneType",
+        usb_path: str = "NoneType",
+        timeout: str = "int",
+    ): ...
+    ...
