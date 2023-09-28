31c1fdbf6a6a61e280f54590430d40c36d775a3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-28 03:10:02 +0000

    Add package wizata-dsapi/0.3.54/wizata_dsapi-0.3.54-py3-none-any.whl

---
+++ packages/wizata-dsapi/wizata_dsapi-0.3.54-py3-none-any.whl/wizata_dsapi/api_interface.py
@@ -0,0 +1,44 @@
+import uuid
+from .solution_component import SolutionComponent
+
+
+class ApiInterface:
+    """
+    Interface with all methods available both to the pipeline runners and the Data Science API.
+    """
+
+    def get_components(self,
+                       label_id: uuid.UUID = None,
+                       twin_id: uuid.UUID = None,
+                       template_id: uuid.UUID = None,
+                       owner_id: uuid.UUID = None,
+                       organization_only: bool = False,
+                       name: str = None):
+        """
+        get components
+        :param label_id: filter on a specific label
+        :param template_id: filter on a specific template
+        :param twin_id: filter on a specific twin
+        :param owner_id: filter on a specific owner_id
+        :param organization_only: work only with organization components (by default - False)
+        :param name: filter on a specific name (contains)
+        """
+        pass
+
+    def create_component(self, component: SolutionComponent):
+        """
+        create a component based on its ID.
+        """
+        pass
+
+    def update_component(self, component: SolutionComponent):
+        """
+        update a component based on its ID.
+        """
+        pass
+
+    def delete_component(self, component_id: uuid.UUID):
+        """
+        delete component
+        """
+        pass
