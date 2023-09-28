38c7df12374a6ec3c7c36c56e55c9ddbbd851581
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-28 14:07:00 +0000

    Add package Proxer/1.2/Proxer-1.2.tar.gz

---
+++ packages/Proxer/Proxer-1.2.tar.gz/Proxer-1.2/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Proxer',
+    version='1.2',
+    packages=find_packages(),
+    install_requires=[
+        "beautifulsoup4==4.12.2",
+        "colorama==0.4.6",
+        "requests",
+    ],
+    author='Shypilo Oleksandr',
+    author_email='ssshipilo@gmail.com',
+    description='Proxer - get a free working proxy simply and free of charge',
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url='https://github.com/ssshipilo/Proxer',
+    project_urls={
+        "Documentation": "https://github.com/ssshipilo/Proxer/blob/main/git/documentation.md",
+        "Source Code": "https://github.com/ssshipilo/Proxer",
+    },
+)
