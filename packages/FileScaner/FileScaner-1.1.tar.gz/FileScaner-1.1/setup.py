dad72b9845ce117cd902cbda1ab0d2ac3e14e51b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-28 15:09:48 +0000

    Add package FileScaner/1.1/FileScaner-1.1.tar.gz

---
+++ packages/FileScaner/FileScaner-1.1.tar.gz/FileScaner-1.1/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.1'
+DESCRIPTION = 'Scan files based on file type'
+LONG_DESCRIPTION = 'Will scan a folder for types of files based on an array and return a pandas dataframe with file info'
+
+# Setting up
+setup(
+    # the name must match the folder name 'verysimplemodule'
+    name="FileScaner",
+    version=1.001,
+    author="Ryan Sanders",
+    author_email="rasanders22@gmail.com",
+    description="Scan files based on file type",
+    requires_python=">=3.9",
+    long_description="Will scan a folder for types of files based on an array and return a pandas dataframe with file info",
+    packages=find_packages(),
+    install_requires=["pandas","pathlib"],  # add any additional packages that
+    # needs to be installed along with your package. Eg: 'caer'
+    keywords=['python', 'scans files'],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Education",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
