ed225cc97e23c7076883cfb3c7249d0a9649af6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-28 14:07:01 +0000

    Add package as3lib/0.0.6/as3lib-0.0.6.tar.gz

---
+++ packages/as3lib/as3lib-0.0.6.tar.gz/as3lib-0.0.6/as3lib/configmodule.py
@@ -0,0 +1,27 @@
+platform = "" #Windows, Linux, or Darwin
+windowmanagertype = "" #linux only, x11 or wayland
+wlcompositor = "" #wayland only, compositor name to identify which commands to use
+moduledirectory = ""
+
+objects = {} #list of objects (testing, might not be here later)
+
+#toplevel
+as3DebugEnable = False
+ErrorReportingEnable = 0
+MaxWarnings = 100
+TraceOutputFileEnable = 0
+TraceOutputFileName = ""
+CurrentWarnings = 0
+MaxWarningsReached = False
+ClearLogsOnStartup = 1
+defaultTraceFilePath = ""
+defaultTraceFilePath_Flash = ""
+
+#display
+width = ""
+height = ""
+refreshrate = ""
+colordepth = ""
+
+#initcheck
+initdone = False
---
+++ packages/as3lib/as3lib-0.0.6.tar.gz/as3lib-0.0.6/as3lib/flash/utils.py
@@ -0,0 +1,41 @@
+def clearInterval():
+   pass
+def clearTimeout():
+   pass
+def describeType():
+   pass
+def escapeMultiByte():
+   pass
+def getDefinitionByName():
+   pass
+def getQualifiedClassName():
+   pass
+def getQualifiedSuperclassName():
+   pass
+def getTimer():
+   pass
+def setInterval():
+   pass
+def setTimeout():
+   pass
+def unescapeMultiByte():
+   pass
+
+class IDataInput:
+   pass
+class IDataOutput:
+   pass
+
+class ByteArray:
+   pass
+class CompressionAlgorithm:
+   DEFLATE = "deflate"
+   LZMA = "lzma"
+   ZLIB = "zlib"
+class Dictionary:
+   pass
+class Endian:
+   BIG_ENDIAN = "bigEndian"
+   LITTLE_ENDIAN = "littleEndian"
+class Timer:
+   pass
