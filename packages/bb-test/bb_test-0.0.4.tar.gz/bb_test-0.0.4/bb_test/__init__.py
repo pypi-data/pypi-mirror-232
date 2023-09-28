b62eab52c58207c4c440aed4f9d82cf2fa75b10e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-28 03:09:23 +0000

    Add package bb-test/0.0.4/bb_test-0.0.4.tar.gz

---
+++ packages/bb-test/bb_test-0.0.4.tar.gz/bb_test-0.0.4/bb_test/__init__.py
@@ -0,0 +1,4 @@
+
+print( "Yey! Running __init__.py in bb_test package!")
+
+from .bb_module import *
---
+++ packages/bb-test/bb_test-0.0.4.tar.gz/bb_test-0.0.4/setup.py
@@ -0,0 +1,13 @@
+
+from setuptools import setup
+setup(
+    name='bb_test',
+    version='0.0.4',
+    description='Brandon\'s test package',
+    long_description='There\'s not a lot to say about it',
+    #url='https://bb-ai.net/KARaML/KARaML_Tools.html',
+    author='brandonb',
+    author_email='B.Bennett@leeds.ac.uk',
+    packages=['bb_test'],
+    classifiers=['Development Status :: 1 - Planning'],
+)
