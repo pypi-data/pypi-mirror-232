7d361f5d88662cf2b1258868160fedc77d9b12e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-28 04:09:47 +0000

    Add package adversarial-insight-ml/0.0.4/adversarial_insight_ml-0.0.4-py3-none-any.whl

---
+++ packages/adversarial-insight-ml/adversarial_insight_ml-0.0.4-py3-none-any.whl/aiml/__init__.py
@@ -0,0 +1,42 @@
+"""
+__init__.py
+
+Adversarial Insight ML
+This is a Python package that provides evaluation of ML models based on 
+adversarial attacks. This module is the entry point for the package.
+"""
+
+
+# Import all individual modules (.py) within pacakge
+from .attack import *
+from .evaluate import *
+from .load_data import *
+from .surrogate_model import *
+from .test_accuracy import *
+
+# Define package-wide variables
+__version__ = "0.0.4"
+__doc__ = "Adversarial Insight ML is a package that provides evaluation of ML models based on adversarial attacks."
+
+# List of symbols to be imported when using "from package import *"
+__all__ = [
+    "attack",
+    "evaluate",
+    "load_data",
+    "surrogate_model",
+    "test_accuracy",
+]
+
+
+# Initialization setup (package-wide configuration)
+def setup_package():
+    """
+    Setup the package.
+
+    This function can be used to perform any necessary initialization or setup
+    when the package is imported.
+    """
+    print("AIML package ({})is being initialized.".format(__version__))
+
+
+setup_package()
---
+++ packages/adversarial-insight-ml/adversarial_insight_ml-0.0.4-py3-none-any.whl/aiml/attack/test_black_box.py
@@ -0,0 +1,12 @@
+"""
+test_black_box.py
+"""
+
+
+from surrogate_model.create_surrogate_model import *
+from attack.test_white_box import *
+
+def black_box_test(black_box_model):
+    #TODO: Implement black-box attack testing
+    surrogate_model = create_surrogate_model(black_box_model)
+    result = test_all_white_box_attack(surrogate_model)
