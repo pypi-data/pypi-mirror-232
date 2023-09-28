3ee2369c84a8bd8cbd1091f4e6cddb9410a2283c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-28 03:07:56 +0000

    Add package lazysdk/0.1.61/lazysdk-0.1.61.tar.gz

---
+++ packages/lazysdk/lazysdk-0.1.61.tar.gz/lazysdk-0.1.61/lazysdk/__init__.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# coding = utf8
+"""
+@ Author : ZeroSeeker
+@ e-mail : zeroseeker@foxmail.com
+@ GitHub : https://github.com/ZeroSeeker
+@ Gitee : https://gitee.com/ZeroSeeker
+"""
+from . import lazyprocess
+from . import lazyrequests
+from . import lazybase64
+from . import lazytime
+from . import lazywebhook
+from . import lazyrandom
+from . import lazyip
+from . import lazyflask
+from . import lazyid
+from . import lazyurl
+from . import lazyua
+from . import lazy_id_card
+from . import showdata
+
