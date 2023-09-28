b2610fa3c78eef3404b048439f79a2076122edd3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-28 14:07:53 +0000

    Add package ecoindex-journey-scraper/1.0.1/ecoindex_journey_scraper-1.0.1.tar.gz

---
+++ packages/ecoindex-journey-scraper/ecoindex_journey_scraper-1.0.1.tar.gz/ecoindex_journey_scraper-1.0.1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ecoindex_journey_scraper',
+    version='1.0.1',
+    packages=find_packages(where='src'),
+    description='Calcul du lecoindex du un parcours',
+    author='Orange Innovation',
+    install_requires=[
+        "ecoindex>=5.4.2",
+        "undetected-chromedriver==3.4.6",
+        "Pillow>=9.2.0",
+        "selenium==4.9",
+        "jinja2>=3.1.0",
+        "ecoindex_scraper>=3.6.0"
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',  # Add this to specify OS compatibility
+    ],
+    python_requires=">=3.8",
+    py_modules=["ecoindex_journey_scraper"],  # Correct the module name
+    package_dir={'': 'src'},
+)
