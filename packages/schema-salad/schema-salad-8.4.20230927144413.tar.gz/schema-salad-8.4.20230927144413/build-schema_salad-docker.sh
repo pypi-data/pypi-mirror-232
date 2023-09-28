bf9c639245a1804e8368f9f8cfc2f3a4bfe3db1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-28 04:08:24 +0000

    Add package schema-salad/8.4.20230927144413/schema-salad-8.4.20230927144413.tar.gz

---
+++ packages/schema-salad/schema-salad-8.4.20230927144413.tar.gz/schema-salad-8.4.20230927144413/build-schema_salad-docker.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -ex
+docker build --file=schema_salad.Dockerfile --tag=quay.io/commonwl/schema_salad .
+docker run quay.io/commonwl/schema_salad /bin/sh -c \
+	'apk add --no-cache py3-pip && pip install pytest-xdist && cd /tmp && pytest --pyargs schema_salad -n auto'
+
+# version=$(git describe --tags)
+# if echo "$version" | grep -vq '\-' >& /dev/null ; then
+#     docker tag quay.io/commonwl/schema_salad quay.io/commonwl/schema_salad:"$version"
+# fi
