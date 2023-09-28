49b10f6661d828be15e371251eb17b17c0b42b0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-28 14:07:19 +0000

    Add package flatpack/0.9.8/flatpack-0.9.8.tar.gz

---
+++ packages/flatpack/flatpack-0.9.8.tar.gz/flatpack-0.9.8/flatpack/modules/lstm.py
@@ -0,0 +1,20 @@
+from .base import Base
+import torch.nn as nn
+
+
+class LSTM(Base):
+    def __init__(self, embed_size, hidden_size, num_layers, vocab_size=None):
+        super(LSTM, self).__init__(embed_size, vocab_size)
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+        self.lstm = nn.LSTM(embed_size, hidden_size, num_layers, batch_first=True)
+
+    def forward(self, x):
+        x = self.embedding(x)
+        print("LSTM - Shape after embedding:", x.shape)
+        out, _ = self.lstm(x)
+        print("LSTM - Shape after LSTM:", out.shape)
+        out = out.reshape(out.size(0) * out.size(1), self.embed_size)
+        print("LSTM - Shape after reshaping:", out.shape)
+        out = self.fc(out)
+        return out
