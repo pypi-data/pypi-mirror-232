5f5eb62f018a515a1cf8292309eee748c4ab89f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-28 03:08:48 +0000

    Add package murfey/0.9.1/murfey-0.9.1-py3-none-any.whl

---
+++ packages/murfey/murfey-0.9.1-py3-none-any.whl/murfey/server/main.py
@@ -0,0 +1,51 @@
+from __future__ import annotations
+
+import logging
+import os
+
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
+from pydantic import BaseSettings
+
+import murfey.server
+import murfey.server.bootstrap
+import murfey.util.models
+from murfey.server import template_files
+
+if os.getenv("MURFEY_DEMO"):
+    from murfey.server.demo_api import router
+else:
+    from murfey.server.api import router
+
+
+log = logging.getLogger("murfey.server.main")
+
+tags_metadata = [murfey.server.bootstrap.tag]
+
+
+class Settings(BaseSettings):
+    murfey_machine_configuration: str = ""
+
+
+settings = Settings()
+
+app = FastAPI(title="Murfey server", debug=True, openapi_tags=tags_metadata)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.mount("/static", StaticFiles(directory=template_files / "static"), name="static")
+app.mount("/images", StaticFiles(directory=template_files / "images"), name="images")
+
+app.include_router(murfey.server.bootstrap.bootstrap)
+app.include_router(murfey.server.bootstrap.cygwin)
+app.include_router(murfey.server.bootstrap.pypi)
+app.include_router(murfey.server.websocket.ws)
+
+app.include_router(router)
