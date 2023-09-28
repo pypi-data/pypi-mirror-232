2e895d31483b9cba7ec46957892e42cb90f671f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-28 04:06:51 +0000

    Add package yroom/0.0.9/yroom-0.0.9.tar.gz

---
+++ packages/yroom/yroom-0.0.9.tar.gz/yroom-0.0.9/CHANGELOG.md
@@ -0,0 +1,9 @@
+# Changelog
+
+## v0.0.9 - 27.9.2023
+
+- `YRoomMessage.has_edits: bool` flag indicating data changes have occurred.
+- New `YRoomClientOptions`: disallow writes to document or awareness state.
+- Always send awareness update after sync step 1.
+
+## v0.0.8
---
+++ packages/yroom/yroom-0.0.9.tar.gz/yroom-0.0.9/src/lib.rs
@@ -0,0 +1,13 @@
+use pyo3::prelude::*;
+
+mod roomsync;
+
+/// A Python module implemented in Rust.
+#[pymodule]
+fn yroom(_py: Python, m: &PyModule) -> PyResult<()> {
+    pyo3_log::init();
+    m.add_class::<roomsync::YRoomManager>()?;
+    m.add_class::<roomsync::YRoomMessage>()?;
+    m.add_class::<roomsync::YRoomClientOptions>()?;
+    Ok(())
+}
