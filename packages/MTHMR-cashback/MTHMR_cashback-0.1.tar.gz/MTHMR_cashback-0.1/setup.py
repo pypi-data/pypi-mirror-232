194284a0273dde08cd478e353690ce5ceb341eb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-28 03:09:52 +0000

    Add package MTHMR-cashback/0.1/MTHMR_cashback-0.1.tar.gz

---
+++ packages/MTHMR-cashback/MTHMR_cashback-0.1.tar.gz/MTHMR_cashback-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="MTHMR_cashback",
+    version="0.1",
+    author="MTHMR",
+    description="Description of your package",
+    packages=find_packages(),
+    install_requires=[
+        # Зависимости вашего пакета
+    ],
+)
