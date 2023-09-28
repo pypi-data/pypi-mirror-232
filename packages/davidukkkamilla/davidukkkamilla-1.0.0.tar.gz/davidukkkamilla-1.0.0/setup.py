69fd2b4cec2d3ec9128b712ba460dec3292c97bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-28 04:07:47 +0000

    Add package davidukkkamilla/1.0.0/davidukkkamilla-1.0.0.tar.gz

---
+++ packages/davidukkkamilla/davidukkkamilla-1.0.0.tar.gz/davidukkkamilla-1.0.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='davidukkkamilla',
+    version='1.0.0',
+    author='davidukkkamilla',
+    author_email='davidukkkamilla@gmail.com',
+    description='--',
+    long_description='Readme content goes here',
+    long_description_content_type='text/markdown',
+    url='https://github.com/davidukkkamilla/Work',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    install_requires=[],
+)
