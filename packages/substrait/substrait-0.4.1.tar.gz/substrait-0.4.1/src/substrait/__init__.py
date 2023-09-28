aef982a172dde16c19b01fd9e6a85e93a00a0c80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-28 14:08:26 +0000

    Add package substrait/0.4.1/substrait-0.4.1.tar.gz

---
+++ packages/substrait/substrait-0.4.1.tar.gz/substrait-0.4.1/src/substrait/__init__.py
@@ -0,0 +1,8 @@
+try:
+    from ._version import __version__
+except ImportError:
+    pass
+
+__substrait_version__ = "0.34.0"
+__substrait_hash__ = "dacc5a9"
+__minimum_substrait_version__ = "0.30.0"
