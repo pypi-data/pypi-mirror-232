f2a0828e793d1041dd92559ef9214943320e7ecf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-28 03:07:57 +0000

    Add package tdta/0.0.3.dev1/tdta-0.0.3.dev1-py3-none-any.whl

---
+++ packages/tdta/tdta-0.0.3.dev1-py3-none-any.whl/tdta/purl_publish.py
@@ -0,0 +1,20 @@
+import os
+
+
+def publish_to_purl(file_path: str) -> str:
+    """
+    Publishes the given taxonomy to the purl system. First checks if PURL system already has a config for the given
+    taxonomy. If not, makes a pull request to create a config.
+    :param file_path: path to the project root folder
+    :return: url of the created pull request or the url of the existing PURL configuration.
+    """
+    print("In PURL action 3.")
+    current_dir = os.path.dirname(os.path.realpath(__file__))
+    print(os.path.abspath(current_dir))
+    print(os.path.abspath(file_path))
+
+    files = [f for f in os.listdir(os.path.abspath(file_path)) if os.path.isfile(f)]
+    for file in files:
+        print(file)
+
+    return "DONE"
