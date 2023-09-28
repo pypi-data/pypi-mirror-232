8af2f988ab8c20ce467b9a7ddf65a669ec05e883
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-28 04:06:20 +0000

    Add package soniscope-jupyter/0.1.8/soniscope_jupyter-0.1.8-py3-none-any.whl

---
+++ packages/soniscope-jupyter/soniscope_jupyter-0.1.8-py3-none-any.whl/soniscope_jupyter/_frontend.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Copyright (c) Alexander Rind & the SoniVis team.
+# Distributed under the terms of the MIT License (see LICENSE.txt).
+
+"""
+Information about the frontend package of the widgets.
+"""
+
+module_name = "soniscope-jupyter"
+module_version = "^0.1.4"
