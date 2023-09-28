98f455066e3ec941996dd3fc8de8ad2fafc53f63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-28 03:07:52 +0000

    Add package medialoopster/0.0.21/medialoopster-0.0.21-py3-none-any.whl

---
+++ packages/medialoopster/medialoopster-0.0.21-py3-none-any.whl/medialoopster/__init__.py
@@ -0,0 +1,11 @@
+"""
+medialoopster
+~~~~~~~~~~~
+medialoopster API wrapper for Python
+--------------------------------------------
+*medialoopster* is a wrapper library for the `HTTP API
+<https://docs.medialoopster.com/api>`_ of `medialoopster
+<https://www.medialoopster.com/>`_.
+"""
+
+from medialoopster.api import Medialoopster
