140914b3a45215eb248ac081cd8a686bab108519
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-28 05:06:00 +0000

    Add package highbalance/0.0.5/highbalance-0.0.5-py3-none-any.whl

---
+++ packages/highbalance/highbalance-0.0.5-py3-none-any.whl/highbalance/__init__.py
@@ -0,0 +1,48 @@
+import discum
+import requests
+import random
+import getpass
+from colorama import Fore, Style, init
+import pyfiglet
+import os
+import os.path
+import concurrent.futures
+import time
+import string
+from pystyle import *
+import base64
+import subprocess
+import sys
+import DiscordTos
+import threading
+import json
+from playsound import playsound
+from json import loads
+from time import sleep
+from json import dumps
+from websocket import WebSocket
+from concurrent.futures import ThreadPoolExecutor
+import easygui, os
+from threading import Thread
+from time import sleep
+import os
+init(autoreset=True)
+colosr = Fore.BLUE + "[" + Style.RESET_ALL
+colosr2 = Fore.BLUE + "]" + Style.RESET_ALL
+color = Fore.RED
+os.system('cls')
+os.system(f"title gostako User chaker On ToP")
+print(Colorate.Vertical(Colors.purple_to_blue, """   
+░██████╗░░█████╗░░██████╗████████╗░█████╗░██╗░░██╗░█████╗░  ██╗░░░██╗░██████╗███████╗██████╗░
+██╔════╝░██╔══██╗██╔════╝╚══██╔══╝██╔══██╗██║░██╔╝██╔══██╗  ██║░░░██║██╔════╝██╔════╝██╔══██╗
+██║░░██╗░██║░░██║╚█████╗░░░░██║░░░███████║█████═╝░██║░░██║  ██║░░░██║╚█████╗░█████╗░░██████╔╝
+██║░░╚██╗██║░░██║░╚═══██╗░░░██║░░░██╔══██║██╔═██╗░██║░░██║  ██║░░░██║░╚═══██╗██╔══╝░░██╔══██╗
+╚██████╔╝╚█████╔╝██████╔╝░░░██║░░░██║░░██║██║░╚██╗╚█████╔╝  ╚██████╔╝██████╔╝███████╗██║░░██║
+░╚═════╝░░╚════╝░╚═════╝░░░░╚═╝░░░╚═╝░░╚═╝╚═╝░░╚═╝░╚════╝░  ░╚═════╝░╚═════╝░╚══════╝╚═╝░░╚═╝
+
+░█████╗░██╗░░██╗░█████╗░██╗░░██╗███████╗██████╗░
+██╔══██╗██║░░██║██╔══██╗██║░██╔╝██╔════╝██╔══██╗
+██║░░╚═╝███████║███████║█████═╝░█████╗░░██████╔╝
+██║░░██╗██╔══██║██╔══██║██╔═██╗░██╔══╝░░██╔══██╗
+╚█████╔╝██║░░██║██║░░██║██║░╚██╗███████╗██║░░██║
+░╚════╝░╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░╚═╝╚══════╝╚═╝░░╚═╝                              """, 1))
