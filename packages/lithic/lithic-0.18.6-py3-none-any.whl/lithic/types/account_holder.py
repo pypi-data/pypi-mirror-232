b88c1273a17c77f4714d3fc9aa04282a834134f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-28 04:10:05 +0000

    Add package lithic/0.18.6/lithic-0.18.6-py3-none-any.whl

---
+++ packages/lithic/lithic-0.18.6-py3-none-any.whl/lithic/types/account_holder.py
@@ -0,0 +1,49 @@
+# File generated from our OpenAPI spec by Stainless.
+
+from typing import List, Optional
+from typing_extensions import Literal
+
+from .._models import BaseModel
+
+__all__ = ["AccountHolder"]
+
+
+class AccountHolder(BaseModel):
+    token: Optional[str] = None
+    """Globally unique identifier for the account holder."""
+
+    account_token: Optional[str] = None
+    """Globally unique identifier for the account."""
+
+    business_account_token: Optional[str] = None
+    """
+    Only applicable for customers using the KYC-Exempt workflow to enroll authorized
+    users of businesses. Pass the account_token of the enrolled business associated
+    with the AUTHORIZED_USER in this field.
+    """
+
+    status: Optional[Literal["ACCEPTED", "REJECTED", "PENDING_RESUBMIT", "PENDING_DOCUMENT"]] = None
+    """KYC and KYB evaluation states.
+
+    Note: `PENDING_RESUBMIT` and `PENDING_DOCUMENT` are only applicable for the
+    `ADVANCED` workflow.
+    """
+
+    status_reasons: Optional[
+        List[
+            Literal[
+                "ADDRESS_VERIFICATION_FAILURE",
+                "AGE_THRESHOLD_FAILURE",
+                "COMPLETE_VERIFICATION_FAILURE",
+                "DOB_VERIFICATION_FAILURE",
+                "ID_VERIFICATION_FAILURE",
+                "MAX_DOCUMENT_ATTEMPTS",
+                "MAX_RESUBMISSION_ATTEMPTS",
+                "NAME_VERIFICATION_FAILURE",
+                "OTHER_VERIFICATION_FAILURE",
+                "RISK_THRESHOLD_FAILURE",
+                "WATCHLIST_ALERT_FAILURE",
+            ]
+        ]
+    ] = None
+    """Reason for the evaluation status."""
