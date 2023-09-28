1a1d13f46c2deb475c53f405157f83ce3d674a3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-28 03:08:49 +0000

    Add package slune-lib/0.0.1/slune-lib-0.0.1.tar.gz

---
+++ packages/slune-lib/slune-lib-0.0.1.tar.gz/slune-lib-0.0.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    description = fh.read()
+
+setup(
+    name="slune-lib",
+    version="0.0.1",
+    author="Henry Bourne",
+    author_email="hwbourne@gmail.com",
+    packages=["slune"],
+    description="A package for performing hyperparameter tuning with the SLURM scheduling system.",
+    long_description=description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/h-aze/slune",
+    license='MIT',
+    python_requires='>=3.8',
+    install_requires=[
+        "argparse",
+        "subprocess",
+        "os",
+        "pandas",
+    ],
+)
