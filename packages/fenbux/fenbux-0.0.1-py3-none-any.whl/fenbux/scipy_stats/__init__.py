4ddd016c5b2b4350c52383ef2fa34bcfe2f2620f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-28 15:10:05 +0000

    Add package fenbux/0.0.1/fenbux-0.0.1-py3-none-any.whl

---
+++ packages/fenbux/fenbux-0.0.1-py3-none-any.whl/fenbux/scipy_stats/__init__.py
@@ -0,0 +1,20 @@
+from ._dists import (
+    bernoulli,
+    beta,
+    binom,
+    chi2,
+    expon,
+    f,
+    gamma,
+    logistic,
+    lognorm,
+    multivariate_normal,
+    norm,
+    pareto,
+    poisson,
+    t,
+    uniform,
+    wald,
+    weibull_min,
+)
+from ._wrapper import _ScipyDist as ScipyDist
