0fe8fda917b098156084df4aa4953adedd7bd334
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-28 15:09:40 +0000

    Add package FileScaner/1.0/FileScaner-1.0.tar.gz

---
+++ packages/FileScaner/FileScaner-1.0.tar.gz/FileScaner-1.0/Scanner/Scanner.py
@@ -0,0 +1,30 @@
+import pandas as pd
+from pathlib import Path
+
+
+class FileScanner:
+    def __init__(self):
+        self.file_array = []
+
+    def scan(self, path, file_type_array, recursive=True):
+        if recursive:
+            p = Path(path).rglob('*.*')
+        else:
+            p = Path(path).glob('*.*')
+
+        only_files = [x for x in p if x.is_file()]
+
+        for file in only_files:
+            file_type = file.suffix[1:]
+            file_name = file.name
+            file_path = file.parent
+            file = fr"{file_path}\{file_name}"
+
+            if file_type in file_type_array:
+                x = (file,file_name, file_type, file_path)
+                self.file_array.append(x)
+
+        df = pd.DataFrame(data=self.file_array, columns=['file', 'file_name', 'file_type', 'file_path'])
+        return df
+
+
---
+++ packages/FileScaner/FileScaner-1.0.tar.gz/FileScaner-1.0/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.1'
+DESCRIPTION = 'Scan files based on file type'
+LONG_DESCRIPTION = 'Will scan a folder for types of files based on an array and return a pandas dataframe with file info'
+
+# Setting up
+setup(
+    # the name must match the folder name 'verysimplemodule'
+    name="FileScaner",
+    version=1.0,
+    author="Ryan Sanders",
+    author_email="rasanders22@gmail.com",
+    description="Scan files based on file type",
+    long_description="Will scan a folder for types of files based on an array and return a pandas dataframe with file info",
+    packages=find_packages(),
+    install_requires=["pandas","pathlib"],  # add any additional packages that
+    # needs to be installed along with your package. Eg: 'caer'
+    keywords=['python', 'scans files'],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Education",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
