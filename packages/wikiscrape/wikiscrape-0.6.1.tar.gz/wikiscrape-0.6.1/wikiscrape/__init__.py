3038e8291062b0be93f116863ee4c02533156c7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-28 14:07:49 +0000

    Add package wikiscrape/0.6.1/wikiscrape-0.6.1.tar.gz

---
+++ packages/wikiscrape/wikiscrape-0.6.1.tar.gz/wikiscrape-0.6.1/wikiscrape/__init__.py
@@ -0,0 +1,9 @@
+from .coordinates import Coordinates
+from .infobox import Infobox
+from .linked_text import LinkedText
+from .markdown import Markdown
+from .wikiobject import Wikiobject
+from .wikipage import Wikipage
+from .wikitable import Wikitable
+
+__all__ = ["Coordinates", "Infobox", "LinkedText", "Markdown", "Wikiobject", "Wikipage", "Wikitable"]
