8558494c4d41985b7416c02dc2a0e48a4a091345
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-28 03:07:22 +0000

    Add package iparapheur-provisioning/1.8.3/iparapheur-provisioning-1.8.3.tar.gz

---
+++ packages/iparapheur-provisioning/iparapheur-provisioning-1.8.3.tar.gz/iparapheur-provisioning-1.8.3/iparapheur_provisioning/apis/paths/api_provisioning_v1_admin_tenant_tenant_id_templates_template_type.py
@@ -0,0 +1,13 @@
+from iparapheur_provisioning.paths.api_provisioning_v1_admin_tenant_tenant_id_templates_template_type.get import ApiForget
+from iparapheur_provisioning.paths.api_provisioning_v1_admin_tenant_tenant_id_templates_template_type.put import ApiForput
+from iparapheur_provisioning.paths.api_provisioning_v1_admin_tenant_tenant_id_templates_template_type.post import ApiForpost
+from iparapheur_provisioning.paths.api_provisioning_v1_admin_tenant_tenant_id_templates_template_type.delete import ApiFordelete
+
+
+class ApiProvisioningV1AdminTenantTenantIdTemplatesTemplateType(
+    ApiForget,
+    ApiForput,
+    ApiForpost,
+    ApiFordelete,
+):
+    pass
---
+++ packages/iparapheur-provisioning/iparapheur-provisioning-1.8.3.tar.gz/iparapheur-provisioning-1.8.3/iparapheur_provisioning/apis/paths/api_provisioning_v1_admin_user.py
@@ -0,0 +1,7 @@
+from iparapheur_provisioning.paths.api_provisioning_v1_admin_user.get import ApiForget
+
+
+class ApiProvisioningV1AdminUser(
+    ApiForget,
+):
+    pass
