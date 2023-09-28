20c51f88ca79b46a7868b7e8ee33ba4c801639bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-28 15:09:52 +0000

    Add package generate-md-links/0.1.2/generate_md_links-0.1.2-py3-none-any.whl

---
+++ packages/generate-md-links/generate_md_links-0.1.2-py3-none-any.whl/src/generate_MD_links.py
@@ -0,0 +1,51 @@
+import os
+import argparse
+from pathlib import Path
+
+
+def find_files_with_extension(directory, extension):
+    result = [p.absolute().as_posix() for p in Path(directory).rglob(f"*{extension}")]
+    return result
+
+
+def create_md_links(file_paths):
+    md_links = []
+    current_section = None
+
+    for path in file_paths:
+        section = os.path.basename(os.path.dirname(path))
+        if section != current_section:
+            md_links.append(f"\n## {section}\n")
+            current_section = section
+        link_name = os.path.splitext(os.path.basename(path))[0]
+        md_links.append(f"- [{link_name}](<{path}>)")
+
+    return md_links
+
+
+def save_to_md_file(file_paths, output_file):
+    md_links = create_md_links(file_paths)
+    with open(output_file, "w", encoding="utf-8") as f:
+        f.write("\n".join(md_links))
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="Generate Markdown links for specified files."
+    )
+    parser.add_argument("output_file", help="Output Markdown file name")
+    parser.add_argument("extension", help="File extension to search for (e.g., .pdf)")
+    parser.add_argument("directory", nargs="+", help="Directories to search for files")
+
+    args = parser.parse_args()
+
+    file_paths = []
+    for directory in args.directory:
+        file_paths.extend(find_files_with_extension(directory, args.extension))
+
+    save_to_md_file(file_paths, args.output_file)
+    print(f"Markdown links saved to {args.output_file}")
+
+
+if __name__ == "__main__":
+    main()
