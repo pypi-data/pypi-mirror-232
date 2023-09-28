2361fff637d96b68d24d571b7769c262b49b5b81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-28 03:09:27 +0000

    Add package berrydb/1.0.8/berrydb-1.0.8.tar.gz

---
+++ packages/berrydb/berrydb-1.0.8.tar.gz/berrydb-1.0.8/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+VERSION = "1.0.8"
+
+DESCRIPTION = "The database for unstructured data and AI apps"
+AUTHOR = "BerryDB"
+URL = "https://app.berrydb.io"
+
+setup(
+    name="berrydb",
+    version=VERSION,
+    description=DESCRIPTION,
+    url=URL,
+    author=AUTHOR,
+    license="Proprietary",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    install_requires=[
+        "requests",
+        "openai",
+        "langchain",
+        "tiktoken",
+        "faiss-cpu",
+        "jq"
+    ],
+    classifiers=[
+        "License :: Other/Proprietary License",
+    ],
+    py_modules=["database", "BerryDB", "utils", "loaders", "embeddings"],
+    packages=find_packages()
+)
