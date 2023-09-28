a9c18292c289ecec2e65c58bbf4d906151f1e175
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-28 14:07:15 +0000

    Add package flatpack/0.9.4/flatpack-0.9.4-py3-none-any.whl

---
+++ packages/flatpack/flatpack-0.9.4-py3-none-any.whl/flatpack/modules/transformer.py
@@ -0,0 +1,18 @@
+from .base import Base
+import torch.nn as nn
+
+
+class Transformer(Base):
+    def __init__(self, d_model, nhead, num_encoder_layers, num_decoder_layers, vocab_size=None):
+        super(Transformer, self).__init__(d_model, vocab_size)
+        self.nhead = nhead
+        self.num_encoder_layers = num_encoder_layers
+        self.num_decoder_layers = num_decoder_layers
+        self.transformer = nn.Transformer(d_model, nhead, num_encoder_layers, num_decoder_layers)
+
+    def forward(self, src, tgt):
+        src = self.embedding(src)
+        tgt = self.embedding(tgt)
+        out = self.transformer(src, tgt)
+        out = self.fc(out)
+        return out
