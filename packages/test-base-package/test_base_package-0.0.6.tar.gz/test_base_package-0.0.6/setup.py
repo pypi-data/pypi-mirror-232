488433b96c63dee19fa1e65c761a377f56775f05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-28 15:09:14 +0000

    Add package test-base-package/0.0.6/test_base_package-0.0.6.tar.gz

---
+++ packages/test-base-package/test_base_package-0.0.6.tar.gz/test_base_package-0.0.6/setup.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+__version__ = '0.0.6'  # 版本号
+requirements = open('requirements.txt').readlines()  # 依赖文件
+
+setup(
+    name='test_base_package',
+    version=__version__,
+    author='自动化测试基础包',
+    author_email='tianjincn@163.com',
+    packages=find_packages(),
+    python_requires='>=3.5.0',
+    install_requires=requirements,  # 安装依赖
+    url="https://www.baidu.com",
+    description='python 自动化测试基础包',
+    py_modules=['test_base_package'],
+    entry_points={
+        'console_scripts': [
+            'basepkg = test_base_package.cli:main',
+        ],
+    },
+    long_description_content_type='text/markdown',
+    readme_renderer="README.md",
+)
