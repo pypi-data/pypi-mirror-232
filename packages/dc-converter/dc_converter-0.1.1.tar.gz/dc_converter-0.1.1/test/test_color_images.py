6c19e166c646e995a0b032a600246bc318bfcf1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-28 04:10:05 +0000

    Add package dc-converter/0.1.1/dc_converter-0.1.1.tar.gz

---
+++ packages/dc-converter/dc_converter-0.1.1.tar.gz/dc_converter-0.1.1/test/test_color_images.py
@@ -0,0 +1,9 @@
+import os.path
+
+from dc_converter.convert_folder import write_color_images
+
+
+def test_add():
+    write_color_images(os.path.expanduser('~/tmp/Tasse/Timo_Tasse_10-ergonomic-standing-2023-09-21T164305.097Z-DCG2202205001888/data.raw'))
+
+
