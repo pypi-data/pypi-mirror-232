ff553b84490c33ed5656a29d4927c41d21e8e209
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-28 15:11:10 +0000

    Add package sifeng/0.2.5/sifeng-0.2.5-py2.py3-none-any.whl

---
+++ packages/sifeng/sifeng-0.2.5-py2.py3-none-any.whl/sifeng/dl/modules/__init__.py
@@ -0,0 +1,6 @@
+from .mdl import MultiheadedSelfAttentionModule # Multiheaded Self Attention Module
+from .mdl import AbsolutePositionalEncodingModule # Positional Encoding Moudle
+from .mdl import FeedForwardLayer # Feed Forward Layer
+from .mdl import TransformerEncoderBlock # Transformer Encoder Block
+from .mdl import MixtureOfExpertsBlock # Mixture of Experts Block
+from .mdl import ResidualBlock # Residual Connection Block
