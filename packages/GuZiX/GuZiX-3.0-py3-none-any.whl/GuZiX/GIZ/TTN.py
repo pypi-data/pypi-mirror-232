24e3776a4364e3e33bdbfd97eabf77b3758ab63e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-28 15:09:51 +0000

    Add package GuZiX/3.0/GuZiX-3.0-py3-none-any.whl

---
+++ packages/GuZiX/GuZiX-3.0-py3-none-any.whl/GuZiX/GIZ/TTN.py
@@ -0,0 +1,8 @@
+import random
+def genrandint(minimal, maximal):
+    random.randint(minimal, maximal)
+def shuffle(value):
+    random.shuffle(value)
+    return value
+def ttn(text):
+    return random.randint(0, len(text))
