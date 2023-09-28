1bb4359d8a59e2afdbce90a5c47879a79ea59148
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-28 04:10:05 +0000

    Add package lithic/0.18.6/lithic-0.18.6.tar.gz

---
+++ packages/lithic/lithic-0.18.6.tar.gz/lithic-0.18.6/src/lithic/types/event.py
@@ -0,0 +1,53 @@
+# File generated from our OpenAPI spec by Stainless.
+
+from typing import Dict
+from datetime import datetime
+from typing_extensions import Literal
+
+from .._models import BaseModel
+
+__all__ = ["Event"]
+
+
+class Event(BaseModel):
+    token: str
+    """Globally unique identifier."""
+
+    created: datetime
+    """An RFC 3339 timestamp for when the event was created. UTC time zone.
+
+    If no timezone is specified, UTC will be used.
+    """
+
+    event_type: Literal[
+        "card.created",
+        "card.shipped",
+        "card_transaction.updated",
+        "digital_wallet.tokenization_approval_request",
+        "digital_wallet.tokenization_result",
+        "digital_wallet.tokenization_two_factor_authentication_code",
+        "dispute.updated",
+        "dispute_evidence.upload_failed",
+        "three_ds_authentication.created",
+        "payment_transaction.created",
+        "payment_transaction.updated",
+        "transfer_transaction.created",
+    ]
+    """Event types:
+
+    - `card.created` - Notification that a card has been created.
+    - `card.shipped` - Physical card shipment notification. See
+      https://docs.lithic.com/docs/cards#physical-card-shipped-webhook.
+    - `card_transaction.updated` - Transaction Lifecycle webhook. See
+      https://docs.lithic.com/docs/transaction-webhooks.
+    - `dispute.updated` - A dispute has been updated.
+    - `digital_wallet.tokenization_approval_request` - Card network's request to
+      Lithic to activate a digital wallet token.
+    - `digital_wallet.tokenization_result` - Notification of the end result of a
+      tokenization, whether successful or failed.
+    - `digital_wallet.tokenization_two_factor_authentication_code` - A code to be
+      passed to an end user to complete digital wallet authentication. See
+      https://docs.lithic.com/docs/tokenization-control#digital-wallet-tokenization-auth-code.
+    """
+
+    payload: Dict[str, object]
