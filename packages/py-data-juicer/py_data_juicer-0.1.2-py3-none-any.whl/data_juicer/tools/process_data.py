eadbfc72fcb318520826e8bfe695f90b0d947ff6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-28 15:09:14 +0000

    Add package py-data-juicer/0.1.2/py_data_juicer-0.1.2-py3-none-any.whl

---
+++ packages/py-data-juicer/py_data_juicer-0.1.2-py3-none-any.whl/data_juicer/tools/process_data.py
@@ -0,0 +1,18 @@
+from loguru import logger
+
+from data_juicer.config import init_configs
+from data_juicer.core import Executor, RayExecutor
+
+
+@logger.catch
+def main():
+    cfg = init_configs()
+    if cfg.executor_type == 'default':
+        executor = Executor(cfg)
+    elif cfg.executor_type == 'ray':
+        executor = RayExecutor(cfg)
+    executor.run()
+
+
+if __name__ == '__main__':
+    main()
