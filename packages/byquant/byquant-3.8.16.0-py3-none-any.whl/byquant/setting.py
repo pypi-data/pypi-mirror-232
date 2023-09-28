cf8679ee10c51fed874f225ab6f44ccfb0588710
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-28 03:07:28 +0000

    Add package byquant/3.8.16.0/byquant-3.8.16.0-py3-none-any.whl

---
+++ packages/byquant/byquant-3.8.16.0-py3-none-any.whl/byquant/setting.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8; py-indent-offset:4 -*-
+###############################################################################
+#
+# Author : Trabi
+# Email : info@byquant.com
+# Copyright (C) 2022-2023 ByQuant.com
+#
+#  ███████   ██    ██   ██████   ██    ██    ████    ██    ██  ████████  
+#  ██    ██   ██  ██   ██    ██  ██    ██   ██  ██   ███   ██     ██     
+#  ███████     ████    ██    ██  ██    ██  ████████  ██ ██ ██     ██         █████  █████  █████
+#  ██    ██     ██     ██   ██   ██    ██  ██    ██  ██   ███     ██         █      █   █  █ █ █
+#  ███████      ██      ███████   ██████   ██    ██  ██    ██     ██     ██  █████  █████  █ █ █
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+
+BYQUANT_API_TOKEN = "8EEF8EDDC81257DE289444F85FE2E56F" #VIP用户需要设置
