9fa761ae85a14d741679c5600321b13675a18ad1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-28 04:07:45 +0000

    Add package alteza/0.7.0/alteza-0.7.0.tar.gz

---
+++ packages/alteza/alteza-0.7.0.tar.gz/alteza-0.7.0/alteza/__main__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+import sys
+
+from .engine import run, Args
+
+
+def main() -> None:
+    run(Args().parse_args())
+
+
+# See: https://chriswarrick.com/blog/2014/09/15/python-apps-the-right-way-entry_points-and-scripts/
+
+if __name__ == "__main__":
+    main()
