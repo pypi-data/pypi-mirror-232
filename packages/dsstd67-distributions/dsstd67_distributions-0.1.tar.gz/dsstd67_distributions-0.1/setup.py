a79a609c3504f07708988dcd593effa719dba068
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-28 04:06:51 +0000

    Add package dsstd67-distributions/0.1/dsstd67_distributions-0.1.tar.gz

---
+++ packages/dsstd67-distributions/dsstd67_distributions-0.1.tar.gz/dsstd67_distributions-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="dsstd67_distributions",
+    version="0.1",
+    description="Gaussian and Binomial distributions",
+    packages=["dsstd67_distributions"],
+    author="Kathryn Jones",
+    author_email="someone@someemail.com",
+    zip_safe=False,
+)
