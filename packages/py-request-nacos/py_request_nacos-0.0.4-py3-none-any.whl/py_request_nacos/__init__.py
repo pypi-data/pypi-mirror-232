197659cbba7aab26727a9070f7d6bd3d442f628f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-28 15:10:07 +0000

    Add package py-request-nacos/0.0.4/py_request_nacos-0.0.4-py3-none-any.whl

---
+++ packages/py-request-nacos/py_request_nacos-0.0.4-py3-none-any.whl/py_request_nacos/__init__.py
@@ -0,0 +1,11 @@
+import requests
+import json
+import numpy as np
+
+
+def request_nacos(nacos_url='http://82.157.147.8:8848/nacos', service_name='encourager'):
+    result = requests.get(nacos_url + '/v1/ns/instance/list?serviceName=' + service_name).text
+    result_json = json.loads(result)
+    hosts = result_json['hosts']
+    host = hosts[np.random.randint(0, len(hosts))]
+    return 'http://'+host['ip']+':'+str(host['port'])
