f927f570d1bee0086126c57d58f5d73685e40dd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-28 04:07:29 +0000

    Add package termii-py/0.0.8/termii_py-0.0.8.tar.gz

---
+++ packages/termii-py/termii_py-0.0.8.tar.gz/termii_py-0.0.8/tests/test_data/campaign.py
@@ -0,0 +1,173 @@
+# flake8: noqa
+
+get_phonebook = {
+    "data": [
+        {
+            "id": "f9c28de9-ab5a-4513-9c9f-338be8e1c390",
+            "name": "labore",
+            "total_number_of_contacts": 0,
+            "date_created": "2021-07-01 14:44:42",
+            "last_updated": "2021-07-01 14:44:42",
+        },
+        {
+            "id": "0d974867-7fc8-4dd9-b069-6ca33dc12930",
+            "name": "numquam",
+            "total_number_of_contacts": 0,
+            "date_created": "2021-07-01 14:44:33",
+            "last_updated": "2021-07-01 14:44:33",
+        },
+        {
+            "id": "b8bd4d35-1dba-4ffb-9fd6-8ecd672801fd",
+            "name": "totam",
+            "total_number_of_contacts": 0,
+            "date_created": "2021-07-01 14:44:16",
+            "last_updated": "2021-07-01 14:44:16",
+        },
+        {
+            "id": "2d9f4a02-85b8-45e5-9f5b-30f93ef472e2",
+            "name": "adeyinka",
+            "total_number_of_contacts": 0,
+            "date_created": "2021-06-29 12:23:42",
+            "last_updated": "2021-06-29 12:23:42",
+        },
+        {
+            "id": "779c2bbe-130b-4ea8-a30e-1e81f7e4d4d9",
+            "name": "testerboos",
+            "total_number_of_contacts": 0,
+            "date_created": "2021-06-28 15:50:06",
+            "last_updated": "2021-06-28 15:50:06",
+        },
+        {
+            "id": "04c3ebcc-3a7e-485a-88c1-68e731386f77",
+            "name": "omnis",
+            "total_number_of_contacts": 22,
+            "date_created": "2021-06-25 15:11:56",
+            "last_updated": "2021-06-30 12:02:23",
+        },
+        {
+            "id": "145d23bd-daef-4403-8236-73f648c63387",
+            "name": "bosun",
+            "total_number_of_contacts": 0,
+            "date_created": "2021-06-25 12:59:10",
+            "last_updated": "2021-06-25 12:59:10",
+        },
+        {
+            "id": "67503708-0725-49eb-9bc6-feee9faea58a",
+            "name": "test",
+            "total_number_of_contacts": 776,
+            "date_created": "2021-06-25 12:58:45",
+            "last_updated": "2021-07-01 15:39:35",
+        },
+    ],
+    "links": {
+        "first": "https://api.ng.termii.com/api/phonebooks?page=1",
+        "last": "https://api.ng.termii.com/api/phonebooks?page=1",
+        "prev": None,
+        "next": None,
+    },
+    "meta": {
+        "current_page": 1,
+        "from": 1,
+        "last_page": 1,
+        "path": "https://api.ng.termii.com/api/phonebooks",
+        "per_page": 15,
+        "to": 8,
+        "total": 8,
+    },
+}
+
+
+get_contacts = {
+    "data": [
+        {
+            "id": 3,
+            "pid": 4,
+            "phone_number": "2347062609181",
+            "email_address": None,
+            "message": None,
+            "company": None,
+            "first_name": None,
+            "last_name": None,
+            "create_at": "2021-06-30 12:02:15",
+            "updated_at": "2021-06-30 12:02:15",
+        },
+        {
+            "id": 4,
+            "pid": 4,
+            "phone_number": "2347051428948",
+            "email_address": None,
+            "message": None,
+            "company": None,
+            "first_name": None,
+            "last_name": None,
+            "create_at": "2021-06-30 12:02:20",
+            "updated_at": "2021-06-30 12:02:20",
+        },
+        {
+            "id": 5,
+            "pid": 4,
+            "phone_number": "2348173343852",
+            "email_address": None,
+            "message": None,
+            "company": None,
+            "first_name": None,
+            "last_name": None,
+            "create_at": "2021-06-30 12:02:20",
+            "updated_at": "2021-06-30 12:02:20",
+        },
+        {
+            "id": 6,
+            "pid": 4,
+            "phone_number": "2348057616056",
+            "email_address": None,
+            "message": None,
+            "company": None,
+            "first_name": None,
+            "last_name": None,
+            "create_at": "2021-06-30 12:02:20",
+            "updated_at": "2021-06-30 12:02:20",
+        },
+        {
+            "id": 7,
+            "pid": 4,
+            "phone_number": "2348060463787",
+            "email_address": None,
+            "message": None,
+            "company": None,
+            "first_name": None,
+            "last_name": None,
+            "create_at": "2021-06-30 12:02:20",
+            "updated_at": "2021-06-30 12:02:20",
+        },
+    ],
+    "links": {
+        "first": "https://api.ng.termii.com/api/phonebooks/04c3ebcc-3a7e-485a-88c1-68e731386f77/contacts?page=1",
+        "last": "https://api.ng.termii.com/api/phonebooks/04c3ebcc-3a7e-485a-88c1-68e731386f77/contacts?page=1",
+        "prev": None,
+        "next": None,
+    },
+    "meta": {
+        "current_page": 1,
+        "from": 1,
+        "last_page": 1,
+        "path": "https://api.ng.termii.com/api/phonebooks/04c3ebcc-3a7e-485a-88c1-68e731386f77/contacts",
+        "per_page": 25,
+        "to": 22,
+        "total": 22,
+    },
+}
+
+
+add_contact = {
+    "data": {
+        "id": 3647982,
+        "phone_number": "2448123696237",
+        "email_address": "test@gmail.com",
+        "message": None,
+        "company": "Termii",
+        "first_name": "test",
+        "last_name": "contact",
+        "create_at": "2021-10-11 10:15:35",
+        "updated_at": "2021-10-11 10:15:35",
+    }
+}
