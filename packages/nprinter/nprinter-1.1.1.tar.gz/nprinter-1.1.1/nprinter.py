cbc70049e9b1caf40fabf5dbd92b5582acb1df89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-28 15:09:16 +0000

    Add package nprinter/1.1.1/nprinter-1.1.1.tar.gz

---
+++ packages/nprinter/nprinter-1.1.1.tar.gz/nprinter-1.1.1/nprinter.py
@@ -0,0 +1,15 @@
+
+'''This is the “nester.py" module, and it provides one function called printlist() which prints lists that may or may not include nested lists. 
+The printing is done depending on the level'''
+
+def printlist(listitems, indent=False, level=0):
+    '''This function accepts a deeply nested list of primitives and a level that defaults to 0. It prints out the list's content using entered level'''
+    for item in listitems:
+        if isinstance(item, list):
+            printlist(item, indent, level+1)
+        else:
+            if indent:
+                for pos in range(level):
+                    print(' ' * 4, end='')
+            print(item)
+
---
+++ packages/nprinter/nprinter-1.1.1.tar.gz/nprinter-1.1.1/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+from os import getcwd
+
+def read(path):
+    return open(getcwd() + '/README.md', mode='r+', encoding='utf-8').read()
+setup(
+    name = 'nprinter',
+    version = '1.1.1',
+    py_modules = ['nprinter'],
+    author = 'nolinearpath',
+    author_email = 'donaldteghen@gmail.com',
+    url = 'http://www.nolinearpath.com',
+    description = 'A simple level-based printer for nested lists',
+    long_description=read('README.md'),
+    long_description_content_type='text/markdown'
+)
