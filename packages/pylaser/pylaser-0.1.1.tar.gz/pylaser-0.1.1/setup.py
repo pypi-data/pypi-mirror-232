a204b1d641bf631da3e0ac7b27a4154cd4994a27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-28 05:06:49 +0000

    Add package pylaser/0.1.1/pylaser-0.1.1.tar.gz

---
+++ packages/pylaser/pylaser-0.1.1.tar.gz/pylaser-0.1.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="pylaser",
+    version="0.1.1",
+    author="simonfall",
+    description="An experimental static type checker and language server for Python.",
+    packages=["pylaser"],
+)
