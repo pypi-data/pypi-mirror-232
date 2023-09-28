c1d5db4c4fac12ffca01fc610850bfa243180925
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-28 14:07:18 +0000

    Add package flatpack/0.9.6/flatpack-0.9.6-py3-none-any.whl

---
+++ packages/flatpack/flatpack-0.9.6-py3-none-any.whl/flatpack/modules/rnn.py
@@ -0,0 +1,17 @@
+from .base import Base
+import torch.nn as nn
+
+
+class RNN(Base):
+    def __init__(self, embed_size, hidden_size, num_layers, vocab_size=None):
+        super(RNN, self).__init__(embed_size, vocab_size)
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+        self.rnn = nn.RNN(embed_size, hidden_size, num_layers, batch_first=True)
+
+    def forward(self, x):
+        x = self.embedding(x)
+        out, _ = self.rnn(x)
+        out = out.reshape(out.size(0), -1)
+        out = self.fc(out)
+        return out
