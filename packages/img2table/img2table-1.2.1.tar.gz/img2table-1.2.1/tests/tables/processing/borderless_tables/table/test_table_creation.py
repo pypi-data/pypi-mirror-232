9a96d3de88d7c69eaf736bae0eeec9a036f9f97c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-28 14:08:30 +0000

    Add package img2table/1.2.1/img2table-1.2.1.tar.gz

---
+++ packages/img2table/img2table-1.2.1.tar.gz/img2table-1.2.1/tests/tables/processing/borderless_tables/table/test_table_creation.py
@@ -0,0 +1,28 @@
+# coding: utf-8
+
+import json
+
+from img2table.tables.objects.cell import Cell
+from img2table.tables.processing.borderless_tables.model import DelimiterGroup, TableRow
+from img2table.tables.processing.borderless_tables.table.table_creation import get_table
+
+
+def test_get_table():
+    with open("test_data/delimiter_group.json", "r") as f:
+        data = json.load(f)
+    delimiter_group = DelimiterGroup(delimiters=[Cell(**c) for c in data.get('delimiters')],
+                                     elements=[Cell(**c) for c in data.get('elements')])
+
+    with open("test_data/contours.json", 'r') as f:
+        contours = [Cell(**el) for el in json.load(f)]
+
+    with open("test_data/rows.json", "r") as f:
+        table_rows = [TableRow(cells=[Cell(**c) for c in row]) for row in json.load(f)]
+
+    result = get_table(columns=delimiter_group,
+                       table_rows=table_rows,
+                       contours=contours)
+
+    assert result.nb_rows == 16
+    assert result.nb_columns == 8
+    assert (result.x1, result.y1, result.x2, result.y2) == (93, 45, 1233, 1060)
