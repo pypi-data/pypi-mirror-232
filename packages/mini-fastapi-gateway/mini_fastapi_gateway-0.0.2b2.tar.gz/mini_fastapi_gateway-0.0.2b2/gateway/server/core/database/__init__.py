f3589986fd207c269421c48da2f014ab619eb39d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-28 14:07:32 +0000

    Add package mini-fastapi-gateway/0.0.2b2/mini_fastapi_gateway-0.0.2b2.tar.gz

---
+++ packages/mini-fastapi-gateway/mini_fastapi_gateway-0.0.2b2.tar.gz/mini_fastapi_gateway-0.0.2b2/gateway/server/core/database/__init__.py
@@ -0,0 +1,26 @@
+"""
+This file is responsible for creating the database engine and session.
+"""
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, Session
+
+from gateway.server.core.config import settings
+
+SQLALCHEMY_DATABASE_URL = settings.db_url
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+
+def get_db() -> Session:
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
