7ef8ea8349243516af2916e30389d29bb8964eac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-28 05:05:56 +0000

    Add package llm-monitor/0.9.0/llm_monitor-0.9.0-py3-none-any.whl

---
+++ packages/llm-monitor/llm_monitor-0.9.0-py3-none-any.whl/llm_monitor/schema/transaction.py
@@ -0,0 +1,23 @@
+from typing import Dict, List, Optional
+
+from pydantic import BaseModel
+
+
+class TransactionRecord(BaseModel):
+    latency_ms: Optional[int] = None
+    status_code: Optional[int] = None
+    input_text: str
+    output_text: Optional[str] = None
+    model: str
+    num_input_tokens: Optional[int] = None
+    num_output_tokens: Optional[int] = None
+    num_total_tokens: Optional[int] = None
+    finish_reason: Optional[str] = None
+    trace_id: Optional[str] = None
+    parent_trace_id: Optional[str] = None
+    output_logprobs: Optional[Dict] = None
+    created_at: str
+
+
+class TransactionRecordBatch(BaseModel):
+    records: List[TransactionRecord]
