7c96b8a6bfb29f1932cac7074fc2b113a19c0684
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-28 14:07:02 +0000

    Add package cornojob-killer/0.0.2/cornojob-killer-0.0.2.tar.gz

---
+++ packages/cornojob-killer/cornojob-killer-0.0.2.tar.gz/cornojob-killer-0.0.2/cornojob_killer/cli.py
@@ -0,0 +1,29 @@
+from abstra.cli import CLI
+import tempfile, time
+
+
+"""
+If you're reading this, you're really paranoid about safety, right?
+
+Although this is a joke, we take security extremely seriously. 
+Abstra is the safest and fastest way to automate your company's processes.
+
+We're not kidding about that.
+
+Want to know more? Contact us at help@abstra.io
+"""
+def main():
+    print("Welcome to cornojob-killer - the only weapon you'll ever need to destroy annoying, repetitive tasks.")
+    input("Ready to kill your corno jobs?\nPress enter to continue.")
+    print("Abstra will start in a temporary directory. Changes will not be saved.")
+    print("\nFor serious use cases, use the `abstra` CLI")
+    print("Read the docs at docs.abstra.io")
+    time.sleep(2)
+    temp_dir = tempfile.mkdtemp()
+    cli = CLI()
+    cli.serve(temp_dir)
+
+
+if __name__ == '__main__':
+    main()
+
