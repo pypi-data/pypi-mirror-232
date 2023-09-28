9c4cac1a45ef2064b7f8c8e076377f6a3397db24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-28 14:06:58 +0000

    Add package wagtail-liveedit/0.0.8/wagtail-liveedit-0.0.8.tar.gz

---
+++ packages/wagtail-liveedit/wagtail-liveedit-0.0.8.tar.gz/wagtail-liveedit-0.0.8/tests/urls.py
@@ -0,0 +1,17 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path, include
+
+try:
+    from wagtail import urls as wagtail_urls
+except ImportError:
+    # Wagtail <5
+    from wagtail.core import urls as wagtail_urls
+
+from .views import page_view
+
+urlpatterns = [
+    path('__liveedit__/', include('liveedit.urls')),
+    path('admin/', include('wagtail.admin.urls')),
+    path('page/', page_view),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
