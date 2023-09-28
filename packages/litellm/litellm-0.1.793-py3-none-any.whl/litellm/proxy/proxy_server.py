b8306ee172d0e9fd749daa8559fa079d258b247a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-28 15:09:35 +0000

    Add package litellm/0.1.793/litellm-0.1.793-py3-none-any.whl

---
+++ packages/litellm/litellm-0.1.793-py3-none-any.whl/litellm/proxy/proxy_server.py
@@ -0,0 +1,58 @@
+import sys, os
+sys.path.insert(
+    0, os.path.abspath("../..")
+)  # Adds the parent directory to the system path
+
+import litellm
+print(litellm.__file__)
+from fastapi import FastAPI, Request
+from fastapi.responses import StreamingResponse
+import json
+
+app = FastAPI()
+user_api_base = None
+user_model = None
+
+def initialize(model, api_base):
+    global user_model, user_api_base
+    user_model = model
+    user_api_base = api_base
+
+
+# for streaming
+def data_generator(response):
+    for chunk in response:
+        yield f"data: {json.dumps(chunk)}\n\n"
+        
+@app.get("/models") # if project requires model list 
+def model_list(): 
+    return dict(
+        data=[{"id": user_model, "object": "model", "created": 1677610602, "owned_by": "openai"}],
+        object="list",
+    )
+
+@app.post("/completions")
+async def completion(request: Request):
+    data = await request.json()
+    if (user_model is None):
+        raise ValueError("Proxy model needs to be set")
+    data["model"] = user_model
+    if user_api_base:
+        data["api_base"] = user_api_base
+    response = litellm.text_completion(**data)
+    if 'stream' in data and data['stream'] == True: # use generate_responses to stream responses
+            return StreamingResponse(data_generator(response), media_type='text/event-stream')
+    return response
+
+@app.post("/chat/completions")
+async def chat_completion(request: Request):
+    data = await request.json()
+    if (user_model is None):
+        raise ValueError("Proxy model needs to be set")
+    data["model"] = user_model
+    if user_api_base:
+        data["api_base"] = user_api_base
+    response = litellm.completion(**data)
+    if 'stream' in data and data['stream'] == True: # use generate_responses to stream responses
+            return StreamingResponse(data_generator(response), media_type='text/event-stream')
+    return response
