dbf5f23dee7724e5bc689c9d4141c55beebe2a84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-28 15:09:55 +0000

    Add package surkit/0.0.2/surkit-0.0.2-py3-none-any.whl

---
+++ packages/surkit/surkit-0.0.2-py3-none-any.whl/surkit/backend/backend.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# -*- coding:UTF-8 -*-
+
+# Module
+# Tensor
+
+def is_tensor(obj):
+    pass
+
+def zeros(shape, dtype):
+    pass
+
+def zeros_like(tensor):
+    pass
+
+def cat(tensor_list, dim):
+    pass
+
+def unsqueeze(tensor, dim):
+    pass
+
+def squeeze(tensor, dim):
+    pass
+
+def predict(model, x):
+    pass
+
+def np_to_tensor(array):
+    pass
+
+def sin(tensor):
+    pass
+
+def cos(tensor):
+    pass
+
+def tan(tensor):
+    pass
+
+def power(tensor, exponent):
+    pass
+
+def exp(tensor):
+    pass
+
+def log(tensor):
+    pass
+
+def sqrt(tensor):
+    pass
+
+def grad():
+    pass
+
+def save():
+    pass
