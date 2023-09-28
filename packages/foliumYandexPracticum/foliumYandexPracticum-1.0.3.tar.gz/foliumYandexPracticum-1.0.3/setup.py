c0c28538a250ac227ccf29abd2a35fcfba8aebee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-28 14:07:07 +0000

    Add package foliumYandexPracticum/1.0.3/foliumYandexPracticum-1.0.3.tar.gz

---
+++ packages/foliumYandexPracticum/foliumYandexPracticum-1.0.3.tar.gz/foliumYandexPracticum-1.0.3/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+def readme():
+  with open('README.md', 'r') as f:
+    return f.read()
+
+setup(
+  name='foliumYandexPracticum',
+  version='1.0.3',
+  author='data.practicum',
+  author_email='data.practicum@yandex.ru',
+  description='Folium library with local version of map',
+  long_description=readme(),
+  long_description_content_type='text/markdown',
+  url='https://python-visualization.github.io/',
+  packages=find_packages(),
+  install_requires=['branca',
+                    'Jinja2',
+                    'numpy',
+                    'requests'],
+  classifiers=[
+    'Programming Language :: Python :: 3.11',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent'
+  ],
+  keywords='example python',
+  project_urls={
+    'Documentation': 'https://python-visualization.github.io/folium/latest/user_guide.html'
+  },
+  python_requires='>=3.7'
+)
