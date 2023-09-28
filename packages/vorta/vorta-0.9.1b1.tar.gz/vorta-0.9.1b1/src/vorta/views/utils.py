b9000e07c0b55a2aa3f6899c26f4854ca36355d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-28 03:09:18 +0000

    Add package vorta/0.9.1b1/vorta-0.9.1b1.tar.gz

---
+++ packages/vorta/vorta-0.9.1b1.tar.gz/vorta-0.9.1b1/src/vorta/views/utils.py
@@ -0,0 +1,17 @@
+from PyQt6.QtGui import QIcon, QImage, QPixmap
+
+from vorta.utils import get_asset, uses_dark_mode
+
+
+def get_colored_icon(icon_name):
+    """
+    Return SVG icon in the correct color.
+    """
+    with open(get_asset(f"icons/{icon_name}.svg"), 'rb') as svg_file:
+        svg_str = svg_file.read()
+    if uses_dark_mode():
+        svg_str = svg_str.replace(b'#000000', b'#ffffff')
+    # Reduce image size to 128 height
+    svg_img = QImage.fromData(svg_str).scaledToHeight(128)
+
+    return QIcon(QPixmap(svg_img))
