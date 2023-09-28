6b0e560847ecb93f098e8bee293d63cb3c5f6c99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-28 14:08:33 +0000

    Add package subtractAirglow/0.1.0.1/subtractAirglow-0.1.0.1.tar.gz

---
+++ packages/subtractAirglow/subtractAirglow-0.1.0.1.tar.gz/subtractAirglow-0.1.0.1/setup.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct  5 13:39:44 2022
+
+@author: edcr4756
+"""
+
+from setuptools import setup, find_packages
+
+with open('requirements.txt') as f:
+      requirements=f.read().splitlines()
+      
+exec(open('subtractAirglow/version.py').read())
+
+setup(
+      name='subtractAirglow',
+      version=__version__,
+      description='Subtract airglow and reconstruct stellar emission from COS G130M spectra',
+      url='https://github.com/AstroAguirre/subtractAirglow.git',
+      download_url='https://github.com/AstroAguirre/subtractAirglow/archive/refs/tags/v0.1.0.tar.gz',
+      author='Fernando Cruz Aguirre',
+      author_email='edwinfernando-cruzaguirre@uiowa.edu',
+      license='MIT',
+      packages=find_packages(),
+      package_data={'subtractAirglow' : ['*.dat','*.npy','*.webp']},
+      include_package_data=True,
+      install_requires=requirements
+      )
