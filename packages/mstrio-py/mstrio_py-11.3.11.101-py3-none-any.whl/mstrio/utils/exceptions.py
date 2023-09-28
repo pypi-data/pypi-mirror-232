dad6725007ac2ee055b1475711630351399fb9f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-28 03:07:29 +0000

    Add package mstrio-py/11.3.11.101/mstrio_py-11.3.11.101-py3-none-any.whl

---
+++ packages/mstrio-py/mstrio_py-11.3.11.101-py3-none-any.whl/mstrio/utils/exceptions.py
@@ -0,0 +1,9 @@
+from mstrio.helpers import NotSupportedError  # NOQA
+from mstrio.utils.helper import deprecation_warning
+
+deprecation_warning(
+    deprecated=("mstrio.utils.exceptions"),
+    new=("mstrio.helpers"),
+    version='11.3.11.103',  # NOSONAR
+    module=True,
+)
