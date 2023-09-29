a9ebdbfbafe4193995c504aba91afda94dfd9f57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-29 03:08:33 +0000

    Add package dbtlogs-prebuilt/0.4/dbtlogs-prebuilt-0.4.tar.gz

---
+++ packages/dbtlogs-prebuilt/dbtlogs-prebuilt-0.4.tar.gz/dbtlogs-prebuilt-0.4/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='dbtlogs-prebuilt',
+    version='0.4',
+    packages=find_packages(),
+    install_requires=[
+        'click',
+        'databricks-sql-connector',
+        'requests',
+        'pytz',
+        'typing',
+        'datetime',
+        'apache-airflow'
+    ],
+    entry_points={
+        'console_scripts': [
+            'dbtlog = dbtlogs.main:dbtlog',
+        ],
+    },
+)
