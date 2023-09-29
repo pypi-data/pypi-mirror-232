f34bbb2713c3ca5502f3a5434760d98267004563
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-29 15:07:49 +0000

    Add package access-nri-intake/0.0.10/access_nri_intake-0.0.10.tar.gz

---
+++ packages/access-nri-intake/access_nri_intake-0.0.10.tar.gz/access_nri_intake-0.0.10/src/access_nri_intake/catalog/__init__.py
@@ -0,0 +1,32 @@
+# Copyright 2023 ACCESS-NRI and contributors. See the top-level COPYRIGHT file for details.
+# SPDX-License-Identifier: Apache-2.0
+
+""" Tools for managing intake-dataframe-catalogs like the ACCESS-NRI catalog """
+
+
+from ..utils import get_jsonschema
+
+CORE_COLUMNS = [
+    "name",
+    "model",
+    "description",
+    "realm",
+    "frequency",
+    "variable",
+]
+YAML_COLUMN = "yaml"
+NAME_COLUMN = "name"
+TRANSLATOR_GROUPBY_COLUMNS = ["model", "realm", "frequency"]
+
+SCHEMA_URL = "https://raw.githubusercontent.com/ACCESS-NRI/schema/d4da77a0e627775c11ba394c0a3f72a2c654971c/experiment_asset.json"
+SCHEMA_HASH = "b18cf5bdd06a6f5bcdc71dfc80f7336c63eb49f6d6f75c2cd3371e59eee5488b"
+
+EXP_JSONSCHEMA, CATALOG_JSONSCHEMA = get_jsonschema(
+    url=SCHEMA_URL, known_hash=SCHEMA_HASH, required=CORE_COLUMNS
+)
+
+COLUMNS_WITH_ITERABLES = [
+    col
+    for col in CORE_COLUMNS
+    if CATALOG_JSONSCHEMA["properties"][col]["type"] == "array"
+]
