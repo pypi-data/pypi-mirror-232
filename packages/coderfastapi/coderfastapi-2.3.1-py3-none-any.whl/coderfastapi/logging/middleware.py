72fc75e828470e2afd512add4a2c0872bbab891f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-29 04:06:42 +0000

    Add package coderfastapi/2.3.1/coderfastapi-2.3.1-py3-none-any.whl

---
+++ packages/coderfastapi/coderfastapi-2.3.1-py3-none-any.whl/coderfastapi/logging/middleware.py
@@ -0,0 +1,43 @@
+from contextvars import ContextVar
+
+from fastapi.logger import logger
+from starlette.middleware.base import BaseHTTPMiddleware, RequestResponseEndpoint
+from starlette.requests import Request
+from starlette.responses import JSONResponse, Response
+
+from coderfastapi.lib.validation.schemas.request import HTTPRequestSchema
+from coderfastapi.logging.context import CloudTraceContext
+
+TRACE_HEADER = "x-cloud-trace-context"
+
+
+class LoggingMiddleware(BaseHTTPMiddleware):
+    cloud_trace_context: CloudTraceContext
+    http_request_context: ContextVar
+
+    def __init__(
+        self,
+        cloud_trace_context: CloudTraceContext,
+        http_request_context: ContextVar,
+        *args,
+        **kwargs,
+    ) -> None:
+        super().__init__(*args, **kwargs)
+        self.cloud_trace_context = cloud_trace_context
+        self.http_request_context = http_request_context
+
+    async def dispatch(
+        self, request: Request, call_next: RequestResponseEndpoint
+    ) -> Response:
+        if TRACE_HEADER in request.headers:
+            self.cloud_trace_context.set(request.headers[TRACE_HEADER])
+
+        self.http_request_context.set(HTTPRequestSchema.from_request(request))
+
+        try:
+            return await call_next(request)
+        except Exception as e:
+            logger.debug(f"Request failed: {e}")
+            return JSONResponse(
+                status_code=500, content={"success": False, "message": e}
+            )
