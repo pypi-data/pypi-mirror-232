1dae1d4dab6fd32f4e72516e99887398051ecb87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-29 03:06:47 +0000

    Add package pyadtpulse/1.1.0/pyadtpulse-1.1.0.tar.gz

---
+++ packages/pyadtpulse/pyadtpulse-1.1.0.tar.gz/pyadtpulse-1.1.0/setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+import os
+import sys
+from pathlib import Path
+
+import setuptools
+
+if sys.argv[-1] == "publish":
+    os.system("python setup.py sdist upload")
+    sys.exit()
+
+# read the contents of your README file
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+long_description = Path(
+    path.join(this_directory, "README.md"), encoding="utf-8"
+).read_text()
+
+setuptools.setup(
+    name="pyadtpulse",
+    version="1.1.0",
+    packages=["pyadtpulse"],
+    description="Python interface for ADT Pulse security systems",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/rsnodgrass/pyadtpulse",
+    author="",
+    author_email="",
+    license="Apache Software License",
+    install_requires=["aiohttp>=3.8.1", "uvloop>=0.17.0"],
+    keywords=["security system", "adt", "home automation", "security alarm"],
+    zip_safe=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+)
