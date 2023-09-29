8ce97de3b392e23a60b714f155821f79ae38ea2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-29 04:06:33 +0000

    Add package ada-py/0.0.44/ada-py-0.0.44.tar.gz

---
+++ packages/ada-py/ada-py-0.0.44.tar.gz/ada-py-0.0.44/src/ada/fem/formats/code_aster/write/write_steps.py
@@ -0,0 +1,22 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from ada.fem import StepEigen, StepImplicit
+
+if TYPE_CHECKING:
+    from ada.api.spatial import Part
+
+from .steps import eigen, static
+
+
+def create_step_str(step: StepEigen | StepImplicit, part: Part) -> str:
+    st = StepEigen.TYPES
+    step_map = {st.STATIC: static.step_static_str, st.EIGEN: eigen.step_eig_str}
+
+    step_writer = step_map.get(step.type, None)
+
+    if step_writer is None:
+        raise NotImplementedError(f'Step type "{step.type}" is not yet supported')
+
+    return step_writer(step, part)
