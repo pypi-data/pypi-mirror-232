8917e2f7bb8af9a02a4a3fb3c0e1122431d63d4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-29 04:06:45 +0000

    Add package pypponi/0.0.1/pypponi-0.0.1.tar.gz

---
+++ packages/pypponi/pypponi-0.0.1.tar.gz/pypponi-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='pypponi',
+    version='0.0.1',
+    packages=['pypponi'],
+    url='https://github.com/RickySal/Pypponi',
+    license='GNU General Public License v3.0',
+    author='RickySal',
+    author_email='41027013+RickySal@users.noreply.github.com',
+    description='A modular data pipeline for Python'
+)
