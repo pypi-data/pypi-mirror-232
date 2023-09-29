a1131c16a6a361c6921a5495c8e023447bff5454
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-29 14:07:34 +0000

    Add package pyconify/0.1.0/pyconify-0.1.0-py3-none-any.whl

---
+++ packages/pyconify/pyconify-0.1.0-py3-none-any.whl/pyconify/__init__.py
@@ -0,0 +1,36 @@
+"""iconify for python. Universal icon framework."""
+
+from importlib.metadata import PackageNotFoundError, version
+
+try:
+    __version__ = version("pyconify")
+except PackageNotFoundError:  # pragma: no cover
+    __version__ = "uninstalled"
+
+__author__ = "Talley Lambert"
+__email__ = "talley.lambert@gmail.com"
+__all__ = [
+    "collection",
+    "collections",
+    "css",
+    "icon_data",
+    "iconify_version",
+    "keywords",
+    "last_modified",
+    "search",
+    "svg",
+    "temp_svg",
+]
+
+from .api import (
+    collection,
+    collections,
+    css,
+    icon_data,
+    iconify_version,
+    keywords,
+    last_modified,
+    search,
+    svg,
+    temp_svg,
+)
