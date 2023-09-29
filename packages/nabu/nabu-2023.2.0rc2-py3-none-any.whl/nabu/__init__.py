e867d1ffaa81b14c408ec8a63d5c74e35ff456b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-29 03:09:08 +0000

    Add package nabu/2023.2.0rc2/nabu-2023.2.0rc2-py3-none-any.whl

---
+++ packages/nabu/nabu-2023.2.0rc2-py3-none-any.whl/nabu/__init__.py
@@ -0,0 +1,16 @@
+__version__ = "2023.2.0-rc2"
+__nabu_modules__ = [
+    "app",
+    "cuda",
+    "estimation",
+    "io",
+    "misc",
+    "opencl",
+    "pipeline",
+    "preproc",
+    "reconstruction",
+    "resources",
+    "thirdparty",
+    "stitching",
+]
+version = __version__
