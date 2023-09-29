98398fb1202ed35679b25010df5f7e39e6002c5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-29 14:08:02 +0000

    Add package fastapi-exception/1.1.0/fastapi_exception-1.1.0.tar.gz

---
+++ packages/fastapi-exception/fastapi_exception-1.1.0.tar.gz/fastapi_exception-1.1.0/fastapi_exception/custom_errors/string_too_long_error.py
@@ -0,0 +1,17 @@
+from typing import Any, Optional
+
+from .validation_error_detail import ValidationErrorDetail
+
+
+class StringTooLongError(ValidationErrorDetail):
+    error_type = 'string_too_long'
+
+    def __init__(
+        self,
+        max_length: int,
+        loc: tuple[int | str, ...],
+        ctx: Optional[dict[str, dict[str, Any]]] = {},
+        input: dict[str, Any] = {},
+    ):
+        ctx = ctx or {'max_length': max_length}
+        super().__init__(self.error_type, loc, '', input, ctx)
