e25f160036d444efd1f7637c0e10f6c2b9653e2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-29 03:07:18 +0000

    Add package ByteBeat/0.1/ByteBeat-0.1.tar.gz

---
+++ packages/ByteBeat/ByteBeat-0.1.tar.gz/ByteBeat-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='ByteBeat',
+      version='0.1',
+      description='Run ByteBeat in Python 3!',
+      author='Itzsten',
+      packages=['ByteBeat'],
+      zip_safe=False)
