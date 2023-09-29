df02480ae46cb0cdf606dfc69b5953a5aeed5df5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-29 03:10:05 +0000

    Add package pyweatherflow-forecast/0.3.4/pyweatherflow_forecast-0.3.4-py3-none-any.whl

---
+++ packages/pyweatherflow-forecast/pyweatherflow_forecast-0.3.4-py3-none-any.whl/pyweatherflow_forecast/__init__.py
@@ -0,0 +1,20 @@
+"""Python Wrapper for WeatherFlow Forecast API."""
+from __future__ import annotations
+from pyweatherflow_forecast.wffcst_lib import (
+    WeatherFlow,
+    WeatherFlowForecastInternalServerError,
+    WeatherFlowForecastBadRequest,
+    WeatherFlowForecastUnauthorized,
+    WeatherFlowForecastWongStationId,
+)
+from pyweatherflow_forecast.data import (
+    WeatherFlowForecastData,
+    WeatherFlowForecastDaily,
+    WeatherFlowForecastHourly,
+    WeatherFlowStationData,
+)
+
+__title__ = "pyweatherflow_forecast"
+__version__ = "0.3.4"
+__author__ = "briis"
+__license__ = "MIT"
