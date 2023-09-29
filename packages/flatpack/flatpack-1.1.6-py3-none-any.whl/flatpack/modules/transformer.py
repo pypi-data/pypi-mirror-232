ed4f6ff726eb97124310812127da0789bd104e90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-29 05:07:27 +0000

    Add package flatpack/1.1.6/flatpack-1.1.6-py3-none-any.whl

---
+++ packages/flatpack/flatpack-1.1.6-py3-none-any.whl/flatpack/modules/transformer.py
@@ -0,0 +1,56 @@
+import torch
+import torch.nn as nn
+from torch.nn.functional import pad
+from .base import Base
+
+
+class Transformer(Base):
+    def __init__(self, d_model, nhead, num_encoder_layers, num_decoder_layers, vocab_size=None):
+        super(Transformer, self).__init__(d_model, vocab_size)
+        self.nhead = nhead
+        self.num_encoder_layers = num_encoder_layers
+        self.num_decoder_layers = num_decoder_layers
+        self.transformer = nn.Transformer(d_model, nhead, num_encoder_layers, num_decoder_layers)
+
+    def forward(self, src, tgt):
+        src = self.embedding(src)
+        tgt = self.embedding(tgt)
+        out = self.transformer(src, tgt)
+        out = out.reshape(out.size(0) * out.size(1), -1)
+        out = self.fc(out)
+        return out
+
+    @classmethod
+    def train_model(cls, indexed_text, seq_length, vocab_size, epochs=100, batch_size=64, device='cpu', **kwargs):
+        model = cls(**kwargs, vocab_size=vocab_size)
+        model.to(device)
+        sos_token_index = 0
+
+        base_result = super(Transformer, cls).train_model(indexed_text, seq_length, vocab_size, epochs, batch_size,
+                                                          device, **kwargs)
+
+        dataloader = base_result.get('dataloader')
+
+        if dataloader is None:
+            raise ValueError("Dataloader is not available from the Base class's train_model method.")
+
+        criterion = nn.CrossEntropyLoss()
+        optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+        for inputs, targets in dataloader:
+            inputs = inputs.to(device)
+            targets = targets.to(device)
+
+            sos_token = torch.full((1, targets.size(1)), sos_token_index, dtype=torch.long, device=device)
+            src = pad(targets[:-1], (0, 0, 1, 0), value=sos_token_index)
+            src = torch.cat((sos_token, src), dim=0)
+
+            outputs = model(src, targets)
+
+            loss = criterion(outputs.view(-1, vocab_size), targets.view(-1))
+            optimizer.zero_grad()
+            loss.backward()
+            torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1)
+            optimizer.step()
+
+        return base_result
