f8dcee750c8c4dc44e5b32b94f4daa7bed88313c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-29 04:07:20 +0000

    Add package krx-competition-37/1.0.0/krx_competition_37-1.0.0.tar.gz

---
+++ packages/krx-competition-37/krx_competition_37-1.0.0.tar.gz/krx_competition_37-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='krx_competition_37',
+    version='1.0.0',
+    description='krx_competition_37',
+    author='sjyoo',
+    author_email='',
+    url='',
+    python_requires='>=3.10'
+)
