cd7db5ccdc4c890a52642621026269033e6a2a9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-29 05:07:23 +0000

    Add package omicverse/1.5.1/omicverse-1.5.1-py3-none-any.whl

---
+++ packages/omicverse/omicverse-1.5.1-py3-none-any.whl/omicverse/__init__.py
@@ -0,0 +1,19 @@
+r"""
+Pyomic (A omic framework for multi-omic analysis)
+"""
+
+try:
+    from importlib.metadata import version
+except ModuleNotFoundError:
+    from pkg_resources import get_distribution
+    version = lambda name: get_distribution(name).version
+
+from . import bulk,single,mofapy2,utils,bulk2single,pp
+#usually
+from .utils._data import read
+from .utils._plot import palette,ov_plot_set,plot_set
+
+name = "omicverse"
+__version__ = version(name)
+
+
