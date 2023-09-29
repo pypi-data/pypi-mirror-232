6f45204af836a25810b5bd51d2f0e49440940eb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-29 04:07:32 +0000

    Add package behavioral-signals-swagger-client/4.3.2/behavioral_signals_swagger_client-4.3.2-py3-none-any.whl

---
+++ packages/behavioral-signals-swagger-client/behavioral_signals_swagger_client-4.3.2-py3-none-any.whl/swagger_client/api/__init__.py
@@ -0,0 +1,9 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from swagger_client.api.api_status_api import APIStatusApi
+from swagger_client.api.administration_api import AdministrationApi
+from swagger_client.api.behavioral_profiling_api import BehavioralProfilingApi
+from swagger_client.api.call_processing_api import CallProcessingApi
