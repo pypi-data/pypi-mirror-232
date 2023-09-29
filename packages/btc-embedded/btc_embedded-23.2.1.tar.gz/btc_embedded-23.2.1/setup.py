a186293c1ea38614c01388657e4fe4d2078f92f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-29 04:06:02 +0000

    Add package btc-embedded/23.2.1/btc_embedded-23.2.1.tar.gz

---
+++ packages/btc-embedded/btc_embedded-23.2.1.tar.gz/btc_embedded-23.2.1/setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+
+setup(
+    name='btc_embedded',
+    version='23.2.1',
+    packages=['btc_embedded'],
+    license='MIT',
+    description='API wrapper for BTC EmbeddedPlatform 23.2p0 REST API',
+    author='Thabo Krick',
+    author_email='thabo.krick@btc-embedded.com',
+    url='https://github.com/btc-embedded/btc-ci-workflow/examples/api',
+    install_requires=[ 'requests', 'pyyaml' ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Testing',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+    ],
+)
