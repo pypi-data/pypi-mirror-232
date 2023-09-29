1b871a06dd25fb2d2b828cfb8b0a8941f7b7b31c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-29 05:07:26 +0000

    Add package patternengine/0.0.4/patternengine-0.0.4-py3-none-any.whl

---
+++ packages/patternengine/patternengine-0.0.4-py3-none-any.whl/patternengine/__init__.py
@@ -0,0 +1,6 @@
+"""A package to emit bullet patterns in bullet hell style shmups."""
+
+import patternengine.bullets  # noqa: F401
+
+from patternengine.engine import *  # noqa: F401, F403
+from patternengine.bullet import *  # noqa: F401, F403
