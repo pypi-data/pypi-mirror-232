b17a546ee16b4e56e00113a48ab734534b8f0018
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-29 04:06:32 +0000

    Add package bloqade/0.5.1/bloqade-0.5.1.tar.gz

---
+++ packages/bloqade/bloqade-0.5.1.tar.gz/bloqade-0.5.1/src/bloqade/__init__.py
@@ -0,0 +1,47 @@
+from bloqade.ir import var, cast, Variable, Literal, start
+from bloqade.serialize import load, save, loads, dumps
+
+from bloqade.builder.factory import (
+    piecewise_linear,
+    piecewise_constant,
+    linear,
+    constant,
+)
+import bloqade.ir as _ir
+from bloqade.constants import RB_C6
+
+
+def tree_depth(depth: int = None):
+    """Setting globally maximum depth for tree printing
+
+    If `depth=None`, return current depth.
+    If `depth` is provided, setting current depth to `depth`
+
+    Args:
+        depth (int, optional): the user specified depth. Defaults to None.
+
+    Returns:
+        int: current updated depth
+    """
+    if depth is not None:
+        _ir.tree_print.max_tree_depth = depth
+    return _ir.tree_print.max_tree_depth
+
+
+__all__ = [
+    "RB_C6",
+    "start",
+    "var",
+    "cast",
+    "Variable",
+    "Literal",
+    "piecewise_linear",
+    "piecewise_constant",
+    "linear",
+    "constant",
+    "set_print_depth",
+    "load",
+    "save",
+    "loads",
+    "dumps",
+]
