3c183c0683a49f3df11d491df87cf762b3ee2058
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-29 04:06:00 +0000

    Add package sphinxcontrib-hep-pdgref/0.1.5/sphinxcontrib-hep-pdgref-0.1.5.tar.gz

---
+++ packages/sphinxcontrib-hep-pdgref/sphinxcontrib-hep-pdgref-0.1.5.tar.gz/sphinxcontrib-hep-pdgref-0.1.5/README.md
@@ -0,0 +1,3 @@
+# PDG role for Sphinx
+
+_This package has been renamed to **[`sphinx-hep-pdgref`](https://pypi.org/project/sphinx-hep-pdgref)**! Please use that package instead._
---
+++ packages/sphinxcontrib-hep-pdgref/sphinxcontrib-hep-pdgref-0.1.5.tar.gz/sphinxcontrib-hep-pdgref-0.1.5/tests/conftest.py
@@ -0,0 +1,11 @@
+# cspell:ignore rootdir
+import pytest
+from sphinx.testing.path import path
+
+pytest_plugins = "sphinx.testing.fixtures"
+collect_ignore = ["roots"]
+
+
+@pytest.fixture(scope="session")
+def rootdir() -> path:
+    return path(__file__).parent.abspath() / "roots"
