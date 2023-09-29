ce829942a4bd496e404d05dd2dd37637c0e56fe7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-29 14:08:03 +0000

    Add package cd2t/2.2.0/cd2t-2.2.0-py3-none-any.whl

---
+++ packages/cd2t/cd2t-2.2.0-py3-none-any.whl/cd2t/__init__.py
@@ -0,0 +1,8 @@
+# pylint: disable=missing-module-docstring
+import cd2t.data_parser
+import cd2t.schema
+from cd2t.errors import *
+from cd2t.results import *
+
+__version__ = "2.2.0"
+version_info = (2, 2, 0)
