e272d527c9f382f4c733e0b0cb413d4fad9dbb04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-29 14:07:53 +0000

    Add package hashcracker/0.0.0b0/hashcracker-0.0.0b0.tar.gz

---
+++ packages/hashcracker/hashcracker-0.0.0b0.tar.gz/hashcracker-0.0.0b0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='hashcracker',
+    version='0.0.0 Beta',
+    description='A Python tool for analyzing and cracking hashes.',
+    author='Md. Shaykhul Islam',
+    author_email='shaykhul@2004.com',
+    url='https://github.com/SHAYKHUL/hashcracker',
+    license='MIT',
+    packages=find_packages(),
+    install_requires=[
+        'bcrypt>=3.2.0', 
+        'pyopencl>=2022.1.1', 
+        'numpy>=1.21.0',  
+    ],
+    entry_points={
+        'console_scripts': [
+            'hashcracker = hashcracker.hashcracker:main', 
+        ],
+    },
+)
