7fbfaa361fa947ad0aa32e53844e677a5f72e6e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-29 04:06:29 +0000

    Add package controllogger/0.2.8.13/controllogger-0.2.8.13.tar.gz

---
+++ packages/controllogger/controllogger-0.2.8.13.tar.gz/controllogger-0.2.8.13/src/controllogger/misc/filter.py
@@ -0,0 +1,10 @@
+from __future__ import annotations
+
+from controllogger.enums.operator import Operator
+from controllogger.misc.base_dataclass import BaseDataclass
+
+
+class Filter(BaseDataclass):
+    key: str
+    value: bool | int | float | str | list[bool | int | float | str] | dict[str, bool | int | float | str]
+    operator: Operator = Operator.EQUALS
