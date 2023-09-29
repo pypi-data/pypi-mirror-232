f4100bbd2a4660177df9ae4b1c55cb287a1c12a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-29 04:06:24 +0000

    Add package polygon-neighbours/1.0/polygon_neighbours-1.0.tar.gz

---
+++ packages/polygon-neighbours/polygon_neighbours-1.0.tar.gz/polygon_neighbours-1.0/setup.py
@@ -0,0 +1,31 @@
+from distutils.core import setup, Extension
+
+polygon_module = Extension('polygon_neighbours',
+                           sources = ['src/polygon_neighbours/find_neighbours.cpp'],
+                     #       include_dirs = ['src/polygon_neighbours'],
+                     #       extra_compile_args = ['-DDOCKER_BUILD', '-Isrc/polygon_neighbours', '-Isrc/polygon_neighbours/cnpy.h']
+                           )
+
+long_description = """Various CPython extensions for manipulating and analysing
+polygons.
+"""
+
+setup (name = 'polygon_neighbours',
+       version = '1.0',
+       description = 'CPython extension for finding bordering polygons.',
+       url = 'https://github.com/sgaebel/polygon_tools',
+       author = 'Dr. Sebastian M. Gaebel',
+       author_email = 'gaebel.sebastian@gmail.com',
+       license = 'MIT',
+       ext_modules = [polygon_module],
+       long_description = long_description,
+       install_requires = ['numpy'],
+       classifiers = [
+              'Development Status :: 5 - Production/Stable',
+              'Intended Audience :: Developers',
+              'Topic :: Scientific/Engineering :: Mathematics',
+              'License :: OSI Approved :: MIT License',
+              'Programming Language :: Python :: 3.10',
+              'Operating System :: POSIX :: Linux'],
+       keywords = 'geometry polygon',
+       python_requires = '>=3.10')
