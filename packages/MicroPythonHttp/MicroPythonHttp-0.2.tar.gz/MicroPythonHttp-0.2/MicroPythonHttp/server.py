efbc4c1d189b8d57bf63a60dcfc52309ad5089ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-29 14:08:12 +0000

    Add package MicroPythonHttp/0.2/MicroPythonHttp-0.2.tar.gz

---
+++ packages/MicroPythonHttp/MicroPythonHttp-0.2.tar.gz/MicroPythonHttp-0.2/MicroPythonHttp/server.py
@@ -0,0 +1,49 @@
+import socket
+import os
+import sys
+
+
+def handle_request(client_socket):
+    request = client_socket.recv(1024).decode("utf-8")
+    headers = request.split("\n")
+    filename = headers[0].split()[1]
+
+    if filename == "/":
+        filename = "/index.html"
+
+    try:
+        with open(os.getcwd() + filename, "rb") as f:
+            content = f.read()
+            response = "HTTP/1.1 200 OK\n".encode() + content
+    except FileNotFoundError:
+        with open(os.path.dirname(os.path.abspath(__file__)) + "/404.html", "rb") as f:
+            content = f.read()
+            response = "HTTP/1.1 404 NOT FOUND\n\n".encode() + content
+
+    client_socket.send(response)
+    client_socket.close()
+
+
+class Server:
+
+    def __init__(self, port=8080):
+        self.port = port
+        self.started = False
+
+    def start(self):
+        server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server_socket.bind(("", self.port))
+        server_socket.listen(5)
+        self.started = True
+
+        while self.started:
+            client_socket, addr = server_socket.accept()
+            handle_request(client_socket)
+
+    if __name__ == "__main__":
+        try:
+            start()
+        except KeyboardInterrupt:
+            started = False
+            print("\nServer shutting down.")
+            sys.exit(0)
