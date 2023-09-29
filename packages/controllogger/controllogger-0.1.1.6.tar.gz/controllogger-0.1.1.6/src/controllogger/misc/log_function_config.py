3ca8886aa6a32f1b3840039e64705beb43b7635a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-29 03:08:21 +0000

    Add package controllogger/0.1.1.6/controllogger-0.1.1.6.tar.gz

---
+++ packages/controllogger/controllogger-0.1.1.6.tar.gz/controllogger-0.1.1.6/src/controllogger/misc/log_function_config.py
@@ -0,0 +1,15 @@
+import dataclasses
+
+from controllogger.enums.log_levels import LogLevels
+from controllogger.misc.base_dataclass import BaseDataclass
+
+
+@dataclasses.dataclass
+class LogFuntionConfig(BaseDataclass):
+    header: bool = True  # Log function header. Default is True.
+    footer: bool = True  # Log function footer. Default is True.
+    header_footer_level: LogLevels = LogLevels.DEBUG  # Log function header and footer level. Default is DEBUG.
+    start_msg: str | None = None  # Log function start message. Default is None.
+    end_msg: str | None = None  # Log function end message. Default is None.
+    start_end_msg_level: LogLevels = LogLevels.INFO  # Log function start message level. Default is DEBUG.
+    measure_time: bool = False  # Log function measure time. Default is False.
