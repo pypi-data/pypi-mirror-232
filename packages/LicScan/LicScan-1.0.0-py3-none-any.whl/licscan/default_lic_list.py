290566af75f9bfd1bacea701738ca9ecb9bd18af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-29 14:08:26 +0000

    Add package LicScan/1.0.0/LicScan-1.0.0-py3-none-any.whl

---
+++ packages/LicScan/LicScan-1.0.0-py3-none-any.whl/licscan/default_lic_list.py
@@ -0,0 +1,8 @@
+"""
+LicScan by @fakerybakery.
+License: NOSCL-C-2.0.
+Please see the source code repositories for documentation, full license text, and more: https://github.com/fakerybakery/licscan
+
+VERSION 1.0.0
+"""
+default_licenses = ["MIT", "ISC", "BSD", "Apache", "Unlicense", "WTFPL", "LGPL", "BSL", "CC0", "NOSCL-C"]
