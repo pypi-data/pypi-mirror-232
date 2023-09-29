5e40c17e27738e4200a3bc294c574c265e0cfd3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-29 03:09:02 +0000

    Add package ais-dom/2023.10.0b1/ais-dom-2023.10.0b1.tar.gz

---
+++ packages/ais-dom/ais-dom-2023.10.0b1.tar.gz/ais-dom-2023.10.0b1/homeassistant/components/safe_mode/__init__.py
@@ -0,0 +1,22 @@
+"""The Safe Mode integration."""
+from homeassistant.components import persistent_notification
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers import config_validation as cv
+from homeassistant.helpers.typing import ConfigType
+
+DOMAIN = "safe_mode"
+
+CONFIG_SCHEMA = cv.empty_config_schema(DOMAIN)
+
+
+async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
+    """Set up the Safe Mode component."""
+    persistent_notification.async_create(
+        hass,
+        (
+            "Asystent domowy działa w trybie awaryjnym. Sprawdź w [logach"
+            " systemu](/config/logs) co poszło nie tak."
+        ),
+        "Tryb Awaryjny",
+    )
+    return True
