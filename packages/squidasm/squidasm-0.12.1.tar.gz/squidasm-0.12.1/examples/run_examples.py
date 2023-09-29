20c5bbf1c8ce788ea730a1e4897ab64f50ef9328
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-29 03:08:10 +0000

    Add package squidasm/0.12.1/squidasm-0.12.1.tar.gz

---
+++ packages/squidasm/squidasm-0.12.1.tar.gz/squidasm-0.12.1/examples/run_examples.py
@@ -0,0 +1,35 @@
+import inspect
+import logging
+import os
+import subprocess
+
+from netqasm.logging.glob import set_log_level
+
+
+def _has_first_argument(function, argument):
+    """Checks if a function takes a named argument as the first argument"""
+    argnames = inspect.getfullargspec(function).args
+    return argnames[0] == "no_output"
+
+
+def main():
+    set_log_level(logging.WARNING)
+    path_to_here = os.path.dirname(os.path.abspath(__file__))
+    for root, _folders, files in os.walk(path_to_here):
+        for filename in files:
+            if (
+                filename.startswith("example") and filename.endswith(".py")
+            ) or filename == "run_simulation.py":
+                filepath = os.path.join(root, filename)
+                print(f"Running example {filepath}")
+                result = subprocess.run(
+                    ["python3", filepath], stdout=subprocess.DEVNULL, cwd=root
+                )
+                if result.returncode != 0:
+                    raise RuntimeError(f"Example {filepath} failed!")
+
+    print("All examples work!")
+
+
+if __name__ == "__main__":
+    main()
