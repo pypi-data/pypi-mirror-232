72a1a4cad19cd50bbee1ff48efb05eaf6d6cce3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-29 04:07:19 +0000

    Add package tests4py/0.0.6/tests4py-0.0.6-py3-none-any.whl

---
+++ packages/tests4py/tests4py-0.0.6-py3-none-any.whl/tests4py/projects/calculator.py
@@ -0,0 +1,77 @@
+import os
+from pathlib import Path
+from typing import List, Optional
+
+from tests4py.constants import PYTHON
+from tests4py.grammars.fuzzer import Grammar, is_valid_grammar
+from tests4py.projects import Project, Status, TestingFramework, TestStatus
+from tests4py.tests.generator import UnittestGenerator, SystemtestGenerator
+from tests4py.tests.utils import API
+
+PROJECT_MAME = "calculator"
+
+
+class Calculator(Project):
+    def __init__(
+        self,
+        bug_id: int,
+        buggy_commit_id: str,
+        fixed_commit_id: str,
+        test_files: List[Path],
+        test_cases: List[str],
+        test_status_fixed: TestStatus = TestStatus.PASSING,
+        test_status_buggy: TestStatus = TestStatus.FAILING,
+        unittests: Optional[UnittestGenerator] = None,
+        systemtests: Optional[SystemtestGenerator] = None,
+        api: Optional[API] = None,
+        loc: int = 0,
+    ):
+        super().__init__(
+            bug_id=bug_id,
+            project_name=PROJECT_MAME,
+            github_url="https://github.com/smythi93/calculator",
+            status=Status.OK,
+            python_version="3.10.9",
+            python_path="",
+            buggy_commit_id=buggy_commit_id,
+            fixed_commit_id=fixed_commit_id,
+            testing_framework=TestingFramework.PYTEST,
+            test_files=test_files,
+            test_cases=test_cases,
+            test_status_fixed=test_status_fixed,
+            test_status_buggy=test_status_buggy,
+            unittests=unittests,
+            systemtests=systemtests,
+            api=api,
+            grammar=grammar,
+            loc=loc,
+            setup=[[PYTHON, "-m", "pip", "install", "."]],
+            test_base=Path("test"),
+            included_files=[os.path.join("src", "calc")],
+        )
+
+
+def register():
+    Calculator(
+        bug_id=1,
+        buggy_commit_id="5d7f01c5497940b7415db22864100d90c575300f",
+        fixed_commit_id="063d988682e407ad25cd94854f1b4d5e3dc282f8",
+        test_files=[
+            Path("tests", "test_calc.py"),
+        ],
+        test_cases=[
+            os.path.join("tests", "test_calc.py") + "::TestCalc::test_main_sqrt_0",
+            os.path.join("tests", "test_calc.py") + "::TestCalc::test_main_sqrt_neg",
+            os.path.join("tests", "test_calc.py") + "::TestCalc::test_sqrt_0",
+            os.path.join("tests", "test_calc.py") + "::TestCalc::test_sqrt_neg",
+        ],
+        loc=20,
+    )
+
+
+grammar: Grammar = {
+    "<start>": [""],
+}
+
+
+assert is_valid_grammar(grammar)
---
+++ packages/tests4py/tests4py-0.0.6-py3-none-any.whl/tests4py/projects/markup.py
@@ -0,0 +1,85 @@
+import os
+from pathlib import Path
+from typing import List, Optional
+
+from tests4py.constants import PYTHON
+from tests4py.grammars.fuzzer import Grammar, is_valid_grammar
+from tests4py.projects import Project, Status, TestingFramework, TestStatus
+from tests4py.tests.generator import UnittestGenerator, SystemtestGenerator
+from tests4py.tests.utils import API
+
+PROJECT_MAME = "markup"
+
+
+class Markup(Project):
+    def __init__(
+        self,
+        bug_id: int,
+        buggy_commit_id: str,
+        fixed_commit_id: str,
+        test_files: List[Path],
+        test_cases: List[str],
+        test_status_fixed: TestStatus = TestStatus.PASSING,
+        test_status_buggy: TestStatus = TestStatus.FAILING,
+        unittests: Optional[UnittestGenerator] = None,
+        systemtests: Optional[SystemtestGenerator] = None,
+        api: Optional[API] = None,
+        loc: int = 0,
+    ):
+        super().__init__(
+            bug_id=bug_id,
+            project_name=PROJECT_MAME,
+            github_url="https://github.com/smythi93/markup",
+            status=Status.OK,
+            python_version="3.10.9",
+            python_path="",
+            buggy_commit_id=buggy_commit_id,
+            fixed_commit_id=fixed_commit_id,
+            testing_framework=TestingFramework.PYTEST,
+            test_files=test_files,
+            test_cases=test_cases,
+            test_status_fixed=test_status_fixed,
+            test_status_buggy=test_status_buggy,
+            unittests=unittests,
+            systemtests=systemtests,
+            api=api,
+            grammar=grammar,
+            loc=loc,
+            setup=[[PYTHON, "-m", "pip", "install", "."]],
+            included_files=[os.path.join("src", PROJECT_MAME)],
+            test_base=Path("tests"),
+        )
+
+
+def register():
+    Markup(
+        bug_id=1,
+        buggy_commit_id="edce326b0518e08a1f296f8704bf97f4688be3d1",
+        fixed_commit_id="638fdb1cf9f27136629b9240efbed08626f905fd",
+        test_files=[
+            Path("tests", "test_markup.py"),
+        ],
+        test_cases=[os.path.join("tests", "test_markup.py::test_quoted_abc")],
+        loc=14,
+    )
+    Markup(
+        bug_id=2,
+        buggy_commit_id="809eefd11860c0dd5c9b4911c1a8cf17e9e63624",
+        fixed_commit_id="4a9dd7d2230ee361dfbfc7f53eb9e7db8ecaed42",
+        test_files=[
+            Path("tests", "test_markup.py"),
+        ],
+        test_cases=[
+            os.path.join("tests", "test_markup.py::test_abc"),
+            os.path.join("tests", "test_markup.py::test_quoted_abc"),
+        ],
+        loc=14,
+    )
+
+
+grammar: Grammar = {
+    "<start>": [""],
+}
+
+
+assert is_valid_grammar(grammar)
