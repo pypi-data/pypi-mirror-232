478c8ccdf2d74d664f69b58952c0afa23784b600
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-29 04:07:43 +0000

    Add package lincs/0.8.7/lincs-0.8.7.tar.gz

---
+++ packages/lincs/lincs-0.8.7.tar.gz/lincs-0.8.7/lincs/liblincs/learning/ucncs-by-sat-by-separation.hpp
@@ -0,0 +1,62 @@
+// Copyright 2023 Vincent Jacques
+
+#ifndef LINCS__LEARNING__UCNCS_BY_SAT_BY_SEPARATION_HPP
+#define LINCS__LEARNING__UCNCS_BY_SAT_BY_SEPARATION_HPP
+
+#include "../io.hpp"
+
+
+namespace lincs {
+
+template<typename SatProblem>
+class SatSeparationUcncsLearning {
+ public:
+  SatSeparationUcncsLearning(const Problem& problem_, const Alternatives& learning_set_) :
+    problem(problem_),
+    learning_set(learning_set_),
+    criteria_count(problem.criteria.size()),
+    categories_count(problem.categories.size()),
+    boundaries_count(categories_count - 1),
+    alternatives_count(learning_set.alternatives.size()),
+    unique_values(),
+    better_alternative_indexes(),
+    worse_alternative_indexes(),
+    better(),
+    separates(),
+    sat()
+  {}
+
+ public:
+  Model perform();
+
+ private:
+  void sort_values();
+  void partition_alternatives();
+  void create_variables();
+  void add_structural_constraints();
+  void add_learning_set_constraints();
+  Model decode(const std::vector<bool>& solution);
+
+ private:
+  const Problem& problem;
+  const Alternatives& learning_set;
+  const unsigned criteria_count;
+  const unsigned categories_count;
+  const unsigned boundaries_count;
+  const unsigned alternatives_count;
+  std::vector<std::vector<float>> unique_values;
+  // Alternatives better than category k
+  std::vector<std::vector<unsigned>> better_alternative_indexes;
+  // Alternatives in category k or worse
+  std::vector<std::vector<unsigned>> worse_alternative_indexes;
+  // better[criterion_index][boundary_index][value_index]: value is better than profile on criterion
+  std::vector<std::vector<std::vector<typename SatProblem::variable_type>>> better;
+  // separates[criterion_index][boundary_index_a][boundary_index_b][good_alternative_index][bad_alternative_index]:
+  // criterion separates alternatives 'good' and 'bad' with regards to profiles 'a' and 'b'
+  std::vector<std::vector<std::vector<std::vector<std::vector<typename SatProblem::variable_type>>>>> separates;
+  SatProblem sat;
+};
+
+}  // namespace lincs
+
+#endif  // LINCS__LEARNING__UCNCS_BY_SAT_BY_SEPARATION_HPP
