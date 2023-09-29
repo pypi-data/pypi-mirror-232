197ef3673a5c1928a4e09cb10492fa1c06f37c09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-29 15:08:10 +0000

    Add package lectorqr/0.1/lectorqr-0.1.tar.gz

---
+++ packages/lectorqr/lectorqr-0.1.tar.gz/lectorqr-0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="lectorqr",
+    version="0.1",
+    description="as un import lectorqr y listo",
+    author="aslskks",
+    author_email="davikenat@gmail.com",
+    install_requires=[
+        "opencv-python",
+        "pyzbar"
+    ],
+)
