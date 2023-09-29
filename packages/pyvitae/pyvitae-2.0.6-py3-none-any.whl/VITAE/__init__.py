bbc0288d3cd60c84d11bcb990b124e3c8d5930e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-29 04:06:18 +0000

    Add package pyvitae/2.0.6/pyvitae-2.0.6-py3-none-any.whl

---
+++ packages/pyvitae/pyvitae-2.0.6-py3-none-any.whl/VITAE/__init__.py
@@ -0,0 +1,13 @@
+__all__ = ['VITAE']
+
+from VITAE.VITAE import VITAE
+from VITAE.utils import load_data
+
+__author__ = "Jin-Hong Du, Tianyu Chen, Ming Gao, and Jingshu Wang"
+__copyright__ = "Copyright 2020, The Trajectory Inference Project"
+
+__license__ = "MIT"
+__version__ = "2.0.6"
+__maintainer__ = "Jin-Hong Du, Tianyu Chen, and Jingshu Wang"
+__email__ = "jinhongd@andrew.cmu.edu"
+__status__ = "Development"
