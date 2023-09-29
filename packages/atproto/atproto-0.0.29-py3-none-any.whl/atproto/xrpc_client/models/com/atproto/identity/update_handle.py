6e654545852d5904f4f2b517554d561599d1b9f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-29 03:10:05 +0000

    Add package atproto/0.0.29/atproto-0.0.29-py3-none-any.whl

---
+++ packages/atproto/atproto-0.0.29-py3-none-any.whl/atproto/xrpc_client/models/com/atproto/identity/update_handle.py
@@ -0,0 +1,25 @@
+##################################################################
+# THIS IS THE AUTO-GENERATED CODE. DON'T EDIT IT BY HANDS!
+# Copyright (C) 2023 Ilya (Marshal) <https://github.com/MarshalX>.
+# This file is part of Python atproto SDK. Licenced under MIT.
+##################################################################
+
+
+import typing as t
+
+import typing_extensions as te
+
+if t.TYPE_CHECKING:
+    pass
+from atproto.xrpc_client.models import base
+
+
+class Data(base.DataModelBase):
+
+    """Input data model for :obj:`com.atproto.identity.updateHandle`."""
+
+    handle: str  #: Handle.
+
+
+class DataDict(te.TypedDict):
+    handle: str  #: Handle.
