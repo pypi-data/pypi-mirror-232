8eb784c5da5f6b8ab538c17b91c965e5aa439b3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-29 05:07:08 +0000

    Add package ogzoengine/1.3.1/ogzoengine-1.3.1.tar.gz

---
+++ packages/ogzoengine/ogzoengine-1.3.1.tar.gz/ogzoengine-1.3.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages, setup
+
+with open("ogzoengine/README.md", "r") as f:
+    long_description_read = f.read()
+
+setup(
+      name='ogzoengine',
+      version='1.3.1',
+      description='basic pygame engine',
+      author='Coguz',
+      packages=['ogzoengine'],
+      long_description=long_description_read,
+      long_description_content_type="text/markdown",
+      zip_safe=False,
+      classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.10",
+        "Operating System :: OS Independent",
+      ],
+      install_requires=["bson >= 0.5.10"],
+      extras_require={
+        "dev": ["pytest>=7.0", "twine>=4.0.2"],
+      },
+      python_requires=">=3.10",
+    )
