efd9a22a918d0930ac8aca34d81f3c380f6909c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-30 03:08:02 +0000

    Add package convertpngs/0.0.2/convertpngs-0.0.2.tar.gz

---
+++ packages/convertpngs/convertpngs-0.0.2.tar.gz/convertpngs-0.0.2/CHANGELOG.md
@@ -0,0 +1,7 @@
+### CHANGE LOG
+
+#### 0.0.1 (29/09/2023)
+- First Release
+
+#### 0.0.2 (29/09/2023)
+- Minor bug fixes
---
+++ packages/convertpngs/convertpngs-0.0.2.tar.gz/convertpngs-0.0.2/convertpngs/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from convertpngs.convert_png import (to_mp4, to_gif)
+    
+
+__version__: str = '0.0.2'
