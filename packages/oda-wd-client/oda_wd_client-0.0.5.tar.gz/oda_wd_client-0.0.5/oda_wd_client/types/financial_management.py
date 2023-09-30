3abd77e54110f55d1407794cd8b70191b0f3bdc8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-30 04:06:53 +0000

    Add package oda-wd-client/0.0.5/oda_wd_client-0.0.5.tar.gz

---
+++ packages/oda-wd-client/oda_wd_client-0.0.5.tar.gz/oda_wd_client-0.0.5/oda_wd_client/types/financial_management.py
@@ -0,0 +1,29 @@
+from oda_wd_client.service.financial_management.types import (
+    AccountingJournalData,
+    Company,
+    ConversionRate,
+    ConversionRateType,
+    CostCenterWorktag,
+    Currency,
+    JournalEntryLineData,
+    JournalSource,
+    LedgerAccount,
+    LedgerType,
+    ProjectWorktag,
+    SpendCategory,
+)
+
+__all__ = [
+    "ConversionRate",
+    "Currency",
+    "Company",
+    "ConversionRateType",
+    "JournalSource",
+    "LedgerType",
+    "SpendCategory",
+    "CostCenterWorktag",
+    "LedgerAccount",
+    "JournalEntryLineData",
+    "AccountingJournalData",
+    "ProjectWorktag",
+]
