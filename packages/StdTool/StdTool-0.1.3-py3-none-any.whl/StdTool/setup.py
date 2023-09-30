1d28bb1f80245a89abf126c877eade92d4d0ce80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-30 03:06:53 +0000

    Add package StdTool/0.1.3/StdTool-0.1.3-py3-none-any.whl

---
+++ packages/StdTool/StdTool-0.1.3-py3-none-any.whl/StdTool/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='StdTool',
+    version='0.1.3',
+    packages=['StdTool'],
+    url='',
+    license='Apache License 2.0',
+    author='MosRat',
+    author_email='whl2075928012@gmail.com',
+    description='Some tools for daily use',
+    install_requires=['tqdm']
+)
