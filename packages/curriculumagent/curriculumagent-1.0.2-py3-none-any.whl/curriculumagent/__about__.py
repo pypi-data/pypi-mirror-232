a908f866d5101774dd08e798f86dc93ce2133fb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-30 03:07:10 +0000

    Add package curriculumagent/1.0.2/curriculumagent-1.0.2-py3-none-any.whl

---
+++ packages/curriculumagent/curriculumagent-1.0.2-py3-none-any.whl/curriculumagent/__about__.py
@@ -0,0 +1,13 @@
+# Provide version-number etc.
+# This file is automatically edited by git!
+# This file must not have any sideeffects or do anything but define meta variables!
+from ._version import get_versions
+
+__version__ = get_versions()['version']
+__copyright__ = '2023, Fraunhofer IEE'
+__title__ = 'CurriculumAgent'
+__description__ = 'CurriculumAgent is a cleanup and improved version of the NeurIPS 2020 Competition Agent by ' \
+                  'Binbinchen.The agent is build to extract action sets of the Grid2Op Environment and then ' \
+                  'use rule-based agent to train a Reinforcement Learning agent.'
+__author__ = 'OE224'
+__authors__ = ['Malte Lehna']
