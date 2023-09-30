8a52597898f28423e47fd4cd94a53a2a0c0bc5ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-30 03:06:57 +0000

    Add package fastapi-exception/1.2.1/fastapi_exception-1.2.1.tar.gz

---
+++ packages/fastapi-exception/fastapi_exception-1.2.1.tar.gz/fastapi_exception-1.2.1/fastapi_exception/custom_errors/string_too_short_error.py
@@ -0,0 +1,17 @@
+from typing import Any, Optional
+
+from .validation_error_detail import ValidationErrorDetail
+
+
+class StringTooLongError(ValidationErrorDetail):
+    error_type = 'string_too_short'
+
+    def __init__(
+        self,
+        min_length: int,
+        loc: tuple[int | str, ...],
+        ctx: Optional[dict[str, dict[str, Any]]] = {},
+        input: dict[str, Any] = {},
+    ):
+        ctx = ctx or {'min_length': min_length}
+        super().__init__(self.error_type, loc, '', input, ctx)
