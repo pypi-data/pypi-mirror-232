101d5823b81d2bdc53f7554a1d8ef6e1317fcf2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-30 04:07:55 +0000

    Add package pipelite/0.1.4.1/pipelite-0.1.4.1-py3-none-any.whl

---
+++ packages/pipelite/pipelite-0.1.4.1-py3-none-any.whl/transformers/doNothingTR.py
@@ -0,0 +1,18 @@
+__author__ = "datacorner.fr"
+__email__ = "admin@datacorner.fr"
+__license__ = "MIT"
+
+import pipelite.utils.constants as C
+from pipelite.parents.Transformer import Transformer
+
+class doNothingTR(Transformer):
+
+    def transform(self, dsStack):
+        """ Just do nothing !
+        Args:
+            inputDataFrames (etlDatasets): multiple dataset in a collection
+        Returns:
+            etlDatasets: same as input
+            int: 0 transformed rows
+        """
+        return dsStack, 0
