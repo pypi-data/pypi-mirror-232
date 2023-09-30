5e2fa374ffb1df74a9d15621de6c921f7190f8d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-30 04:07:23 +0000

    Add package django-swagger-utils/0.0.8/django_swagger_utils-0.0.8-py2-none-any.whl

---
+++ packages/django-swagger-utils/django_swagger_utils-0.0.8-py2-none-any.whl/django_swagger_utils/server/constants/app_structure.py
@@ -0,0 +1,29 @@
+APP_STRUCTURE = {
+    "adapters": [],
+    "api_specs": [],
+    "conf": [],
+    "constants": [],
+    "dtos": [],
+    "entities": [],
+    "exceptions": [],
+    "models": [],
+    "tests": [
+        {
+            "entities": [],
+            "interactors": [],
+            "presenters": [],
+            "storages": [],
+            "views": []
+        }
+    ],
+    "utils": [],
+    "signals": [],
+    "interactors": [
+        {
+            "presenters": [],
+            "storages": []
+        }
+    ],
+    "presenters": [],
+    "storages": []
+}
