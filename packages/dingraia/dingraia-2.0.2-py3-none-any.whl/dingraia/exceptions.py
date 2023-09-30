72851c77d8cd26456c88c344591e7427a2833222
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-30 14:07:38 +0000

    Add package dingraia/2.0.2/dingraia-2.0.2-py3-none-any.whl

---
+++ packages/dingraia/dingraia-2.0.2-py3-none-any.whl/dingraia/exceptions.py
@@ -0,0 +1,18 @@
+class ConfigError(Exception):
+    pass
+
+
+class GroupSecureKeyError(Exception):
+    pass
+
+
+class UploadFileError(Exception):
+    pass
+
+
+class UploadFileSizeError(Exception):
+    pass
+
+
+class SystemError(Exception):
+    pass
