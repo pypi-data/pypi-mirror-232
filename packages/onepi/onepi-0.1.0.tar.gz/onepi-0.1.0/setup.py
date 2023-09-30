fbc76b59c2bef3f14146e24e2ac256c79dc18b09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-30 14:08:55 +0000

    Add package onepi/0.1.0/onepi-0.1.0.tar.gz

---
+++ packages/onepi/onepi-0.1.0.tar.gz/onepi-0.1.0/setup.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+name = "onepi"
+version = "0.1.0"
+description = "Python library to interface with BotnRoll One A"
+url = "https://github.com/ninopereira/bnronepi/tree/main/onepi"
+author = "Nino Pereira"
+author_email = "ninopereira.pt@gmail.com"
+license = "MIT"
+packages = ["onepi"]
+classifiers = [
+    "Development Status :: 3 - Alpha",
+    "Intended Audience :: Developers",
+    "License :: OSI Approved :: MIT License",
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.6",
+    "Programming Language :: Python :: 3.7",
+    "Programming Language :: Python :: 3.8",
+    "Programming Language :: Python :: 3.9",
+]
+
+# Package dependencies
+install_requires = ["spidev"]
+
+setup(
+    name=name,
+    version=version,
+    description=description,
+    url=url,
+    author=author,
+    author_email=author_email,
+    license=license,
+    classifiers=classifiers,
+    packages=find_packages(),
+    install_requires=install_requires,
+)
