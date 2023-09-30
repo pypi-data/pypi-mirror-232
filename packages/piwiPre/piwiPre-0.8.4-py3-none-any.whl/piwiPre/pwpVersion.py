54238475f56096c3e6d4b6c39c44face038bbdcd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-30 04:07:05 +0000

    Add package piwiPre/0.8.4/piwiPre-0.8.4-py3-none-any.whl

---
+++ packages/piwiPre/piwiPre-0.8.4-py3-none-any.whl/piwiPre/pwpVersion.py
@@ -0,0 +1,17 @@
+# ---------------------------------------------------------------------------------------------------------------
+# piwiPre project
+# This program and library is licenced under the European Union Public Licence v1.2 (see LICENCE)
+# developed by fabien.battini(at)gmail.com
+# ---------------------------------------------------------------------------------------------------------------
+
+
+class PwpVersion:
+    help = '0.8.4 at 09/29/2023 19:10:21'
+    spec = '0.8.4'
+    release = 0
+    version = 8
+    patch = 4
+    date = '09/29/2023 19:10:21'
+
+    def __init__(self):
+        pass
