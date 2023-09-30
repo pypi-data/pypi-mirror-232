0ba0f04f87a4a9e496636322b3e17af682e7dae2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-30 04:07:03 +0000

    Add package scrapy-spider-metadata/0.1.0/scrapy-spider-metadata-0.1.0.tar.gz

---
+++ packages/scrapy-spider-metadata/scrapy-spider-metadata-0.1.0.tar.gz/scrapy-spider-metadata-0.1.0/scrapy_spider_metadata/_utils.py
@@ -0,0 +1,52 @@
+from collections import deque
+from typing import Optional, Tuple, TypeVar, Union, get_args
+
+
+def get_generic_param(
+    cls: type, expected: Union[type, Tuple[type, ...]]
+) -> Optional[type]:
+    """Search the base classes recursively breadth-first for a generic class and return its param.
+
+    Returns the param of the first found class that is a subclass of ``expected``.
+    """
+    visited = set()
+    queue = deque([cls])
+    while queue:
+        node = queue.popleft()
+        visited.add(node)
+        for base in getattr(node, "__orig_bases__", []):
+            origin = getattr(base, "__origin__", None)
+            if origin and issubclass(origin, expected):
+                result = get_args(base)[0]
+                if not isinstance(result, TypeVar):
+                    return result
+            queue.append(base)
+    return None
+
+
+def _normalize_param(key, value, defs, /):
+    extra = value.pop("json_schema_extra", None)
+    if extra:
+        value.update(extra)
+
+    allof = value.pop("allOf", None)
+    if allof is not None:
+        for entry in allof:
+            ref = entry.pop("$ref", None)
+            if ref:
+                def_id = ref.rsplit("/", maxsplit=1)[1]
+                entry.update(defs[def_id])
+            entry.pop("title", None)
+            value.update(entry)
+
+    if "title" not in value:
+        value["title"] = key.title().replace("_", " ")
+
+
+def normalize_param_schema(schema, /):
+    params = schema.get("properties", None)
+    if not params:
+        return
+    defs = schema.pop("$defs", None)
+    for key, value in params.items():
+        _normalize_param(key, value, defs)
