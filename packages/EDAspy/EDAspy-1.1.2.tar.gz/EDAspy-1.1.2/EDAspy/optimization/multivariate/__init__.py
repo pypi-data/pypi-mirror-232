883fc92588cc412c852740f465da6bd17eb0c691
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-30 03:07:12 +0000

    Add package EDAspy/1.1.2/EDAspy-1.1.2.tar.gz

---
+++ packages/EDAspy/EDAspy-1.1.2.tar.gz/EDAspy-1.1.2/EDAspy/optimization/multivariate/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# __init__.py
+
+from .egna import EGNA
+from .emna import EMNA
+from .speda import SPEDA
+from .keda import MultivariateKEDA
+from .ebna import EBNA
