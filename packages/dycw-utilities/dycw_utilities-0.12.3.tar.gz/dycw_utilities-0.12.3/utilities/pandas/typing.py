12553b69bafde367bd26370eb7564427bee51c4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-30 04:07:03 +0000

    Add package dycw-utilities/0.12.3/dycw_utilities-0.12.3.tar.gz

---
+++ packages/dycw-utilities/dycw_utilities-0.12.3.tar.gz/dycw_utilities-0.12.3/utilities/pandas/typing.py
@@ -0,0 +1,40 @@
+from __future__ import annotations  # pragma: no cover
+
+import datetime as dt  # pragma: no cover
+from typing import TYPE_CHECKING, Any  # pragma: no cover
+
+from pandas import (  # pragma: no cover
+    BooleanDtype,
+    CategoricalDtype,
+    DatetimeTZDtype,
+    Index,
+    Int64Dtype,
+    Series,
+    StringDtype,
+)
+from typing_extensions import TypeAlias  # pragma: no cover
+
+if TYPE_CHECKING:  # pragma: no cover
+    IndexA: TypeAlias = Index[Any]
+    IndexB: TypeAlias = Index[bool]
+    IndexBn: TypeAlias = Index[BooleanDtype]
+    IndexC: TypeAlias = Index[CategoricalDtype]
+    IndexD: TypeAlias = Index[dt.datetime]
+    IndexDhk: TypeAlias = Index[DatetimeTZDtype]
+    IndexDutc: TypeAlias = Index[DatetimeTZDtype]
+    IndexF: TypeAlias = Index[float]
+    IndexI: TypeAlias = Index[int]
+    IndexI64: TypeAlias = Index[Int64Dtype]
+    IndexS: TypeAlias = Index[StringDtype]
+
+    SeriesA: TypeAlias = Series[Any]
+    SeriesB: TypeAlias = Series[bool]
+    SeriesBn: TypeAlias = Series[BooleanDtype]
+    SeriesC: TypeAlias = Series[CategoricalDtype]
+    SeriesD: TypeAlias = Series[dt.datetime]
+    SeriesDhk: TypeAlias = Series[DatetimeTZDtype]
+    SeriesDutc: TypeAlias = Series[DatetimeTZDtype]
+    SeriesF: TypeAlias = Series[float]
+    SeriesI: TypeAlias = Series[int]
+    SeriesI64: TypeAlias = Series[Int64Dtype]
+    SeriesS: TypeAlias = Series[StringDtype]
