b77b0457f2d053b3a7bd5d779b31e1728d30449e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-30 04:06:52 +0000

    Add package pypremod/0.1.4/pypremod-0.1.4.tar.gz

---
+++ packages/pypremod/pypremod-0.1.4.tar.gz/pypremod-0.1.4/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='pypremod',
+      description='Python package for Premod solver',
+      url='https://www.sintef.no',
+      install_requires=[
+          'numpy',
+          'pandas',
+          'pytest',
+          'scipy',
+          'matplotlib',
+          'pypremod-calm==0.1.2',
+          'pypremod-strength==0.1.6'
+      ],
+      version = '0.1.4',
+      packages=['premod'])
+
