dca9f99f500d8a01c252fe8f941b1811ed0216b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-30 04:07:02 +0000

    Add package sail/0.11.0/sail-0.11.0-py3-none-any.whl

---
+++ packages/sail/sail-0.11.0-py3-none-any.whl/sail/__init__.py
@@ -0,0 +1,12 @@
+__version__ = "0.11.0"
+__package_name__ = "SAIL"
+_logging_level = "INFO"
+
+
+def set_logging_level(logging_level):
+    global _logging_level
+    _logging_level = logging_level
+
+
+def get_logging_level():
+    return _logging_level
