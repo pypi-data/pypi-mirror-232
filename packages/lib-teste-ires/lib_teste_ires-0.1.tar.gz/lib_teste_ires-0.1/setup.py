3da4aa58f0d0c5e32c74891f256aa26b00ea4b7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-30 03:08:48 +0000

    Add package lib-teste-ires/0.1/lib_teste_ires-0.1.tar.gz

---
+++ packages/lib-teste-ires/lib_teste_ires-0.1.tar.gz/lib_teste_ires-0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='lib_teste_ires',
+    version='0.1',
+    url='https://github.com/Ires-ia/lib',
+    author='Ygor Nogatz',
+    author_email='seu.email@example.com',
+    description='A short description',
+    packages=find_packages(),
+    install_requires=[],
+    long_description="A long description",
+)
