0d4a4691fa94eafce8ba7c76c3818187070ef72c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-30 14:07:58 +0000

    Add package seaplane-framework.api/0.0.12/seaplane_framework.api-0.0.12.tar.gz

---
+++ packages/seaplane-framework.api/seaplane_framework.api-0.0.12.tar.gz/seaplane_framework.api-0.0.12/seaplane_framework/api/apis/tag_to_api.py
@@ -0,0 +1,29 @@
+import typing_extensions
+
+from seaplane_framework.api.apis.tags import TagValues
+from seaplane_framework.api.apis.tags.stream_api import StreamApi
+from seaplane_framework.api.apis.tags.flow_api import FlowApi
+from seaplane_framework.api.apis.tags.object_api import ObjectApi
+from seaplane_framework.api.apis.tags.endpoint_api import EndpointApi
+from seaplane_framework.api.apis.tags.key_value_api import KeyValueApi
+
+TagToApi = typing_extensions.TypedDict(
+    "TagToApi",
+    {
+        TagValues.STREAM: StreamApi,
+        TagValues.FLOW: FlowApi,
+        TagValues.OBJECT: ObjectApi,
+        TagValues.ENDPOINT: EndpointApi,
+        TagValues.KEYVALUE: KeyValueApi,
+    },
+)
+
+tag_to_api = TagToApi(
+    {
+        TagValues.STREAM: StreamApi,
+        TagValues.FLOW: FlowApi,
+        TagValues.OBJECT: ObjectApi,
+        TagValues.ENDPOINT: EndpointApi,
+        TagValues.KEYVALUE: KeyValueApi,
+    }
+)
