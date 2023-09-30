6a4ec2591711697314efe13f25886d3c6eeaafa6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-30 03:07:18 +0000

    Add package django-idp-user/2.2.4/django_idp_user-2.2.4.tar.gz

---
+++ packages/django-idp-user/django_idp_user-2.2.4.tar.gz/django_idp_user-2.2.4/idp_user/producer.py
@@ -0,0 +1,32 @@
+import json
+
+from django.conf import settings
+from django.core.serializers.json import DjangoJSONEncoder
+from kafka import KafkaProducer
+
+from idp_user.utils.classes import Singleton
+from idp_user.utils.functions import get_kafka_bootstrap_servers
+import ssl
+
+
+class Producer(metaclass=Singleton):
+    __connection = None
+
+    def __init__(self):
+        self.__connection = KafkaProducer(
+            bootstrap_servers=get_kafka_bootstrap_servers(include_uri_scheme=False),
+            value_serializer=lambda v: json.dumps(v, cls=DjangoJSONEncoder).encode('utf-8'),
+            api_version=(2, 6, 2),
+            ssl_context=ssl.create_default_context(purpose=ssl.Purpose.SERVER_AUTH),
+            security_protocol="SSL"
+        )
+
+    def send_message(self, topic: str, key: str, data: dict):
+        self.__connection.send(
+            topic=f"{settings.APP_ENV}_{topic}",
+            key=key.encode('utf-8'),
+            value=data
+        )
+        # Sometimes messages do not get sent.
+        # Flushing after each message seems to solve the issue
+        self.__connection.flush()
