20c0e65fd1ae22506fcd30ab56b44c33fa41e355
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-30 14:07:57 +0000

    Add package studioai/0.2.6/studioai-0.2.6.tar.gz

---
+++ packages/studioai/studioai-0.2.6.tar.gz/studioai-0.2.6/studioai/stats/inferential/base.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+# ================================================================================================ #
+# Project    : Artificial Intelligence & Data Science Studio                                       #
+# Version    : 0.1.0                                                                               #
+# Python     : 3.10.12                                                                             #
+# Filename   : /studioai/stats/inferential/base.py                                                 #
+# ------------------------------------------------------------------------------------------------ #
+# Author     : John James                                                                          #
+# Email      : john.james.ai.studio@gmail.com                                                      #
+# URL        : https://github.com/john-james-ai/studioai                                           #
+# ------------------------------------------------------------------------------------------------ #
+# Created    : Tuesday August 22nd 2023 07:44:59 pm                                                #
+# Modified   : Friday September 29th 2023 12:58:10 pm                                              #
+# ------------------------------------------------------------------------------------------------ #
+# License    : MIT License                                                                         #
+# Copyright  : (c) 2023 John James                                                                 #
+# ================================================================================================ #
+"""Base classes used throughout the inferential package."""
+from __future__ import annotations
+from abc import ABC, abstractmethod
+import logging
+from dataclasses import dataclass
+
+from studioai.stats.inferential.profile import StatTestProfile
+from studioai.data.dataclass import DataClass
+from studioai.service.io import IOService
+
+# ------------------------------------------------------------------------------------------------ #
+logger = logging.getLogger(__name__)
+# ------------------------------------------------------------------------------------------------ #
+ANALYSIS_TYPES = {
+    "univariate": "Univariate",
+    "bivariate": "Bivariate",
+    "multivariate": "Multivariate",
+}
+
+
+# ------------------------------------------------------------------------------------------------ #
+@dataclass
+class StatTestResult(DataClass):
+    test: str = None
+    hypothesis: str = None
+    H0: str = None
+    statistic: str = None
+    value: float = 0
+    pvalue: float = 0
+    alpha: float = 0.05
+
+    @abstractmethod
+    def result(self) -> str:
+        """Result in APA Style"""
+
+    def _report_alpha(self) -> str:
+        a = int(self._alpha * 100)
+        return f"significant at {a}%."
+
+    def _report_pvalue(self, pvalue: float) -> str:
+        """Rounds the pvalue in accordance with the APA Style Guide 7th Edition"""
+        if pvalue < 0.001:
+            return "p<.001"
+        else:
+            return "p=" + str(round(pvalue, 4))
+
+
+# ------------------------------------------------------------------------------------------------ #
+class StatisticalTest(ABC):
+    """Base class for Statistical Tests"""
+
+    def __init__(self, io: IOService = IOService, *args, **kwargs) -> None:
+        self._io = io
+        self._logger = logging.getLogger(f"{self.__class__.__name__}")
+
+    @property
+    @abstractmethod
+    def profile(self) -> StatTestProfile:
+        """Returns the statistical test profile."""
+
+    @property
+    @abstractmethod
+    def result(self) -> StatTestResult:
+        """Returns a Statistical Test Result object."""
+
+    @abstractmethod
+    def run(self) -> None:
+        """Performs the statistical test and creates a result object."""
+
+
+# ------------------------------------------------------------------------------------------------ #
+class StatAnalysis(ABC):
+    """Base class for Statistical Measurer classes"""
+
+    def __init__(self, io: IOService = IOService, *args, **kwargs) -> None:
+        self._io = io
+        self._logger = logging.getLogger(f"{self.__class__.__name__}")
+
+    @property
+    @abstractmethod
+    def result(self) -> StatTestResult:
+        """Returns a Statistical Test Result object."""
+
+    @abstractmethod
+    def run(self) -> None:
+        """Performs the statistical test and creates a result object."""
+
+    def _report_pvalue(self, pvalue: float) -> str:  # pragma: no cover
+        """Rounds the pvalue in accordance with the APA Style Guide 7th Edition"""
+        if pvalue < 0.001:
+            return "p<.001"
+        else:
+            return "p=" + str(round(pvalue, 4))
+
+    def _report_alpha(self) -> str:  # pragma: no cover
+        a = int(self._alpha * 100)
+        return f"significant at {a}%."
