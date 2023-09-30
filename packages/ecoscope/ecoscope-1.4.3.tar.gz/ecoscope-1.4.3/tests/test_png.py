6ca1ae72b0ed6546526537f2a6690ecaae1157cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-30 03:06:44 +0000

    Add package ecoscope/1.4.3/ecoscope-1.4.3.tar.gz

---
+++ packages/ecoscope/ecoscope-1.4.3.tar.gz/ecoscope-1.4.3/tests/test_png.py
@@ -0,0 +1,11 @@
+import sys
+import os
+from ecoscope.mapping.map import EcoMap
+
+sys.path.append("../ecoscope")
+
+
+m = EcoMap(draw_control=False)
+
+relative_path = os.path.join("tests/test_output/png_map.png")
+m.to_png(relative_path)
