f3a9be04006979c155edc5fa7e3849ed1385aab9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-30 14:08:39 +0000

    Add package bane/4.8.8/bane-4.8.8.tar.gz

---
+++ packages/bane/bane-4.8.8.tar.gz/bane-4.8.8/bane/__init__.py
@@ -0,0 +1,34 @@
+#coding: utf-8
+
+
+__version__='4.8.8'
+
+__author__="Ala Bouali"
+
+__author_nicknames__=["Merlin", "Chaotic Mind", "Ala", "Mr. Ego", "G0D Ala"]
+
+__author_email__="ala.bouali.1997@gmail.com"
+
+__author_github__="https://github.com/AlaBouali"
+
+__author_linkedin__="https://tn.linkedin.com/in/ala-bouali"
+
+
+
+from bane.common.payloads import *
+from bane.ddos.ddos import *
+from bane.cryptographers.hasher import *
+from bane.utils.pager import *
+from bane.gather_info.info_s import *
+from bane.utils.extrafun import *
+from bane.bruteforce.bruteforcer import *
+from bane.scanners.botnet.iot import *
+from bane.scanners.vulnerabilities.vulns import *
+from bane.scanners.cms.wp import *
+from bane.utils.proxer import *
+from bane.scanners.network_protocols.amplifier import *
+from bane.utils.updating import *
+from bane.utils.swtch import *
+from bane.scanners.cms.drupal import *
+from bane.scanners.cms.joomla import *
+from bane.scanners.cms.magento import *
