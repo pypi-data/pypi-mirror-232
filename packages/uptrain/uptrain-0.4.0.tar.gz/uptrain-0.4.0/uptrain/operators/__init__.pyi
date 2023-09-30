4b172dce9bd836124497cc7efb1756eb37ee28e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-30 04:08:13 +0000

    Add package uptrain/0.4.0/uptrain-0.4.0.tar.gz

---
+++ packages/uptrain/uptrain-0.4.0.tar.gz/uptrain-0.4.0/uptrain/operators/__init__.pyi
@@ -0,0 +1,135 @@
+__all__ = [
+    # base
+    "Operator",
+    "ColumnOp",
+    "TransformOp",
+    "register_op",
+    "register_custom_op",
+    "get_output_col_name_at",
+    "deserialize_operator",
+    # drift
+    "ParamsDDM",
+    "ParamsADWIN",
+    "ConceptDrift",
+    # embs
+    "Distribution",
+    "UMAP",
+    # table
+    "ColumnExpand",
+    "ColumnComparison",
+    # metrics
+    "Accuracy",
+    # similarity
+    "CosineSimilarity",
+    # vis
+    "BarChart",
+    "Histogram",
+    "LineChart",
+    "ScatterPlot",
+    "MultiPlot",
+    "Table",
+    "CustomPlotlyChart",
+    # language - also include all the subimports
+    "language",
+    "GrammarScore",
+    "OpenaiEval",
+    "PromptEval",
+    "Embedding",
+    "RougeScore",
+    "DocsLinkVersion",
+    "TextLength",
+    "WordCount",
+    "TextComparison",
+    "KeywordDetector",
+    "OpenAIGradeScore",
+    "ModelGradeScore",
+    "PromptGenerator",
+    "TextCompletion",
+    "OutputParser",
+    "ResponseFactualScore",
+    "ContextRelevance",
+    "ResponseRelevance",
+    "ResponseCompleteness",
+    "ResponseCompletenessWrtContext",
+    "LanguageCritique",
+    "ToneCritique",
+    "GuidelineAdherenceScore",
+    # io - also include all the subimports
+    "io",
+    "ExcelReader",
+    "CsvReader",
+    "JsonReader",
+    "JsonWriter",
+    "DeltaReader",
+    "DeltaWriter",
+    "BigQueryReader",
+    "DuckDBReader",
+    "BigQueryWriter",
+    # code
+    "code",
+    "ParseCreateStatements",
+    "ParseSQL",
+    "ValidateTables",
+    "ExecuteAndCompareSQL",
+]
+
+from .base import (
+    Operator,
+    ColumnOp,
+    TransformOp,
+    register_op,
+    register_custom_op,
+    get_output_col_name_at,
+    deserialize_operator,
+)
+from .drift import ConceptDrift, ParamsADWIN, ParamsDDM
+from .embs import Distribution, UMAP
+from .table import Table, ColumnExpand, ColumnComparison
+from .metrics import Accuracy
+from .similarity import CosineSimilarity
+from .chart import (
+    BarChart,
+    LineChart,
+    ScatterPlot,
+    Histogram,
+    MultiPlot,
+    CustomPlotlyChart,
+)
+
+from . import language
+from .language.grammar import GrammarScore
+from .language.openai_evals import OpenaiEval, PromptEval
+from .language.embedding import Embedding
+from .language.rouge import RougeScore
+from .language.text import (
+    DocsLinkVersion,
+    TextLength,
+    TextComparison,
+    KeywordDetector,
+    WordCount,
+)
+from .language.model_grade import ModelGradeScore, OpenAIGradeScore
+from .language.generation import PromptGenerator, TextCompletion, OutputParser
+from .language.with_context import (
+    ResponseFactualScore,
+    ContextRelevance,
+    ResponseCompleteness,
+    ResponseRelevance,
+    ResponseCompletenessWrtContext,
+)
+from .language.critique import LanguageCritique, ToneCritique
+from .language.guideline import GuidelineAdherenceScore
+
+from . import io
+from .io.base import CsvReader, JsonReader, DeltaReader, JsonWriter, DeltaWriter
+from .io.excel import ExcelReader
+from .io.bq import BigQueryReader, BigQueryWriter
+from .io.duck import DuckDBReader
+
+from . import code
+from .code.sql import (
+    ParseCreateStatements,
+    ParseSQL,
+    ValidateTables,
+    ExecuteAndCompareSQL,
+)
