30ffb40a10102852ba55ed067c5b255380f3df7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-30 04:09:05 +0000

    Add package g2-custom-logger/1.0.4/g2_custom_logger-1.0.4-py3-none-any.whl

---
+++ packages/g2-custom-logger/g2_custom_logger-1.0.4-py3-none-any.whl/g2_custom_logger/logger.py
@@ -0,0 +1,37 @@
+import logging
+
+def setup_logger(logfile="my_log.log"):
+    logging.basicConfig(filename=logfile, level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    return logging.getLogger(__name__)
+
+
+class MyLogger:
+    def __init__(self, name='g2-custom-logger', level=logging.INFO):
+        self.logger = logging.getLogger(name)
+        self.logger.setLevel(level)
+        self.handler = logging.StreamHandler()
+        self.formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        self.handler.setFormatter(self.formatter)
+        self.logger.addHandler(self.handler)
+
+    def custom_format(self, message):
+        return f"Custom Prefix: {message}"
+    
+    def debug(self, message):
+        self.logger.debug(message)
+
+    def info(self, message):
+        self.logger.info(message)
+
+    def warning(self, message):
+        self.logger.warning(message)
+
+    def error(self, message):
+        self.logger.error(message)
+
+    def critical(self, message):
+        self.logger.critical(message)
+
+
+def create_logger(name='g2-custom-logger', level=logging.INFO):
+    return MyLogger(name, level)
