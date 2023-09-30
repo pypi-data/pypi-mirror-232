a9be3a7393807f593c16c546f50ee586f6a30d7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-30 03:07:45 +0000

    Add package extrawest-ocpi/2023.9.28/extrawest_ocpi-2023.9.28-py3-none-any.whl

---
+++ packages/extrawest-ocpi/extrawest_ocpi-2023.9.28-py3-none-any.whl/py_ocpi/core/endpoints/v_2_1_1/emsp.py
@@ -0,0 +1,34 @@
+from py_ocpi.core.enums import ModuleID
+from py_ocpi.core.data_types import URL
+from py_ocpi.core.config import settings
+from py_ocpi.modules.versions.v_2_1_1.schemas import (
+    Endpoint,
+    VersionNumber,
+)
+
+URL_BASE = (
+    f"https://{settings.OCPI_HOST}/{settings.OCPI_PREFIX}/"
+    f"emsp/{VersionNumber.v_2_1_1.value}"
+)
+
+
+CREDENTIALS_AND_REGISTRATION = Endpoint(
+    identifier=ModuleID.credentials_and_registration,
+    url=URL(f"{URL_BASE}/{ModuleID.credentials_and_registration.value}"),
+)
+
+LOCATIONS = Endpoint(
+    identifier=ModuleID.locations,
+    url=URL(f"{URL_BASE}/{ModuleID.locations.value}"),
+)
+
+CDRS = Endpoint(
+    identifier=ModuleID.cdrs,
+    url=URL(f"{URL_BASE}/{ModuleID.cdrs.value}"),
+)
+
+ENDPOINTS_LIST = {
+    ModuleID.credentials_and_registration: CREDENTIALS_AND_REGISTRATION,
+    ModuleID.locations: LOCATIONS,
+    ModuleID.cdrs: CDRS,
+}
