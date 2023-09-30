7773e0f674fb7965524d873f98d7a4aa5b065237
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-30 03:06:45 +0000

    Add package ev-station-assets/0.1.0/ev_station_assets-0.1.0.tar.gz

---
+++ packages/ev-station-assets/ev_station_assets-0.1.0.tar.gz/ev_station_assets-0.1.0/ev_station_assets/assets.py
@@ -0,0 +1,9 @@
+from abstract.assets import AbstractBattery, AbstractBatteryState
+
+
+class GenericBatteryState(AbstractBatteryState):
+    pass
+
+
+class GenericBattery(AbstractBattery):
+    pass
