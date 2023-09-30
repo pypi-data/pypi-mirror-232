efa794f2999111e0592ca6b575c2652ece79d7b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-30 03:07:31 +0000

    Add package gazix/1.0.0/gazix-1.0.0-py3-none-any.whl

---
+++ packages/gazix/gazix-1.0.0-py3-none-any.whl/gazix/cli.py
@@ -0,0 +1,52 @@
+#########################################################################
+#                                                                       #
+#  This file is part of gazix.                                          #
+#                                                                       #
+#  gazix is free software: you can redistribute it and/or modify        #
+#  it under the terms of the GNU General Public License as published by #
+#  the Free Software Foundation, either version 3 of the License, or    #
+#  (at your option) any later version.                                  #
+#                                                                       #
+#  gazix is distributed in the hope that it will be useful,             #
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of       #
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        # 
+#  GNU General Public License for more details.                         #
+#                                                                       #
+#  You should have received a copy of the GNU General Public License    #
+#  along with gazix. If not, see <https://www.gnu.org/licenses/>.       #
+#                                                                       #
+#########################################################################
+
+import sys
+from .utils.option import option
+from .migration.labels import Labels
+
+def help():
+    print("gazix [option]")
+    print("\toption:")
+    print("\thelp          Print the help")
+    print("\tcp-MR         Copy all the merge requests from a source to a target repositories")
+    print("\tcp-issues     Copy all the issues from a source to a target repositories")
+    print("\tcp-labels     Copy all the labels from a source to a target repositories")
+
+
+def main():
+    program = None
+    try:
+        program = sys.argv[1]
+        status = "help" in program
+        if status:
+            raise Exception
+    except:
+        help()
+        exit()
+    try:
+        args = sys.argv[2:]
+    except:
+        args = None
+    if option(program, "cp-labels"):
+        Labels(args)
+
+
+if __name__ == "__main__":
+    main()
