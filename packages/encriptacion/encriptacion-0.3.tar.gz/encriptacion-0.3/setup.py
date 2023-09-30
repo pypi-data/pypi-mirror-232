d37eb1ee5566c2f20d26d714f9c4deae2fb02f8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-30 14:07:39 +0000

    Add package encriptacion/0.3/encriptacion-0.3.tar.gz

---
+++ packages/encriptacion/encriptacion-0.3.tar.gz/encriptacion-0.3/setup.py
@@ -0,0 +1,15 @@
+from setuptools import find_packages, setup
+
+setup(
+ author="aslskks",
+ install_requires=[
+  "cryptography",
+  "PyQt5",
+  "encriptacion2",
+  "requests"
+ ],
+ name="encriptacion",
+ version="0.3",
+ author_email="davikenat@gmail.com",
+ description="solo as import encriptacion y import encriptacion2 en otro dispositivo o en el mismo solo es para resivir una clave"
+)
