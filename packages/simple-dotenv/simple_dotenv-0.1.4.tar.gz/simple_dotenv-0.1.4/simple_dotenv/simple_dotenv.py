60aa2587b1641efddde9e4c7863b1587d001967d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-30 04:07:26 +0000

    Add package simple-dotenv/0.1.4/simple_dotenv-0.1.4.tar.gz

---
+++ packages/simple-dotenv/simple_dotenv-0.1.4.tar.gz/simple_dotenv-0.1.4/simple_dotenv/simple_dotenv.py
@@ -0,0 +1,29 @@
+import os
+class GetEnv():
+    def __init__(self, key:str):
+        self.key = key
+
+        lines = []
+
+
+        with open('.env', "r") as file:
+            for line in file:
+                line = line.rstrip()
+                lines.append(line)
+
+
+        def create_lookup(env_arr):
+            lookup_table = {}
+            for item in env_arr:
+                separator = item.index('=')
+                var = item[:separator]
+                value = item[separator+1:]
+                value = value.replace("'",'')
+
+                lookup_table[var] = value
+            return lookup_table
+
+        self.lib= create_lookup(lines)
+
+    def __str__(self):
+        return self.lib[self.key]
