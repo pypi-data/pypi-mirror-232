576aeb59fbea7c4705a6e8fd030729ec9a9c5a94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-30 14:07:30 +0000

    Add package galactory/0.10.1/galactory-0.10.1.tar.gz

---
+++ packages/galactory/galactory-0.10.1.tar.gz/galactory-0.10.1/galactory/api/v2/collections.py
@@ -0,0 +1,189 @@
+# -*- coding: utf-8 -*-
+# (c) 2022 Brian Scholer (@briantist)
+
+import semver
+from base64io import Base64IO
+from flask import Response, jsonify, abort, url_for, request, current_app
+
+from . import bp as v2
+from ... import constants as C
+from ...utilities import (
+    discover_collections,
+    collected_collections,
+    _collection_listing,
+    authorize,
+    _chunk_to_temp,
+    upload_collection_from_hashed_tempfile,
+)
+from ...upstream import ProxyUpstream
+
+
+@v2.route('/collections')
+@v2.route('/collections/')
+def collections():
+    repository = authorize(request, current_app.config['ARTIFACTORY_PATH'])
+    scheme = current_app.config.get('PREFERRED_URL_SCHEME')
+
+    results = _collection_listing(repo=repository, scheme=scheme)
+    return jsonify(results=results)
+
+
+@v2.route('/collections/<namespace>/<collection>')
+@v2.route('/collections/<namespace>/<collection>/')
+def collection(namespace, collection):
+    repository = authorize(request, current_app.config['ARTIFACTORY_PATH'])
+    upstream = current_app.config['PROXY_UPSTREAM']
+    no_proxy = current_app.config['NO_PROXY_NAMESPACES']
+    cache_minutes = current_app.config['CACHE_MINUTES']
+    cache_read = current_app.config['CACHE_READ']
+    cache_write = current_app.config['CACHE_WRITE']
+    scheme = current_app.config.get('PREFERRED_URL_SCHEME')
+
+    upstream_result = None
+    if upstream and (not no_proxy or namespace not in no_proxy):
+        proxy = ProxyUpstream(repository, upstream, cache_read, cache_write, cache_minutes)
+        upstream_result = proxy.proxy(request)
+
+    results = _collection_listing(repository, namespace, collection, scheme=scheme)
+
+    if not (results or upstream_result):
+        abort(C.HTTP_NOT_FOUND)
+
+    result = None
+    if results:
+        if len(results) > 1:
+            abort(C.HTTP_INTERNAL_SERVER_ERROR)
+        result = results[0]
+
+    if upstream_result:
+        if result is None:
+            result = upstream_result
+        else:
+            result = max((result, upstream_result), key=lambda r: semver.VersionInfo.parse(r['latest_version']['version']))
+
+    return jsonify(result)
+
+
+@v2.route('/collections/<namespace>/<collection>/versions')
+@v2.route('/collections/<namespace>/<collection>/versions/')
+def versions(namespace, collection):
+    results = []
+    repository = authorize(request, current_app.config['ARTIFACTORY_PATH'])
+    upstream = current_app.config['PROXY_UPSTREAM']
+    no_proxy = current_app.config['NO_PROXY_NAMESPACES']
+    cache_minutes = current_app.config['CACHE_MINUTES']
+    cache_read = current_app.config['CACHE_READ']
+    cache_write = current_app.config['CACHE_WRITE']
+    scheme = current_app.config.get('PREFERRED_URL_SCHEME')
+
+    upstream_result = None
+    if upstream and (not no_proxy or namespace not in no_proxy):
+        proxy = ProxyUpstream(repository, upstream, cache_read, cache_write, cache_minutes)
+        upstream_result = proxy.proxy(request)
+
+    collections = collected_collections(repository, namespace=namespace, name=collection, scheme=scheme)
+
+    if not (collections or upstream_result):
+        abort(C.HTTP_NOT_FOUND)
+
+    if len(collections) > 1:
+        abort(C.HTTP_INTERNAL_SERVER_ERROR)
+
+    vers = set()
+    for _, c in collections.items():
+        for v, i in c['versions'].items():
+            results.append(
+                {
+                    'href': url_for(
+                        'api.v2.version',
+                        namespace=i['namespace']['name'],
+                        collection=i['name'],
+                        version=v,
+                        _external=True,
+                        _scheme=scheme,
+                    ),
+                    'version': v,
+                }
+            )
+            vers.add(v)
+
+    if upstream_result:
+        for item in upstream_result['results']:
+            if item['version'] not in vers:
+                results.append(item)
+
+    out = {
+        'count': len(results),
+        'next': None,
+        'previous': None,
+        'results': results,
+    }
+    return jsonify(out)
+
+
+@v2.route('/collections/<namespace>/<collection>/versions/<version>')
+@v2.route('/collections/<namespace>/<collection>/versions/<version>/')
+def version(namespace, collection, version):
+    repository = authorize(request, current_app.config['ARTIFACTORY_PATH'])
+    upstream = current_app.config['PROXY_UPSTREAM']
+    no_proxy = current_app.config['NO_PROXY_NAMESPACES']
+    cache_minutes = current_app.config['CACHE_MINUTES']
+    cache_read = current_app.config['CACHE_READ']
+    cache_write = current_app.config['CACHE_WRITE']
+    scheme = current_app.config.get('PREFERRED_URL_SCHEME')
+
+    try:
+        info = next(discover_collections(repository, namespace=namespace, name=collection, version=version, scheme=scheme))
+    except StopIteration:
+        if upstream and (not no_proxy or namespace not in no_proxy):
+            proxy = ProxyUpstream(repository, upstream, cache_read, cache_write, cache_minutes)
+            upstream_result = proxy.proxy(request)
+            return jsonify(upstream_result)
+        else:
+            abort(C.HTTP_NOT_FOUND)
+
+    out = {
+        'artifact': {
+            'filename': info['filename'],
+            'sha256': info['sha256'],
+            'size': info['size'],
+        },
+        'collection': {
+            'href': url_for(
+                'api.v2.collection',
+                namespace=namespace,
+                collection=collection,
+                _external=True,
+                _scheme=scheme,
+            ),
+            'name': info['name'],
+        },
+        'namespace': info['namespace'],
+        'download_url': info['download_url'],
+        'hidden': False,
+        'href': url_for(request.endpoint, _external=True, _scheme=scheme, **request.view_args),
+        'id': 0,
+        'metadata': info['collection_info'],
+        'version': version,
+    }
+    return jsonify(out)
+
+
+@v2.route('/collections', methods=['POST'])
+@v2.route('/collections/', methods=['POST'])
+def publish():
+    sha256 = request.form['sha256']
+    file = request.files['file']
+    skip_configured_auth = current_app.config['PUBLISH_SKIP_CONFIGURED_AUTH']
+    property_fallback = current_app.config.get('USE_PROPERTY_FALLBACK', False)
+    _scheme = current_app.config.get('PREFERRED_URL_SCHEME')
+
+    target = authorize(request, current_app.config['ARTIFACTORY_PATH'] / file.filename, skip_configured_auth=skip_configured_auth)
+
+    with _chunk_to_temp(Base64IO(file)) as tmp:
+        if tmp.sha256 != sha256:
+            abort(Response(f"Hash mismatch: uploaded=='{sha256}', calculated=='{tmp.sha256}'", C.HTTP_INTERNAL_SERVER_ERROR))
+
+        upload_collection_from_hashed_tempfile(target, tmp, property_fallback=property_fallback)
+
+    return jsonify(task=url_for('api.v2.import_singleton', _external=True, _scheme=_scheme))
---
+++ packages/galactory/galactory-0.10.1.tar.gz/galactory-0.10.1/galactory/utilities.py
@@ -0,0 +1,292 @@
+# -*- coding: utf-8 -*-
+# (c) 2022 Brian Scholer (@briantist)
+
+import json
+import semver
+import math
+import hashlib
+import gzip
+
+import typing as t
+
+from datetime import datetime
+from tempfile import SpooledTemporaryFile
+from urllib.request import urlopen
+from urllib3 import Retry
+from requests.adapters import HTTPAdapter
+from requests import Session
+
+from flask import url_for, request, current_app, abort, Response, Request
+from flask.json.provider import DefaultJSONProvider
+from artifactory import ArtifactoryPath, ArtifactoryException
+from dohq_artifactory.auth import XJFrogArtApiAuth, XJFrogArtBearerAuth
+
+from . import constants as C
+from .iter_tar import iter_tar
+
+
+class DateTimeIsoFormatJSONProvider(DefaultJSONProvider):
+    @staticmethod
+    def default(o: t.Any) -> t.Any:
+        if isinstance(o, datetime):
+            return o.isoformat()
+
+        return super().default(o)
+
+
+
+def _session_with_retries(retry=None, auth=None) -> Session:
+    if retry is None:
+        retry = Retry(connect=5, read=3, redirect=2, status=6, other=3, backoff_factor=0.1, raise_on_status=False)
+
+    adapter = HTTPAdapter(max_retries=retry)
+    session = Session()
+    session.auth = auth
+    session.mount('http://', adapter)
+    session.mount('https://', adapter)
+
+    return session
+
+
+def authorize(request: Request, artifactory_path: ArtifactoryPath, retry=None, skip_configured_auth: bool = False) -> ArtifactoryPath:
+    auth = None
+    if not skip_configured_auth:
+        accesstoken = current_app.config['ARTIFACTORY_ACCESS_TOKEN']
+        apikey = current_app.config['ARTIFACTORY_API_KEY']
+        if accesstoken is not None:
+            auth = XJFrogArtBearerAuth(accesstoken)
+        elif apikey is not None:
+            auth = XJFrogArtApiAuth(apikey)
+
+    if current_app.config['USE_GALAXY_AUTH'] and (not current_app.config['PREFER_CONFIGURED_AUTH'] or auth is None):
+        galaxy_auth_type = current_app.config['GALAXY_AUTH_TYPE']
+        authorization = request.headers.get('Authorization')
+        if authorization:
+            token = authorization.split(' ')[1]
+            if galaxy_auth_type == 'access_token':
+                auth = XJFrogArtBearerAuth(token)
+            elif galaxy_auth_type == 'api_key':
+                auth = XJFrogArtApiAuth(token)
+            else:
+                raise ValueError(f"Unknown galaxy auth type '{galaxy_auth_type}'.")
+
+    session = _session_with_retries(retry=retry, auth=auth)
+    return ArtifactoryPath(artifactory_path, session=session)
+
+
+def load_manifest_from_archive(handle, seek_to_zero_after=True):
+    with gzip.GzipFile(fileobj=handle, mode='rb') as gz:
+        for fname, data in iter_tar(gz):
+            if fname.lower() in ('manifest.json', './manifest.json'):
+                data = json.loads(data)
+                if seek_to_zero_after:
+                    handle.seek(0)
+                return data
+
+
+def discover_collections(repo, namespace=None, name=None, version=None, fast_detection=True, scheme=None):
+    for p in repo:
+        if fast_detection:
+            # we're going to use the naming convention to eliminate candidates early,
+            # to avoid excessive additional requests for properties and stat that slow
+            # down the listing immensely as the number of collections grows.
+            try:
+                f_namespace, f_name, f_version = p.name.replace('.tar.gz', '').split('-')
+            except ValueError:
+                pass
+            else:
+                if not all(
+                    (
+                        not namespace or f_namespace == namespace,
+                        not name or f_name == name,
+                        not version or f_version == version
+                    )
+                ):
+                    continue
+
+        info = p.stat()
+        if info.is_dir:
+            continue
+
+        props = p.properties
+        if not props.get('collection_info'):
+            continue
+
+        collection_info = json.loads(props['collection_info'][0])
+
+        coldata = {
+            'collection_info': collection_info,
+            'fqcn': props['fqcn'][0],
+            'created': info.ctime.isoformat(),
+            'modified': info.mtime.isoformat(),
+            'namespace': {'name': props['namespace'][0]},
+            'name': props['name'][0],
+            'filename': p.name,
+            'sha256': info.sha256,
+            'size': info.size,
+            'download_url': url_for(
+                'download.download',
+                filename=p.name,
+                _external=True,
+                _scheme=scheme,
+            ),
+            'mime_type': info.mime_type,
+            'version': props['version'][0],
+            'semver': semver.VersionInfo.parse(props['version'][0]),
+        }
+
+        if all(
+            (
+                not namespace or coldata['namespace']['name'] == namespace,
+                not name or coldata['name'] == name,
+                not version or coldata['version'] == version
+            )
+        ):
+            yield coldata
+
+
+def _latest_collection_version(reference_collection: dict, difference_collection: dict, *, property: str = "semver"):
+    """Returns the latest of two collections."""
+    if (reference_collection[property].prerelease is None) == (difference_collection[property].prerelease is None):
+        return max(reference_collection, difference_collection, key=lambda x: x[property])
+
+    return reference_collection if reference_collection[property].prerelease is None else difference_collection
+
+
+def collected_collections(repo, namespace=None, name=None, scheme=None):
+    collections = {}
+
+    for c in discover_collections(repo, namespace=namespace, name=name, scheme=scheme):
+        version = c['version']
+        col = collections.setdefault(c['fqcn'], {})
+        versions = col.setdefault('versions', {})
+        versions[version] = c
+        try:
+            latest = col['latest']
+        except KeyError:
+            col['latest'] = c
+        else:
+            col['latest'] = _latest_collection_version(latest, c)
+
+    return collections
+
+
+def _collection_listing(repo, namespace=None, collection=None, scheme=None):
+    collections = collected_collections(repo, namespace, collection)
+
+    results = []
+
+    for _, i in collections.items():
+        latest = i['latest']
+
+        result = {
+            'href': url_for(request.endpoint, _external=True, _scheme=scheme, **request.view_args),
+            'name': latest['name'],
+            'namespace': latest['namespace'],
+            'created': latest['created'],
+            'modified': latest['modified'],
+            'versions_url': url_for(
+                'api.v2.versions',
+                namespace=latest['namespace']['name'],
+                collection=latest['name'],
+                _external=True,
+                _scheme=scheme,
+            ),
+            'latest_version': {
+                'href': url_for(
+                    'api.v2.version',
+                    namespace=latest['namespace']['name'],
+                    collection=latest['name'],
+                    version=latest['version'],
+                    _external=True,
+                    _scheme=scheme,
+                ),
+                "version": latest['version'],
+            }
+        }
+        results.append(result)
+
+    return results
+
+
+def lcm(a, b, *more):
+    z = lcm(b, *more) if more else b
+    return abs(a * z) // math.gcd(a, z)
+
+
+class HashedTempFile():
+    def __init__(self, handle, md5, sha1, sha256, close=True) -> None:
+        self.handle = handle
+        self.md5 = md5
+        self.sha1 = sha1
+        self.sha256 = sha256
+        self._close = close
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, exc_tb):
+        if self._close:
+            self.handle.close()
+
+
+def _chunk_to_temp(fsrc, iterator=None, spool_size=5*1024*1024, seek_to_zero=True, chunk_multiplier=64, close=True) -> HashedTempFile:
+    md5sum = hashlib.md5()
+    sha1sum = hashlib.sha1()
+    sha256sum = hashlib.sha256()
+    common_block_size = lcm(md5sum.block_size, sha1sum.block_size, sha256sum.block_size)
+    chunk_size = chunk_multiplier * common_block_size
+
+    it = iter(lambda: fsrc.read(chunk_size), b'') if iterator is None else iterator(chunk_size)
+
+    tmp = SpooledTemporaryFile(max_size=spool_size)
+
+    for chunk in it:
+        md5sum.update(chunk)
+        sha1sum.update(chunk)
+        sha256sum.update(chunk)
+        tmp.write(chunk)
+
+    if seek_to_zero:
+        tmp.seek(0)
+
+    return HashedTempFile(tmp, md5sum.hexdigest(), sha1sum.hexdigest(),  sha256sum.hexdigest(), close=close)
+
+
+def upload_collection_from_hashed_tempfile(artifact: ArtifactoryPath, tmpfile: HashedTempFile, property_fallback: bool = False) -> t.Dict[str, t.Any]:
+    try:
+        manifest = load_manifest_from_archive(tmpfile.handle)
+    except Exception:
+        abort(Response("Error loading manifest from collection archive.", C.HTTP_INTERNAL_SERVER_ERROR))
+    else:
+        ci = manifest['collection_info']
+        props = {
+            'collection_info': json.dumps(ci),
+            'namespace': ci['namespace'],
+            'name': ci['name'],
+            'version': ci['version'],
+            'fqcn': f"{ci['namespace']}.{ci['name']}"
+        }
+
+    params = props
+    if property_fallback:
+        params = {}
+
+    try:
+        artifact.deploy(
+            tmpfile.handle,
+            md5=tmpfile.md5,
+            sha1=tmpfile.sha1,
+            sha256=tmpfile.sha256,
+            parameters=params,
+            quote_parameters=True
+        )
+    except ArtifactoryException as exc:
+        cause = exc.__cause__
+        current_app.logger.debug(cause)
+        abort(Response(cause.response.text, cause.response.status_code))
+    else:
+        if property_fallback:
+            artifact.properties = props
+
+    return props
