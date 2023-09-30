705900917ed1ad2b67b9ab55163aadf71f71a8f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-30 03:06:37 +0000

    Add package musicmaster/1.1.13/musicmaster-1.1.13.tar.gz

---
+++ packages/musicmaster/musicmaster-1.1.13.tar.gz/musicmaster-1.1.13/music/backend/downloads/download.py
@@ -0,0 +1,18 @@
+from music.backend.utils import Path, iteration
+
+from . import downloader, jobs, postprocessor, uploader
+
+
+def download():
+    songs = jobs.get()
+    if songs:
+        for chunk in iteration.chunked(songs, chunk_size=20):
+            download_songs_chunk(chunk)
+    if not Path.processed_songs.is_empty():
+        uploader.start()
+
+
+def download_songs_chunk(songs):
+    downloader.download(songs)
+    postprocessor.process_downloads()
+    jobs.remove(songs)
