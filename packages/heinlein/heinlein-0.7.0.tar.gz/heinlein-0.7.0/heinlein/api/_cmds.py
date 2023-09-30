69ca8274608abd17ad4b6ca7ff7b124781011892
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-30 14:07:20 +0000

    Add package heinlein/0.7.0/heinlein-0.7.0.tar.gz

---
+++ packages/heinlein/heinlein-0.7.0.tar.gz/heinlein-0.7.0/heinlein/api/_cmds.py
@@ -0,0 +1,54 @@
+import pathlib
+from pathlib import Path
+
+from heinlein import manager
+
+"""
+Backend API functions
+"""
+
+
+def add(name, dtype, path, *args, **kwargs) -> bool:
+    """
+    Add a location on disk to a dataset
+    """
+    cwd = Path.cwd()
+
+    if path != "cwd":
+        path = cwd / path
+    else:
+        path = cwd
+    if not path.exists():
+        print(f"Error: {path} not found!")
+        return
+
+    mgr = manager.get_manager(name)
+    mgr.add_data(dtype, path, *args, **kwargs)
+    return True
+
+
+def remove(name: str, dtype: str):
+    """
+    Remove a datatype from a dataset
+    """
+    if not manager.managers.FileManager.exists(name):
+        print(f"Error: dataset {name} does not exist!")
+        return True
+    mgr = manager.get_manager(name)
+    mgr.remove_data(dtype)
+    return True
+
+
+def get_path(name: str, dtype: str) -> pathlib.Path:
+    """
+    Get the path to a specific data type in a specific datset
+    """
+    if not manager.managers.FileManager.exists(name):
+        print(f"Error: dataset {name} does not exist!")
+        return True
+    mgr = manager.get_manager(name)
+    try:
+        path = mgr.get_path(dtype)
+        return path
+    except KeyError:
+        print(f"No data of dtype {dtype} found for dataset {name}")
