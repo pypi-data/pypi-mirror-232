82b4d86f74b783099a67591771ef9f2c00e5ce62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-30 04:08:47 +0000

    Add package pylayout/0.0.12/pylayout-0.0.12.tar.gz

---
+++ packages/pylayout/pylayout-0.0.12.tar.gz/pylayout-0.0.12/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="pylayout",
+    version="0.0.12",
+    license="MIT",
+    author="Roman Smolnyk",
+    author_email="poma23324@gmail.com",
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    url="https://gitlab.com/roman-smolnyk/pylayout",
+    keywords="Keyboard layout",
+    install_requires=[
+        "pywin32; platform_system=='Windows'",
+    ],
+    description="Get/Set keyboard layout",
+)
