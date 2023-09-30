545f2c192cf389b20b2b7988c921db15e24e7783
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-30 14:07:25 +0000

    Add package bokeh/3.3.0.dev3/bokeh-3.3.0.dev3-py3-none-any.whl

---
+++ packages/bokeh/bokeh-3.3.0.dev3-py3-none-any.whl/bokeh/server/static/js/lib/core/util/assert.d.ts
@@ -0,0 +1,6 @@
+export declare class AssertionError extends Error {
+}
+export declare function assert(condition: boolean | (() => boolean), message?: string): asserts condition;
+export declare function assert_debug(condition: boolean | (() => boolean), message?: string): asserts condition;
+export declare function unreachable(): never;
+//# sourceMappingURL=assert.d.ts.map
