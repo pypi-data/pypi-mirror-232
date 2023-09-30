98b3b67fcd849c8b5818d64162a89fe888a7ca8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-30 14:08:55 +0000

    Add package pyqtcolor/2.0/pyqtcolor-2.0.tar.gz

---
+++ packages/pyqtcolor/pyqtcolor-2.0.tar.gz/pyqtcolor-2.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+setup(
+    name="pyqtcolor",
+    version='2.0',
+    license='Eclipse Public License 2.0',
+    authors=["smithshwan"],
+    author_email="hertzsmith@gmail.com",
+    description="Bring Colorful Code",
+    long_description='Documentation: https://github.com/hertzsmith/pyqtcolor',
+    packages=['pyqtcolor']
+)
+
