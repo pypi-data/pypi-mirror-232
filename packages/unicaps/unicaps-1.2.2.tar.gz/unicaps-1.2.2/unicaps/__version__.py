122c9bed9dd6d88689118a6b34389c64d45d15ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-30 03:06:30 +0000

    Add package unicaps/1.2.2/unicaps-1.2.2.tar.gz

---
+++ packages/unicaps/unicaps-1.2.2.tar.gz/unicaps-1.2.2/unicaps/__version__.py
@@ -0,0 +1,9 @@
+""" Version info """
+
+__title__ = 'unicaps'
+__description__ = 'Python CAPTCHA solving for Humans.'
+__url__ = 'https://github.com/sergey-scat/unicaps'
+__version__ = '1.2.2'
+__author__ = 'Sergey Scat'
+__license__ = 'Apache 2.0'
+__copyright__ = 'Copyright 2020-2023 Sergey Scat'
