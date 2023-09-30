e6f85d95fa54b498f464f5ea4c339030fe429fc8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-30 14:07:38 +0000

    Add package encriptacion2/0.1/encriptacion2-0.1.tar.gz

---
+++ packages/encriptacion2/encriptacion2-0.1.tar.gz/encriptacion2-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+ author="aslskks",
+ author_email="davikenat@gmail.com",
+ install_requires=[
+ ],
+ name="encriptacion2",
+ version="0.1",
+ description="packete de ayuda de encriptacion"
+)
