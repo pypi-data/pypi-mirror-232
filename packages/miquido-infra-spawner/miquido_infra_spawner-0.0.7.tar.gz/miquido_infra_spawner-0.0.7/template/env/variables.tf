c91a9e0a49600877c7b2793b2a2cf8f9b4ead123
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-30 03:07:18 +0000

    Add package miquido-infra-spawner/0.0.7/miquido_infra_spawner-0.0.7.tar.gz

---
+++ packages/miquido-infra-spawner/miquido_infra_spawner-0.0.7.tar.gz/miquido_infra_spawner-0.0.7/template/env/variables.tf
@@ -0,0 +1,23 @@
+variable "gitlab_registry_token" {
+  type = string
+}
+
+variable "project" {
+  type = string
+}
+
+variable "domain_prefix" {
+  type = string
+}
+
+variable "alb_priority" {
+  type = number
+}
+
+variable "gitlab_repo" {
+  type = string
+}
+
+variable "environment" {
+  type = string
+}
