de203593b8ad2ab9b859e3fc28dbab0f41d95697
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-30 04:08:08 +0000

    Add package macrosynergy/0.0.42/macrosynergy-0.0.42.tar.gz

---
+++ packages/macrosynergy/macrosynergy-0.0.42.tar.gz/macrosynergy-0.0.42/macrosynergy/version.py
@@ -0,0 +1,8 @@
+# THIS FILE IS GENERATED FROM macrosynergy SETUP.PY
+short_version: str = '0.0.42'
+version: str = '0.0.42'
+full_version: str = '0.0.42'
+git_revision: str = 'e2880bd0a7b8f1de6c25266d94bd0ab78ab8887d'
+release: bool = True
+if not release:
+    version = full_version
