76f4c5d338e09953367fe88e36fe2a84e5956176
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-30 03:06:53 +0000

    Add package open-quant-app/0.1.5.3/open_quant_app-0.1.5.3-py3-none-any.whl

---
+++ packages/open-quant-app/open_quant_app-0.1.5.3-py3-none-any.whl/open_quant_app/utils/LogUtils.py
@@ -0,0 +1,20 @@
+import os.path
+from datetime import datetime
+
+
+class LogUtils:
+    def __init__(self, config: dict):
+        self.log_dir = config['log']['output_dir']
+        if not os.path.exists(self.log_dir):
+            os.makedirs(self.log_dir)
+
+        today_timestamp = datetime.today().strftime('%Y-%m-%d')
+        self.log_file_path = os.path.join(self.log_dir, f"{today_timestamp}.log")
+
+    def log(self, message: str):
+        timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        log_message = f"[{timestamp}] {message}\n"
+        with open(self.log_file_path, 'a') as file:
+            file.write(log_message)
+
+
