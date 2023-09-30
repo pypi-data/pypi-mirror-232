2193e9b7c4dc31eee2bf06db061f81b0fc0f2a6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-30 03:06:46 +0000

    Add package citation-sorter/0.3.1/citation_sorter-0.3.1.tar.gz

---
+++ packages/citation-sorter/citation_sorter-0.3.1.tar.gz/citation_sorter-0.3.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+with open("README.md") as fh:
+    long_description = fh.read()
+
+setup(
+    name="citation_sorter",
+    version="0.3.1",
+    description=(
+        "CitationSorter is a Python package for sorting and organizing citations in LaTeX documents."
+    ),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/markusbink/latex-citation-sorter",
+    packages=find_packages(),
+    install_requires=[
+        "argparse",
+    ],
+    entry_points={
+        'console_scripts': [
+            'citation_sorter=citation_sorter.citation_sorter:main'
+        ]
+    },
+)
