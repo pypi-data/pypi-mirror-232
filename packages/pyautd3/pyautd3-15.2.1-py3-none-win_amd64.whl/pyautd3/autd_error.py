9c0860bb19d984f79643e772d9de24c718fc8ec4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-30 03:07:39 +0000

    Add package pyautd3/15.2.1/pyautd3-15.2.1-py3-none-win_amd64.whl

---
+++ packages/pyautd3/pyautd3-15.2.1-py3-none-win_amd64.whl/pyautd3/autd_error.py
@@ -0,0 +1,28 @@
+'''
+File: autd_error.py
+Project: pyautd3
+Created Date: 28/05/2023
+Author: Shun Suzuki
+-----
+Last Modified: 21/09/2023
+Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
+-----
+Copyright (c) 2023 Shun Suzuki. All rights reserved.
+
+'''
+
+import ctypes
+from typing import Optional
+
+
+class AUTDError(Exception):
+    msg: str
+
+    def __init__(self, err: Optional[ctypes.Array[ctypes.c_char]] = None):
+        self.msg = err.value.decode("utf-8") if err is not None else ""
+
+    def __str__(self):
+        return self.msg
+
+    def __repr__(self):
+        return self.msg
