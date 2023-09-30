4c89a484d17224d5f15578767b15a63df7009e9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-30 04:08:55 +0000

    Add package g2-custom-logger/1.0.1/g2_custom_logger-1.0.1.tar.gz

---
+++ packages/g2-custom-logger/g2_custom_logger-1.0.1.tar.gz/g2_custom_logger-1.0.1/g2_custom_logger/logger.py
@@ -0,0 +1,23 @@
+import logging
+
+def setup_logger(logfile="my_log.log"):
+    logging.basicConfig(filename=logfile, level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    return logging.getLogger(__name__)
+
+
+class MyLogger:
+    def __init__(self, name='my_logger', level=logging.INFO):
+        self.logger = logging.getLogger(name)
+        self.logger.setLevel(level)
+        self.handler = logging.StreamHandler()
+        self.formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        self.handler.setFormatter(self.formatter)
+        self.logger.addHandler(self.handler)
+
+
+    def custom_format(self, message):
+        return f"Custom Prefix: {message}"
+
+
+def create_logger(name='my_logger', level=logging.INFO):
+    return MyLogger(name, level)
