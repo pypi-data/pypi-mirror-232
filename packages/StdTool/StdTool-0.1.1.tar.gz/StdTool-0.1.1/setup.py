5dd8be9217816e9227b78c2aeb853ee1a8f14dca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-30 03:06:46 +0000

    Add package StdTool/0.1.1/StdTool-0.1.1.tar.gz

---
+++ packages/StdTool/StdTool-0.1.1.tar.gz/StdTool-0.1.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='StdTool',
+    version='0.1.1',
+    packages=['iolib', 'runlib', 'syslib'],
+    url='',
+    license='Apache License 2.0',
+    author='MosRat',
+    author_email='whl2075928012@gmail.com',
+    description='Some tools for daily use',
+    requires=['tqdm']
+)
