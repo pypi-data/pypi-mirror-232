92c887ef6360c011bde4d3bf87906462802fec8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-30 03:07:47 +0000

    Add package volmdlr/0.13.0.post4/volmdlr-0.13.0.post4-pp310-pypy310_pp73-win_amd64.whl

---
+++ packages/volmdlr/volmdlr-0.13.0.post4-pp310-pypy310_pp73-win_amd64.whl/volmdlr/__init__.py
@@ -0,0 +1,29 @@
+"""
+Volmdlr init module.
+"""
+# -*- coding: utf-8 -*-
+import math
+
+import pkg_resources
+
+__version__ = pkg_resources.require("volmdlr")[0].version
+
+from volmdlr.core_compiled import (O2D, O3D, OXY, X2D, X3D, Y2D, Y3D, Z3D,
+                                   Basis2D, Basis3D, Frame2D, Frame3D,
+                                   LineSegment2DPointDistance,
+                                   LineSegment3DPointDistance, Matrix22,
+                                   Matrix33, Point2D, Point3D, Vector2D,
+                                   Vector3D)
+from volmdlr.bspline_fitting import interpolate_curve
+from volmdlr.bspline_compiled import rational_derivatives, derivatives
+
+
+TWO_PI = 2 * math.pi
+
+XYZ = Basis3D(X3D, Y3D, Z3D)
+YZX = Basis3D(Y3D, Z3D, X3D)
+ZXY = Basis3D(Z3D, X3D, Y3D)
+
+OXYZ = Frame3D(O3D, X3D, Y3D, Z3D)
+OYZX = Frame3D(O3D, Y3D, Z3D, X3D)
+OZXY = Frame3D(O3D, Z3D, X3D, Y3D)
