00b5178a82cf48f99c1738b92556d6593f28d55f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-30 14:07:45 +0000

    Add package globestylizer/0.1.6/globestylizer-0.1.6.tar.gz

---
+++ packages/globestylizer/globestylizer-0.1.6.tar.gz/globestylizer-0.1.6/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='globestylizer',
+    version='0.1.6',
+    packages=find_packages(where=''),  
+    install_requires=[
+        'pandas',
+    ],
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+)
