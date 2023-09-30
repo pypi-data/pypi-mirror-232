7a0a232b231a0b650cab7794eb8bd91b0eddc421
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-30 03:07:51 +0000

    Add package dfv/0.26.0/dfv-0.26.0-py3-none-any.whl

---
+++ packages/dfv/dfv-0.26.0-py3-none-any.whl/dfv/form.py
@@ -0,0 +1,36 @@
+from typing import cast, TypeVar
+
+from django.forms import BaseForm
+from django.http import HttpRequest, QueryDict
+
+from dfv import is_get, is_head, is_patch, is_post
+from dfv.view_stack import is_view_fn_request_target
+
+T_FORM = TypeVar("T_FORM", bound=BaseForm)
+
+
+def _convert_querydict_to_initial_values(qd: QueryDict) -> dict[str, str]:
+    return {k: v for k, v in qd.items()}
+
+
+def create_form(request: HttpRequest, form_class: type[T_FORM], **kwargs) -> T_FORM:
+    if is_get(request) or is_head(request) or not is_view_fn_request_target(request):
+        form = form_class(**kwargs)
+    elif is_patch(request):
+        qd = QueryDict(request.body, encoding=request.encoding)
+
+        initial = {}
+        if "initial" in kwargs:
+            initial = kwargs["initial"]
+            del kwargs["initial"]
+        initial_from_query = _convert_querydict_to_initial_values(qd)
+        initial.update(initial_from_query)
+
+        form = form_class(initial=initial, **kwargs)
+    else:
+        form = form_class(data=request.POST, files=request.FILES, **kwargs)
+    return cast(T_FORM, form)
+
+
+def is_valid_submit(request: HttpRequest, form: BaseForm) -> bool:
+    return is_post(request) and form.is_valid()
