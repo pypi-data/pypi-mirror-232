ef2f92dedc5691d63665421941c4443fd30b2238
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-30 14:07:15 +0000

    Add package swgoh-api/1.0.0/swgoh_api-1.0.0.tar.gz

---
+++ packages/swgoh-api/swgoh_api-1.0.0.tar.gz/swgoh_api-1.0.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='swgoh_api',
+      version='1.0.0',
+      description='Library for getting info about Guilds, Players, Units and e.t.c from https://swgoh.gg',
+      packages=['swgoh_api'],
+      author_email='da9ert0n@gmail.com',
+      zip_safe=False)
