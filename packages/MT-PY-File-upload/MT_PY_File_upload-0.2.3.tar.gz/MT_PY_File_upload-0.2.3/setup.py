30f338d1c0d63406a92c829a48be8d872c7848d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-30 03:08:16 +0000

    Add package MT-PY-File-upload/0.2.3/MT_PY_File_upload-0.2.3.tar.gz

---
+++ packages/MT-PY-File-upload/MT_PY_File_upload-0.2.3.tar.gz/MT_PY_File_upload-0.2.3/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='mt_django_file_upload',
+    version='1.0.0',
+    description='Custom file upload package for Django',
+    author='Viraj Poojari',
+    author_email='virajp.in@mouritech.com',
+    packages=['mt_django_file_upload'],
+)
