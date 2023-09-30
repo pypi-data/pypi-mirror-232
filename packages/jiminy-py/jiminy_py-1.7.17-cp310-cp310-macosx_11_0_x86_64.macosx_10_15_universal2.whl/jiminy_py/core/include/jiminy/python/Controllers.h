20fb915afa0ade7ee205d53105d930ff60657c93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-30 04:07:21 +0000

    Add package jiminy-py/1.7.17/jiminy_py-1.7.17-cp310-cp310-macosx_11_0_x86_64.macosx_10_15_universal2.whl

---
+++ packages/jiminy-py/jiminy_py-1.7.17-cp310-cp310-macosx_11_0_x86_64.macosx_10_15_universal2.whl/jiminy_py/core/include/jiminy/python/Controllers.h
@@ -0,0 +1,14 @@
+#ifndef CONTROLLERS_PYTHON_H
+#define CONTROLLERS_PYTHON_H
+
+
+namespace jiminy
+{
+namespace python
+{
+    void exposeAbstractController(void);
+    void exposeControllerFunctor(void);
+}  // End of namespace python.
+}  // End of namespace jiminy.
+
+#endif  // CONTROLLERS_PYTHON_H
