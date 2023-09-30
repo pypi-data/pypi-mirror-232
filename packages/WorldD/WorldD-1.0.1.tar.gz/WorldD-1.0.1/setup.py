f1d1eecb1f5b0b4245800eb83afd046c4c97c359
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-30 04:08:39 +0000

    Add package WorldD/1.0.1/WorldD-1.0.1.tar.gz

---
+++ packages/WorldD/WorldD-1.0.1.tar.gz/WorldD-1.0.1/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+	long_description = fh.read()
+
+setuptools.setup(
+	name='WorldD',
+	version='1.0.1',
+	author="Aleks Baran",
+	author_email="legominefan@gmail.com",
+	description="Level editor made specifically for pygame games",
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+	url="https://github.com/NotMEE12/WorldD",
+	packages=['WorldD'],
+	requires=['pygame', 'tomlkit'],
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	],
+	package_dir={'WorldD': 'src/WorldD'},
+	package_data={'WorldD': ['*.toml', 'assets/*.png']}
+)
