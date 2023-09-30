25b3f1f31e556d28b90d77a0c1eea252ee2982f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-30 03:07:07 +0000

    Add package xlandsat/0.4.0/xlandsat-0.4.0-py3-none-any.whl

---
+++ packages/xlandsat/xlandsat-0.4.0-py3-none-any.whl/xlandsat/tests/test_datasets.py
@@ -0,0 +1,40 @@
+# Copyright (c) 2022 The xlandsat developers.
+# Distributed under the terms of the MIT License.
+# SPDX-License-Identifier: MIT
+"""
+Test the download functions
+"""
+import pathlib
+
+import pytest
+
+from ..datasets import (
+    fetch_brumadinho_after,
+    fetch_brumadinho_before,
+    fetch_liverpool,
+    fetch_liverpool_panchromatic,
+    fetch_manaus,
+    fetch_momotombo,
+    fetch_roraima,
+)
+
+
+@pytest.mark.parametrize("untar", [False, True], ids=["archive", "folder"])
+def test_fetching_functions(untar):
+    "Check that the download functions work"
+    functions = [
+        fetch_brumadinho_after,
+        fetch_brumadinho_before,
+        fetch_liverpool,
+        fetch_liverpool_panchromatic,
+        fetch_manaus,
+        fetch_momotombo,
+        fetch_roraima,
+    ]
+    for func in functions:
+        path = pathlib.Path(func(untar=untar))
+        assert path.exists()
+        if untar:
+            assert path.is_dir()
+        else:
+            assert not path.is_dir()
