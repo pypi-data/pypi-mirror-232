ba563de2737524023c88e6ee04040ae075be6281
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-30 03:08:21 +0000

    Add package unic/0.2.2/unic-0.2.2.tar.gz

---
+++ packages/unic/unic-0.2.2.tar.gz/unic-0.2.2/unic/time_unit/unixtime/unixtime_model.py
@@ -0,0 +1,28 @@
+from datetime import datetime, timezone, timedelta
+from unic.utils import check_parameter, config_parser
+
+
+class UnixtimeModel:
+    def convert(self, data: str, **kwargs: any) -> int:
+        check_parameter.check_parameter_count(kwargs)
+
+        if len(kwargs) == 1:
+            tz = kwargs.get("tz", None)
+            check_parameter.check_parameter_value(tz)
+            parameter = config_parser.parse_toml("timezone")
+            timezone_hour = parameter[tz]["value"]
+        else:
+            timezone_hour = 0
+
+        except_milisecond = data[0:19]
+        milisecond = data[20:23]
+
+        dt_timestamp = datetime.strptime(
+            except_milisecond, "%Y-%m-%d %H:%M:%S"
+        ) + timedelta(hours=timezone_hour)
+
+        dt_unixtime = (
+            str(int(dt_timestamp.replace(tzinfo=timezone.utc).timestamp()))
+        ) + milisecond
+
+        return int(dt_unixtime)
