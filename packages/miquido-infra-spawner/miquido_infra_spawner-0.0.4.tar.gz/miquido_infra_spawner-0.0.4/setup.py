abecf5f7717a034ca6eeec2ece81b33fea949b12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-30 03:07:22 +0000

    Add package miquido-infra-spawner/0.0.4/miquido_infra_spawner-0.0.4.tar.gz

---
+++ packages/miquido-infra-spawner/miquido_infra_spawner-0.0.4.tar.gz/miquido_infra_spawner-0.0.4/setup.py
@@ -0,0 +1,29 @@
+from distutils.core import setup
+
+setup(
+    name='miquido_infra_spawner',
+    packages=['miquido_infra_spawner'],
+    version='0.0.4',
+    license='MIT',
+    description='Spawning Terraform repositories',
+    author='Marek',
+    author_email='marek.moscichowski@miquido.com',
+    keywords=['GITLAB', 'Terraform'],
+    install_requires=[
+        'requests',
+        'python-dateutil'
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
