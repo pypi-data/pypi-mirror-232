d3c134b75afd073c3c299e1b92f270508f57e42c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-30 14:07:56 +0000

    Add package mathobju/0.1.1/mathobju-0.1.1.tar.gz

---
+++ packages/mathobju/mathobju-0.1.1.tar.gz/mathobju-0.1.1/principal.c
@@ -0,0 +1,14 @@
+
+
+#include <stdio.h>
+#include <math.h>
+
+
+int add(int a, int b){
+    return a + b;
+}
+
+
+double piso(double numero){
+    return round(numero);
+}
---
+++ packages/mathobju/mathobju-0.1.1.tar.gz/mathobju-0.1.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, Extension, find_packages
+
+
+setup (
+    name="mathobju",
+    version="0.1.1",
+    description="A math library",
+    author="N/A",
+    license="MIT",
+    ext_modules=[Extension('main', sources=['principal.c'])],
+    packages=find_packages(),
+
+)
