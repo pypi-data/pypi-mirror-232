ad40e0aee32e30b0ad740dc98ced6026b277b30f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-30 03:08:14 +0000

    Add package lilac/0.1.3/lilac-0.1.3-py3-none-any.whl

---
+++ packages/lilac/lilac-0.1.3-py3-none-any.whl/lilac/sources/__init__.py
@@ -0,0 +1,19 @@
+"""Sources for ingesting data into Lilac."""
+
+from .csv_source import CSVSource
+from .gmail_source import GmailSource
+from .huggingface_source import HuggingFaceSource
+from .json_source import JSONSource
+from .langsmith import LangSmithSource
+from .pandas_source import PandasSource
+from .parquet_source import ParquetSource
+
+__all__ = [
+  'HuggingFaceSource',
+  'CSVSource',
+  'JSONSource',
+  'GmailSource',
+  'PandasSource',
+  'ParquetSource',
+  'LangSmithSource',
+]
