d03d5b5cb5a9289623ef3211cc6113e8b38fc29a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-01 03:08:37 +0000

    Add package Loglan-Core/0.1.0/Loglan-Core-0.1.0.tar.gz

---
+++ packages/Loglan-Core/Loglan-Core-0.1.0.tar.gz/Loglan-Core-0.1.0/tests/loglan_core/test_author.py
@@ -0,0 +1,13 @@
+import pytest
+from loglan_core import Author
+from sqlalchemy import select
+
+@pytest.mark.usefixtures("db_session")
+class TestAuthor:
+    def test_repr(self, db_session):
+        author = db_session.query(Author).filter(Author.id == 1).first()
+        assert str(author) == '<BaseAuthor ID 1 L4>'
+
+    def test_contribution(self, db_session):
+        author = db_session.query(Author).filter(Author.id == 1).first()
+        assert len(author.contribution) == 4
