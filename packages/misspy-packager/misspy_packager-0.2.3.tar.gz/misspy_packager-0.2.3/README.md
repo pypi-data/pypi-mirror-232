61152e1742c35ab8a69d9ac94459fa3a027e551f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-01 03:07:25 +0000

    Add package misspy-packager/0.2.3/misspy_packager-0.2.3.tar.gz

---
+++ packages/misspy-packager/misspy_packager-0.2.3.tar.gz/misspy_packager-0.2.3/README.md
@@ -0,0 +1,2 @@
+# Automatic version bump tool based on tags of the latest release  used in misspy's GitHub Actions.
+Available under the MIT License.
---
+++ packages/misspy-packager/misspy_packager-0.2.3.tar.gz/misspy_packager-0.2.3/misspy_packager/packager.py
@@ -0,0 +1,35 @@
+import subprocess
+import os
+import tomllib
+
+import requests
+from cleo.commands.command import Command
+from cleo.helpers import argument
+
+
+class publish(Command):
+    name = "publish"
+    description = ""
+    arguments = [argument("token", description="gh_token")]
+
+    def handle(self):
+        resp = requests.get(
+            "https://api.github.com/repos/misspy-development/misspy/releases/latest"
+        )
+        resp = resp.json()
+        token = self.argument("token")
+        with open("pyproject.toml", "rb") as f:
+            pyproject = tomllib.load(f)
+            print(pyproject)
+            pyproject["tool.poetry"]["version"] = resp["tag_name"]
+        subprocess.run(
+            f"git remote set-url origin https://{token}@github.com/misspy-development/misspy.git",
+            shell=True,
+        )
+        subprocess.run("poetry config pypi-token.pypi " + os.environ.get('PYPI_TOKEN'))
+        subprocess.run('git commit -m "update toml version"', shell=True)
+        subprocess.run("git push origin develop", shell=True)
+        subprocess.run("git checkout master", shell=True)
+        subprocess.run("git merge develop", shell=True)
+        subprocess.run("git push origin master", shell=True)
+        subprocess.run("poetry publish --build", shell=True)
