e9ff244e3a6e97fbf82479a75548e4b7423577d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-01 03:08:24 +0000

    Add package generate-dummyf/1.1.3/generate-dummyf-1.1.3.tar.gz

---
+++ packages/generate-dummyf/generate-dummyf-1.1.3.tar.gz/generate-dummyf-1.1.3/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+VERSION = '1.1.3' 
+DESCRIPTION = 'Generate different types (json, csv, excel, word, txt) of dummy files'
+
+# Setting up
+setup(
+        name="generate-dummyf",
+        version=VERSION,
+        author="Rubina Karki",
+        author_email="rubinakarki369@gmail.com",
+        description=DESCRIPTION,
+        long_description=open('README.md').read(),
+        long_description_content_type="text/markdown",
+        packages=["generate_dummyf"],
+        url='https://github.com/rubinakarkii/generate-dummyf',
+        license="LICENSE",
+        install_requires=['openpyxl','python-docx'], 
+)
