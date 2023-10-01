fa7d3da2cb8943f00c046c48018112f94defea92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-01 03:07:00 +0000

    Add package pipelite/0.1.4.3/pipelite-0.1.4.3-py3-none-any.whl

---
+++ packages/pipelite/pipelite-0.1.4.3-py3-none-any.whl/pipelines/directPL.py
@@ -0,0 +1,97 @@
+__author__ = "datacorner.fr"
+__email__ = "admin@datacorner.fr"
+__license__ = "MIT"
+
+from pipelite.parents.Pipeline import Pipeline
+from pipelite.etlDatasets import etlDatasets
+
+class directPL(Pipeline):
+    def __init__(self, config, log):
+        super().__init__(config, log)
+
+    def extract(self) -> int: 
+        """This method must be surchaged and aims to collect the data from the datasource to provides the corresponding dataframe
+        Returns:
+            pd.DataFrame: Dataset in a pd.Dataframe object
+        """
+        try:
+            totalCountExtracted = 0
+            self.log.info("*** Extraction treatment ***")
+            for item in self.extractors:
+                self.log.info("Extracting data from the Data Source {}".format(item.name))
+                if (item.extract()):
+                    self.log.info(" <{}> Rows: {} | Columns: {} ".format(item.name, item.count, item.content.columns))
+                    totalCountExtracted += item.count
+            return totalCountExtracted
+        except Exception as e:
+            self.log.error("{}".format(e))
+            return 0
+        
+    def transform(self): 
+        """ Make some modifications in the Dataset(s) after gathering the data and before loading
+        Returns:
+            pd.DataFrame: Output Dataframe
+            int: Total Number of transformed rows
+        """
+        try:
+            self.log.info("*** Data Transformation treatment ***")
+            # Select only the Inputs needed for the transformer, initialize the data source stack with all extractors first
+            # this dsPipelineStack object will store all the initial datasets (extractors) but also the transformation results.
+            dsPipelineStack = etlDatasets()
+            for extractorItem in self.extractors:   
+                self.log.debug("Adding Extractor/dataset {} in the data stack".format(extractorItem.name))
+                extractorItem.content.name = extractorItem.name
+                dsPipelineStack.add(extractorItem.content)
+            totalCountTransformed = 0
+
+            # Execute the Transformers stack on the inputs/extractors
+            for transformerItem in self.transformers:   # Pass through all the Tranformers ...
+                dsInputs = etlDatasets()
+                for dsItem in dsPipelineStack:
+                    if (dsItem.name in transformerItem.dsInputs):
+                        self.log.info("Including dataset {} for the transformation".format(dsItem.name))
+                        self.log.info(" <{}> Rows: {} | Columns: {} ".format(dsItem.name, dsItem.count, dsItem.columns))
+                        dsInputs.add(dsItem.copy())
+                self.log.info("Apply Transformation via transformer {} ...".format(transformerItem.name))
+                if (not dsInputs.empty):
+                    dsOutputs, tfCount = transformerItem.transform(dsInputs)
+                    dsPipelineStack.merge(dsOutputs)
+                    totalCountTransformed += tfCount
+                    self.log.info("Number of rows transformed {} / {}".format(tfCount, totalCountTransformed))
+                else:
+                    self.log.warning("The Tranformer {} has no input, by pass it !".format(transformerItem.name))
+
+            return dsPipelineStack, totalCountTransformed
+        except Exception as e:
+            self.log.error("{}".format(e))
+            return None, 0
+        
+    def load(self, dsPipelineStack) -> int:
+        """ Load the dataset transformed in one or more loaders.
+            Only load the datasets which are referenced as Data Source Load and are in the Stack.
+            Be Careful: the loaders are not in the stack by default (because they don't still have data)
+            so To load, 2 options:
+                1) Use a name which exists in the extractors
+                2) Use a Tranformer to create a new dataset
+        Args:
+            dfDataset (pd.DataFrame): DataFrame with the Data to load in one or several data sources
+        Returns:
+            bool: False if error
+        """
+        try:
+            totalCountLoaded = 0
+            self.log.info("*** Loading treatment ***")
+            for item in self.loaders:
+                # Load only the dataset which have a loader
+                dsToLoad = dsPipelineStack.getFromName(item.name)
+                if (dsToLoad == None):
+                    self.log.warning("There are no data to load into the Data Source {}".format(item.name))
+                else:
+                    self.log.info("Loading content to the Data Source {}".format(dsToLoad.name))
+                    item.content = dsToLoad
+                    totalCountLoaded += item.load()
+                    self.log.info(" <{}> Rows: {} | Columns: {} ".format(item.name, item.count, item.content.columns))
+            return totalCountLoaded
+        except Exception as e:
+            self.log.error("{}".format(e))
+            return 0
---
+++ packages/pipelite/pipelite-0.1.4.3-py3-none-any.whl/transformers/lookupTR.py
@@ -0,0 +1,92 @@
+__author__ = "datacorner.fr"
+__email__ = "admin@datacorner.fr"
+__license__ = "MIT"
+
+import pipelite.constants as C
+from pipelite.parents.Transformer import Transformer
+from pipelite.etlDatasets import etlDatasets
+import importlib.resources
+
+CFGFILES_DSOBJECT = "lookupTR.json"
+
+class lookupTR(Transformer):
+    """ Transcode a column from a main dataset with data from the lookup table.
+        * If data is found in the lookup table -> then the column is replace in the main dataset
+        * If the data is not found -> row is dropped
+        The join is made on the keys (main-column-key, lookup-column-key)
+        The column wich remains is lookup-column-keep (it replaces the main-column-key column content)
+    Args:
+        Transformer (class): Transformer template
+    """
+    def __init__(self, config, log):
+        super().__init__(config, log)
+        self.lookupDatasetName = C.EMPTY
+        self.lookupDatasetColKey = C.EMPTY
+        self.lookupDatasetColKeep = C.EMPTY
+        self.mainDatasetName = C.EMPTY
+        self.mainColKey = C.EMPTY
+
+    @property
+    def parametersValidationFile(self):
+        filename = importlib.resources.files(C.RESOURCE_PKGFOLDER_TRANSFORMERSS).joinpath(CFGFILES_DSOBJECT)
+        return str(filename)
+    
+    def initialize(self, params) -> bool:
+        """ Initialize and makes some checks (params) for that transformer
+        Args:
+            params (json): parameters
+        Returns:
+            bool: False if error
+        """
+        try:
+            self.lookupDatasetName = params.getParameter("lookup")["ds-name"]
+            self.lookupDatasetColKey = params.getParameter("lookup")["key"]
+            self.mainDatasetName = params.getParameter("main")["ds-name"]
+            self.mainColKey = params.getParameter("main")["key"]
+            self.lookupDatasetColKeep = params.getParameter("lookup")["keep"]
+            return (len(self.lookupDatasetName) != 0 and 
+                    len(self.lookupDatasetColKey) != 0 and 
+                    len(self.mainDatasetName) != 0 and 
+                    len(self.mainColKey) != 0 and 
+                    len(self.lookupDatasetColKeep) != 0)
+        except Exception as e:
+            self.log.error("transcoderTR.initialize() Error -> {}".format(str(e)))
+            return False
+    
+    def transform(self, dsStack):
+        """ Returns all the data in a etlDataset format
+        Args:
+            inputDataFrames (etlDatasets): multiple datasets (inputs)
+        Returns:
+            etlDatasets: Output etlDatasets of the transformer(s)
+            int: Number of rows transformed
+        """
+
+        try:
+            # identify the main & the lookup dataset first
+            if (dsStack[0].name == self.mainDatasetName):
+                dsMain, dsLookup = dsStack[0], dsStack[1]
+            else:
+                dsMain, dsLookup = dsStack[1],dsStack[0]
+            # Change the lookup column name to the main one for the lookup itself
+            dsLookup.renameColumn(self.lookupDatasetColKey, self.mainColKey)
+            originalRecCount = dsMain.count
+            self.log.debug("There are {} records in the original dataset".format(originalRecCount))
+            dsMain.lookupWith(dsLookup, self.mainColKey) # Effective lookup
+            dsMain.dropLineNaN(self.lookupDatasetColKeep) # drop the NA values (lookup failed / no values as result)
+            # Reshape the dataset (columns changes)
+            dsMain.dropColumn(self.mainColKey)
+            dsMain.renameColumn(self.lookupDatasetColKeep, self.mainColKey)
+            # display Nb of rows removed
+            iNbRemoved = originalRecCount - dsMain.count
+            if (iNbRemoved != 0):
+                self.log.warning("{} records have been removed by the transformation".format(iNbRemoved))
+            # Return the output as a collection with only one item with the excepted name
+            dsOutputs = etlDatasets()
+            # Create from the source another instance of the data
+            dsMain.name = self.dsOutputs[0]
+            dsOutputs.add(dsMain)
+            return dsOutputs, iNbRemoved
+        except Exception as e:
+            self.log.info("lookupTR.transform() -> ".format(e))
+            return dsStack, 0
