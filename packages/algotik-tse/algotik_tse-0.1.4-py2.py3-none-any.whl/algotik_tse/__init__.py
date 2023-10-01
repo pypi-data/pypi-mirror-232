0657a2b1c105226616e62c0b9ca4cd46a2d4b46a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-01 03:06:57 +0000

    Add package algotik-tse/0.1.4/algotik_tse-0.1.4-py2.py3-none-any.whl

---
+++ packages/algotik-tse/algotik_tse-0.1.4-py2.py3-none-any.whl/algotik_tse/__init__.py
@@ -0,0 +1,11 @@
+"""Top-level package for algotik-tse."""
+
+__author__ = """Mohsen Alipour"""
+__email__ = 'alipour@algotik.ir'
+__version__ = '0.1.4'
+
+from core.getter import get
+
+__all__ = [
+    "get",
+]
