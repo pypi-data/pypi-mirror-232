bc2ff4527f178a11f72b893182dd8e948fec8453
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-01 03:08:25 +0000

    Add package rmugicag-chromedriver-autoinstaller/1.0.0/rmugicag_chromedriver_autoinstaller-1.0.0.tar.gz

---
+++ packages/rmugicag-chromedriver-autoinstaller/rmugicag_chromedriver_autoinstaller-1.0.0.tar.gz/rmugicag_chromedriver_autoinstaller-1.0.0/setup.py
@@ -0,0 +1,61 @@
+# coding: utf-8
+import os
+import sys
+
+from setuptools import setup
+
+__author__ = "Ricardo Mugica <rmugicag@gmail.com>"
+
+
+with open("README.md") as readme_file:
+    long_description = readme_file.read()
+
+
+# 'setup.py publish' shortcut.
+if sys.argv[-1] == "publish":
+    os.system("python3 setup.py sdist bdist_wheel")
+    os.system("twine upload dist/*")
+    sys.exit()
+elif sys.argv[-1] == "clean":
+    import shutil
+
+    if os.path.isdir("build"):
+        shutil.rmtree("build")
+    if os.path.isdir("dist"):
+        shutil.rmtree("dist")
+    if os.path.isdir("chromedriver_autoinstaller.egg-info"):
+        shutil.rmtree("chromedriver_autoinstaller.egg-info")
+
+
+setup(
+    name="rmugicag_chromedriver_autoinstaller",
+    version="1.0.0",
+    author="Ricardo Mugica",
+    author_email="rmugicag@gmail.com",
+    description="Automatically install chromedriver that supports the currently installed version of chrome.",
+    license="MIT",
+    keywords="chromedriver chrome chromium selenium",
+    url="https://github.com/rmugicag/python-chromedriver-autoinstaller",
+    packages=["chromedriver_autoinstaller"],
+    entry_points={
+        "console_scripts": [
+            "chromedriver-path=chromedriver_autoinstaller.utils:print_chromedriver_path"
+        ],
+    },
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    python_requires=">=3.6",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Topic :: Software Development :: Testing",
+        "Topic :: System :: Installation/Setup",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
+)
