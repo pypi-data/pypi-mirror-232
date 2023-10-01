e0f9724c302793a49e09fd2a592cc097282de375
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-01 03:07:18 +0000

    Add package pypcapkit/1.2.2.post1/pypcapkit-1.2.2.post1-pp39-none-any.whl

---
+++ packages/pypcapkit/pypcapkit-1.2.2.post1-pp39-none-any.whl/pcapkit/corekit/version.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""Version Info
+==================
+
+.. module:: pcapkit.corekit.version
+
+:mod:`pcapkit.corekit.version` contains :obj:`tuple`
+like class :class:`~pcapkit.corekit.version.VersionInfo`,
+which is originally designed alike :class:`sys.version_info`.
+
+"""
+import collections
+
+__all__ = ['VersionInfo']
+
+VersionInfo = collections.namedtuple('VersionInfo', ['major', 'minor'])
+VersionInfo.__doc__ = """VersionInfo is alike :class:`sys.version_info`."""
