b5adbbaa2699c50d21b5c7043e347561c76bf5c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-01 03:08:25 +0000

    Add package plmidi/1.0.3/plmidi-1.0.3.tar.gz

---
+++ packages/plmidi/plmidi-1.0.3.tar.gz/plmidi-1.0.3/plmidi_setup.cpp
@@ -0,0 +1,15 @@
+#if !defined(_MSC_VER) || !defined(_WIN32)
+#   error "This code must be compiled with MSVC on Windows"
+#endif
+
+#include "plmidi_sound.hpp"
+
+PYBIND11_MODULE(plmidi, m) {
+    //m.def("sound", _plmidi::sound);
+    m.def("sound_by_midiOutShortMsg", _plmidi::sound_by_midiOutShortMsg);
+    m.def("sound_by_mciSendCommand", _plmidi::sound_by_mciSendCommand);
+    // plmidiInitError
+    py::register_exception<_plmidi::plmidiExc_InitErr>(m, "plmidiInitError");
+    // OpenMidiFileError
+    py::register_exception<_plmidi::OpenMidiFileError>(m, "OpenMidiFileError");
+}
