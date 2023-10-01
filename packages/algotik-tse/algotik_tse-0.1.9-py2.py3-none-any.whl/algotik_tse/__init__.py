516be33442d8763de970949b782dd5b9fc02fc29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-01 03:08:24 +0000

    Add package algotik-tse/0.1.9/algotik_tse-0.1.9-py2.py3-none-any.whl

---
+++ packages/algotik-tse/algotik_tse-0.1.9-py2.py3-none-any.whl/algotik_tse/__init__.py
@@ -0,0 +1,8 @@
+"""Top-level package for algotik-tse."""
+
+__author__ = """Mohsen Alipour"""
+__email__ = 'alipour@algotik.ir'
+__version__ = '0.1.9'
+
+from algotik_tse.algotik_tse.algotik_tse import say_hello2
+from algotik_tse.algotik_tse.new_deck import deck_me
